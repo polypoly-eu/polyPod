4491b94ff2cd9d6c0fb3ecf145582f3ddea31190
Lars Hupel <lars.hupel@mytum.de>
2020-04-15 13:58:34 +0200

    docs

--- README.md
+++ README.md
@@ -3,3 +3,26 @@
 ![Node CI](https://github.com/polypoly-eu/rdf/workflows/Node%20CI/badge.svg)
 
 TypeScript implementation of the RDF Data Model spec
+
+## Overview
+
+This repository provides a minimal implementation of the [RDFJS Data Model Spec](https://rdf.js.org/data-model-spec/).
+It is tested against the [executable specification](https://github.com/polypoly-eu/rdf-spec).
+
+The implementation is mostly a clone of the [reference implementation](https://github.com/rdfjs-base/data-model).
+It improves upon this in two ways:
+
+1. implemented in TypeScript and mechanically tested against the spec
+2. resulting objects are instances of exported classes (see documentation for details)
+
+## Example
+
+```typescript
+import {dataFactory} from "@polypoly-eu/rdf";
+
+const subject = dataFactory.namedNode('http://example.org/subject');
+const predicate = dataFactory.namedNode('http://example.org/predicate');
+const object = dataFactory.namedNode('http://example.org/object');
+const graph = dataFactory.namedNode('http://example.org/graph');
+const quad = dataFactory.quad(subject, predicate, object, graph);
+```
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
+/**
+ * This module contains the entire implementation of the data factory. See [[dataFactory]] for an entrypoint.
+ *
+ * The implementation is assumed to be constrained by the type definitions. Use from non-typechecked code that passes
+ * invalid arguments results in undefined behaviour.
+ *
+ * @packageDocumentation
+ */
+
 import * as RDF from "rdf-js";
 
 export abstract class Model {
@@ -90,6 +99,10 @@ export class Variable extends Model implements RDF.Variable {
 export class DefaultGraph extends Model implements RDF.DefaultGraph {
     static readonly instance: DefaultGraph = new DefaultGraph();
 
+    private constructor() {
+        super();
+    }
+
     termType: "DefaultGraph" = "DefaultGraph";
     value: "" = "";
 }
@@ -110,6 +123,31 @@ export class Quad implements RDF.Quad {
     }
 }
 
+/**
+ * A spec-compliant implementation of an RDFJS data factory supporting variables.
+ *
+ * The type of quads generated is [[Quad]], which restricts the term types of subject, predicate, object and graph
+ * appropriately. For example, it is not permitted to use a [[Literal]] in subject position.
+ *
+ * The [[DataFactory.triple]] method is provided without guarantees since it is not covered by the spec, but mandated
+ * by the types. Use [[DataFactory.quad]] instead with only three arguments.
+ *
+ * The values returned by this factory satisfy two additional assumptions:
+ *
+ * 1. They are direct instances of exported classes, such as [[BlankNode]].
+ *    There is no manual fiddling with prototypes.
+ * 2. Those exported classes are subclasses of [[Model]] (except [[Quad]]).
+ *
+ * These guarantees are important for users of the data factory who want to transmit the values across serialization
+ * boundaries. The spec mandates that all entities come with a JVM-style `equals` method. Unfortunately, when
+ * transporting JS objects through any kind of channel (`JSON.stringify`, `MessagePort`, `postMessage`, ...) they lose
+ * their methods and prototype. It is hence crucial that the prototype can be restored by the receiver of such an
+ * object. Sadly, the reference implementation makes that hard by not having dedicated classes; instead, when entities
+ * are created, the prototype is manually constructed and subsequently not exposed. This implementation solves the
+ * problem through the exported classes that can reattached after deserialization.
+ *
+ * For the semantics of the methods, refer to [the spec](https://rdf.js.org/data-model-spec/).
+ */
 export class DataFactory implements RDF.DataFactory<Quad> {
     blankNode(value?: string): BlankNode {
         return new BlankNode(value);
@@ -140,4 +178,10 @@ export class DataFactory implements RDF.DataFactory<Quad> {
     }
 }
 
+/**
+ * The default instance of [[DataFactory]].
+ *
+ * It is usually not necessary to create other instances of [[DataFactory]], except for when extending the class with
+ * additional methods.
+ */
 export const dataFactory: DataFactory = new DataFactory();
