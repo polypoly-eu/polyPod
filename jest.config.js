141896557f446838cf84351f31fb5f7e833bb075
Lars Hupel <lars.hupel@mytum.de>
2020-04-29 15:08:23 +0200

    migrate to ts-jest

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,14 @@
 module.exports = {
     roots: ["<rootDir>/src"],
-    testEnvironment: "node"
+    testEnvironment: "node",
+    preset: "ts-jest/presets/js-with-ts",
+
+    globals: {
+        "ts-jest": {
+            tsConfig: {
+                "allowJs": true,
+                "rootDir": "./"
+            }
+        }
+    }
 };
--- src/tests/util.ts
+++ src/tests/util.ts
@@ -1,12 +1,14 @@
 import tempy from "tempy";
 import {promises as fs} from "fs";
-import {rollup} from "rollup";
+import {OutputOptions, rollup, RollupOptions} from "rollup";
 // @ts-ignore
-import {configs} from "../../build/rollup-common";
+import {configs as _configs} from "../../build/rollup-common";
+
+const configs: Record<string, RollupOptions & { output: OutputOptions }> = _configs as any;
 
 export async function tempBundle(target: string): Promise<string> {
     const targetPath = tempy.file({ extension: "js" });
-    const rollupBuild = await rollup(configs[target]);
+    const rollupBuild = await rollup((configs as any)[target]);
     const outputOptions = configs[target].output;
     const { output } = await rollupBuild.generate(outputOptions);
     await fs.writeFile(targetPath, output[0].code, { encoding: "utf-8" });
