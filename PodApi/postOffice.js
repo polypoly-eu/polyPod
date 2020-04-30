8e5aefb05b2a7a7042ff6800f9810575b1b5306a
carmenb80 <carmen.burmeister@innoq.com>
2020-04-30 13:50:21 +0200

    Remove unused functions

--- PodApi/postOffice.js
+++ PodApi/postOffice.js
@@ -15,49 +15,13 @@ class PostOffice {
             window.parent.postMessage(messageData, "*");
         }
     }
-    
-    getValue(key, cb) {
-        let id = this.messageId++;
-        this.registry.set(id, cb);
-        
-        let data = { id, key }
-        
-        window.parent.postMessage({ command: "getValue", ...data }, "*");
-    }
-    
-    setValue(key, value, cb) {
-        let id = this.messageId++;
-        this.registry.set(id, cb);
-        
-        let data = { id, key, value }
-        
-        window.parent.postMessage({ command: "setValue", ...data }, "*");
-    }
-    
-    addQuads(quads, cb) {
-        let id = this.messageId++;
-        this.registry.set(id, cb);
-        
-        let data = { id, quads }
-        
-        window.parent.postMessage({ command: "addQuads", ...data }, "*");
-    }
-    
-    selectQuads(matcher, cb) {
-        let id = this.messageId++;
-        this.registry.set(id, cb);
-        
-        let data = { id, matcher }
-        
-        window.parent.postMessage({ command: "selectQuads", ...data }, "*");
-    }
 
     receiveMessage(event) {
         let data = event.data;
         if (data.id) {
-            let cb = this.registry.get(data.id);
+            let callback = this.registry.get(data.id);
             this.registry.delete(data.id);
-            cb(data.result);
+            callback(data.result);
         }
     }
 }
