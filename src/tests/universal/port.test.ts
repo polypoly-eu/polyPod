7c8fd58dd0f2c30a02a3302009ea9781b406cb25
Lars Hupel <lars.hupel@mytum.de>
2020-05-05 10:25:13 +0200

    improve test coverage

--- src/tests/universal/port.test.ts
+++ src/tests/universal/port.test.ts
@@ -1,6 +1,11 @@
 import {portSpec} from "../../specs/port";
 import {bubblewrapLifecycle, forwardLifecycle, loopbackLifecycle} from "../_lifecycles";
 import {Bubblewrap} from "@polypoly-eu/bubblewrap";
+import {receiveSingle} from "../../port";
+import chai, {assert} from "chai";
+import chaiAsPromised from "chai-as-promised";
+
+chai.use(chaiAsPromised);
 
 describe("Universal/Port", () => {
 
@@ -20,6 +25,14 @@ describe("Universal/Port", () => {
 
         portSpec(forwardLifecycle(loopbackLifecycle, loopbackLifecycle));
 
+    });
+
+    it("receiveSingle", async () => {
+        const [send, receive] = (await loopbackLifecycle<number>()).value;
+        const promise = receiveSingle(receive);
+        send.send(1);
+        send.send(2);
+        await assert.eventually.equal(promise, 1);
     });
 
 });
