648e84eeb0f4d47edc972fc6a58697d4d582a749
Lars Hupel <lars.hupel@mytum.de>
2020-05-12 12:07:24 +0200

    use `quad` instead of `triple`

--- data/test-feature-build/index.js
+++ data/test-feature-build/index.js
@@ -1,7 +1,7 @@
 export default class ExampleFeature {
     async init(pod) {
         const {polyIn} = pod;
-        const quad = polyIn.factory.triple(
+        const quad = polyIn.factory.quad(
             polyIn.factory.namedNode("http://example.org/s"),
             polyIn.factory.namedNode("http://example.org/p"),
             polyIn.factory.namedNode("http://example.org/o")
--- data/test-feature.js
+++ data/test-feature.js
@@ -2,7 +2,7 @@ var Feature = (() => {
     class ExampleFeature {
         async init(pod) {
             const {polyIn} = pod;
-            const quad = polyIn.factory.triple(
+            const quad = polyIn.factory.quad(
                 polyIn.factory.namedNode("http://example.org/s"),
                 polyIn.factory.namedNode("http://example.org/p"),
                 polyIn.factory.namedNode("http://example.org/o")
