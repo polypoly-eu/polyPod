17988edb781202a25f7723e45b9f23f2dc9e0b29
Jacek Bilski <jacek.bilski@innoq.com>
2020-06-04 12:23:36 +0200

    I can find a text in a Feature's IFrame.

--- app/src/androidTest/java/eu/polypoly/pod/android/SecondFragmentInstrumentedTest.kt
+++ app/src/androidTest/java/eu/polypoly/pod/android/SecondFragmentInstrumentedTest.kt
@@ -5,9 +5,14 @@ import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.web.assertion.WebViewAssertions.webMatches
+import androidx.test.espresso.web.sugar.Web.onWebView
+import androidx.test.espresso.web.webdriver.DriverAtoms.*
+import androidx.test.espresso.web.webdriver.Locator
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
+import org.hamcrest.CoreMatchers.containsString
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -21,7 +26,8 @@ class SecondFragmentInstrumentedTest {
 
     @Test
     fun firstFragmentIsShown() {
-        onView(withText("Hello first fragment")).check(matches(isDisplayed()))
+        onView(withText("Hello first fragment"))
+            .check(matches(isDisplayed()))
     }
 
     @Test
@@ -37,5 +43,16 @@ class SecondFragmentInstrumentedTest {
 
         onView(withId(R.id.button_first))
             .check(doesNotExist())
+    }
+
+    @Test
+    fun canFindTextInWebView() {
+        onView(withId(R.id.button_first))
+            .perform(click())   // go to second fragment
+
+        onWebView()
+            .inWindow(selectFrameByIdOrName("harness"))
+            .withElement(findElement(Locator.CLASS_NAME, "helloWorldStyle"))
+            .check(webMatches(getText(), containsString("Hello World!")))
     }
 }
