6c4ab8eb90758bf90b49c3c70a538e730cc1bd3e
carmenb80 <carmen.burmeister@innoq.com>
2020-06-24 11:14:33 +0200

    Add unit tests for http post

--- PolyPodApp/PolyPodTests/PolyOutTests.swift
+++ PolyPodApp/PolyPodTests/PolyOutTests.swift
@@ -8,21 +8,32 @@
 
 import XCTest
 
-var sessionData: Data?
+var sessionGetData: Data?
+var sessionPostData: Data?
 var sessionError: Error?
 
 class NetworkSessionMock: NetworkSession {
 
     func loadData(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) {
-        let response = HTTPURLResponse(url: request.url!, statusCode: 200, httpVersion: "HTTP/1.1", headerFields: [:])
-        completionHandler(sessionData, response, sessionError)
+        let response = HTTPURLResponse(url: request.url!, statusCode: 200, httpVersion: "HTTP/1.1", headerFields: request.allHTTPHeaderFields)
+        if request.httpMethod == "POST" {
+            if request.httpBody != nil {
+                completionHandler(sessionPostData, response, sessionError)
+            } else {
+                completionHandler(nil, response, URLError(.cancelled))
+            }
+        } else {
+            completionHandler(sessionGetData, response, sessionError)
+        }
     }
 }
 
 class PolyOutTests: XCTestCase {
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        sessionGetData = nil
+        sessionPostData = nil
+        sessionError = nil
     }
 
     override func tearDown() {
@@ -41,16 +52,15 @@ class PolyOutTests: XCTestCase {
         }
     }
     
-    func testMakeHttpRequest() {
+    func testMakeHttpGetRequest_Valid() {
         let URL_STRING = "https://example.org/"
         
         let polyOut = PolyOut(session: NetworkSessionMock())
         
-        let expectationValid = XCTestExpectation(description: "Test valid response")
-        sessionError = nil
-        let responseText = "This is the response"
-        sessionData = responseText.data(using: .utf8)
-        
+        let responseText = "This is the get response"
+        sessionGetData = responseText.data(using: .utf8)
+
+        let expectation = XCTestExpectation(description: "Test valid response")
         polyOut.makeHttpRequest(urlString: URL_STRING, requestInit: FetchRequestInit(initData: [:]), completionHandler: { fetchResponse in
             XCTAssertNotNil(fetchResponse, "fetchResponse is nil")
             
@@ -60,22 +70,91 @@ class PolyOutTests: XCTestCase {
             
             XCTAssertEqual(fetchResponse!.bufferedText, responseText)
             
-            expectationValid.fulfill()
+            expectation.fulfill()
         })
         
-        wait(for: [expectationValid], timeout: 1.0)
+        wait(for: [expectation], timeout: 1.0)
+    }
+    
+    func testMakeHttpGetRequest_Error() {
+        let URL_STRING = "https://example.org/"
         
-        sessionError = URLError(.badURL)
-        sessionData = nil
-        let expectationError = XCTestExpectation(description: "Test error")
+        let polyOut = PolyOut(session: NetworkSessionMock())
         
+        sessionError = URLError(.unknown)
+        
+        let expectation = XCTestExpectation(description: "Test error")
         polyOut.makeHttpRequest(urlString: URL_STRING, requestInit: FetchRequestInit(initData: [:]), completionHandler: { fetchResponse in
             XCTAssertNil(fetchResponse, "fetchResponse is not nil")
-            
-            expectationError.fulfill()
+
+            expectation.fulfill()
         })
         
-        wait(for: [expectationError], timeout: 1.0)
+        wait(for: [expectation], timeout: 1.0)
+    }
+    
+    func testMakeHttpPostRequest_Valid() {
+        let URL_STRING = "https://example.org/"
+        
+        let polyOut = PolyOut(session: NetworkSessionMock())
+        
+        let responseText = "This is the post response"
+        sessionPostData = responseText.data(using: .utf8)
+        let requestInitData = [MessagePackValue("method") : MessagePackValue("post"),
+                               MessagePackValue("body") : MessagePackValue("This is the body")]
+            
+        let expectation = XCTestExpectation(description: "Test valid response")
+        polyOut.makeHttpRequest(urlString: URL_STRING, requestInit: FetchRequestInit(initData: requestInitData), completionHandler: { fetchResponse in
+            XCTAssertNotNil(fetchResponse, "fetchResponse is nil")
+            
+            XCTAssertEqual(fetchResponse!.url, URL_STRING)
+            
+            XCTAssertEqual(fetchResponse!.status, 200)
+            
+            XCTAssertEqual(fetchResponse!.bufferedText, responseText)
+            
+            expectation.fulfill()
+        })
+        
+        wait(for: [expectation], timeout: 1.0)
+    }
+    
+    func testMakeHttpPostRequest_BodyMissing() {
+        let URL_STRING = "https://example.org/"
+        
+        let polyOut = PolyOut(session: NetworkSessionMock())
+        
+        let responseText = "This is the post response"
+        sessionPostData = responseText.data(using: .utf8)
+        let requestInitData = [MessagePackValue("method") : MessagePackValue("post")]
+            
+        let expectation = XCTestExpectation(description: "Test valid response")
+        polyOut.makeHttpRequest(urlString: URL_STRING, requestInit: FetchRequestInit(initData: requestInitData), completionHandler: { fetchResponse in
+            XCTAssertNil(fetchResponse, "fetchResponse is nil")
+            
+            expectation.fulfill()
+        })
+        
+        wait(for: [expectation], timeout: 1.0)
+    }
+    
+    func testMakeHttpPostRequest_Error() {
+        let URL_STRING = "https://example.org/"
+        
+        let polyOut = PolyOut(session: NetworkSessionMock())
+        
+        sessionError = URLError(.unknown)
+        let requestInitData = [MessagePackValue("method") : MessagePackValue("post"),
+        MessagePackValue("body") : MessagePackValue("This is the body")]
+        
+        let expectation = XCTestExpectation(description: "Test error")
+        polyOut.makeHttpRequest(urlString: URL_STRING, requestInit: FetchRequestInit(initData: requestInitData), completionHandler: { fetchResponse in
+            XCTAssertNil(fetchResponse, "fetchResponse is not nil")
+            
+            expectation.fulfill()
+        })
+        
+        wait(for: [expectation], timeout: 1.0)
     }
 
 }
