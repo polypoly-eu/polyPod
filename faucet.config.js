a2dbd9284fdcf808cf47b3aeed2ff98ca839a771
Lucas Dohmen <lucas@dohmen.io>
2020-06-25 10:29:01 +0200

    First working version: Render DOM with React

--- faucet.config.js
+++ faucet.config.js
@@ -7,5 +7,16 @@ module.exports = {
       "react": "React",
       "react-dom": "ReactDOM"
     }
+  }],
+
+  static: [{
+    source: "./src/index.html",
+    target: "./dist/index.html",
+  }, {
+    source: "./node_modules/react/umd/react.development.js",
+    target: "./dist/react.js",
+  }, {
+    source: "./node_modules/react-dom/umd/react-dom.development.js",
+    target: "./dist/react-dom.js",
   }]
 }
