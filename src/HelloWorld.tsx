95ac24a4d9c602971c4c4311f2ee98c13ce198d8
Lucas Dohmen <lucas@dohmen.io>
2020-06-26 16:12:32 +0200

    Example for using the polypod-api

--- src/HelloWorld.tsx
+++ src/HelloWorld.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { pod } from "@polypoly-eu/feature-bootstrap";
 /* import {Button} from "@polypoly-eu/poly-look"; */
 
 const view = (
@@ -14,3 +15,9 @@ ReactDOM.render(
   view,
   document.getElementById("feature")
 );
+
+pod.then(async pod => {
+  let response = await pod.polyOut.fetch("http://example.org");
+  let content = await response.text();
+  console.log(content);
+});
