b8fc228b70cc61f68c8fe9e43de08e80766d7e41
Lars Hupel <lars.hupel@innoq.com>
2020-07-13 11:29:31 +0200

    export more testing hooks

--- src/testkit.ts
+++ src/testkit.ts
@@ -1,5 +1,12 @@
 import {Page} from "puppeteer";
 import {exposedPromise} from "exposed-promises";
+import {Server} from "http";
+import {detectFeature} from "./cli/_common";
+import {Volume} from "memfs";
+import {dataset} from "@rdfjs/dataset";
+import fetch from "node-fetch";
+import {serve} from "./serve";
+import {Pod, DefaultPod} from "@polypoly-eu/poly-api";
 
 interface TestResult {
     failures: number;
@@ -10,6 +17,19 @@ declare global {
     interface Window {
         testCompleted(result: TestResult): void;
     }
+}
+
+export function inMemoryPod(): Pod {
+    return new DefaultPod(
+        dataset(),
+        new Volume().promises as any,
+        fetch
+    );
+}
+
+export async function testServer(port: number, pod: Pod = inMemoryPod()): Promise<Server> {
+    const [dir, manifest] = await detectFeature({});
+    return serve(port, pod, dir, manifest);
 }
 
 export async function preparePage(page: Page): Promise<() => Promise<TestResult>> {
