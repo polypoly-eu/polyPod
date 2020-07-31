177b1053c395b2cb05f1eb28902188cbd79823cf
Lars Hupel <lars.hupel@innoq.com>
2020-07-31 09:32:50 +0200

    workaround for unhandled promise rejection

--- src/tests/unfetch.test.ts
+++ src/tests/unfetch.test.ts
@@ -6,6 +6,18 @@ const jsdom = new JSDOM();
 
 global.XMLHttpRequest = jsdom.window.XMLHttpRequest;
 
+// <https://github.com/jsdom/jsdom/issues/2375>
+process.on("unhandledRejection", (reason, p) => {
+    p.catch((err) => {
+        if (typeof err === "object" && err.name === "StatusCodeError") {
+            // do nothing;
+        } else {
+            console.dir(err);
+            process.exit(1);
+        }
+    });
+});
+
 describe("unfetch", () => {
     fetchSpec(fetch, getHttpbinUrl());
 });
