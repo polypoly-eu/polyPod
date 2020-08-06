e71c156c4d95c91a7e12fdf2e241f10eda1fdc25
Lars Hupel <lars.hupel@innoq.com>
2020-08-06 10:00:17 +0200

    Async pod

---
+++ src/async.ts
@@ -0,0 +1,58 @@
+import {
+    RequestInit,
+    Response,
+    Pod,
+    PolyOut,
+    PolyIn,
+    EncodingOptions,
+    Matcher,
+    Stats,
+} from "@polypoly-eu/poly-api";
+import { DataFactory, Quad } from "rdf-js";
+
+class AsyncPolyOut implements PolyOut {
+    constructor(private readonly promise: Promise<PolyOut>) {}
+
+    async fetch(input: string, init?: RequestInit): Promise<Response> {
+        return (await this.promise).fetch(input, init);
+    }
+
+    readFile(path: string, options: EncodingOptions): Promise<string>;
+    readFile(path: string): Promise<Uint8Array>;
+    async readFile(path: string, options?: EncodingOptions): Promise<string | Uint8Array> {
+        if (options) return (await this.promise).readFile(path, options);
+        else return (await this.promise).readFile(path);
+    }
+
+    async writeFile(path: string, content: string, options: EncodingOptions): Promise<void> {
+        return (await this.promise).writeFile(path, content, options);
+    }
+    async stat(path: string): Promise<Stats> {
+        return (await this.promise).stat(path);
+    }
+    async readdir(path: string): Promise<string[]> {
+        return (await this.promise).readdir(path);
+    }
+}
+
+class AsyncPolyIn implements PolyIn {
+    constructor(private readonly promise: Promise<PolyIn>) {}
+
+    async select(matcher: Partial<Matcher>): Promise<Quad[]> {
+        return (await this.promise).select(matcher);
+    }
+
+    async add(...quads: Quad[]): Promise<void> {
+        return (await this.promise).add(...quads);
+    }
+}
+
+export class AsyncPod implements Pod {
+    readonly polyOut: PolyOut;
+    readonly polyIn: PolyIn;
+
+    constructor(private readonly promise: Promise<Pod>, public readonly dataFactory: DataFactory) {
+        this.polyOut = new AsyncPolyOut(promise.then((pod) => pod.polyOut));
+        this.polyIn = new AsyncPolyIn(promise.then((pod) => pod.polyIn));
+    }
+}
---
+++ src/tests/async.test.ts
@@ -0,0 +1,31 @@
+import { Pod, DefaultPod } from "@polypoly-eu/poly-api";
+import { Volume } from "memfs";
+import { dataset } from "@rdfjs/dataset";
+import fetch from "node-fetch";
+import { getHttpbinUrl, podSpec } from "@polypoly-eu/poly-api/dist/spec";
+import { AsyncPod } from "../async";
+import { DataFactory } from "@polypoly-eu/rdf";
+
+describe("Async pod", () => {
+    describe("Resolved promise", () => {
+        const fs = new Volume().promises as any;
+        const underlying = new DefaultPod(dataset(), fs, fetch);
+
+        podSpec(
+            new AsyncPod(Promise.resolve(underlying), new DataFactory(false)),
+            "/",
+            getHttpbinUrl()
+        );
+    });
+
+    describe("Delayed promise", () => {
+        const fs = new Volume().promises as any;
+        const underlying = new DefaultPod(dataset(), fs, fetch);
+
+        const delayed = new Promise<Pod>((resolve) => {
+            setTimeout(() => resolve(underlying), 500);
+        });
+
+        podSpec(new AsyncPod(delayed, new DataFactory(false)), "/", getHttpbinUrl());
+    });
+});
