f02894fe229efd3ddc8cd602d33b0713a87af928
Jacek Bilski <jacek.bilski@innoq.com>
2020-08-10 15:01:24 +0200

    Removing unused FloatingActionButton.

--- app/src/main/kotlin/eu/polypoly/pod/android/MainActivity.kt
+++ app/src/main/kotlin/eu/polypoly/pod/android/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,11 +12,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
