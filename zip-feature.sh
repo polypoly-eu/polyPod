8331538802c414f92f90c6af471fb9c1e55ac7ca
carmenb80 <carmen.burmeister@innoq.com>
2020-08-25 09:57:31 +0200

    Add script for zipping a feature

---
+++ zip-feature.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd Features
+D=$1
+cd $D
+FEATURE_NAME="$(basename $(pwd))"
+cd dist
+zip -r ../$FEATURE_NAME.zip .
+cd ..
+mv $FEATURE_NAME.zip ../../PolyPodApp/Features/$FEATURE_NAME
+cd ..
