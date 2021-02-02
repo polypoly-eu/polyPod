bea5cd62cf72f81c1e68a059704797a47bd4177d
Oleksandr Paraska <shoniko@gmail.com>
2021-02-02 01:40:45 +0100

    Fix linting in aop-ts

--- eslint-config-polypoly/.eslintrc.js
+++ eslint-config-polypoly/.eslintrc.js
@@ -1,24 +1,27 @@
 module.exports = {
     parser: "@typescript-eslint/parser",
-    plugins: [
-        "@typescript-eslint"
-    ],
+    plugins: ["@typescript-eslint"],
     extends: [
         "eslint:recommended",
         "plugin:@typescript-eslint/eslint-recommended",
         "plugin:@typescript-eslint/recommended",
         "prettier/@typescript-eslint",
-        "plugin:prettier/recommended"
+        "plugin:prettier/recommended",
     ],
     rules: {
-        "semi": 2,
+        semi: 2,
         "@typescript-eslint/camelcase": "off",
         "@typescript-eslint/no-explicit-any": "off",
         "@typescript-eslint/no-non-null-assertion": "off",
         "@typescript-eslint/ban-ts-comment": "off",
-        "@typescript-eslint/explicit-function-return-type": ["error", {
-            allowExpressions: true
-        }],
-        "@typescript-eslint/no-this-alias": "off"
+        "@typescript-eslint/explicit-function-return-type": [
+            "error",
+            {
+                allowExpressions: true,
+            },
+        ],
+        "@typescript-eslint/no-this-alias": "off",
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unused-vars": ["warn"],
     },
 };
