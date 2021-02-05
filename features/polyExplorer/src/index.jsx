2348dfcd83f72ee3bbe1f08b0c5cf755aed20494
Richard Stoffels <richard.stoffels@polypoly.net>
2021-02-05 16:35:19 +0100

    Fixing linting errors

--- features/polyExplorer/src/index.jsx
+++ features/polyExplorer/src/index.jsx
@@ -4,6 +4,5 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import PolyExplorer from "./components/polyExplorer.jsx";
 
-
 ReactDOM.render(<PolyExplorer />, document.getElementById("feature"));
 
