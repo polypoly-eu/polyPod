dd8c35b1cb5473121a3222db0a6e07d335a7665b
Oleksandr Paraska <shoniko@gmail.com>
2021-02-06 17:54:04 +0100

    Cosmetic fix. Rebuilt package-lock in fetch-spec

--- dummy-server/.eslintrc.cjs
+++ dummy-server/.eslintrc.cjs
@@ -1,17 +1,16 @@
 module.exports = {
-  extends: ["@polypoly-eu/eslint-config-polypoly"],
-  globals: {
-      "process": true,
-      "console": true,
-      "setTimeout": true
-  },
-  overrides: [
-    {
-        // For some reason, plugin:react/recommended doesn't set these options
-        files: ["*.js"],
-        parserOptions: {
-            sourceType: "module",
-        }
-    }
-    ]
+    extends: ["@polypoly-eu/eslint-config-polypoly"],
+    globals: {
+        process: true,
+        console: true,
+        setTimeout: true,
+    },
+    overrides: [
+        {
+            files: ["*.js"],
+            parserOptions: {
+                sourceType: "module",
+            },
+        },
+    ],
 };
