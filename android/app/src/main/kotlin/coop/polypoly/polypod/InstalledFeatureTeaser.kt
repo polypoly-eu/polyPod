d4de993ef9b66aa90d3ddf6cd3b5d229d98a5422
Felix Dahlke <felix.dahlke@polypoly.net>
2021-02-09 21:51:05 +0100

    Implement basic feature card view

--- android/app/src/main/kotlin/coop/polypoly/polypod/InstalledFeatureTeaser.kt
+++ android/app/src/main/kotlin/coop/polypoly/polypod/InstalledFeatureTeaser.kt
@@ -3,31 +3,31 @@ package coop.polypoly.polypod
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
 
-class InstalledFeatureTeaser(private val originatingFragment: Fragment, private val installedFeatures: List<String>) : RecyclerView.Adapter<InstalledFeatureTeaser.ViewHolder>() {
+class InstalledFeatureTeaser(private val originatingFragment: Fragment, private val features: List<String>) : RecyclerView.Adapter<InstalledFeatureTeaser.ViewHolder>() {
 
-    class ViewHolder(val itemListView: ConstraintLayout) : RecyclerView.ViewHolder(itemListView)
+    class ViewHolder(val featureCardView: CardView) : RecyclerView.ViewHolder(featureCardView)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val itemListView = LayoutInflater.from(parent.context)
-            .inflate(R.layout.installed_feature_teaser, parent, false) as ConstraintLayout
-        return ViewHolder(itemListView)
+        val featureCardView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.installed_feature_teaser, parent, false) as CardView
+        return ViewHolder(featureCardView)
     }
 
-    override fun getItemCount() = installedFeatures.size
+    override fun getItemCount() = features.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val textView = holder.itemListView.getViewById(R.id.textView) as TextView
-        textView.text = installedFeatures[position]
-        holder.itemListView.setOnClickListener {
+        val featureNameView = holder.featureCardView.findViewById(R.id.feature_name) as TextView
+        featureNameView.text = features[position]
+        holder.featureCardView.setOnClickListener {
             // FIXME - navigation assumes we're coming from FirstFragment, which might not necessary be true
             val action =
                 FeatureListFragmentDirections.actionFeatureListFragmentToFeatureFragment(
-                    installedFeatures[position]
+                    features[position]
                 )
             findNavController(originatingFragment).navigate(action)
         }
