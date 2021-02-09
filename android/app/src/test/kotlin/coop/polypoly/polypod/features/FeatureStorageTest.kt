a6098a8d284fc14156af73124df6372f3c6dd7b7
Felix Dahlke <felix.dahlke@polypoly.net>
2021-02-09 16:18:51 +0100

    Fix FeatureStorage tests
    The Config() annotation tells Robolectric to run against the minSdk
    version rather than the latest SDK. Android Studio seems to ship with
    Java 8, whereas Robolectric requires Java 9 to work with SDK 29.

--- android/app/src/test/kotlin/coop/polypoly/polypod/features/FeatureStorageTest.kt
+++ android/app/src/test/kotlin/coop/polypoly/polypod/features/FeatureStorageTest.kt
@@ -7,12 +7,14 @@ import com.google.common.truth.Truth.assertThat
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
 import org.robolectric.annotation.LooperMode
 import org.slf4j.LoggerFactory
 import java.io.File
 
 @LooperMode(LooperMode.Mode.PAUSED)
 @RunWith(AndroidJUnit4::class)
+@Config(sdk = [Config.OLDEST_SDK])
 class FeatureStorageTest {
     companion object {
         @Suppress("JAVA_CLASS_ON_COMPANION")
@@ -20,20 +22,20 @@ class FeatureStorageTest {
     }
 
     private val context: Context = ApplicationProvider.getApplicationContext<Context>()
-    private val wallet = FeatureStorage()
+    private val featureStorage = FeatureStorage()
 
     private lateinit var featuresDir: File
 
     @Before
     fun setup() {
-        val mainDir = context.getExternalFilesDir(null)
+        val mainDir = context.filesDir
         featuresDir = File(mainDir, "features")
         featuresDir.mkdirs()
     }
 
     @Test
     fun whenNoFeaturesAreInstalled_featuresListIsEmpty() {
-        val result = wallet.listFeatures(context)
+        val result = featureStorage.listFeatures(context)
         assertThat(result).isEmpty()
     }
 
@@ -41,7 +43,7 @@ class FeatureStorageTest {
     fun whenOneFeatureIsInstalled_featuresListContainsItsName() {
         val featureFile = File(featuresDir, "feature1.zip")
         assertThat(featureFile.createNewFile()).isTrue()
-        val result = wallet.listFeatures(context)
+        val result = featureStorage.listFeatures(context)
         assertThat(result).hasSize(1)
         assertThat(result).containsExactly("feature1")
     }
