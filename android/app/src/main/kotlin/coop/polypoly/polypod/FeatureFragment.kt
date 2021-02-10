32694d1a50544d0233f6a1d8215039718e119955
Felix Dahlke <felix.dahlke@polypoly.net>
2021-02-10 15:29:49 +0100

    Remove unused import

--- android/app/src/main/kotlin/coop/polypoly/polypod/FeatureFragment.kt
+++ android/app/src/main/kotlin/coop/polypoly/polypod/FeatureFragment.kt
@@ -7,7 +7,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.*
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
