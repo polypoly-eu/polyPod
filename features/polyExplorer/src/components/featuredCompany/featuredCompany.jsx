0f3ee0632dedb21c6bd42fad4fd8eb22f827add1
Richard Stoffels <richard.stoffels@polypoly.net>
2021-02-11 16:33:24 +0100

    Adding datasharing buttons

--- features/polyExplorer/src/components/featuredCompany/featuredCompany.jsx
+++ features/polyExplorer/src/components/featuredCompany/featuredCompany.jsx
@@ -3,15 +3,49 @@ import { default as stylesArray } from "./featuredCompanyStyle.json";
 
 const FeaturedCompany = ({ company }) => {
     const styles = stylesArray[0];
+    console.log(company);
+
+    const getContentButtons = () => {
+        if (company.jurisdictions === undefined)
+            return (
+                <div style={styles.contentButtonHolder}>
+                    <button style={styles.dataSharedButton}>
+                        shares {company.dataTypesShared.length} datatypes
+                    </button>
+                    <button style={styles.purposesSharedButton}>
+                        for {company.dataSharingPurposes.length} purposes
+                    </button>
+                    <button style={styles.companiesSharedButton}>
+                        with {company.sharedWithCompanies.length} companies
+                    </button>
+                    <button style={styles.jurisdictionsSharedButton}>
+                        in X jurisdictions
+                    </button>
+                </div>
+            );
+        return (
+            <div style={styles.contentButtonHolder}>
+                <button style={styles.dataSharedButton}>
+                    shares {company.dataTypesShared.length} datatypes
+                </button>
+                <button style={styles.purposesSharedButton}>
+                    for {company.dataSharingPurposes.length} purposes
+                </button>
+                <button style={styles.companiesSharedButton}>
+                    with {company.sharedWithCompanies.length} companies
+                </button>
+                <button style={styles.jurisdictionsSharedButton}>
+                    in {company.jurisdictions.children.length} jurisdictions
+                </button>
+            </div>
+        );
+    };
 
     return (
         <div style={styles.companyCard}>
             <h2 style={styles.companyName}>{company.name}</h2>
-            <p style={styles.companyText}>
-                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                diam nonumy eirmod tempor invidunt ut labore et dolore magna
-            </p>
-            <button></button>
+            <p style={styles.companyText}>Company Information</p>
+            {getContentButtons()}
         </div>
     );
 };
