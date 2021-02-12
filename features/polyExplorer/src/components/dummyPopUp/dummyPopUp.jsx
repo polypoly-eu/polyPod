45888e77494a8b544af363216292d2b0b1d93358
Richard Stoffels <richard.stoffels@polypoly.net>
2021-02-12 09:34:37 +0100

    Make a dummy PopUp

---
+++ features/polyExplorer/src/components/dummyPopUp/dummyPopUp.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+const DummyPopUp = ({ text, onPopUpClose }) => {
+    const style = {
+        container: {
+            backgroundColor: "#eaeaea",
+            position: "absolute",
+            top: "10%",
+            left: "10%",
+            bottom: "10%",
+            right: "10%",
+            zIndex: "200",
+            borderRadius: "20px",
+        },
+        text: {
+            textAlign: "center",
+        },
+        button: {
+            backgroundColor: "#9e9e9e",
+            height: "10%",
+            width: "20%",
+            position: "absolute",
+            top: "85%",
+            padding: "8px",
+            color: "#ffffff",
+            left: "40%",
+            borderRadius: "8px",
+            border: "transparent",
+        },
+        background: {
+            position: "absolute",
+            backgroundColor: "#0F1938",
+            width: "100%",
+            height: "100%",
+        },
+    };
+
+    console.log("popup");
+    return (
+        <div style={style.background}>
+            <div style={style.container}>
+                <h2 style={style.text}>You have invoked the {text} popup</h2>
+                <button onClick={() => onPopUpClose()} style={style.button}>
+                    Close
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default DummyPopUp;
--- features/polyExplorer/src/index.jsx
+++ features/polyExplorer/src/index.jsx
@@ -1,25 +1,37 @@
 "use strict";
 
-import * as React from "react";
+import React from "react";
 import * as ReactDOM from "react-dom";
 import PolyExplorer from "./components/polyExplorer/polyExplorer.jsx";
+import DummyPopUp from "./components/dummyPopUp/dummyPopUp.jsx";
 
-function registerPodNavActions() {
-    // This is just a crutch until we have real callbacks for the info and
-    // search action.
-    function alert(text) {
-        window.alert(`You pressed ${text}. This is a pop-up for you!`);
+const runPolyExplorer = () => {
+    const handlePopUpClose = () => {
+        ReactDOM.render(<PolyExplorer />, document.getElementById("feature"));
+    };
+
+    function registerPodNavActions() {
+        // This is just a crutch until we have real callbacks for the info and
+        // search action.
+        function alert(text) {
+            ReactDOM.render(
+                <DummyPopUp text={text} onPopUpClose={handlePopUpClose} />,
+                document.getElementById("feature")
+            );
+        }
+
+        if (!window.podNav) return;
+
+        window.podNav.actions = {
+            info: () => alert("Here be info!"),
+            search: () => alert("Here be search!"),
+        };
+        window.podNav.registerAction("info");
+        window.podNav.registerAction("search");
     }
 
-    if (!window.podNav) return;
+    registerPodNavActions();
+    ReactDOM.render(<PolyExplorer />, document.getElementById("feature"));
+};
 
-    window.podNav.actions = {
-        info: () => alert("Here be info!"),
-        search: () => alert("Here be search!"),
-    };
-    window.podNav.registerAction("info");
-    window.podNav.registerAction("search");
-}
-
-registerPodNavActions();
-ReactDOM.render(<PolyExplorer />, document.getElementById("feature"));
+runPolyExplorer();
