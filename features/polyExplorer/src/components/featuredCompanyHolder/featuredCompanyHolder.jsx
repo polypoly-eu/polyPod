e8f55b7bfb23233ad43ff014c65319e695599b52
Richard Stoffels <richard.stoffels@polypoly.net>
2021-02-15 15:46:00 +0100

    Adding Swiper.js and implementing it in feature company holder.

--- features/polyExplorer/src/components/featuredCompanyHolder/featuredCompanyHolder.jsx
+++ features/polyExplorer/src/components/featuredCompanyHolder/featuredCompanyHolder.jsx
@@ -1,17 +1,30 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.css";
+import SwiperCore from "swiper";
+
 import FeaturedCompany from "../featuredCompany/featuredCompany.jsx";
 import "./featuredCompanyHolder.css";
 
 const FeaturedCompanyHolder = ({ featuredCompanies, onShowScreenChange }) => {
     return (
-        <div className="featured-company-card-container">
-            {featuredCompanies.map((company, index) => (
-                <FeaturedCompany
-                    key={index}
-                    company={company}
-                    onShowScreenChange={onShowScreenChange}
-                />
-            ))}
+        <div className="featured-company-holder">
+            <Swiper
+                spaceBetween={1}
+                slidesPerView={1}
+                onSlideChange={() => console.log("slide change")}
+                onSwiper={(swiper) => console.log(swiper)}
+            >
+                {featuredCompanies.map((company, index) => (
+                    <SwiperSlide>
+                        <FeaturedCompany
+                            key={index}
+                            company={company}
+                            onShowScreenChange={onShowScreenChange}
+                        />
+                    </SwiperSlide>
+                ))}
+            </Swiper>
         </div>
     );
 };
