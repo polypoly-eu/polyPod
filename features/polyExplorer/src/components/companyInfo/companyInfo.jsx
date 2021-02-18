a57c535817fa257b4423d579ed533292fc2d81b4
Richard Stoffels <richard.stoffels@polypoly.net>
2021-02-18 15:37:02 +0100

    Introducing (empty) new components companyInfo and featuredCompanyInfo.

---
+++ features/polyExplorer/src/components/companyInfo/companyInfo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CompanyInfo = () => {
+    return 0;
+};
+
+export default CompanyInfo;
---
+++ features/polyExplorer/src/components/featuredCompanyInfo/featuredCompanyInfo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FeaturedCompanyInfo = () => {
+    return 0;
+};
+
+export default FeaturedCompanyInfo;
