c74b130a7ed43aa6edd4b1a40ef56f7f11184f26
Felix Dahlke <felix.dahlke@polypoly.net>
2021-02-18 15:39:43 +0100

    Show company list 80% like in the designs

--- features/polyExplorer/src/components/companyList/companyList.jsx
+++ features/polyExplorer/src/components/companyList/companyList.jsx
@@ -2,11 +2,30 @@ import React from "react";
 import CompanyShortInfo from "../companyShortInfo/companyShortInfo.jsx";
 import "./companyList.css";
 
+function groupCompanies(companies) {
+    const sorted = companies.sort((a, b) => a.name.localeCompare(b.name));
+    const groups = {};
+    sorted.forEach((company) => {
+        const key = company.name[0].toUpperCase();
+        groups[key] = groups[key] || [];
+        groups[key].push(company);
+    });
+    return groups;
+}
+
 const CompanyList = ({ companies }) => {
+    const companyGroups = groupCompanies(companies);
     return (
         <div className="company-list">
-            {companies.map((company, index) => (
-                <CompanyShortInfo key={index} company={company} />
+            {Object.entries(companyGroups).map(([label, companies], index) => (
+                <div key={index} className="company-group">
+                    <div className="company-group-label">{label}</div>
+                    <div className="company-group-companies">
+                        {companies.map((company, index) => (
+                            <CompanyShortInfo key={index} company={company} />
+                        ))}
+                    </div>
+                </div>
             ))}
         </div>
     );
