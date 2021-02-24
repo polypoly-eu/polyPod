9ce774ffbc9ed359697b0369bbe7c26a1eb3346e
Richard Stoffels <richard.stoffels@polypoly.net>
2021-02-24 15:59:42 +0100

    Fix path in fetch-data.

--- features/polyExplorer/fetch-polypedia-data.js
+++ features/polyExplorer/fetch-polypedia-data.js
@@ -1,7 +1,13 @@
 const shell = require("shelljs");
 const desiredRevision = "cd5da73c430aa4c6b4c30be951cf8bbd90f576fe";
 const path = "./polypedia_data";
+
+if (!shell.which("git")) {
+    shell.echo("Sorry, this script requires git");
+    shell.exit(1);
+}
+
 shell.cd(path);
 shell.exec("git clone https://github.com/polypoly-eu/polypedia-data/");
 shell.exec(`git reset --hard ${desiredRevision}`);
-shell.cp("./data/3_integrated/polyexplorer ./polypedia_data");
+shell.cp("./data/3_integrated/polyexplorer/*", "./polypedia_data");
