a5ea3d5fefe7c093c1cf1e219d0ba8f7809c6c03
Richard Stoffels <richard.stoffels@polypoly.net>
2021-02-24 15:45:10 +0100

    Comment in desiredRevision.

--- features/polyExplorer/fetch-polypedia-data.js
+++ features/polyExplorer/fetch-polypedia-data.js
@@ -3,5 +3,5 @@ const desiredRevision = "cd5da73c430aa4c6b4c30be951cf8bbd90f576fe";
 const path = "./polypedia_data";
 shell.cd(path);
 shell.exec("git clone https://github.com/polypoly-eu/polypedia-data/");
-//shell.exec(`git reset --hard ${desiredRevision}`);
+shell.exec(`git reset --hard ${desiredRevision}`);
 shell.cp("./data/3_integrated/polyexplorer ./polypedia_data");
