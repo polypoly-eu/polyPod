c243f6bad5128284ef17439c71a72d1e8ff65a52
Felix Dahlke <felix.dahlke@polypoly.net>
2021-02-25 20:37:42 +0100

    Fix linting issue

--- features/polyExplorer/src/components/screens/companyInfoScreen/companyInfoScreen.jsx
+++ features/polyExplorer/src/components/screens/companyInfoScreen/companyInfoScreen.jsx
@@ -51,11 +51,7 @@ const CompanyInfo = ({ company }) => {
             </div>
         ),
         structure: 0,
-        revenue: (
-            <pre>
-                {JSON.stringify(company.annualRevenue, null, 2)}
-            </pre>
-        ),
+        revenue: <pre>{JSON.stringify(company.annualRevenue, null, 2)}</pre>,
     };
     const featuredTabContent = {
         location: (
