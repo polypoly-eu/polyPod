7999f83df067279f627c5c7bf2e91ba0361a3736
Richard Stoffels <richard.stoffels@polypoly.net>
2021-02-25 15:15:38 +0100

    Unnecessary useState gone.

--- features/polyExplorer/src/components/screens/mainScreen/mainScreen.jsx
+++ features/polyExplorer/src/components/screens/mainScreen/mainScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 import i18n from "../../../i18n.js";
 import FeaturedCompanyHolder from "../../featuredCompanyHolder/featuredCompanyHolder.jsx";
