637b2814de01458ee111697a520940cfe9045718
Felix Dahlke <felix.dahlke@polypoly.net>
2021-02-28 08:56:00 +0100

    Add German translations (and set language to German)

--- features/polyExplorer/src/i18n.js
+++ features/polyExplorer/src/i18n.js
@@ -8,6 +8,12 @@ import companyInfoScreenEn from "./locales/en/companyInfoScreen.json";
 import companySearchScreenEn from "./locales/en/companySearchScreen.json";
 import mainScreenEn from "./locales/en/mainScreen.json";
 
+import commonDe from "./locales/de/common.json";
+import companyFilterScreenDe from "./locales/de/companyFilterScreen.json";
+import companyInfoScreenDe from "./locales/de/companyInfoScreen.json";
+import companySearchScreenDe from "./locales/de/companySearchScreen.json";
+import mainScreenDe from "./locales/de/mainScreen.json";
+
 const strings = {
     en: {
         common: commonEn,
@@ -16,12 +22,19 @@ const strings = {
         companySearchScreen: companySearchScreenEn,
         mainScreen: mainScreenEn,
     },
+    de: {
+        common: commonDe,
+        companyFilterScreen: companyFilterScreenDe,
+        companyInfoScreen: companyInfoScreenDe,
+        companySearchScreen: companySearchScreenDe,
+        mainScreen: mainScreenDe,
+    },
 };
 
 export default {
     t: (key, options = {}) => {
         const [namespace, keyInNamespace] = key.split(/:(.+)/);
-        let translation = strings["en"][namespace][keyInNamespace];
+        let translation = strings["de"][namespace][keyInNamespace];
         for (let [name, value] of Object.entries(options))
             translation = translation.replace(`{{${name}}}`, value);
         return translation;
