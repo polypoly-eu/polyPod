34ed60841f35a49b02fd248933761ed89602a09c
Felix Dahlke <felix.dahlke@polypoly.net>
2021-03-02 14:15:01 +0100

    Support API level 24
    I've tested various Android versions, and without any changes,
    polyExplorer currently only works on API level 28, which supports only a
    fraction of the devices out there.
    
    In the end, I decided to include a polyfill for Object.fromEntries in
    the polyPod itself, to keep the burden of supporting old WebView
    versions low on feature developers. If we find more stuff that's
    missing, we can add more polyfills. We should however consider to move
    away from the system WebView - as long as we use it, features need to be
    tested very carefully on older Android versions.

--- android/app/src/main/assets/container/container.js
+++ android/app/src/main/assets/container/container.js
@@ -1,20 +1,33 @@
-const {port1, port2} = new MessageChannel();
-
-let outerPort;
-
 // outerPort - bootstrap's end of a channel with the Pod
 // port1 - bootstrap's end of a channel with the Feature
 // port2 - Feature's end of a channel with the bootstrap
-
+//
 // Pod <-> innerPort <-> outerPort <-> boostrap <-> port1 <-> port2 <-> Feature
+const {port1, port2} = new MessageChannel();
+let outerPort;
 
-window.onmessage = event => {
-    outerPort = event.ports[0];
-    outerPort.onmessage = event => {
-        // console.log(`Data coming from Pod to the Feature`);
-        // console.dir(event.data);
-        const bytes = Uint8Array.from(atob(event.data), c => c.charCodeAt(0));
-        port1.postMessage(bytes);
+// To keep the burden of supporting older system WebView versions low on feature
+// developers, we polyfill some modern JavaScript features.
+// The more permanent solution is to not rely on the system WebView anymore.
+function initPolyfills() {
+    if (!Object.fromEntries) {
+        Object.fromEntries = function(entries) {
+            const obj = {};
+            entries.forEach(([key, value]) => obj[key] = value);
+            return obj;
+        };
+    }
+}
+
+function initMessaging() {
+    window.onmessage = event => {
+        outerPort = event.ports[0];
+        outerPort.onmessage = event => {
+            // console.log(`Data coming from Pod to the Feature`);
+            // console.dir(event.data);
+            const bytes = Uint8Array.from(atob(event.data), c => c.charCodeAt(0));
+            port1.postMessage(bytes);
+        }
     }
 }
 
@@ -40,4 +53,6 @@ function loadFeature() {
     iFrame.src = `features/${featureName}/index.html`;
 }
 
+initPolyfills();
+initMessaging();
 loadFeature();
