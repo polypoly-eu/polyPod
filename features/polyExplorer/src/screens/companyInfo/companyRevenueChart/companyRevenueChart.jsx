61250a85be486e54314a32e70aace376f0d0a4c1
Richard Stoffels <richard.stoffels@polypoly.net>
2021-03-03 14:04:17 +0100

    Add i18n & delete log

--- features/polyExplorer/src/screens/companyInfo/companyRevenueChart/companyRevenueChart.jsx
+++ features/polyExplorer/src/screens/companyInfo/companyRevenueChart/companyRevenueChart.jsx
@@ -4,69 +4,62 @@ import "./companyRevenueChart.css";
 
 const CompanyRevenueChart = ({ annualRevenues }) => {
     const years = [2015, 2016, 2017, 2018, 2019];
-    if (annualRevenues) {
-        const numSteps = 4;
-        console.log(annualRevenues);
 
-        //from 2015
-        const recentAnnualRevenues = annualRevenues?.filter(
-            (e) => e.year >= years[0] && e.year <= years[years.length - 1]
+    const numSteps = 4;
+
+    //from 2015
+    const recentAnnualRevenues = annualRevenues?.filter(
+        (e) => e.year >= years[0] && e.year <= years[years.length - 1]
+    );
+
+    const getHighestAmount = () => {
+        let highest = 0;
+        recentAnnualRevenues?.forEach((e) =>
+            e.amount > highest ? (highest = e.amount) : null
         );
+        return highest;
+    };
 
-        const getHighestAmount = () => {
-            let highest = 0;
-            recentAnnualRevenues?.forEach((e) =>
-                e.amount > highest ? (highest = e.amount) : null
-            );
-            return highest;
-        };
-
-        const getYearlyAmount = (year, unitNumber) => {
-            let amount;
-            annualRevenues.forEach((e) => {
-                e.year == year ? (amount = e.amount / unitNumber) : null;
-            });
-            return amount ? amount : null;
-        };
-
-        const getUnit = (value) => {
-            const b = Math.pow(10, 9);
-            const m = Math.pow(10, 6);
-            if (value >= b) {
-                return ["B", b];
-            } else if (value >= m) {
-                return ["M", m];
-            } else {
-                return ["K", 1000];
-            }
-        };
-
-        const getScaleVars = (value) => {
-            const [unit, unitNumber] = getUnit(value);
-            let scaleRef = Math.ceil(value / unitNumber / numSteps) * numSteps;
-            if (scaleRef - value / unitNumber < 1) scaleRef += 4;
-            return [scaleRef, unit, unitNumber];
-        };
-
-        const getHeight = (amount, scaleRef) => {
-            //max percentage(max is 64%) * amount / scaleRef * 100
-            return amount == null ? 0 : ((0.64 * amount) / scaleRef) * 100;
-        };
-
-        const highestValue = getHighestAmount();
-        const [scaleRef, unit, unitNumber] = getScaleVars(highestValue);
-        let scale = [
-            0,
-            scaleRef * 0.25,
-            scaleRef * 0.5,
-            scaleRef * 0.75,
-            scaleRef,
-        ];
-        const amounts = [];
-        years.forEach((year) => {
-            amounts.push(getYearlyAmount(year, unitNumber));
+    const getYearlyAmount = (year, unitNumber) => {
+        let amount;
+        annualRevenues.forEach((e) => {
+            e.year == year ? (amount = e.amount / unitNumber) : null;
         });
+        return amount ? amount : null;
+    };
 
+    const getUnit = (value) => {
+        const b = Math.pow(10, 9);
+        const m = Math.pow(10, 6);
+        if (value >= b) {
+            return ["B", b];
+        } else if (value >= m) {
+            return ["M", m];
+        } else {
+            return ["K", 1000];
+        }
+    };
+
+    const getScaleVars = (value) => {
+        const [unit, unitNumber] = getUnit(value);
+        let scaleRef = Math.ceil(value / unitNumber / numSteps) * numSteps;
+        if (scaleRef - value / unitNumber < 1) scaleRef += 4;
+        return [scaleRef, unit, unitNumber];
+    };
+
+    const getHeight = (amount, scaleRef) => {
+        //max percentage(max is 64%) * amount / scaleRef * 100
+        return amount == null ? 0 : ((0.64 * amount) / scaleRef) * 100;
+    };
+
+    const highestValue = getHighestAmount();
+    const [scaleRef, unit, unitNumber] = getScaleVars(highestValue);
+    let scale = [0, scaleRef * 0.25, scaleRef * 0.5, scaleRef * 0.75, scaleRef];
+    const amounts = [];
+    years.forEach((year) => {
+        amounts.push(getYearlyAmount(year, unitNumber));
+    });
+    if (amounts.length == 0) {
         return (
             <div className="revenue-chart-container">
                 <div className="revenue-chart">
@@ -152,8 +145,8 @@ const CompanyRevenueChart = ({ annualRevenues }) => {
             </div>
         );
     } else {
-        const scale = [0, 20, 40, 60, 80];
-        const unit = "M";
+        const defaultScale = [0, 20, 40, 60, 80];
+        const defaultUnit = "M";
 
         return (
             <div className="revenue-chart-container">
@@ -183,28 +176,30 @@ const CompanyRevenueChart = ({ annualRevenues }) => {
                         {years[4]}
                     </div>
                     <div className="y-axis" style={{ bottom: "95%" }}>
-                        {scale[4]}
+                        {defaultScale[4]}
                     </div>
                     <div className="y-axis" style={{ bottom: "75%" }}>
-                        {scale[3]}
+                        {defaultScale[3]}
                     </div>
                     <div className="y-axis" style={{ bottom: "55%" }}>
-                        {scale[2]}
+                        {defaultScale[2]}
                     </div>
                     <div className="y-axis" style={{ bottom: "35%" }}>
-                        {scale[1]}
+                        {defaultScale[1]}
                     </div>
                     <div className="y-axis" style={{ bottom: "15%" }}>
-                        {scale[0]}
+                        {defaultScale[0]}
                     </div>
                     <div className="unit">
-                        {i18n.t(`companyInfoScreen:${unit}`)} (EUR)
+                        {i18n.t(`companyInfoScreen:${defaultUnit}`)} (EUR)
                     </div>
                 </div>
                 <div className="no-data-text">
                     More Data will be available soon!
                 </div>
-                <p className="source">Source: polyPedia</p>
+                <p className="source">
+                    {i18n.t("companyInfoScreen:source")}: polyPedia
+                </p>
             </div>
         );
     }
