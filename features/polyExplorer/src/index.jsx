1977b5cde302a3eaba79ce992b1335331f1ab367
Felix Dahlke <felix.dahlke@polypoly.net>
2021-03-04 07:27:45 +0100

    Move global styles out of polyExplorer and screen

--- features/polyExplorer/src/index.jsx
+++ features/polyExplorer/src/index.jsx
@@ -4,4 +4,6 @@ import React from "react";
 import * as ReactDOM from "react-dom";
 import PolyExplorer from "./components/polyExplorer/polyExplorer.jsx";
 
+import "./styles.css";
+
 ReactDOM.render(<PolyExplorer />, document.getElementById("feature"));
