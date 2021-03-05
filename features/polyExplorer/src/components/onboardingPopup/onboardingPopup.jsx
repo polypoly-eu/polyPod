c3c7a07070ec2a76db3a19e5a2d751bded274228
Felix Dahlke <felix.dahlke@polypoly.net>
2021-03-05 18:28:29 +0100

    Adjust onboarding popup to the new design

--- features/polyExplorer/src/components/onboardingPopup/onboardingPopup.jsx
+++ features/polyExplorer/src/components/onboardingPopup/onboardingPopup.jsx
@@ -6,14 +6,14 @@ const OnboardingPopup = ({ onClose, onMoreInfo }) => {
     return (
         <div className="onboarding-popup-container">
             <div className="onboarding-popup">
-                <h1>{i18n.t("onboardingPopup:title")}</h1>
                 <div className="onboarding-popup-content">
+                    <img src="images/onboarding-dialog.svg"></img>
+                    <h1>{i18n.t("onboardingPopup:title")}</h1>
                     <div
                         dangerouslySetInnerHTML={{
                             __html: i18n.t("onboardingPopup:text"),
                         }}
                     ></div>
-                    <div className="image-placeholder"></div>
                 </div>
                 <div className="button-container">
                     <a onClick={onMoreInfo}>
