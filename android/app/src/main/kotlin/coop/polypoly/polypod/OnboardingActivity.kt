7b8aea611c1397f8b087b67601c3b751d28de198
Felix Dahlke <felix.dahlke@polypoly.net>
2021-03-06 11:14:13 +0100

    Make the onboarding look / work largely as intended

--- android/app/src/main/kotlin/coop/polypoly/polypod/OnboardingActivity.kt
+++ android/app/src/main/kotlin/coop/polypoly/polypod/OnboardingActivity.kt
@@ -1,6 +1,7 @@
 package coop.polypoly.polypod
 
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -17,12 +18,42 @@ class OnboardingActivity : AppCompatActivity() {
         }
 
         val carousel = findViewById<CarouselView>(R.id.carousel)
-        val strings = listOf("Here be onboarding!", "Here be more onboarding!")
+        // TODO: Don't hard code these strings
+        val strings = listOf(
+            mapOf(
+                "headlineMain" to "Hallo, darf ich\nmich vorstellen?",
+                "headlineSub" to "Ich bin Ihr\npolyPod.",
+                "bodyText" to "In Zukunft möchte ich gerne dafür sorgen, dass Privates privat bleibt. Das klappt natürlich nicht sofort, aber Schritt für Schritt schaffen wir das gemeinsam."
+            ),
+            mapOf(
+                "headlineMain" to "Im ersten Schritt\nerkunden wir",
+                "headlineSub" to "gemeinsam den\nFluss der Daten.",
+                "bodyText" to "Um etwas zu verändern, müssen wir uns bewusst werden, was genau mit unseren Daten passiert. Lassen Sie uns gemeinsam herausfinden, wie Unternehmen mit Ihren Daten umgehen."
+            ),
+            mapOf(
+                "headlineMain" to "Ein erster Schritt\ndem viele folgen.",
+                "headlineSub" to "Damit Privates\nprivat bleibt.",
+                "bodyText" to "Nach und nach biete ich Ihnen immer mehr Möglichkeiten, Ihre Privatsphäre zu schützen. Ich melde mich natürlich, sobald eine neue Funktion für Sie bereit steht."
+            ))
+
         carousel.pageCount = strings.size
         carousel.setViewListener { position ->
-            val helloText = TextView(applicationContext)
-            helloText.text = strings[position]
-            helloText
+            val slide = layoutInflater.inflate(R.layout.onboarding_slide, null)
+            mapOf(
+                "headlineMain" to R.id.headline_main,
+                "headlineSub" to R.id.headline_sub,
+                "bodyText" to R.id.body_text
+            ).forEach { (key, viewId) ->
+                slide.findViewById<TextView>(viewId).text = strings[position][key]
+            }
+            if (position == strings.size - 1) {
+                val button = slide.findViewById<View>(R.id.end_onboarding_button)
+                button.visibility = View.VISIBLE
+                button.setOnClickListener {
+                    finish()
+                }
+            }
+            slide
         }
     }
 }
