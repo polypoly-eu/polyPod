72e548c581a522bc794e4748d0cc8c7d58f554b6
Felix Dahlke <felix.dahlke@polypoly.net>
2021-03-08 10:22:29 +0100

    Hide aggregation text on info screen for now

--- features/polyExplorer/src/screens/info/info.jsx
+++ features/polyExplorer/src/screens/info/info.jsx
@@ -35,9 +35,12 @@ const InfoScreen = ({ onClose }) => (
             }}
         ></div>
 
-        <h1>{i18n.t("infoScreen:headline.aggregation")}</h1>
-        <img src="images/info-aggregation.svg"></img>
-        <p>{i18n.t("infoScreen:text.aggregation")}</p>
+        {/* Hidden for now until we can come up with a text*/}
+        <div style={{ display: "none" }}>
+            <h1>{i18n.t("infoScreen:headline.aggregation")}</h1>
+            <img src="images/info-aggregation.svg"></img>
+            <p>{i18n.t("infoScreen:text.aggregation")}</p>
+        </div>
 
         <h1>{i18n.t("infoScreen:headline.presentation")}</h1>
         <div
