da3784aeaa32f93499992de898a39c6ad91f7ae8
Felix Dahlke <felix.dahlke@polypoly.net>
2021-03-09 20:31:56 +0100

    Simplify code
    It was a cute idea with the templates and everything, but ultimately
    more code and harder to read than plain HTML.

--- features/polyPreview/src/index.js
+++ features/polyPreview/src/index.js
@@ -4,53 +4,6 @@ import i18n from "./i18n.js";
 import "swiper/swiper-bundle.css";
 import "./styles.css";
 
-const slideData = [
-    {
-        headline: { innerHTML: i18n.t("common:slide1.headline") },
-        subHeadline: { innerHTML: i18n.t("common:slide1.subHeadline") },
-        bodyText: { innerHTML: i18n.t("common:slide1.bodyText") },
-    },
-    {
-        headline: { innerHTML: i18n.t("common:slide7.headline") },
-        subHeadline: { innerHTML: i18n.t("common:slide7.subHeadline") },
-        bodyText: { innerHTML: i18n.t("common:slide7.bodyText") },
-        learnMoreButton: { hidden: false },
-    },
-];
-
-function instantiateSlideContent(data, outerSlide) {
-    const contentTemplate = document.getElementById(
-        outerSlide
-            ? "outer-slide-content-template"
-            : "inner-slide-content-template"
-    );
-    const content = contentTemplate.content.cloneNode(true);
-    const elements = content.querySelectorAll("[data-key]");
-    for (let element of elements) {
-        const elementData = data[element.dataset.key] || {};
-        console.log(elementData);
-        for (let [property, value] of Object.entries(elementData))
-            element[property] = value;
-    }
-    return content.childNodes;
-}
-
-function instantiateSlides() {
-    const slideTemplate = document.getElementById("slide-template");
-    const swiperWrapper = document.querySelector(".swiper-wrapper");
-    for (let [i, slideDataItem] of slideData.entries()) {
-        const contentNodes = instantiateSlideContent(
-            slideDataItem,
-            i === 0 || i === slideData.length - 1
-        );
-        const slide = slideTemplate.content.cloneNode(true);
-        slide
-            .querySelector("[data-key='slideContentWrapper']")
-            .append(...contentNodes);
-        swiperWrapper.append(...slide.childNodes);
-    }
-}
-
 function initSwiper() {
     SwiperCore.use([Pagination]);
     new SwiperCore(".swiper-container", {
@@ -60,18 +13,10 @@ function initSwiper() {
     });
 }
 
-function initUi() {
-    instantiateSlides();
-    initSwiper();
-}
-
 function insertStrings() {
-    const elements = document.querySelectorAll("[data-i18n-key]");
-    for (let element of elements) {
-        const key = element.dataset.i18nKey;
-        element.textContent = i18n.t(key);
-    }
+    for (let element of document.querySelectorAll("[data-i18n-key]"))
+        element.innerHTML = i18n.t(element.dataset.i18nKey);
 }
 
-initUi();
+initSwiper();
 insertStrings();
