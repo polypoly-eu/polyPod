6241c95023138582aa4388723ee7d448d92bcb55
Felix Dahlke <felix.dahlke@polypoly.net>
2021-03-11 11:41:25 +0100

    Reformat FeatureCardAdapter

--- android/app/src/main/kotlin/coop/polypoly/polypod/FeatureCardAdapter.kt
+++ android/app/src/main/kotlin/coop/polypoly/polypod/FeatureCardAdapter.kt
@@ -10,11 +10,18 @@ import androidx.navigation.fragment.NavHostFragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import coop.polypoly.polypod.features.Feature
 
-class FeatureCardAdapter(private val originatingFragment: Fragment, private val features: List<Feature>) : RecyclerView.Adapter<FeatureCardAdapter.ViewHolder>() {
+class FeatureCardAdapter(
+    private val originatingFragment: Fragment,
+    private val features: List<Feature>
+) : RecyclerView.Adapter<FeatureCardAdapter.ViewHolder>() {
 
-    class ViewHolder(val featureCardView: View) : RecyclerView.ViewHolder(featureCardView)
+    class ViewHolder(val featureCardView: View) :
+        RecyclerView.ViewHolder(featureCardView)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewHolder {
         val featureCardView = LayoutInflater.from(parent.context)
             .inflate(R.layout.feature_card, parent, false)
         return ViewHolder(featureCardView)
@@ -35,9 +42,11 @@ class FeatureCardAdapter(private val originatingFragment: Fragment, private val 
     }
 
     private fun updateTexts(view: View, feature: Feature) {
-        mapOf(R.id.feature_name to feature.name,
+        mapOf(
+            R.id.feature_name to feature.name,
             R.id.feature_author to feature.author,
-            R.id.feature_description to feature.description).forEach {
+            R.id.feature_description to feature.description
+        ).forEach {
             (view.findViewById(it.key) as TextView).text = it.value
         }
     }
@@ -50,7 +59,10 @@ class FeatureCardAdapter(private val originatingFragment: Fragment, private val 
         view.setOnClickListener {
             // FIXME - navigation assumes we're coming from FirstFragment, which might not necessary be true
             val action =
-                FeatureListFragmentDirections.actionFeatureListFragmentToFeatureFragment(feature.name, feature.fileName)
+                FeatureListFragmentDirections.actionFeatureListFragmentToFeatureFragment(
+                    feature.name,
+                    feature.fileName
+                )
             findNavController(originatingFragment).navigate(action)
         }
     }
