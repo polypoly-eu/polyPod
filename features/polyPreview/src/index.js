004eeb00722394e8ae44ef90530dc5b75025de50
Felix Dahlke <felix.dahlke@polypoly.net>
2021-03-11 19:00:22 +0100

    Fix linter errors

--- features/polyPreview/src/index.js
+++ features/polyPreview/src/index.js
@@ -4,11 +4,9 @@ import i18n from "./i18n.js";
 import "swiper/swiper-bundle.css";
 import "./styles.css";
 
-if (!window.podNav) {
-    window.podNav = {
-        openUrl: (url) => window.open(url, "_blank"),
-    };
-}
+const podNav = window.podNav || {
+    openUrl: (url) => window.open(url, "_blank"),
+};
 
 function initSwiper() {
     SwiperCore.use([Pagination]);
@@ -21,7 +19,7 @@ function initSwiper() {
 
 function initLearnMoreButton() {
     const button = document.getElementById("learn-more");
-    button.addEventListener("click", function() {
+    button.addEventListener("click", function () {
         podNav.openUrl(button.dataset.target);
     });
 }
