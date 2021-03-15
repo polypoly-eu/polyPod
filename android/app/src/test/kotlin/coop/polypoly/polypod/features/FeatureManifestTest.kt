5a59dc7132b538267a51f1d0942e0266d2397fd1
Oleksandr Paraska <shoniko@gmail.com>
2021-03-15 13:46:43 +0100

    Update the test

--- android/app/src/test/kotlin/coop/polypoly/polypod/features/FeatureManifestTest.kt
+++ android/app/src/test/kotlin/coop/polypoly/polypod/features/FeatureManifestTest.kt
@@ -1,7 +1,5 @@
 package coop.polypoly.polypod.features
 
-import android.content.Context
-import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.truth.Truth.assertThat
 import org.junit.Test
@@ -9,17 +7,36 @@ import org.junit.runner.RunWith
 import org.robolectric.annotation.Config
 import org.robolectric.annotation.LooperMode
 
+
+private const val manifestString = """
+    {
+        "name": "testManifest",
+        "description": "testDescription",
+        "thumbnail": "assets/thumbnail.png",
+        "primaryColor": "#000000",
+        "links": {
+            "link1": "https://example.com/1",
+            "link2": "https://example.com/2"
+        }
+    }
+    """
+
 @LooperMode(LooperMode.Mode.PAUSED)
 @RunWith(AndroidJUnit4::class)
 @Config(sdk = [Config.OLDEST_SDK])
 class FeatureManifestTest {
-    private val manifestString = "{\"name\": \"testManifest\"}"
-
 
     @Test
     fun correctManifestParsing_works() {
         val manifest = FeatureManifest(manifestString).getManifest()
         assertThat(manifest.name).isEqualTo("testManifest")
+        assertThat(manifest.description).isEqualTo("testDescription")
+        assertThat(manifest.links["link1"]).isEqualTo(
+            "https://example.com/1"
+        )
+        assertThat(manifest.links["link2"]).isEqualTo(
+            "https://example.com/2"
+        )
     }
 
 }
