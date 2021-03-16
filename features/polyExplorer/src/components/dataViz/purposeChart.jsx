9c25eb251cf23ccca1c68153c7dca733e1cbb4e6
Richard Stoffels <richard.stoffels@polypoly.net>
2021-03-16 17:13:54 +0100

    Add purpose chart

--- features/polyExplorer/src/components/dataViz/purposeChart.jsx
+++ features/polyExplorer/src/components/dataViz/purposeChart.jsx
@@ -2,24 +2,86 @@ import React from "react";
 
 import i18n from "../../i18n.js";
 
-const PurposeChart = () => {
-    /*
+import "./purposeChart.css";
+
+const PurposeChart = ({ purposes }) => {
     const getHighestCount = () => {
-        console.log(typeof purposes);
         let highest = 0;
         purposes.forEach((e) => {
             e.count > highest ? (highest = e.count) : null;
         });
+        return highest;
+    };
+
+    purposes.sort((a, b) => b.count - a.count);
+
+    const fillScale = (highest, multiple) => {
+        const scale = [];
+        console.log(
+            `highest: ${highest}, multiple:${multiple}; theother thing: ${
+                Math.ceil(highest / multiple) * multiple
+            }`
+        );
+        for (
+            let i = multiple;
+            i <= Math.ceil(highest / multiple) * multiple;
+            i += multiple
+        ) {
+            scale.push(i);
+        }
+        return scale;
     };
 
     const calculateScaleValues = (highest) => {
-        if (highest <= 80) {
-        }
+        if (highest < 35) return fillScale(highest, 5);
+        else if (highest <= 70) return fillScale(highest, 10);
+        else if (highest <= 140) return fillScale(highest, 20);
+        else if (highest <= 200) return fillScale(highest, 25);
+        else if (highest <= 400) return fillScale(highest, 50);
+        else return fillScale(highest, 100);
     };
-*/
-    //const scaleValues = calculateScaleValues(getHighestCount());
+    const highestCount = getHighestCount();
+    const scaleValues = calculateScaleValues(highestCount);
+    const scale = (
+        <div className="scale">
+            <div className="origin">
+                <p>0</p>
+                <div></div>
+            </div>
+            {scaleValues.map((value, index) => (
+                <div key={index} className="tick-box">
+                    <div className="half-tick"></div>
+                    <div className="tick"></div>
+                    <p>{value}</p>
+                </div>
+            ))}
+        </div>
+    );
 
-    //const scale = <div className="scale"></div>;
+    const bars = (
+        <div className="bars">
+            {purposes.map((p, index) => (
+                <div key={index} className="bar-box">
+                    <div className="above-bar">
+                        <p className="name">
+                            {
+                                p[
+                                    i18n.t(
+                                        "dataExplorationScreen:from.polyPedia.translation"
+                                    )
+                                ]
+                            }
+                        </p>
+                        <img src="./images/question-circle-light.svg" />
+                    </div>
+                    <div
+                        className="bar"
+                        style={{ width: (p.count / highestCount) * 95 + "%" }}
+                    ></div>
+                </div>
+            ))}
+        </div>
+    );
 
     return (
         <div className="purpose-chart">
@@ -32,11 +94,13 @@ const PurposeChart = () => {
                     </div>
                     <div className="fill"></div>
                     <div className="help">
-                        <img src="./images/question-circle-light.svg" />
+                        <img src="./images/question-circle.svg" />
                         <div>{i18n.t("common:how-to-read")}</div>
                     </div>
                 </div>
+                {scale}
             </div>
+            {bars}
         </div>
     );
 };
