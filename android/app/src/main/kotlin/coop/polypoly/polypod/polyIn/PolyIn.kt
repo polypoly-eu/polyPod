0335bbcb6b6e100edd2a4059ba7a57d26c44d52e
Oleksandr Paraska <shoniko@gmail.com>
2021-03-22 06:26:14 +0100

    Jena implementation

--- android/app/src/main/kotlin/coop/polypoly/polypod/polyIn/PolyIn.kt
+++ android/app/src/main/kotlin/coop/polypoly/polypod/polyIn/PolyIn.kt
@@ -1,40 +1,89 @@
 package coop.polypoly.polypod.polyIn
 
-import coop.polypoly.polypod.polyIn.rdf.Matcher
-import coop.polypoly.polypod.polyIn.rdf.Quad
-import org.apache.jena.rdf.model.Model
-import org.msgpack.core.MessageFormat
-import org.msgpack.core.MessagePack
-import org.apache.jena.rdf.model.ModelFactory
+import coop.polypoly.polypod.polyIn.rdf.*
+import org.apache.jena.rdf.model.*
 import java.io.File
 
 
-open class PolyIn {
+open class PolyIn(private val databaseName: String = "database.nt") {
+    val NS = "polypoly"
+
+    private var model: Model = load()
+
     open suspend fun select(matcher: Matcher): List<Quad> {
         var retList: List<Quad> = listOf()
-        File("database.json").inputStream().use { inputStream ->
-            MessagePack.newDefaultUnpacker(inputStream).use { unpacker ->
-                while (unpacker.hasNext()) {
-                    val vmt: MessageFormat = unpacker.nextFormat
-                    retList = retList.plus(
-                        Quad.codec.decode(unpacker.unpackValue())
-                    )
-                }
-            }
+
+        // TODO: Kotlin-fy this
+        val stmtsIterator = model.listStatements(
+            if (matcher.subject == null) null else ResourceFactory.createResource(matcher.subject.iri),
+            if (matcher.predicate == null) null else ResourceFactory.createProperty(matcher.predicate.iri),
+            matcher.`object`?.iri,
+        )
+        while (stmtsIterator.hasNext()) {
+            val stmt = stmtsIterator.next()
+            retList = retList.plus(QuadBuilder.new()
+                .withDefaultGraph()
+                .withSubject(IRI(stmt.subject.uri))
+                .withPredicate(IRI(stmt.predicate.uri))
+                .withObject(IRI(stmt.`object`.toString())
+                ).build()
+            )
         }
         return retList
     }
 
     open suspend fun add(quads: List<Quad>) {
-        // create an empty Model
-        File("database.json").outputStream().use { out ->
-            model.write(out)
+        quads.forEach { quad ->
+            model = model.add(
+                quadSubjectToResource(quad.subject),
+                model.createProperty(quad.predicate.iri),
+                quadObjectToResource(quad.`object`)
+            )
+        }
+        save()
+    }
 
-            MessagePack.newDefaultPacker(out).use { packer ->
-                for (quad in quads) {
-                    Quad.codec.encode(quad).writeTo(packer)
-                }
-            }
+    private fun load(): Model {
+        model = ModelFactory.createDefaultModel()
+
+        val database = File(databaseName)
+        if (!database.exists()) {
+            database.createNewFile()
+        }
+        database.inputStream().use { inputStream ->
+            model = model.read(inputStream, null, "N-TRIPLE")
+        }
+        return model
+    }
+
+    private fun save() {
+        File(databaseName).outputStream().use { out ->
+            model = model.write(out, "N-TRIPLE")
+        }
+    }
+
+    open fun clean() {
+        model = model.removeAll()
+        save()
+    }
+
+    private fun quadSubjectToResource(quadSubject: QuadSubject): Resource {
+        return when (quadSubject) {
+            is BlankNodeSubject -> model.createProperty(
+                NS, quadSubject.subject.value)
+            is IRISubject -> model.createResource(quadSubject.subject.iri)
+        }
+    }
+
+    private fun quadObjectToResource(quadObject: QuadObject): RDFNode {
+        return when (quadObject) {
+            is BlankNodeObject -> model.createProperty(
+                NS, quadObject.`object`.value
+            )
+            is LiteralObject -> model.createTypedLiteral(
+                quadObject.`object`.value
+            )
+            is IRIObject -> model.createResource(quadObject.`object`.iri)
         }
     }
 }
--- android/app/src/test/kotlin/coop/polypoly/polypod/polyin/PolyInTest.kt
+++ android/app/src/test/kotlin/coop/polypoly/polypod/polyin/PolyInTest.kt
@@ -23,54 +23,71 @@ class PolyInTest {
 
     @Test
     fun storingStrings_works() {
-        val polyIn: PolyIn = PolyIn()
+        val polyIn = PolyIn("test_database.nt")
+        polyIn.clean()
+
         var storageData: List<Quad>  = listOf()
-        storageData = storageData.plus(QuadBuilder.new().withDefaultGraph(
-        ).withObject(
-            BlankNode("testObject")
-        ).withSubject(
-            BlankNode("testSubj")
-        ).withPredicate(
-            IRI("testPredicate")
-        ).build())
+        storageData = storageData.plus(QuadBuilder.new().withDefaultGraph()
+            .withObject(BlankNode("privateData"))
+            .withSubject(BlankNode("someCompany"))
+            .withPredicate(IRI("https://polypoly.coop/storing"))
+            .build()
+        )
         runBlocking {
             polyIn.add(storageData)
         }
-        var returnedData: List<Quad> = listOf()
+        var returnedData: List<Quad>
         runBlocking {
             returnedData = polyIn.select(
-                Matcher(IRI(""), IRI(""), IRI(""))
+                Matcher(null, null, null)
             )
         }
-        Truth.assertThat(returnedData).isEqualTo(storageData)
+        Truth.assertThat(returnedData.size).isEqualTo(storageData.size)
+        Truth.assertThat(returnedData[0].predicate).isEqualTo(storageData[0].predicate)
     }
 
     @Test
     fun matcher_works() {
-        val polyIn: PolyIn = PolyIn()
-        val storageData: List<Quad>  = listOf()
-        storageData.plus(QuadBuilder.new().withDefaultGraph(
-        ).withObject(
-            BlankNode("testObject")
-        ).withSubject(
-            BlankNode("testSubj")
-        ).withPredicate(
-            IRI("testPredicate")
-        ).build())
+        val polyIn = PolyIn("test_database.nt")
+        polyIn.clean()
+
+        var storageData: List<Quad>  = listOf()
+        storageData = storageData.plus(QuadBuilder.new().withDefaultGraph()
+            .withObject(BlankNode("privateData"))
+            .withSubject(BlankNode("someCompany"))
+            .withPredicate(IRI("https://polypoly.coop/storing"))
+            .build()
+        )
+
+        storageData = storageData.plus(QuadBuilder.new().withDefaultGraph()
+            .withObject(BlankNode("privateData2"))
+            .withSubject(BlankNode("someCompany2"))
+            .withPredicate(IRI("https://polypoly.coop/storing"))
+            .build()
+        )
+
+        storageData = storageData.plus(QuadBuilder.new().withDefaultGraph()
+            .withObject(BlankNode("privateData3"))
+            .withSubject(BlankNode("someCompany3"))
+            .withPredicate(IRI("https://polypoly.coop/justChecking"))
+            .build()
+        )
+
         runBlocking {
-            testScope.launch {
-                polyIn.add(storageData)
-            }
+            polyIn.add(storageData)
         }
-        var returnedData: List<Quad> = listOf()
+        var returnedData: List<Quad>
         runBlocking {
-            testScope.launch {
-                returnedData = polyIn.select(
-                    Matcher(IRI(""), IRI(""), IRI(""))
+            returnedData = polyIn.select(
+                Matcher(
+                    null,
+                    IRI("https://polypoly.coop/justChecking"),
+                    null
                 )
-            }
+            )
         }
 
-        Truth.assertThat(returnedData).isEqualTo(storageData)
+        Truth.assertThat(returnedData.size).isEqualTo(1)
+        Truth.assertThat(returnedData[0].predicate).isEqualTo(storageData[2].predicate)
     }
 }
