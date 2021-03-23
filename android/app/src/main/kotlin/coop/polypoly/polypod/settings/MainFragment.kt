f22e153ab5d2bcd5a854c67543d4e054ab25073f
Felix Dahlke <felix.dahlke@polypoly.net>
2021-03-23 15:37:48 +0100

    Add the legal notice settings section
    Not particularly elegantly solved at the moment, but we can reduce
    duplication later.
    
    The amount of workarounds needed for something this simple is definitely
    fishy :D

---
+++ android/app/src/main/kotlin/coop/polypoly/polypod/settings/MainFragment.kt
@@ -0,0 +1,36 @@
+package coop.polypoly.polypod.settings
+
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import coop.polypoly.polypod.BuildConfig
+import coop.polypoly.polypod.R
+
+class MainFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(
+        savedInstanceState: Bundle?,
+        rootKey: String?
+    ) {
+        setPreferencesFromResource(R.xml.settings, rootKey)
+        val version =
+            "${BuildConfig.VERSION_NAME} (${BuildConfig.VERSION_CODE})"
+        findPreference<Preference>("version")?.summary = version
+
+        findPreference<Preference>("imprint")?.onPreferenceClickListener =
+            Preference.OnPreferenceClickListener {
+                view?.findNavController()?.navigate(R.id.ImprintFragment)
+                true
+            }
+        findPreference<Preference>("privacy_policy")?.onPreferenceClickListener =
+            Preference.OnPreferenceClickListener {
+                view?.findNavController()?.navigate(R.id.PrivacyPolicyFragment)
+                true
+            }
+        findPreference<Preference>("terms_of_use")?.onPreferenceClickListener =
+            Preference.OnPreferenceClickListener {
+                view?.findNavController()?.navigate(R.id.TermsOfUseFragment)
+                true
+            }
+    }
+}
