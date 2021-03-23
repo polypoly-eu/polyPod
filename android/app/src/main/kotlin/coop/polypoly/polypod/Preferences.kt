3480719a0171e7fdb92c65df8268e79206850a93
Felix Dahlke <felix.dahlke@polypoly.net>
2021-03-23 12:15:20 +0100

    Add update notifications

--- android/app/src/main/kotlin/coop/polypoly/polypod/Preferences.kt
+++ android/app/src/main/kotlin/coop/polypoly/polypod/Preferences.kt
@@ -6,16 +6,39 @@ import androidx.preference.PreferenceManager
 class Preferences {
     companion object {
         private const val firstRunKey = "firstRun"
+        private const val seenNotificationIdKey = "seenNotificationId"
+        private const val seenPushNotificationIdKey = "seenPushNotificationId"
 
-        fun isFirstRun(context: Context): Boolean {
-            val prefs = PreferenceManager.getDefaultSharedPreferences(context)
-            return prefs.getBoolean(firstRunKey, true)
-        }
+        private fun getPrefs(context: Context) =
+            PreferenceManager.getDefaultSharedPreferences(context)
+
+        fun isFirstRun(context: Context): Boolean =
+            getPrefs(context).getBoolean(firstRunKey, true)
 
         fun setFirstRun(context: Context, firstRun: Boolean) {
-            val prefs = PreferenceManager.getDefaultSharedPreferences(context)
-            val edit = prefs.edit()
+            val edit = getPrefs(context).edit()
             edit.putBoolean(firstRunKey, firstRun)
+            edit.commit()
+        }
+
+        fun getSeenNotificationId(context: Context): Int =
+            getPrefs(context).getInt(seenNotificationIdKey, 0)
+
+        fun setSeenNotificationId(context: Context, seenNotificationId: Int) {
+            val edit = getPrefs(context).edit()
+            edit.putInt(seenNotificationIdKey, seenNotificationId)
+            edit.commit()
+        }
+
+        fun getSeenPushNotificationId(context: Context): Int =
+            getPrefs(context).getInt(seenPushNotificationIdKey, 0)
+
+        fun setSeenPushNotificationId(
+            context: Context,
+            seenPushNotificationId: Int
+        ) {
+            val edit = getPrefs(context).edit()
+            edit.putInt(seenPushNotificationIdKey, seenPushNotificationId)
             edit.commit()
         }
     }
---
+++ android/app/src/main/kotlin/coop/polypoly/polypod/updatenotification/UpdateNotification.kt
@@ -0,0 +1,27 @@
+package coop.polypoly.polypod.updatenotification
+
+import android.content.Context
+import coop.polypoly.polypod.Preferences
+import coop.polypoly.polypod.R
+
+class UpdateNotification(private val context: Context) {
+    val id = context.resources.getInteger(R.integer.update_notification_id)
+    val title = context.getString(R.string.update_notification_title)
+    val text = context.getString(R.string.update_notification_text)
+    val pushDelay =
+        context.resources.getInteger(R.integer.update_notification_push_delay)
+
+    val inAppNotificationSeen: Boolean
+        get() = Preferences.getSeenNotificationId(context) == id
+
+    val pushNotificationSeen: Boolean
+        get() = Preferences.getSeenPushNotificationId(context) == id
+
+    fun markInAppNotificationSeen() {
+        Preferences.setSeenNotificationId(context, id)
+    }
+
+    fun markPushNotificationSeen() {
+        Preferences.setSeenPushNotificationId(context, id)
+    }
+}
