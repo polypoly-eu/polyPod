c80c3afac1af3ab57ccf9360c2c76f0e58dd88cd
Richard Stoffels <richard.stoffels@polypoly.net>
2021-03-23 12:53:27 +0100

    Add legend and make regioninfo an explorationinfo screen

--- features/polyExplorer/src/screens/dataRegionInfo/dataRegionInfo.jsx
+++ features/polyExplorer/src/screens/dataRegionInfo/dataRegionInfo.jsx
@@ -1,22 +1,60 @@
 import React from "react";
 
 import i18n from "../../i18n.js";
-import Screen from "../../components/screen/screen.jsx";
+import ExplorationInfoScreen from "../../components/explorationInfoScreen/explorationInfoScreen.jsx";
 
 import "./dataRegionInfo.css";
 
 const DataRegionInfo = ({ onClose }) => {
     return (
-        <Screen className="data-region-info-screen" light={true}>
+        <ExplorationInfoScreen className="data-region-info-screen" light={true}>
             <h1 className="heading">
                 {i18n.t("dataRegionInfoScreen:heading")}
             </h1>
-            <img src="./images/maps/world.svg" alt="" />
+            <img src="./images/maps/jurisdictions/world.svg" alt="" />
+            <div className="legend-container">
+                <div className="jurisdictions-label">
+                    <p>{i18n.t("companyDetailsScreen:jurisdictions")}:</p>
+                </div>
+                <div className="legend">
+                    <div>
+                        <div className="circle China"></div>
+                        <p>{i18n.t("common:jurisdiction.china")}</p>
+                    </div>
+                    <div>
+                        <div className="circle Five-Eyes"></div>
+                        <p>{i18n.t("common:jurisdiction.fiveEyes")}</p>
+                    </div>
+                    <div>
+                        <div className="circle Russia"></div>
+                        <p>{i18n.t("common:jurisdiction.russia")}</p>
+                    </div>
+                    <div>
+                        <div className="circle EU-GDPR"></div>
+                        <p>{i18n.t("common:jurisdiction.euGdpr")}</p>
+                    </div>
+                    <div>
+                        <div className="circle Others"></div>
+                        <p>{i18n.t("common:jurisdiction.undisclosed")}</p>
+                    </div>
+                </div>
+            </div>
             <p>{i18n.t("dataRegionInfoScreen:world")}</p>
             <h2 className="sub-heading">
                 {i18n.t("common:jurisdiction.euGdpr")}
             </h2>
-            <img src="./images/maps/europe.svg" alt="" />
+            <img src="./images/maps/jurisdictions/eu-gdpr.svg" alt="" />
+            <div className="legend-container">
+                <div className="jurisdictions-label">
+                    <p>{i18n.t("companyDetailsScreen:jurisdictions")}:</p>
+                </div>
+                <div className="legend">
+                    <div>
+                        <div className="circle EU-GDPR"></div>
+                        <p>{i18n.t("common:jurisdiction.euGdpr")}</p>
+                    </div>
+                </div>
+            </div>
             <div
                 dangerouslySetInnerHTML={{
                     __html: i18n.t("dataRegionInfoScreen:euGdpr.text"),
@@ -26,10 +64,21 @@ const DataRegionInfo = ({ onClose }) => {
                 {i18n.t("dataRegionInfoScreen:subHeading.fiveEyes")}
             </h2>
             <img
-                src="./images/maps/five-eyes.svg"
+                src="./images/maps/jurisdictions/five-eyes.svg"
                 alt=""
                 className="world-map"
             />
+            <div className="legend-container">
+                <div className="jurisdictions-label">
+                    <p>{i18n.t("companyDetailsScreen:jurisdictions")}:</p>
+                </div>
+                <div className="legend">
+                    <div>
+                        <div className="circle Five-Eyes"></div>
+                        <p>{i18n.t("common:jurisdiction.fiveEyes")}</p>
+                    </div>
+                </div>
+            </div>
             <h2 className="sub-sub-heading">USA</h2>
             <div
                 dangerouslySetInnerHTML={{
@@ -47,7 +96,18 @@ const DataRegionInfo = ({ onClose }) => {
             <h2 className="sub-heading">
                 {i18n.t("common:jurisdiction.russia")}
             </h2>
-            <img src="./images/maps/russia.svg" alt="" />
+            <img src="./images/maps/jurisdictions/russia.svg" alt="" />
+            <div className="legend-container">
+                <div className="jurisdictions-label">
+                    <p>{i18n.t("companyDetailsScreen:jurisdictions")}:</p>
+                </div>
+                <div className="legend">
+                    <div>
+                        <div className="circle Russia"></div>
+                        <p>{i18n.t("common:jurisdiction.russia")}</p>
+                    </div>
+                </div>
+            </div>
             <div
                 dangerouslySetInnerHTML={{
                     __html: i18n.t("dataRegionInfoScreen:russia.text"),
@@ -56,7 +116,18 @@ const DataRegionInfo = ({ onClose }) => {
             <h2 className="sub-heading">
                 {i18n.t("common:jurisdiction.china")}
             </h2>
-            <img src="./images/maps/china.svg" alt="" />
+            <img src="./images/maps/jurisdictions/china.svg" alt="" />
+            <div className="legend-container">
+                <div className="jurisdictions-label">
+                    <p>{i18n.t("companyDetailsScreen:jurisdictions")}:</p>
+                </div>
+                <div className="legend">
+                    <div>
+                        <div className="circle China"></div>
+                        <p>{i18n.t("common:jurisdiction.china")}</p>
+                    </div>
+                </div>
+            </div>
             <div
                 dangerouslySetInnerHTML={{
                     __html: i18n.t("dataRegionInfoScreen:china.text"),
@@ -65,7 +136,18 @@ const DataRegionInfo = ({ onClose }) => {
             <h2 className="sub-heading">
                 {i18n.t("common:jurisdiction.undisclosed")}
             </h2>
-            <img src="./images/maps/others.svg" alt="" />
+            <img src="./images/maps/jurisdictions/others.svg" alt="" />
+            <div className="legend-container">
+                <div className="jurisdictions-label">
+                    <p>{i18n.t("companyDetailsScreen:jurisdictions")}:</p>
+                </div>
+                <div className="legend">
+                    <div>
+                        <div className="circle Others"></div>
+                        <p>{i18n.t("common:jurisdiction.undisclosed")}</p>
+                    </div>
+                </div>
+            </div>
             <div
                 dangerouslySetInnerHTML={{
                     __html: i18n.t("dataRegionInfoScreen:undisclosed.text"),
@@ -74,7 +156,7 @@ const DataRegionInfo = ({ onClose }) => {
             <button onClick={onClose}>
                 {i18n.t("infoScreen:button.explore")}
             </button>
-        </Screen>
+        </ExplorationInfoScreen>
     );
 };
 
