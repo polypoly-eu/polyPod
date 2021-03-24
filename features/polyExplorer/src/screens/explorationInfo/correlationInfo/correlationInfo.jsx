da3cf290c615c298da7281c728db4b708c5411b0
Richard Stoffels <richard.stoffels@polypoly.net>
2021-03-24 09:58:13 +0100

    Add dataCorrelationInfo

--- features/polyExplorer/src/screens/explorationInfo/correlationInfo/correlationInfo.jsx
+++ features/polyExplorer/src/screens/explorationInfo/correlationInfo/correlationInfo.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
 import i18n from "../../../i18n.js";
+import highlights from "../../../data/highlights.js";
 import ExplorationInfoScreen from "../../../components/explorationInfoScreen/explorationInfoScreen.jsx";
-import Infographic from "../../../components/infographic/infographic.jsx";
 
-const CorrelationInfo = ({ onClose }) => {
+const CorrelationInfo = ({ company, onClose }) => {
     return (
         <ExplorationInfoScreen
             className="correlation-info"
@@ -12,7 +12,18 @@ const CorrelationInfo = ({ onClose }) => {
             onClose={onClose}
         >
             <p>{i18n.t("explorationCorrelationInfoScreen:text.1")}</p>
-            <Infographic svgName="correlation" />
+            <img
+                src={`./images/infographics/correlation/${i18n.t(
+                    "common:country.code"
+                )}.svg`}
+            />
+            <p>
+                {
+                    highlights[company.name].dataTypeCorrelation.explanation[
+                        i18n.t("common:country.code")
+                    ]
+                }
+            </p>
         </ExplorationInfoScreen>
     );
 };
