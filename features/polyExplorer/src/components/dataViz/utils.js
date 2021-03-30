f80f6ff890e0d33cbf1b1a5d7d2c41fc0b37abbe
Felix Dahlke <felix.dahlke@polypoly.net>
2021-03-30 11:35:25 +0200

    Add data type highlight slide

--- features/polyExplorer/src/components/dataViz/utils.js
+++ features/polyExplorer/src/components/dataViz/utils.js
@@ -34,6 +34,49 @@ function appendLabel(container, text, props = {}) {
     return label;
 }
 
+function findCircleLabelPosition(labelBounds, circle, distance) {
+    const topY = circle.y - circle.r - labelBounds.height - distance;
+    const y =
+        (topY >= 0 ? topY : circle.y + circle.r + distance) +
+        labelBounds.height / 2;
+    return {
+        x: circle.x,
+        y,
+    };
+}
+
+function appendCircleLabel(container, circle, text, props = {}) {
+    const circleLabel = container.append("g").attr("class", "circle-label");
+    const label = appendLabel(circleLabel, text, props);
+    const bounds = label.node().getBBox();
+    const lineLength = 8;
+    const labelPosition = findCircleLabelPosition(bounds, circle, lineLength);
+    label.attr(
+        "transform",
+        `translate(${labelPosition.x}, ${labelPosition.y})`
+    );
+    const lineY =
+        labelPosition.y < circle.y
+            ? circle.y - circle.r - lineLength
+            : circle.y + circle.r;
+    circleLabel
+        .append("line")
+        .style("stroke", "white")
+        .style("stroke-width", 1)
+        .attr("x1", circle.x)
+        .attr("y1", lineY)
+        .attr("x2", circle.x)
+        .attr("y2", lineY + lineLength);
+}
+
+function findNode(selection, matchFunction) {
+    let match = null;
+    selection.filter(matchFunction).each((node) => (match = node));
+    return match;
+}
+
 export default {
     appendLabel,
+    appendCircleLabel,
+    findNode,
 };
