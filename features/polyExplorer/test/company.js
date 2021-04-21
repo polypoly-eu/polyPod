7af19bd4cd7a77611230b5418ac01930de618c99
Richard Stoffels <richard.stoffels@polypoly.net>
2021-04-21 14:07:38 +0200

    Delete startsWithSpecialChar unit test

--- features/polyExplorer/test/company.js
+++ features/polyExplorer/test/company.js
@@ -3,13 +3,13 @@
 import assert from "assert";
 import * as company from "../src/company.js";
 
-describe("companies", function () {
+describe("company", function () {
     it("String sort comparison works", function () {
         const strings = ["#*'§12", "%/$$ab", "abc", "%/{&/z", "z"];
-        assert(companies.compare(strings[0], strings[1]) < 0);
-        assert(companies.compare(strings[1], strings[0]) > 0);
-        assert(companies.compare(strings[1], strings[2]) < 0);
-        assert(companies.compare(strings[2], strings[3]) < 0);
-        assert(companies.compare(strings[3], strings[4]) == 0);
+        assert(company.compare(strings[0], strings[1]) < 0);
+        assert(company.compare(strings[1], strings[0]) > 0);
+        assert(company.compare(strings[1], strings[2]) < 0);
+        assert(company.compare(strings[2], strings[3]) < 0);
+        assert(company.compare(strings[3], strings[4]) == 0);
     });
 });
