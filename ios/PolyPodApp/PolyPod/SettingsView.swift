be8a49d9fddb0b735122d816f09a6d8e40b59905
Felix Dahlke <felix.dahlke@polypoly.net>
2021-04-22 17:50:39 +0200

    Finish settings screen

--- ios/PolyPodApp/PolyPod/SettingsView.swift
+++ ios/PolyPodApp/PolyPod/SettingsView.swift
@@ -2,54 +2,117 @@ import SwiftUI
 
 struct SettingsView: View {
     var closeAction: () -> Void = {}
+    @State private var activeSection = Sections.main
     
     var body: some View {
         VStack(alignment: .leading, spacing: 0) {
             NavigationBar(
-                leading: AnyView(Button(action: closeAction) {
+                leading: AnyView(Button(action: back) {
                     Image("NavIconBackDark").renderingMode(.original)
                 }),
                 center: AnyView(
-                    Text("Settings")
-                        .foregroundColor(Color.PolyPod.darkForeground)
-                        .font(.custom("Jost-Medium", size: 16))
-                        .kerning(-0.16)
+                    Text({
+                        switch activeSection {
+                        case .main:
+                            return "settings_title"
+                        case .imprint:
+                            return "settings_imprint_title"
+                        case .privacyPolicy:
+                            return "settings_privacy_policy_title"
+                        case .termsOfUse:
+                            return "settings_terms_of_use_title"
+                        }
+                    }())
+                    .foregroundColor(Color.PolyPod.darkForeground)
+                    .font(.custom("Jost-Medium", size: 16))
+                    .kerning(-0.16)
                 )
             )
             .background(Color.PolyPod.lightBackground)
             
             Divider()
             
-            List() {
-                Section(header: SettingsHeader("settings_about_section")) {
-                    SettingsButton(label: "settings_version", action: {})
-                }
-                .listRowInsets(
-                    EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
-                )
-                
-                Section(header: SettingsHeader("settings_legal_section")) {
-                    SettingsButton(label: "settings_imprint_title", action: {})
-                    SettingsButton(
-                        label: "settings_privacy_policy_title",
-                        action: {}
-                    )
-                    SettingsButton(
-                        label: "settings_terms_of_use_title",
-                        action: {}
-                    )
-                }
-                .listRowInsets(
-                    EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
-                )
+            switch activeSection {
+            case .main:
+                MainSection(activeSection: $activeSection)
+            case .imprint:
+                HTMLView(text: "settings_imprint_text")
+            case .privacyPolicy:
+                HTMLView(text: "settings_privacy_policy_text")
+            case .termsOfUse:
+                HTMLView(text: "settings_terms_of_use_text")
             }
         }
     }
+    
+    private func back() {
+        if activeSection == .main {
+            closeAction()
+            return
+        }
+        activeSection = .main
+    }
+}
+
+private enum Sections {
+    case main, imprint, privacyPolicy, termsOfUse
 }
 
 struct SettingsView_Previews: PreviewProvider {
     static var previews: some View {
         SettingsView()
+    }
+}
+
+private struct MainSection: View {
+    @Binding var activeSection: Sections
+    @State private var showVersion = false
+    
+    private var displayVersion: String {
+        guard let info = Bundle.main.infoDictionary else {
+            return "Unknown"
+        }
+        let marketingVersion = info["CFBundleShortVersionString"] ?? "Unknown"
+        let buildNumber = info["CFBundleVersion"] ?? "Unknown"
+        return "\(marketingVersion) (\(buildNumber))"
+    }
+    
+    var body: some View {
+        List() {
+            Section(header: SettingsHeader("settings_about_section")) {
+                SettingsButton(
+                    label: "settings_version",
+                    action: { showVersion = true }
+                )
+                .alert(isPresented: $showVersion) {
+                    Alert(
+                        title: Text("settings_version"),
+                        message: Text(displayVersion)
+                    )
+                }
+            }
+            .listRowInsets(
+                EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
+            )
+            
+            Section(header: SettingsHeader("settings_legal_section")) {
+                SettingsButton(
+                    label: "settings_imprint_title",
+                    action: { activeSection = .imprint }
+                )
+                SettingsButton(
+                    label: "settings_privacy_policy_title",
+                    action: { activeSection = .privacyPolicy }
+                )
+                SettingsButton(
+                    label: "settings_terms_of_use_title",
+                    action: { activeSection = .termsOfUse }
+                )
+            }
+            .listRowInsets(
+                EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
+            )
+        }
     }
 }
 
