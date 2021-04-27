b6d31c32f0f6b854cae7775cd98e636e611c042e
Richard Stoffels <richard.stoffels@polypoly.net>
2021-04-27 15:42:14 +0200

    Fix imports (polylook-localStubs) and remove links for now

--- features/polyPinion/src/screens/home/ActiveSurveys.tsx
+++ features/polyPinion/src/screens/home/ActiveSurveys.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import MainSurveyCard from "./MainSurveyCard";
 import { QuestionnaireListContext } from "../../context/questionnaire-list-context";
-import { Tabs, Tab } from "@polypoly-eu/poly-look/lib/tabs";
+import { Tabs, Tab } from "../../components/common/tabs";
 
 export default function ActiveSurveys() {
     const { questionnaireList } = React.useContext(QuestionnaireListContext);
@@ -11,17 +11,18 @@ export default function ActiveSurveys() {
     //);
 
     // TODO: Adjust the link targets
+    // took the hrefs out for now as they were creating a new container inside the other one
     return (
         <>
             <Tabs>
                 <Tab active={true}>
-                    <a href="/">Featured</a>
+                    <a>Featured</a>
                 </Tab>
                 <Tab>
-                    <a href="/">Übermittelt</a>
+                    <a>Übermittelt</a>
                 </Tab>
                 <Tab>
-                    <a href="/">Abgelaufen</a>
+                    <a>Abgelaufen</a>
                 </Tab>
             </Tabs>
             {activeQuestionnaire.map((questionnaire) => (
