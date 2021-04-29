b08bb4709abe9338a2a5322867714fedaf78b20c
Richard Stoffels <richard.stoffels@polypoly.net>
2021-04-29 18:58:02 +0200

    Add Company tests

--- features/polyExplorer/test/company.js
+++ features/polyExplorer/test/company.js
@@ -2,5 +2,51 @@
 
 import assert from "assert";
 import * as company from "../src/company.js";
+import testCompanyJSON from "./testCompanyData.js";
+import testGlobalJSON from "./testGlobal.js";
 
-describe("company", function () {});
+describe("company", function () {
+    const testCompany = new company.Company(testCompanyJSON, testGlobalJSON);
+    const testCompanyJSONModified = { ...testCompanyJSON };
+    testCompanyJSONModified.name = "%%Az";
+    const testCompany2 = new company.Company(
+        testCompanyJSONModified,
+        testGlobalJSON
+    );
+    const specialCharacters = /[!@#$%^&*()_+\-=[\]{};':"\\|,.<>?]+/;
+
+    it("Company: getters work", function () {
+        //Activate this when ppid exists
+        //assert(testCompany.ppid === testCompanyJSON.ppid)
+        assert(testCompany.name === testCompanyJSON.name);
+        assert(testCompany.featured === testCompanyJSON.featured);
+        assert(testCompany.jurisdiction === testCompanyJSON.jurisdiction);
+        assert(testCompany.location.city === testCompanyJSON.location.city);
+        assert(
+            testCompany.location.countryCode ===
+                testCompanyJSON.location.countryCode
+        );
+        assert(testCompany.annualRevenues === testCompanyJSON.annualRevenues);
+        assert(testCompany.dataRecipients === testCompanyJSON.dataRecipients);
+        assert(
+            testCompany.dataSharingPurposes ===
+                testCompanyJSON.dataSharingPurposes
+        );
+        assert(testCompany.dataTypesShared === testCompanyJSON.dataTypesShared);
+        assert(
+            testCompany.jurisdictionsShared ===
+                testCompanyJSON.jurisdictionsShared
+        );
+        assert(
+            testCompany.industryCategory === testCompanyJSON.industryCategory
+        );
+        assert(testCompany.description === testCompanyJSON.description);
+        assert(
+            specialCharacters.test(testCompany.firstNameChar) &&
+                testCompany.firstNameChar.length == 1
+        );
+    });
+    it("Company: compareNames() works", function () {
+        assert(testCompany.compareNames(testCompany2) < 0);
+    });
+});
