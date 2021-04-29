6efaffa55756141b4d50518c95af2065ac76fbae
Richard Stoffels <richard.stoffels@polypoly.net>
2021-04-29 15:10:59 +0200

    Delete tests for now

--- features/polyExplorer/test/company.js
+++ features/polyExplorer/test/company.js
@@ -3,13 +3,4 @@
 import assert from "assert";
 import * as company from "../src/company.js";
 
-describe("company", function () {
-    it("String sort comparison works", function () {
-        const strings = ["#*'§12", "%/$$ab", "abc", "%/{&/z", "z"];
-        assert(company.compare(strings[0], strings[1]) < 0);
-        assert(company.compare(strings[1], strings[0]) > 0);
-        assert(company.compare(strings[1], strings[2]) < 0);
-        assert(company.compare(strings[2], strings[3]) < 0);
-        assert(company.compare(strings[3], strings[4]) == 0);
-    });
-});
+describe("company", function () {});
