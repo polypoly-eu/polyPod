54ef4eddf1f9a7a6152b4ec9f2cd6e7e961bc1ec
Richard Stoffels <richard.stoffels@polypoly.net>
2021-05-05 10:23:51 +0200

    Refactor data-pipeline script for readability

--- features/lexicon/scripts/update-lexicon-data.js
+++ features/lexicon/scripts/update-lexicon-data.js
@@ -14,33 +14,50 @@ client
     })
     .then(function (response) {
         for (let translationData of response.results) {
-            const terms = translationData.data.body;
+            const groups = translationData.data.body;
             const languageCode = translationData.lang.substring(0, 2);
-            lexicon[languageCode] = {};
-            for (let i = 1; i < terms.length; i++) {
-                const group = {};
-                for (let term of terms[i].items) {
-                    let explanation = "";
-                    for (let textPart of term.answer) {
-                        if (textPart.type == "paragraph")
-                            explanation += `<p>${textPart.text}</p>`;
-                        else explanation += textPart.text;
-                    }
-                    group[term.question[0].text] = explanation;
-                }
-                lexicon[languageCode][
-                    terms[i].items[0].question[0].text[0]
-                ] = group;
+            lexicon[languageCode] = loadLanguage(groups);
+        }
+        writeLexiconFile();
+    });
+
+function loadTermExplanation(term) {
+    let explanation = "";
+    for (let textPart of term) {
+        if (textPart.type == "paragraph")
+            explanation += `<p>${textPart.text}</p>`;
+        else explanation += textPart.text;
+    }
+    return explanation;
+}
+
+function loadSortingGroup(terms) {
+    const group = {};
+    for (let term of terms) {
+        group[term.question[0].text] = loadTermExplanation(term.answer);
+    }
+    return group;
+}
+
+function loadLanguage(groups) {
+    const language = {};
+    for (let i = 1; i < groups.length; i++) {
+        language[groups[i].items[0].question[0].text[0]] = loadSortingGroup(
+            groups[i].items
+        );
+    }
+    return language;
+}
+
+function writeLexiconFile() {
+    fs.writeFile(
+        "src/data/lexicon.json",
+        JSON.stringify(lexicon),
+        "utf8",
+        (err) => {
+            if (err) {
+                console.log(err);
             }
         }
-        fs.writeFile(
-            "src/data/lexicon.json",
-            JSON.stringify(lexicon),
-            "utf8",
-            (err) => {
-                if (err) {
-                    console.log(err);
-                }
-            }
-        );
-    });
+    );
+}
