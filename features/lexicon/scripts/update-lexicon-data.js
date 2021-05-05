fa90725cd193f9c3e1d237290b14c4d47ac33491
Richard Stoffels <richard.stoffels@polypoly.net>
2021-05-05 10:06:10 +0200

    Add english version to data script, get exampledata for offline development

--- features/lexicon/scripts/update-lexicon-data.js
+++ features/lexicon/scripts/update-lexicon-data.js
@@ -5,30 +5,34 @@ const apiEndpoint = "https://polypoly-coop.cdn.prismic.io/api/v2";
 const apiToken =
     "MC5ZSkVmQ2hBQUFDRUFXQVpZ.77-977-977-9X--_ve-_vTzvv73vv70K77-9BgpieU3vv73vv70N77-9UO-_vQvvv73vv73vv73vv70P77-977-9K--_vQ";
 
-const client = Prismic.client(
-    apiEndpoint,
-    { accessToken: apiToken },
-    { lang: "*" }
-);
-const lexicon = { de: {}, en: {} };
+const client = Prismic.client(apiEndpoint, { accessToken: apiToken });
+const lexicon = {};
 
 //german translation
 client
-    .query(Prismic.Predicates.at("document.type", "lexicon_page"))
+    .query(Prismic.Predicates.at("document.type", "lexicon_page"), {
+        lang: "*",
+    })
     .then(function (response) {
-        const terms = response.results[0].data.body;
-        for (let i = 1; i < terms.length; i++) {
-            const group = {};
-            for (let term of terms[i].items) {
-                let explanation = "";
-                for (let textPart of term.answer) {
-                    if (textPart.type == "paragraph")
-                        explanation += `<p>${textPart.text}</p>`;
-                    else explanation += textPart.text;
+        for (let translationData of response.results) {
+            const terms = translationData.data.body;
+            const languageCode = translationData.lang.substring(0, 2);
+            lexicon[languageCode] = {};
+            for (let i = 1; i < terms.length; i++) {
+                const group = {};
+                for (let term of terms[i].items) {
+                    let explanation = "";
+                    for (let textPart of term.answer) {
+                        if (textPart.type == "paragraph")
+                            explanation += `<p>${textPart.text}</p>`;
+                        else explanation += textPart.text;
+                    }
+                    group[term.question[0].text] = explanation;
                 }
-                group[term.question[0].text] = explanation;
+                lexicon[languageCode][
+                    terms[i].items[0].question[0].text[0]
+                ] = group;
             }
-            lexicon.de[terms[i].items[0].question[0].text[0]] = group;
         }
         fs.writeFile(
             "src/data/lexicon.json",
