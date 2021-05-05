7f185df41044d44210f260e89522fd5fb4e4d843
Felix Dahlke <felix.dahlke@polypoly.net>
2021-05-05 09:54:22 +0200

    Expose BrowserPod as a regular module

--- podjs/rollup.config.js
+++ podjs/rollup.config.js
@@ -4,6 +4,28 @@ import sucrase from "@rollup/plugin-sucrase";
 
 export default [
     {
+        input: "src/index.ts",
+        output: [
+            {
+                file: "dist/index.es.js",
+                format: "esm",
+            },
+            {
+                file: "dist/index.js",
+                format: "cjs",
+            },
+        ],
+        plugins: [
+            sucrase({
+                exclude: ["node_modules/**"],
+                transforms: ["typescript"],
+            }),
+        ],
+        external: [
+            "@polypoly-eu/rdf",
+        ],
+    },
+    {
         input: "src/pod.ts",
         output: [
             {
