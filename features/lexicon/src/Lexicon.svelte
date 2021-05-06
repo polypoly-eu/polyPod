c6a6f3977debfc690db4943015706a8c621dcf71
mteresa-jimenez <mteresa.jn@gmail.com>
2021-05-06 14:04:41 +0200

    Add global CSS styles

--- features/lexicon/src/Lexicon.svelte
+++ features/lexicon/src/Lexicon.svelte
@@ -1,66 +1,92 @@
+<style>
+@font-face {
+    font-family: "Jost";
+    src: url("../fonts/jost_regular.ttf");
+}
+
+:global(body) {
+    background-color: #3749a9;
+    padding: 0;
+}
+
+* {
+    color: white;
+    box-sizing: border-box;
+    font-family: Jost;
+    font-weight: 500;
+}
+
+main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+}
+@media (min-width: 640px) {
+    main {
+        max-width: none;
+    }
+}
+</style>
+
 <script>
-    import allTranslations from "./translations/translations.js";
-	export let lexicon;
-    let showTerm = null;
-    let searchString = "";
-    const translations = allTranslations[lexicon.language]
+import allTranslations from "./translations/translations.js";
+export let lexicon;
+let showTerm = null;
+let searchString = "";
+const translations = allTranslations[lexicon.language];
 
-    function handleClickTerm(term) {
-        showTerm = term
-    }
+function handleClickTerm(term) {
+    showTerm = term;
+}
 
-    function handleBack() {
-        showTerm = null;
-    }
+function handleBack() {
+    showTerm = null;
+}
 
-    function handleSearch(value) {
-        searchString = value
-    }
+function handleSearch(value) {
+    searchString = value;
+}
 
-    function handleClear() {
-        searchString = ""
-    }
+function handleClear() {
+    searchString = "";
+}
 </script>
 
 <main>
     <div class="lexicon">
         {#if showTerm}
-        <div class="term-description">
-            <h2>{showTerm}</h2>
-            {@html lexicon.description(showTerm)}
-            <button on:click={() => handleBack()}>{translations.back}</button>
-        </div>
-        {:else }
-        <div class="list">
-            <input type="text" value={searchString} placeholder={translations.search} on:input={(e) => handleSearch(e.target.value)}/>
-            <button on:click={() => handleClear()}>X</button>
-            {#if searchString}
-                {#each lexicon.search(searchString) as entry}
-                    <h2 on:click={() => handleClickTerm(entry)}>{entry}</h2>
-                {/each}
-            {:else}
-            {#each lexicon.groups as group}
-                <h1>{group}</h1>
-                {#each lexicon.groupEntries(group) as entry}
-                    <h2 on:click={() => handleClickTerm(entry)}>{entry}</h2>
-                {/each}
-            {/each}
-            {/if}
-        </div>
+            <div class="term-description">
+                <h2>{showTerm}</h2>
+                {@html lexicon.description(showTerm)}
+                <button on:click="{() => handleBack()}"
+                    >{translations.back}</button>
+            </div>
+        {:else}
+            <div class="list">
+                <input
+                    type="text"
+                    value="{searchString}"
+                    placeholder="{translations.search}"
+                    on:input="{(e) => handleSearch(e.target.value)}" />
+                <button on:click="{() => handleClear()}">X</button>
+                {#if searchString}
+                    {#each lexicon.search(searchString) as entry}
+                        <h2 on:click="{() => handleClickTerm(entry)}">
+                            {entry}
+                        </h2>
+                    {/each}
+                {:else}
+                    {#each lexicon.groups as group}
+                        <h1>{group}</h1>
+                        {#each lexicon.groupEntries(group) as entry}
+                            <h2 on:click="{() => handleClickTerm(entry)}">
+                                {entry}
+                            </h2>
+                        {/each}
+                    {/each}
+                {/if}
+            </div>
         {/if}
     </div>
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
