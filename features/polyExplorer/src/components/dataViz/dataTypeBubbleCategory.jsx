b173a1c5c0be7831f83bf5a6be71af65231710bc
Adrian Ferreres Esteller <ardiadrianadri@gmail.com>
2021-05-18 15:02:58 +0200

    shore(duplicate code): rename files
     Rename the old dataTypeBubbles for dataTypeBubblesAll and change the name
     of the new component dataBubbles for dataTypeBubbles

--- features/polyExplorer/src/components/dataViz/dataTypeBubbleCategory.jsx
+++ features/polyExplorer/src/components/dataViz/dataTypeBubbleCategory.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import * as d3 from "d3";
 import i18n from "../../i18n.js";
 import utils from "./utils.js";
-import DataBubbles from "./dataBubbles.jsx";
+import DataTypeBubbles from "./dataTypeBubbles.jsx";
 
 /*
     Component to visualize data in a non-ordered bubble-diagram
@@ -58,12 +58,12 @@ const DataTypeBubbleCategory = ({
     };
 
     return (
-        <DataBubbles
+        <DataTypeBubbles
             data={data}
             width={width}
             height={height}
             drawLeafs={drawBubblesLeafs}
-        ></DataBubbles>
+        ></DataTypeBubbles>
     );
 };
 
