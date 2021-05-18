f676a02ca5dd6182a9d84837f7b779e7c3ac6d27
Richard Stoffels <richard.stoffels@polypoly.net>
2021-05-18 14:46:54 +0200

    Taking out header and footer as they will be part of polypod

--- features/polyPinion/src/screens/home/HomeScreen.tsx
+++ features/polyPinion/src/screens/home/HomeScreen.tsx
@@ -1,34 +1,10 @@
 import ActiveSurveys from "./ActiveSurveys";
 import React from "react";
-import { Link } from "react-router-dom";
-import { SmallHeader } from "../../components/common/header";
-import { CenteredFooter } from "../../components/common/footer";
-import { Layout } from "../../components/common/layout";
-
-function HomeHeader() {
-    return (
-        <SmallHeader>
-            <h1 className="page-title">
-                <Link to="/home">polyPod</Link>
-            </h1>
-        </SmallHeader>
-    );
-}
-
-function HomeFooter() {
-    return (
-        <CenteredFooter>
-            <button>Prüfe auf neue Umfragen</button>
-        </CenteredFooter>
-    );
-}
 
 export default function HomeScreen() {
     return (
-        <Layout header={<HomeHeader />} footer={<HomeFooter />}>
-            <main>
-                <ActiveSurveys />
-            </main>
-        </Layout>
+        <main>
+            <ActiveSurveys />
+        </main>
     );
 }
