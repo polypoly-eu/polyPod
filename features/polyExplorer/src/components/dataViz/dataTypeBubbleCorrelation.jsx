65ba45cbf14706e23fba33a9c9529333a608ab09
Adrian Ferreres Esteller <ardiadrianadri@gmail.com>
2021-05-18 15:11:01 +0200

    fix(import): fix wrong import

--- features/polyExplorer/src/components/dataViz/dataTypeBubbleCorrelation.jsx
+++ features/polyExplorer/src/components/dataViz/dataTypeBubbleCorrelation.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import * as d3 from "d3";
 import i18n from "../../i18n.js";
 import utils from "./utils.js";
-import DataTypeBubbles from "./dataTypeBubbles";
+import DataTypeBubbles from "./dataTypeBubbles.jsx";
 
 /*
     Component to visualize data in a non-ordered bubble-diagram
