0a6067839dace2c181d6f769ac887313b49211f2
JJ Merelo <jjmerelo@gmail.com>
2021-05-20 10:15:24 +0200

    Adding babel config :heavy_plus_sign:

---
+++ core/utils/silly-i18n/test/basic.test.js
@@ -0,0 +1,8 @@
+import I18n from "../src/index.js";
+
+describe( "Test basic configuration", () => {
+
+    it( "is created correctly", () => {
+        const i18n = new I18n( "foo", { "foo": { "bar": "baz" }})
+    })
+});
