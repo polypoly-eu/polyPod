480fc10ecc1804890b492f5fef1822e952f7c13e
JJ Merelo <jjmerelo@gmail.com>
2021-05-20 11:41:44 +0200

    :white_check_mark: translations

--- core/utils/silly-i18n/test/basic.test.js
+++ core/utils/silly-i18n/test/basic.test.js
@@ -1,8 +1,18 @@
 import {determineLanguage, I18n} from "../src/index.js";
 
+const LANGUAGE = "foo"
+let i18n
+
+beforeAll( () => {
+    i18n = new I18n( LANGUAGE , { [LANGUAGE]: {"quux": { "bar": "baz" }}})
+})
 describe( "Test basic configuration", () => {
 
     it( "is created correctly", () => {
-        const i18n = new I18n( "foo", { "foo": { "bar": "baz" }})
+        expect(i18n).toBeInstanceOf(I18n)
+    })
+
+    it( "Translates correctly", () => {
+        expect(i18n.t( "quux:bar")).toBe("baz")
     })
 });
