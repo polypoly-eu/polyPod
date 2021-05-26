ac44c1369f184c36af1e2d28acd562e630c42fef
JJ Merelo <jjmerelo@gmail.com>
2021-05-26 10:05:35 +0200

    Covers default language... Although it does not make a lot of sense

--- core/utils/silly-i18n/test/basic.test.js
+++ core/utils/silly-i18n/test/basic.test.js
@@ -1,4 +1,4 @@
-import { determineLanguage, I18n } from "../src/index.js";
+import { defaultLanguage, determineLanguage, I18n } from "../src/index.js";
 
 const LANGUAGE = "foo";
 let i18n;
@@ -12,6 +12,7 @@ describe("Test language determination", () => {
         expect(determineLanguage()).toEqual(expect.stringMatching(/\w{2,}/));
     });
 });
+
 describe("Test basic configuration", () => {
     it("is created correctly", () => {
         expect(i18n).toBeInstanceOf(I18n);
@@ -25,3 +26,8 @@ describe("Test basic configuration", () => {
         }).toThrow(Error);
     });
 });
+
+describe("Test default constructor", () => {
+    const i18nDefault = new I18n("LANGUAGE", { [LANGUAGE]: { quux: { bar: "baz" } } });
+    expect(i18nDefault.language).toBe(defaultLanguage)
+})
