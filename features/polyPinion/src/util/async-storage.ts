25e3733192afe4567bffb8c628f30487cb4dace5
Richard Stoffels <richard.stoffels@polypoly.net>
2021-05-27 14:57:43 +0200

    Fix error by adding index as separate storage entity

--- features/polyPinion/src/util/async-storage.ts
+++ features/polyPinion/src/util/async-storage.ts
@@ -1,4 +1,6 @@
 const namespace = "http://polypoly.coop/schema/polyPinion/#";
+const questionnairePredicate = "answers";
+const indexPredicate = "this does not make sense";
 const { dataFactory, polyIn } = window.pod;
 
 const AsyncStorage = {
@@ -16,9 +18,30 @@ const AsyncStorage = {
 
     async setItem(key: string, value: string): Promise<void> {
         const quad = dataFactory.quad(
-            dataFactory.namedNode(`${namespace}${key}`),
-            dataFactory.namedNode(`${namespace}answers`),
-            dataFactory.namedNode(`${namespace}${value}`)
+            dataFactory.literal(`${namespace}${key}`),
+            dataFactory.literal(`${namespace}${questionnairePredicate}`),
+            dataFactory.literal(`${namespace}${value}`)
+        );
+        polyIn.add(quad);
+    },
+
+    async getIndex(key: string): Promise<string | null> {
+        // use non empty matcher when implemented in pod.js
+        const quads = await polyIn.select({});
+        return (
+            quads.find(
+                ({ subject, predicate }) =>
+                    subject.value === `${namespace}${key}` &&
+                    predicate.value === `${namespace}answers`
+            )?.object.value || null
+        );
+    },
+
+    async setIndex(key: string, value: string): Promise<void> {
+        const quad = dataFactory.quad(
+            dataFactory.literal(`${namespace}${key}`),
+            dataFactory.literal(`${namespace}${indexPredicate}`),
+            dataFactory.literal(`${namespace}${value}`)
         );
         polyIn.add(quad);
     },
