7c548f83158206a60ff00ff29617309075cf51e7
Richard Stoffels <richard.stoffels@polypoly.net>
2021-05-27 12:34:59 +0200

    Start adding storage

--- features/polyPinion/src/util/async-storage.ts
+++ features/polyPinion/src/util/async-storage.ts
@@ -1,14 +1,26 @@
-const storage = new Map<string, string>();
-
-//TODO implement storage
+const namespace = "http://polypoly.coop/schema/polyPinion/#";
+const { dataFactory, polyIn } = window.pod;
 
 const AsyncStorage = {
     async getItem(key: string): Promise<string | null> {
-        return storage.get(key);
+        // use non empty matcher when implemented in pod.js
+        const quads = await polyIn.select({});
+        return (
+            quads.find(
+                ({ subject, predicate }) =>
+                    subject.value === `${namespace}${key}` &&
+                    predicate.value === `${namespace}answers`
+            )?.object || null
+        );
     },
 
     async setItem(key: string, value: string): Promise<void> {
-        storage.set(key, value);
+        const quad = dataFactory.quad(
+            dataFactory.namedNode(`${namespace}${key}`),
+            dataFactory.namedNode(`${namespace}answers`),
+            dataFactory.namedNode(`${namespace}${value}`)
+        );
+        polyIn.add(quad);
     },
 };
 
