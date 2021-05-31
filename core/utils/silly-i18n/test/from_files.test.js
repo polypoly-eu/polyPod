b0a03310bc637edb70f2e03b6d1ef29cdd8ae7ce
JJ Merelo <jjmerelo@gmail.com>
2021-05-31 14:29:42 +0200

    :rotating_light: mainly

--- core/utils/silly-i18n/test/from_files.test.js
+++ core/utils/silly-i18n/test/from_files.test.js
@@ -1,8 +1,4 @@
-import {
-    determineLanguage,
-    FileNotFoundError,
-    I18n,
-} from "../src/index.js";
+import { FileNotFoundError, I18n } from "../src/index.js";
 
 let i18n;
 
@@ -10,16 +6,22 @@ beforeAll(async () => {
     i18n = await I18n.fromFiles("test/locales");
 });
 
+describe("Test existing keys", () => {
+    it("Has the right amount of keys", () => {
+        expect(Object.keys(i18n.translations)).toEqual(1);
+    });
+});
+
 describe("Test possible errors", () => {
     it("Throws when the directory does not exist", () => {
-        I18n.fromFiles("WAT").catch( (error) => {
+        I18n.fromFiles("WAT").catch((error) => {
             console.log(error);
             expect(error).toBeInstanceOf(FileNotFoundError);
             expect(error.message).toEqual(expect.stringMatching(/found/));
         });
     });
     it("Throws when the argument is not a directory", () => {
-        I18n.fromFiles("test/locales/es/common.json").catch( (error) => {
+        I18n.fromFiles("test/locales/es/common.json").catch((error) => {
             console.log(error);
             expect(error).toBeInstanceOf(FileNotFoundError);
             expect(error.message).toEqual(expect.stringMatching(/not really/));
