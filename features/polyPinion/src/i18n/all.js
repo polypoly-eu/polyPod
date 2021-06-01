51f39f80a6bb410f773844a8b5bb0ebe28f4944f
JJ Merelo <jjmerelo@gmail.com>
2021-06-01 10:23:11 +0200

    :rotating_light: mainly

--- features/polyPinion/src/i18n/all.js
+++ features/polyPinion/src/i18n/all.js
@@ -1,7 +1,7 @@
-import en from './en';
-import de from './de';
+import en from "./en";
+import de from "./de";
 
 export default {
-  en: en,
-  de: de,
+    en: en,
+    de: de,
 };
--- features/polyPinion/src/i18n/i18n.js
+++ features/polyPinion/src/i18n/i18n.js
@@ -1,19 +1,17 @@
-import React from 'react';
-import i18n from 'i18next';
-import {useTranslation, initReactI18next} from 'react-i18next';
-import resources from './all.js';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import resources from "./all.js";
 
-i18n
-  .use(initReactI18next) // passes i18n down to react-i18next
-  .init({
-    debug: false,
-    resources: resources,
-    lng: 'en',
-    fallbackLng: 'en',
+i18n.use(initReactI18next) // passes i18n down to react-i18next
+    .init({
+        debug: false,
+        resources: resources,
+        lng: "en",
+        fallbackLng: "en",
 
-    interpolation: {
-      escapeValue: false,
-    },
-  });
+        interpolation: {
+            escapeValue: false,
+        },
+    });
 
-export {i18n};
+export { i18n };
