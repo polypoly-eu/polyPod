e28b17a8d5bbc23acf25d1467eacc5c702a0a8cc
Adrian Ferreres Esteller <ardiadrianadri@gmail.com>
2021-06-02 16:17:25 +0200

    feat(prod4pod): finish unit and integrated test

--- features/poly-look/test/markdown-reader/MarkdownRender.test.js
+++ features/poly-look/test/markdown-reader/MarkdownRender.test.js
@@ -2,13 +2,13 @@ import { html, fixture, expect } from "@open-wc/testing";
 import "../../poly-look.js";
 
 describe("MarkdownRender", () => {
+  const text =
+    '# This is a test @@<poly-first-capitalize paragraph="example of paragraph"></<poly-first-capitalize>@@';
+
   it(`
     have to use the markdown-element for the markdown text and render
     the rest of the custom tags
   `, async () => {
-    const text =
-      '# This is a test @@<poly-first-capitalize paragraph="example of paragraph"></<poly-first-capitalize>@@';
-
     const el = await fixture(
       html`<poly-markdown-render .text=${text}></poly-markdown-render>`
     );
@@ -23,6 +23,20 @@ describe("MarkdownRender", () => {
   });
 
   it(`
-    have to render a background image if the attribute backgroundImage has 
-  `, () => {});
+    have to render a background image if the attribute backgroundImage has value
+  `, async () => {
+    const image = "/public/image/test.jpg";
+    const el = await fixture(
+      html`<poly-markdown-render
+        .text=${text}
+        .backgroundImage=${image}
+      ></poly-markdown-render>`
+    );
+
+    const imageBackground = el.shadowRoot.querySelector(".decorator");
+
+    expect(imageBackground.attributes[1].value).to.equal(
+      `background-image:url(${image})`
+    );
+  });
 });
