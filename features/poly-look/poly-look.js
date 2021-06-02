7ebe2277a95b684adf4d59749332ccd49018f6b4
Adrian Ferreres Esteller <ardiadrianadri@gmail.com>
2021-06-02 11:06:40 +0200

    feat(prod4pod-450): wip doing test for components

--- features/poly-look/poly-look.js
+++ features/poly-look/poly-look.js
@@ -1,8 +1,14 @@
 import { Tab, TabsLine } from "./src/tabs";
-import { MarkdownRender } from "./src/markdown-reader";
+import {
+  MarkdownRender,
+  MarkdownRequester,
+  MarkdownReader,
+} from "./src/markdown-reader";
 import { FirstCapitalize } from "./src/text-effects";
 
 window.customElements.define("poly-tab", Tab);
 window.customElements.define("poly-tabs", TabsLine);
-window.customElements.define("poly-markdown-reader", MarkdownRender);
+window.customElements.define("poly-markdown-render", MarkdownRender);
 window.customElements.define("poly-first-capitalize", FirstCapitalize);
+window.customElements.define("poly-markdown-requester", MarkdownRequester);
+window.customElements.define("poly-markdown-reader", MarkdownReader);
