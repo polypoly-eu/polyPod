4cee15c3d5c25c0a13434d5ef6488ef89ee52216
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2021-06-03 10:12:12 +0200

    Eliminated all non-working cypress configuration stuff

--- core/utils/silly-i18n/cypress/plugins/index.js
+++
@@ -1,9 +0,0 @@
-const webpackPreprocessor = require('@cypress/webpack-preprocessor')
-
-module.exports = (on) => {
-  const options = {
-    webpackOptions: require('../../webpack.config.js'),
-  }
-
-  on('file:preprocessor', webpackPreprocessor(options))
-}
