580303600874ae950f47144f0c0ffe5ec3562257
Adrian Ferreres Esteller <ardiadrianadri@gmail.com>
2021-06-04 13:56:34 +0200

    fix(prod4pod-467): hide clear button if input is empty

--- features/lexicon/src/Lexicon.svelte
+++ features/lexicon/src/Lexicon.svelte
@@ -254,7 +254,11 @@ function handleBack() {
 }
 
 function handleSearch(value) {
+    console.log("NNN pre searchString: ", searchString);
+    console.log("NNN value: ", value);
     searchString = value;
+    console.log("NNN post searchString: ", searchString);
+    console.log(`NNN This is ${searchString ? "true": "false" }`);
 }
 
 function handleClear() {
