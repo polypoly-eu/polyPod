6ef123b40a86a499f89e962f730c89a5f70c1892
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2021-06-07 13:00:24 +0200

    :rotating_light: and add fix to package.json

--- features/polyExplorer/src/i18n.js
+++ features/polyExplorer/src/i18n.js
@@ -1,26 +1,30 @@
 import fs from "fs";
-import { FileNotFoundError, I18n, determineLanguage } from "@polypoly-eu/silly-i18n";
+import {
+    FileNotFoundError,
+    I18n,
+    determineLanguage,
+} from "@polypoly-eu/silly-i18n";
 
 const localesDirName = "locales";
 let localesDir;
 
-if (fs.existsSync( localesDirName )) {
+if (fs.existsSync(localesDirName)) {
     localesDir = localesDirName;
-} else if ( fs.existsSync( "src/" + localesDirName) ) {
+} else if (fs.existsSync("src/" + localesDirName)) {
     localesDir = "src/" + localesDirName;
 } else {
-    throw new FileNotFoundError( "Can't find translations dir " + localesDir );
+    throw new FileNotFoundError("Can't find translations dir " + localesDir);
 }
 
-const knownLanguages = ["en","de"];
+const knownLanguages = ["en", "de"];
 const localLanguage = determineLanguage();
 let language;
 
-if ( localLanguage in knownLanguages ) {
+if (localLanguage in knownLanguages) {
     language = localLanguage;
 } else {
     language = "en";
 }
 
-const i18n = I18n.fromFiles( localesDir, language );
+const i18n = I18n.fromFiles(localesDir, language);
 export default i18n;
--- features/polyExplorer/test/i18n.js
+++ features/polyExplorer/test/i18n.js
@@ -4,17 +4,17 @@ import assert from "assert";
 import i18n from "../src/i18n.js";
 
 describe("i18n framework", () => {
-    it( "Includes known namespaces", () => {
-        assert( i18n.namespaces.length >= 3);
+    it("Includes known namespaces", () => {
+        assert(i18n.namespaces.length >= 3);
     });
-    it( "Includes used keys", () => {
-        console.log( i18n );
-            [
-                "infoScreen:headline.aggregation",
-                "mainScreen:tab.featuredCompanies",
-                "infographic:featuredCompany.text1"
-            ].forEach( (key) => {
-                assert(i18n.t(key))
-            });
+    it("Includes used keys", () => {
+        console.log(i18n);
+        [
+            "infoScreen:headline.aggregation",
+            "mainScreen:tab.featuredCompanies",
+            "infographic:featuredCompany.text1",
+        ].forEach((key) => {
+            assert(i18n.t(key));
+        });
     });
 });
