e08cbed57a0914d21a538c8efc46c414aa0b941a
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2021-06-07 18:24:50 +0200

    Can't use polyfills, fs is not reliable. Back to drawing board

--- features/polyExplorer/rollup.config.js
+++ features/polyExplorer/rollup.config.js
@@ -4,7 +4,6 @@ import json from "@rollup/plugin-json";
 import css from "rollup-plugin-css-only";
 import resolve from "@rollup/plugin-node-resolve";
 import svg from "rollup-plugin-svg";
-import nodePolyfills from 'rollup-plugin-polyfill-node';
 
 export default {
     input: "src/index.jsx",
@@ -25,7 +24,6 @@ export default {
         svg(),
         css({ output: "css/bundle.css" }),
         json(),
-        nodePolyfills(),
         sucrase({
             transforms: ["jsx"],
             production: true,
