d37e82de7c4231654f72b2bb61b1644cc37b95a1
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2021-06-08 16:36:09 +0200

    Delete local configuration which does not make a lot of sense

--- core/api/fetch-spec/.eslintrc.cjs
+++ core/api/fetch-spec/.eslintrc.cjs
@@ -1,16 +1,1 @@
-module.exports = {
-    env: {
-        browser: true,
-        es6: true,
-        jest: true,
-    },
-    overrides: [
-        {
-            files: ["*.js"],
-            parserOptions: {
-                sourceType: "module",
-                ecmaVersion: 2020
-            }
-        }
-    ]
-};
+module.exports = {};
