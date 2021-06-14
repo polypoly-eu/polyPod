d6c9c1e6434219903ec139ff57f61ccb058dd266
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2021-06-14 14:44:08 +0200

    :fire: of course much simpler to just run build before if needed

--- features/example/cypress/plugins/index.js
+++
@@ -1,8 +0,0 @@
-const rollupPreprocessor = require('@bahmutov/cy-rollup')
-
-/**
- * @type {Cypress.PluginConfig}
- */
-module.exports = (on, config) => {
-  on('file:preprocessor', rollupPreprocessor());
-}
