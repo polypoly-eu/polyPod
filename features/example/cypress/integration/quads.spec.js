873b3c3ef2c5e8caa62151ea56a33b7833ec6a2c
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2021-06-14 12:46:38 +0200

    :white_check_mark: different elements

--- features/example/cypress/integration/quads.spec.js
+++ features/example/cypress/integration/quads.spec.js
@@ -1,8 +1,18 @@
+const ths = ['Subject','Verb','Predicate']
 describe("Example", () => {
     it("should show quads correctly", () => {
         cy.visit("dist/index.html");
-        cy.get(".feature").should("be.visible", {
-            timeout: 1000
+        cy.get("#feature").should("be.visible", {
+            timeout: 30000
+        });
+        cy.get("table").should("be.visible");
+        cy.get("tbody").should("be.visible");
+        cy.get("tr").should("be.visible");
+        cy.get("tr").then( items => {
+            console.log("Items, ", items);
+            for (let i = 0; i < ths.length; i++ ) {
+                expect( items[i].to.contain.text( ths[i]))
+            }
         });
     });
 });
