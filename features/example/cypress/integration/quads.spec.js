f32bc3b4a2e5188647e6eaed492726abc6695615
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2021-06-14 12:03:12 +0200

    Checking in unfinished tests

--- features/example/cypress/integration/quads.spec.js
+++ features/example/cypress/integration/quads.spec.js
@@ -1,5 +1,8 @@
 describe("Example", () => {
     it("should show quads correctly", () => {
         cy.visit("dist/index.html");
+        cy.get(".feature").should("be.visible", {
+            timeout: 1000
+        });
     });
 });
