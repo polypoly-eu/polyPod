3d312822ed6f739b2e8e65c2f972660979032fc1
Adrian Ferreres Esteller <ardiadrianadri@gmail.com>
2021-06-16 09:27:56 +0200

    fix(prod4pod-396): trying to solve the linter configuration problems

--- core/utils/poly-look/.eslintrc.js
+++ core/utils/poly-look/.eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  extends: ["eslint:recommended"],
   parser: "babel-eslint",
   parserOptions: {
     ecmaVersion: 6,
