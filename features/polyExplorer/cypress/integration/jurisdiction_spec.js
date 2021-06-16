1dbe363b3cc886472bcd4e2acb66f133eb9c1491
Adrian Ferreres Esteller <ardiadrianadri@gmail.com>
2021-06-16 14:48:46 +0200

    feat(e2e): add e2e test for jurisdiction graph

---
+++ features/polyExplorer/cypress/integration/jurisdiction_spec.js
@@ -0,0 +1,26 @@
+describe("Jurisdiction", () => {
+    beforeEach(() => {
+        cy.visit("/");
+        cy.get(".button-container button")
+            .click()
+            .then(() => {
+                return cy
+                    .get(
+                        ".swiper-slide.swiper-slide-active .data-sharing-section.jurisdictions-shared .data-sharing-gauge"
+                    )
+                    .click();
+            });
+    });
+
+    it("should throw an error if the graph has changed", () => {
+        cy.matchImageSnapshot("jurisdictions0");
+    });
+
+    it("should throw an error if the jurisdiction info has changed", () => {
+        cy.get(".jurisdiction-tree-container .data-sharing-legend")
+            .click()
+            .then(() => {
+                cy.matchImageSnapshot("jurisdictionsinfo0");
+            });
+    });
+});
