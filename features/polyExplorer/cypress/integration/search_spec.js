57cb00411e8d9b34e49d4b5ff35c227751d225f9
Adrian Ferreres Esteller <ardiadrianadri@gmail.com>
2021-06-17 15:26:19 +0200

    feat(PROD4POD-425): add e2e test for search

---
+++ features/polyExplorer/cypress/integration/search_spec.js
@@ -0,0 +1,41 @@
+describe("Search", () => {
+    beforeEach(() => {
+        cy.visit("/");
+        cy.get(".button-container button")
+            .click()
+            .then(() => {
+                cy.get(
+                    ".swiper-slide-active > .scrolling-area > .featured-company-card"
+                ).trigger("keyup", { key: "s" });
+            });
+    });
+
+    it(`should be able to clear the content of the input clicking on the clear button`, () => {
+        cy.matchImageSnapshot("search0");
+        cy.get(".search-bar-input")
+            .type("Amazon")
+            .then(() => {
+                cy.matchImageSnapshot("search1");
+                return cy.get(".clr.active").click();
+            })
+            .then(() => {
+                cy.matchImageSnapshot("search0");
+            });
+    });
+
+    it(`should be able to arrive at the data graph through the search`, () => {
+        cy.get(".suggestions > :nth-child(1)")
+            .click()
+            .then(() => {
+                cy.matchImageSnapshot("search2");
+                return cy.get(".tab-button-container > :nth-child(2)").click();
+            })
+            .then(() => {
+                cy.matchImageSnapshot("search3");
+                return cy.get(".explore-data-btn").click();
+            })
+            .then(() => {
+                cy.matchImageSnapshot("search4");
+            });
+    });
+});
