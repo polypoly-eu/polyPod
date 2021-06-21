e38f5478a89b714fbe63e541cb9d6f4c2f159540
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2021-06-21 11:19:32 +0200

    :rotating_light: :straight_ruler:

--- android/app/src/main/kotlin/coop/polypoly/polypod/bubblewrap/FetchResponseCodec.kt
+++ android/app/src/main/kotlin/coop/polypoly/polypod/bubblewrap/FetchResponseCodec.kt
@@ -7,12 +7,15 @@ import org.msgpack.value.ValueFactory
 
 class FetchResponseCodec : Codec<FetchResponse> {
     override fun encode(result: FetchResponse): Value {
-        val codec = Codec.kvArray(Codec.string, Codec.id).taggedClass("@polypoly-eu/remote-pod.FetchResponse")
+        val codec = Codec.kvArray(Codec.string, Codec.id).taggedClass(
+            "@polypoly-eu/remote-pod.FetchResponse"
+        )
         val response = HashMap<String, Value>()
         if (result.bodyContent != null)
             response["bufferedText"] = Codec.string.encode(result.bodyContent!!)
         if (result.status != null)
-            response["status"] = ValueFactory.newInteger(result.status!!.toLong())
+            response["status"] =
+                ValueFactory.newInteger(result.status!!.toLong())
         if (result.ok != null)
             response["ok"] = ValueFactory.newBoolean(result.ok!!)
         return codec.encode(response)
