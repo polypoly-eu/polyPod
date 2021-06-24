74f53287f49f05fd219e1086d42bddf6a6542736
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2021-06-24 11:32:04 +0200

    Latest :rotating_light: from ktlint

--- android/app/src/androidTest/kotlin/coop/polypoly/polypod/PodApiTestDouble.kt
+++ android/app/src/androidTest/kotlin/coop/polypoly/polypod/PodApiTestDouble.kt
@@ -3,7 +3,10 @@ package coop.polypoly.polypod
 import coop.polypoly.polypod.polyIn.PolyInTestDouble
 import coop.polypoly.polypod.polyOut.PolyOutTestDouble
 
-class PodApiTestDouble(override val polyOut: PolyOutTestDouble, override val polyIn: PolyInTestDouble) : PodApi(polyOut, polyIn) {
+class PodApiTestDouble(
+    override val polyOut: PolyOutTestDouble,
+    override val polyIn: PolyInTestDouble
+) : PodApi(polyOut, polyIn) {
     fun reset() {
         polyOut.reset()
         polyIn.reset()
