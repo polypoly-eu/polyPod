ddc0aea620b9d79e9067ef237c9fa98d5a93180b
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2021-06-25 12:32:59 +0200

    Eliminates more references

--- features/polyExplorer/cypress/support/commands.js
+++
@@ -1,7 +0,0 @@
-import { addMatchImageSnapshotCommand } from "cypress-image-snapshot/command";
-
-addMatchImageSnapshotCommand({
-    failureThreshold: 0.015,
-    failureThresholdType: "percent",
-    customDiffConfig: { threshold: 0.13 },
-});
