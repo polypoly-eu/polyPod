eba3d08e9bf9c9c8c8b2aa497cca104f5b1b5ef0
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2021-07-01 09:08:48 +0200

    :rotating_light:

--- core/utils/poly-look/src/globalTheme.js
+++ core/utils/poly-look/src/globalTheme.js
@@ -1,6 +1,6 @@
 import { css, unsafeCSS } from "lit-element";
 
-const PREFIX = 'poly'
+const PREFIX = "poly";
 
 const globalTheme = css`
   :host {
@@ -21,14 +21,28 @@ const globalTheme = css`
     );
 
     /* Text colors */
-    --${unsafeCSS(PREFIX)}-color-text-dark: var(--color-text-dark, var(--${unsafeCSS(PREFIX)}-color-dark));
-    --${unsafeCSS(PREFIX)}-color-text-light: var(--color-text-light, var(--${unsafeCSS(PREFIX)}-color-light));
+    --${unsafeCSS(
+      PREFIX
+    )}-color-text-dark: var(--color-text-dark, var(--${unsafeCSS(
+  PREFIX
+)}-color-dark));
+    --${unsafeCSS(
+      PREFIX
+    )}-color-text-light: var(--color-text-light, var(--${unsafeCSS(
+  PREFIX
+)}-color-light));
 
     /* Data exploration colors */
-    --${unsafeCSS(PREFIX)}-data-exp-data-types: var(--data-exp-data-types, #fe8988);
+    --${unsafeCSS(
+      PREFIX
+    )}-data-exp-data-types: var(--data-exp-data-types, #fe8988);
     --${unsafeCSS(PREFIX)}-data-exp-purposes: var(--data-exp-purposes, #3ba6ff);
-    --${unsafeCSS(PREFIX)}-data-exp-companies: var(--data-exp-companies, #7ee8a2);
-    --${unsafeCSS(PREFIX)}-data-exp-data-regions: var(--data-exp-data-regions, #edf2f7);
+    --${unsafeCSS(
+      PREFIX
+    )}-data-exp-companies: var(--data-exp-companies, #7ee8a2);
+    --${unsafeCSS(
+      PREFIX
+    )}-data-exp-data-regions: var(--data-exp-data-regions, #edf2f7);
 
     /* Size */
     --${unsafeCSS(PREFIX)}-max-width: var(--max-width, 412px);
@@ -36,8 +50,12 @@ const globalTheme = css`
     --${unsafeCSS(PREFIX)}-letter-spacing: var(--letter-spacing, -0.01em);
 
     /* Font */
-    --${unsafeCSS(PREFIX)}-font-family-regular-name: var(--font-family-regular-name, "Jost");
-    --${unsafeCSS(PREFIX)}-font-family-light-name: var(--font-family-light-name, "Jost Light");
+    --${unsafeCSS(
+      PREFIX
+    )}-font-family-regular-name: var(--font-family-regular-name, "Jost");
+    --${unsafeCSS(
+      PREFIX
+    )}-font-family-light-name: var(--font-family-light-name, "Jost Light");
     --${unsafeCSS(PREFIX)}-font-family-medium-name: var(
       --font-family-medium-name,
       "Jost Medium"
@@ -49,13 +67,19 @@ const globalTheme = css`
 
     /* Font sizes */
     --${unsafeCSS(PREFIX)}-headline-font-size: var(--headline-font-size, 38px);
-    --${unsafeCSS(PREFIX)}-body-text-font-size: var(--body-text-font-size, 20px);
+    --${unsafeCSS(
+      PREFIX
+    )}-body-text-font-size: var(--body-text-font-size, 20px);
     --${unsafeCSS(PREFIX)}-button-font-size: var(--button-font-size, 18px);
 
     /* Font weight */
     --${unsafeCSS(PREFIX)}-font-weight-regular: var(--font-weight-regular, 400);
-    --${unsafeCSS(PREFIX)}-font-weight-irrelevant: var(--font-weight-irrelevant, 200);
-    --${unsafeCSS(PREFIX)}-font-weight-important: var(--font-weight-important, 800);
+    --${unsafeCSS(
+      PREFIX
+    )}-font-weight-irrelevant: var(--font-weight-irrelevant, 200);
+    --${unsafeCSS(
+      PREFIX
+    )}-font-weight-important: var(--font-weight-important, 800);
   }
 `;
 
