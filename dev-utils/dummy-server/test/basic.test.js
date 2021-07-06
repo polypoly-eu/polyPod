0ec5953b7aa37786ac8a11515dbee7f1884d1498
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2021-07-06 09:44:59 +0200

    Extend tests to several verbs :white_check_mark:

--- dev-utils/dummy-server/test/basic.test.js
+++ dev-utils/dummy-server/test/basic.test.js
@@ -3,12 +3,16 @@ import { app } from "../src/dummy-server.js";
 
 describe( "Root route works", () => {
     it("Should return message", done => {
-      request(app)
-        .get("/")
-        .then( response => {
-            expect(response.statusCode).toBe(200);
-            expect(response.res.text).toEqual(expect.stringContaining("GET HTTP"));
-            done();
-        });
+        ["get","put", "delete"].forEach( verb => {
+            request(app)
+                .[verb]("/")
+                .then( response => {
+                    expect(response.statusCode).toBe(200);
+                    expect(response.res.text).toEqual(
+                        expect.stringContaining(`${verb.toUpperCase()} HTTP`)
+                    );
+                    done();
+                });
+        })
     } );
 });
