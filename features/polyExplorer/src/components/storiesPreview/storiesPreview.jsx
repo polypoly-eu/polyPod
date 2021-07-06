4aac897ac6c10eb809f6d79fd8ed8126ed181e39
mteresa-jimenez <mteresa.jn@gmail.com>
2021-07-06 18:36:28 +0200

    PROD4POD-575 - style StoryPreview component

--- features/polyExplorer/src/components/storiesPreview/storiesPreview.jsx
+++ features/polyExplorer/src/components/storiesPreview/storiesPreview.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import LinkButton from "../buttons/linkButton/linkButton.jsx";
+import i18n from "../../i18n.js";
+
+import "./storiesPreview.css";
 
 const Preview = ({ story }) => {
     return (
         <LinkButton route={story.route} className="preview">
+            <img src={story.img} />
             <h3>{story.title}</h3>
             <p>{story.previewText}</p>
         </LinkButton>
@@ -13,9 +17,18 @@ const Preview = ({ story }) => {
 const StoriesPreview = ({ storiesMetadata }) => {
     return (
         <div className="story-preview">
-            {Object.values(storiesMetadata).map((story, index) => (
-                <Preview key={index} story={story} />
-            ))}
+            <div className="preview-scrollable">
+                {Object.values(storiesMetadata).map((story, index) => (
+                    <Preview key={index} story={story} />
+                ))}
+                <div className="gradient"></div>
+            </div>
+            <div className="preview-btn-area">
+                <p>{i18n.t("storiesPreviewScreen:more.stories")}</p>
+                <button className="preview-btn">
+                    {i18n.t("storiesPreviewScreen:button.notifications")}
+                </button>
+            </div>
         </div>
     );
 };
