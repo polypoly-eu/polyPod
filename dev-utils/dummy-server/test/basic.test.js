b038e1e7d593d2060f9aea3b4008a340732c9e74
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2021-07-06 10:39:43 +0200

    :white_check_mark: with POST

--- dev-utils/dummy-server/test/basic.test.js
+++ dev-utils/dummy-server/test/basic.test.js
@@ -1,18 +1,33 @@
 const request = require("supertest");
 import { app } from "../src/dummy-server.js";
 
-describe( "Root route works", () => {
-    it("Should return message", done => {
-        ["get","put", "delete"].forEach( verb => {
+describe("Root route works", () => {
+    it("Should return message", (done) => {
+        ["get", "put", "delete"].forEach((verb) => {
             request(app)
-                .[verb]("/")
-                .then( response => {
+                [verb]("/")
+                .then((response) => {
                     expect(response.statusCode).toBe(200);
                     expect(response.res.text).toEqual(
                         expect.stringContaining(`${verb.toUpperCase()} HTTP`)
                     );
                     done();
                 });
-        })
-    } );
+        });
+    });
+});
+
+describe("Post'ing works", () => {
+    it("Should return posted body", (done) => {
+        const body = "Foo";
+        const anObject = {};
+        anObject[body] = "";
+        request(app)
+            .post("/")
+            .send(body)
+            .then((response) => {
+                expect(response.body).toStrictEqual(anObject);
+                done();
+            });
+    });
 });
