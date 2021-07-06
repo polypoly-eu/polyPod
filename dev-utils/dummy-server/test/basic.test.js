99169b864e860f26c65e9fe796336e652f19a94e
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2021-07-06 09:37:22 +0200

    Another :white_check_mark:

--- dev-utils/dummy-server/test/basic.test.js
+++ dev-utils/dummy-server/test/basic.test.js
@@ -7,6 +7,7 @@ describe( "Root route works", () => {
         .get("/")
         .then( response => {
             expect(response.statusCode).toBe(200);
+            expect(response.res.text).toEqual(expect.stringContaining("GET HTTP"));
             done();
         });
     } );
