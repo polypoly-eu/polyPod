020d5f2ab831507e41baa04054c8a4b8b21cfc55
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2021-07-08 12:56:12 +0200

    Structure generated

--- dev-utils/fb-zip-file/scripts/import.js
+++ dev-utils/fb-zip-file/scripts/import.js
@@ -1,11 +1,37 @@
 import JSZip from "jszip";
 import fs from "fs";
+import { isPrimitive } from "util";
 
 const fbZipLocation = process.env.FB_ZIP_LOCATION;
 
 fs.readFile(fbZipLocation, function(err, data) {
     if (err) throw err;
     JSZip.loadAsync(data).then(function (zip) {
-        console.log(zip);
+        let structure = {};
+        console.log(Object.keys(zip.files).sort());
+        const files = Object.keys(zip.files).sort();
+        for ( const f in files  ) {
+            const key = files[f];
+            const fragments = key.split("/");
+            const thisFile = zip.files[key];
+            if (thisFile['dir'] ) {
+                if ( fragments.length === 2 ) {
+                    structure[fragments[0]] = {files: []}
+                } else if ( fragments.length === 3 ) {
+                    structure[fragments[0]][fragments[1]] = { files: []}
+                } else if ( fragments.length === 4 ) { 
+                    structure[fragments[0]][fragments[1]][fragments[2]] = { files: [] }
+                }
+            } else {
+                if ( fragments.length === 2 ) {
+                    structure[fragments[0]]['files'].push( fragments[1] );
+                } else if ( fragments.length === 3 ) {
+                    structure[fragments[0]][fragments[1]]['files'].push(fragments[2] );
+                } else if ( fragments.length === 3 ) { 
+                    structure[fragments[0]][fragments[1]][fragments[2]]['files'].push(fragments[3] );
+                }
+            }
+        }
+        console.log(structure);
     });
 });
