7566285b44f1a4b826d35160a21434e2131dffc9
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2021-07-08 13:19:19 +0200

    :rotating_light:

--- dev-utils/fb-zip-file/scripts/import.js
+++ dev-utils/fb-zip-file/scripts/import.js
@@ -1,43 +1,55 @@
 import JSZip from "jszip";
 import fs from "fs";
-import { isPrimitive } from "util";
 
 const fbZipLocation = process.env.FB_ZIP_LOCATION;
 
-fs.readFile(fbZipLocation, function(err, data) {
+fs.readFile(fbZipLocation, function (err, data) {
     if (err) throw err;
     JSZip.loadAsync(data).then(function (zip) {
         let structure = {};
         const files = Object.keys(zip.files).sort();
-        for ( const f in files  ) {
+        for (const f in files) {
             const key = files[f];
             const fragments = key.split("/");
             const thisFile = zip.files[key];
-            if (thisFile['dir'] ) {
-                if ( fragments.length === 2 ) {
-                    structure[fragments[0]] = {leaves: []}
-                } else if ( fragments.length === 3 ) {
-                    structure[fragments[0]][fragments[1]] = { leaves: []}
-                } else if ( fragments.length === 4 ) { 
-                    structure[fragments[0]][fragments[1]][fragments[2]] = { leaves: [] }
-                } else if ( fragments.length === 5 ) { 
-                    structure[fragments[0]][fragments[1]][fragments[2]][fragments[3]] = { leaves: [] }
-                } else if ( fragments.length === 6 ) { 
-                    structure[fragments[0]][fragments[1]][fragments[2]][fragments[3]][fragments[4]] = { leaves: [] }
+            if (thisFile["dir"]) {
+                if (fragments.length === 2) {
+                    structure[fragments[0]] = { leaves: [] };
+                } else if (fragments.length === 3) {
+                    structure[fragments[0]][fragments[1]] = { leaves: [] };
+                } else if (fragments.length === 4) {
+                    structure[fragments[0]][fragments[1]][fragments[2]] = {
+                        leaves: [],
+                    };
+                } else if (fragments.length === 5) {
+                    structure[fragments[0]][fragments[1]][fragments[2]][
+                        fragments[3]
+                    ] = { leaves: [] };
+                } else if (fragments.length === 6) {
+                    structure[fragments[0]][fragments[1]][fragments[2]][
+                        fragments[3]
+                    ][fragments[4]] = { leaves: [] };
                 }
             } else {
-                if ( fragments.length === 2 ) {
-                    structure[fragments[0]]['leaves'].push( fragments[1] );
-                } else if ( fragments.length === 3 ) {
-                    structure[fragments[0]][fragments[1]]['leaves'].push(fragments[2] );
-                } else if ( fragments.length === 4 ) { 
-                    structure[fragments[0]][fragments[1]][fragments[2]]['leaves'].push(fragments[3] );
-                } else if ( fragments.length === 5 ) { 
-                    structure[fragments[0]][fragments[1]][fragments[2]][fragments[3]]['leaves'].push(fragments[4] );
-                } else if ( fragments.length === 6 ) { 
-                    structure[fragments[0]][fragments[1]][fragments[2]][fragments[3]][fragments[4]]['leaves'].push(fragments[5] );
+                if (fragments.length === 2) {
+                    structure[fragments[0]]["leaves"].push(fragments[1]);
+                } else if (fragments.length === 3) {
+                    structure[fragments[0]][fragments[1]]["leaves"].push(
+                        fragments[2]
+                    );
+                } else if (fragments.length === 4) {
+                    structure[fragments[0]][fragments[1]][fragments[2]][
+                        "leaves"
+                    ].push(fragments[3]);
+                } else if (fragments.length === 5) {
+                    structure[fragments[0]][fragments[1]][fragments[2]][
+                        fragments[3]
+                    ]["leaves"].push(fragments[4]);
+                } else if (fragments.length === 6) {
+                    structure[fragments[0]][fragments[1]][fragments[2]][
+                        fragments[3]
+                    ][fragments[4]]["leaves"].push(fragments[5]);
                 }
-
             }
         }
         console.log(JSON.stringify(structure));
