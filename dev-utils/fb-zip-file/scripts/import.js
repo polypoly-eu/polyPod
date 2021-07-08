c79af7df0877290c0a320315352e4ffd7607780c
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2021-07-08 13:14:14 +0200

    Serialize to JSON

--- dev-utils/fb-zip-file/scripts/import.js
+++ dev-utils/fb-zip-file/scripts/import.js
@@ -15,22 +15,31 @@ fs.readFile(fbZipLocation, function(err, data) {
             const thisFile = zip.files[key];
             if (thisFile['dir'] ) {
                 if ( fragments.length === 2 ) {
-                    structure[fragments[0]] = {files: []}
+                    structure[fragments[0]] = {leaves: []}
                 } else if ( fragments.length === 3 ) {
-                    structure[fragments[0]][fragments[1]] = { files: []}
+                    structure[fragments[0]][fragments[1]] = { leaves: []}
                 } else if ( fragments.length === 4 ) { 
-                    structure[fragments[0]][fragments[1]][fragments[2]] = { files: [] }
+                    structure[fragments[0]][fragments[1]][fragments[2]] = { leaves: [] }
+                } else if ( fragments.length === 5 ) { 
+                    structure[fragments[0]][fragments[1]][fragments[2]][fragments[3]] = { leaves: [] }
+                } else if ( fragments.length === 6 ) { 
+                    structure[fragments[0]][fragments[1]][fragments[2]][fragments[3]][fragments[4]] = { leaves: [] }
                 }
             } else {
                 if ( fragments.length === 2 ) {
-                    structure[fragments[0]]['files'].push( fragments[1] );
+                    structure[fragments[0]]['leaves'].push( fragments[1] );
                 } else if ( fragments.length === 3 ) {
-                    structure[fragments[0]][fragments[1]]['files'].push(fragments[2] );
-                } else if ( fragments.length === 3 ) { 
-                    structure[fragments[0]][fragments[1]][fragments[2]]['files'].push(fragments[3] );
+                    structure[fragments[0]][fragments[1]]['leaves'].push(fragments[2] );
+                } else if ( fragments.length === 4 ) { 
+                    structure[fragments[0]][fragments[1]][fragments[2]]['leaves'].push(fragments[3] );
+                } else if ( fragments.length === 5 ) { 
+                    structure[fragments[0]][fragments[1]][fragments[2]][fragments[3]]['leaves'].push(fragments[4] );
+                } else if ( fragments.length === 6 ) { 
+                    structure[fragments[0]][fragments[1]][fragments[2]][fragments[3]][fragments[4]]['leaves'].push(fragments[5] );
                 }
+
             }
         }
-        console.log(structure);
+        console.log(JSON.stringify(structure));
     });
 });
