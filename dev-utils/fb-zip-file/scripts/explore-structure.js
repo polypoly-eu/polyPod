c280bc31323013683fdb38318e9989e8c45d8fbf
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2021-07-12 09:13:42 +0200

    Use better data structures

--- dev-utils/fb-zip-file/scripts/explore-structure.js
+++ dev-utils/fb-zip-file/scripts/explore-structure.js
@@ -10,22 +10,21 @@ const localFolder = ".data";
 
 glob(`${localFolder}/*.json`, (error, files) => {
     if (error) throw Error("Some problem reading files in data folder");
-    let allKeys = {};
+    let allKeys = new Set();
     let localKeys = [];
     files.forEach((f) => {
-        let theseKeys = {};
+        let theseKeys = [];
         let thisData = JSON.parse(readFileSync(f));
         for (let key in thisData) {
-            console.log( "File ", f, " key ", key );
-            allKeys[key] = true;
-            theseKeys[key] = true;
+            allKeys.add(key);
+            theseKeys.push(key);
         }
-        localKeys.push(Object.keys(theseKeys));
+        localKeys.push(theseKeys);
     });
     let commonKeys = localKeys[0].filter((key) => localKeys[1].includes(key));
     for (let i = 2; i < localKeys.length; i++) {
         commonKeys = commonKeys.filter((key) => localKeys[i].includes(key));
     }
-    console.log("Common keys →\n", commonKeys);
-    console.log("All keys →\n", allKeys);
+    console.log("Common keys →\n", commonKeys.sort());
+    console.log("All keys →\n", Array.from(allKeys).sort());
 });
