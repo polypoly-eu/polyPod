a97e48785118a3089d451d89a57342af50673421
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2021-07-12 08:51:34 +0200

    Adds a JSON data structure explorer

---
+++ dev-utils/fb-zip-file/scripts/explore-structure.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+// Explore the structure of existing JSON files representing the structure of the filesystem in (possibly edited) FB zip files
+
+import glob from "glob";
+import { readFileSync } from "fs";
+
+// Files are included in a local .data folder
+const localFolder = ".data";
+
+glob(`${localFolder}/*.json`, (error, files) => {
+    if ( error ) throw Error( "Some problem reading files in data folder");
+    let allKeys = {};
+    let localKeys = [];
+    files.forEach( (f) => {
+        let theseKeys = {};
+        let thisData = JSON.parse(readFileSync(f));
+        for ( let key in thisData ) {
+            allKeys[key] = true;
+            theseKeys[key] = true;
+        }
+        localKeys.push( Object.keys(theseKeys) );
+    });
+    let commonKeys = localKeys[0].filter( key => localKeys[1].includes(key));
+    for ( let i = 2; i < localKeys.length; i++ ) {
+        commonKeys = commonKeys.filter( key => localKeys[i].includes(key) );
+    }
+    console.log("Common keys →\n", commonKeys);
+    console.log("All keys →\n", allKeys);
+})
