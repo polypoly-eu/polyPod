083f08a83218b4dfedcbcb97ac99b2d1c8d9a3be
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2021-07-12 08:52:53 +0200

    :rotating_light:

--- dev-utils/fb-zip-file/scripts/explore-structure.js
+++ dev-utils/fb-zip-file/scripts/explore-structure.js
@@ -9,22 +9,22 @@ import { readFileSync } from "fs";
 const localFolder = ".data";
 
 glob(`${localFolder}/*.json`, (error, files) => {
-    if ( error ) throw Error( "Some problem reading files in data folder");
+    if (error) throw Error("Some problem reading files in data folder");
     let allKeys = {};
     let localKeys = [];
-    files.forEach( (f) => {
+    files.forEach((f) => {
         let theseKeys = {};
         let thisData = JSON.parse(readFileSync(f));
-        for ( let key in thisData ) {
+        for (let key in thisData) {
             allKeys[key] = true;
             theseKeys[key] = true;
         }
-        localKeys.push( Object.keys(theseKeys) );
+        localKeys.push(Object.keys(theseKeys));
     });
-    let commonKeys = localKeys[0].filter( key => localKeys[1].includes(key));
-    for ( let i = 2; i < localKeys.length; i++ ) {
-        commonKeys = commonKeys.filter( key => localKeys[i].includes(key) );
+    let commonKeys = localKeys[0].filter((key) => localKeys[1].includes(key));
+    for (let i = 2; i < localKeys.length; i++) {
+        commonKeys = commonKeys.filter((key) => localKeys[i].includes(key));
     }
     console.log("Common keys →\n", commonKeys);
     console.log("All keys →\n", allKeys);
-})
+});
