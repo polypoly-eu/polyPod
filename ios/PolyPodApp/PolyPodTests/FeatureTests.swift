d0d90795a5dbaad84ec659d80c2ef7e7e30bff05
Felix Dahlke <felix.dahlke@polypoly.net>
2021-07-16 14:40:03 +0200

    Refactor and improve fixture creation and removal

--- ios/PolyPodApp/PolyPodTests/FeatureTests.swift
+++ ios/PolyPodApp/PolyPodTests/FeatureTests.swift
@@ -1,14 +1,76 @@
 import SwiftUI
 import XCTest
 
+private let fixturesPath = FileManager.default.temporaryDirectory
+    .appendingPathComponent("feature-fixtures")
+
+private func removeFixturesDirectory() {
+    let fileManager = FileManager.default
+    if fileManager.fileExists(atPath: fixturesPath.path) {
+        try! fileManager.removeItem(at: fixturesPath)
+    }
+}
+
+private func createFixture(
+    languageCode: String? = nil,
+    withThumbnail: Bool = false
+) -> Feature {
+    let featurePath = fixturesPath.appendingPathComponent(
+        ProcessInfo.processInfo.globallyUniqueString
+    )
+    try! FileManager.default.createDirectory(
+        at: featurePath,
+        withIntermediateDirectories: true
+    )
+    
+    let thumbnailFileName = "thumbnail.png"
+    if withThumbnail {
+        let thumbnailPath =
+            featurePath.appendingPathComponent(thumbnailFileName)
+        try! "".write(to: thumbnailPath, atomically: true, encoding: .utf8)
+    }
+    
+    let manifestPath = featurePath.appendingPathComponent("manifest.json")
+    let manifest = """
+    {
+        "name": "name",
+        "author": "author",
+        "description": "description",
+        "thumbnail": "\(withThumbnail ? thumbnailFileName : "")",
+        "primaryColor": "#0000ff",
+        "links": { "example": "https://example.com" },
+        "translations": {
+            "de": {
+                "description": "description_de",
+                "primaryColor": "grün!",
+                "links": { "example": "https://example.de" }
+            }
+        }
+    }
+    """
+    try! manifest.write(to: manifestPath, atomically: true, encoding: .utf8)
+    
+    let feature = Feature.load(path: featurePath, languageCode: languageCode)
+    XCTAssertNotNil(feature, "Feature failed to load - invalid JSON?")
+    return feature!
+}
+
 class FeatureTests: XCTestCase {
+    override class func setUp() {
+        removeFixturesDirectory()
+    }
+    
+    override class func tearDown() {
+        removeFixturesDirectory()
+    }
+    
     func testPropertyAccessWorks() {
-        let feature = createFeatureFixture()
+        let feature = createFixture()
         XCTAssertEqual("name", feature.name)
     }
     
     func testLinkLookupByTargetWorks() {
-        let feature = createFeatureFixture()
+        let feature = createFixture()
         XCTAssertEqual(
             "https://example.com",
             feature.findUrl(target: "example")
@@ -16,7 +78,7 @@ class FeatureTests: XCTestCase {
     }
 
     func testLinkLookupByUrlWorks() {
-        let feature = createFeatureFixture()
+        let feature = createFixture()
         XCTAssertEqual(
             "https://example.com",
             feature.findUrl(target: "https://example.com")
@@ -24,12 +86,12 @@ class FeatureTests: XCTestCase {
     }
     
     func testTranslatedPropertyAccessWorks() {
-        let feature = createFeatureFixture(languageCode: "de")
+        let feature = createFixture(languageCode: "de")
         XCTAssertEqual("description_de", feature.description)
     }
 
     func testTranslatedLinkAccessWorks() {
-        let feature = createFeatureFixture(languageCode: "de")
+        let feature = createFixture(languageCode: "de")
         XCTAssertEqual(
             "https://example.de",
             feature.findUrl(target: "example")
@@ -37,78 +99,22 @@ class FeatureTests: XCTestCase {
     }
     
     func testColorParsedCorrectly() {
-        let feature = createFeatureFixture()
+        let feature = createFixture()
         XCTAssertEqual(Color(red: 0, green: 0, blue: 1), feature.primaryColor)
     }
 
     func testInvalidColorIgnored() {
-        let feature = createFeatureFixture(languageCode: "de")
+        let feature = createFixture(languageCode: "de")
         XCTAssertEqual(Color.clear, feature.primaryColor)
     }
     
     func testThumbnailUrlMissing() {
-        let feature = createFeatureFixture(withThumbnail: false)
+        let feature = createFixture(withThumbnail: false)
         XCTAssertNil(feature.thumbnail)
     }
     
     func testThumbnailUrlValid() {
-        let feature = createFeatureFixture(withThumbnail: true)
+        let feature = createFixture(withThumbnail: true)
         XCTAssertEqual("thumbnail.png", feature.thumbnail?.lastPathComponent)
     }
-    
-    private func createFeatureFixture(
-        languageCode: String? = nil,
-        withThumbnail: Bool = false
-    ) -> Feature {
-        let fileManager = FileManager.default
-        let featurePath = fileManager
-            .temporaryDirectory
-            .appendingPathComponent("featureFixture")
-        if fileManager.fileExists(atPath: featurePath.path) {
-            try! fileManager.removeItem(at: featurePath)
-        }
-        try! fileManager.createDirectory(
-            at: featurePath,
-            withIntermediateDirectories: false
-        )
-        
-        let thumbnailFileName = "thumbnail.png"
-        if withThumbnail {
-            let thumbnailPath =
-                featurePath.appendingPathComponent(thumbnailFileName)
-            try! "".write(to: thumbnailPath, atomically: true, encoding: .utf8)
-        }
-        
-        let manifestPath = featurePath.appendingPathComponent("manifest.json")
-        let manifest = """
-        {
-            "name": "name",
-            "author": "author",
-            "description": "description",
-            "thumbnail": "\(withThumbnail ? thumbnailFileName : "")",
-            "primaryColor": "#0000ff",
-            "links": { "example": "https://example.com" },
-            "translations": {
-                "de": {
-                    "description": "description_de",
-                    "primaryColor": "grün!",
-                    "links": { "example": "https://example.de" }
-                }
-            }
-        }
-        """
-        try! manifest.write(
-            to: manifestPath,
-            atomically: true,
-            encoding: .utf8
-        )
-        
-        let feature = Feature.load(
-            path: featurePath,
-            languageCode: languageCode
-        )
-        XCTAssertNotNil(feature, "Feature failed to load - invalid JSON?")
-        return feature!
-    }
 }
-
