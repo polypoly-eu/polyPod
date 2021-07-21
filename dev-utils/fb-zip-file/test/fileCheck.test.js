6b00f6ee9937ee95d5be829eaf8f373cdb75dbda
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2021-07-21 08:25:53 +0200

    Adds :white_check_mark: for file structure

--- dev-utils/fb-zip-file/test/fileCheck.test.js
+++ dev-utils/fb-zip-file/test/fileCheck.test.js
@@ -2,9 +2,11 @@ import { commonStructure } from "../src/globals.js";
 import { FiFileChecker } from "../src/fi-file-checker";
 
 let fileChecker;
+let zipFileRoutes;
 
 beforeAll(() => {
-    fileChecker = new FiFileChecker(Object.keys(commonStructure()));
+    zipFileRoutes = Object.keys(commonStructure());
+    fileChecker = new FiFileChecker(zipFileRoutes);
 });
 
 describe("Tautological test", () => {
@@ -12,5 +14,17 @@ describe("Tautological test", () => {
         expect(fileChecker.checkStructure(Object.keys(commonStructure()))).toBe(
             true
         );
+    });
+});
+
+describe("Tests different route sets", () => {
+    it("should pass with one route more", () => {
+        let excessFileRoutes = zipFileRoutes.slice();
+        excessFileRoutes.push( "foo/bar/baz ");
+        expect(fileChecker.checkStructure(excessFileRoutes)).toBe( true );
+    });
+    it("should not pass with one route less", () => {
+        let defectFileRoutes = zipFileRoutes.slice(0,-1);
+        expect(fileChecker.checkStructure(defectFileRoutes)).toBe( false );
     });
 });
