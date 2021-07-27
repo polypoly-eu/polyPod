a91cbbd4a4283a4b5cc66511e126423696c9a173
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2021-07-27 07:57:39 +0200

    Includes structure tests

--- features/facebookImport/cypress/integration/basic_spec.js
+++ features/facebookImport/cypress/integration/basic_spec.js
@@ -1,7 +1,16 @@
 import "../../dist/pod";
+import commonStructure from "../../src/static/commonStructure";
 
-describe("Simple test", () => {
+describe("Simple tests", () => {
     it("finds window.pod", () => {
         expect(window.pod).to.not.be.null;
+    });
+    it("imports structure JSON", () => {
+        expect(commonStructure).to.not.be.null;
+        ["events", "location", "posts", "profile_information"].forEach(
+            (key) => {
+                expect(Object.keys(commonStructure)).to.include(key);
+            }
+        );
     });
 });
