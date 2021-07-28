6a03cf00d8e60a1858740e2f00ce870f39cf8ba1
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2021-07-28 08:33:45 +0200

    Adds single-key objects

--- dev-utils/fb-zip-file/scripts/api-mappings.js
+++ dev-utils/fb-zip-file/scripts/api-mappings.js
@@ -12,7 +12,17 @@ fs.readFile(fbZipLocation, function (err, data) {
             const str = await thisFile.async("string");
             const data = JSON.parse(str);
             if (Object.keys(data).length == 1) {
-                mapping[thisFile.name] = Object.keys(data)[0];
+                const api = Object.keys(data)[0];
+                if (
+                    data[api].constructor !== Array &&
+                    Object.keys(data[api]).length == 1
+                ) {
+                    mapping[thisFile.name] = `${api}/${
+                        Object.keys(data[api])[0]
+                    }`;
+                } else {
+                    mapping[thisFile.name] = api;
+                }
             }
         }
         console.log(JSON.stringify(mapping));
