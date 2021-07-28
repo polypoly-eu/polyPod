24b12136a04027a1256030511d853d974da7c6f2
Adrian Ferreres Esteller <ardiadrianadri@gmail.com>
2021-07-28 08:40:27 +0200

    feat(prod4pod-636): wip add summary structure

---
+++ features/polyExplorer/src/components/clusterStories/messengers/summary.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+// import i18n from "../../../i18n";
+
+import "./summary.css";
+
+// const i18nHeader = "clusterMessengerStory";
+
+const Summary = () => {
+    return (
+        <div className="messenger-summary">
+            <h1>This will be the summary</h1>
+        </div>
+    );
+};
+
+export default Summary;
