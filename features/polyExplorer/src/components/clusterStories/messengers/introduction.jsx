3ed9ff141bc575b8a5a0d7e978db2f18f6e0e61d
Adrian Ferreres Esteller <ardiadrianadri@gmail.com>
2021-07-28 15:06:00 +0200

    feat(prod4pod-636): wip styles summary

--- features/polyExplorer/src/components/clusterStories/messengers/introduction.jsx
+++ features/polyExplorer/src/components/clusterStories/messengers/introduction.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
 import i18n from "../../../i18n";
-import { INTRO_ANIMATIONS } from "../../../constants";
+import { StoryParagraph } from "./storyParagraph";
 
 import "./introduction.css";
 
@@ -17,18 +17,7 @@ const listOfMessengerApps = [
     "WhatsApp",
 ];
 
-const phoneCorrectionX = 20;
-const pohneCorrectionY = 50;
-const circleRotateDeg = 40;
-const circleScale = 2.5;
-
-const Introduction = ({ animation, setHeight }) => {
-    const [circleStyles, updateCircleStyles] = useState({});
-    const guyImage = useRef();
-    const messengerList = useRef();
-    const circleImage = useRef();
-    const wholeIntroduction = useRef();
-
+const Introduction = () => {
     function _getBulletsMessengerApps() {
         return listOfMessengerApps.map((messenger, index) => (
             <li key={index} className="messenger-el">
@@ -37,96 +26,25 @@ const Introduction = ({ animation, setHeight }) => {
         ));
     }
 
-    function _deScaleAnimation() {
-        const circleStyles = {
-            transition: "transform 1s ease-in",
-            transform: "scale(1)",
-        };
-
-        updateCircleStyles(circleStyles);
-    }
-
-    function _animationBackward() {
-        const circleStyles = {
-            transition: "transform 1s ease-in",
-            transform: `translate( ${phoneCorrectionX}px, ${pohneCorrectionY}px) rotate(-${circleRotateDeg}deg)`,
-        };
-
-        updateCircleStyles(circleStyles);
-    }
-
-    function _fullAnimationBackward() {
-        _deScaleAnimation();
-        setTimeout(() => {
-            _animationBackward();
-        }, 0);
-    }
-
-    function _animationForward() {
-        const listPosition = messengerList.current.getBoundingClientRect();
-        const circlePosition = circleImage.current.getBoundingClientRect();
-        const listCenterX = Math.ceil(
-            listPosition.left + listPosition.width / 2
-        );
-        const listCenterY = Math.ceil(
-            listPosition.top + listPosition.height / 2
-        );
-        const circleCenterX = Math.ceil(
-            circlePosition.left + circlePosition.width / 2
-        );
-        const circleCenterY = Math.ceil(
-            circlePosition.top + circlePosition.height / 2
-        );
-
-        const moveOnX = listCenterX - circleCenterX;
-        const moveOnY = listCenterY - circleCenterY;
-
-        updateCircleStyles({
-            ...circleStyles,
-            transition: "transform 1s ease-in",
-            transform: `translate(${moveOnX}px, ${moveOnY}px) rotate(-90deg) scale(${circleScale})`,
-        });
-    }
-
-    useEffect(() => {
-        const { height } = wholeIntroduction.current.getBoundingClientRect();
-        setHeight(height);
-        switch (animation) {
-            case INTRO_ANIMATIONS.FORDWARD:
-                _animationForward();
-                break;
-            case INTRO_ANIMATIONS.BACKWARD:
-                _fullAnimationBackward();
-                break;
-        }
-    }, [animation]);
-
     return (
-        <div className="messenger-intro" ref={wholeIntroduction}>
+        <div className="messenger-intro">
             <h1 className="story-title">{i18n.t(`${i18nHeader}:title`)}</h1>
-            <p className="story-paragraph one">
+            <StoryParagraph as="p" className="one">
                 {i18n.t(`${i18nHeader}:intro.paragraph.one`)}
-            </p>
-            <div
-                className="story-circle"
-                style={circleStyles}
-                ref={circleImage}
-            ></div>
+            </StoryParagraph>
             <img
-                ref={guyImage}
                 className="story-intro-img"
                 src="images/stories/messenger/intro-guy.svg"
                 alt={i18n.t(`${i18nHeader}:intro.image.alt`)}
             />
-            <p className="story-paragraph two upper">
+            <StoryParagraph as="p" className="two upper">
                 {i18n.t(`${i18nHeader}:intro.paragraph.two`)}
-            </p>
-            <ul
-                className="story-paragraph messenger-list upper"
-                ref={messengerList}
+            </StoryParagraph>
+            <StoryParagraph as="ul"
+                className="messenger-list upper"
             >
                 {_getBulletsMessengerApps()}
-            </ul>
+            </StoryParagraph>
         </div>
     );
 };
