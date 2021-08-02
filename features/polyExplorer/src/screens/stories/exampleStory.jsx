45ccc55b92529a68fc7fed6bbc45ae254650ee03
mteresa-jimenez <mteresa.jn@gmail.com>
2021-08-02 15:29:56 +0200

    Make overviewTab component dynamic

--- features/polyExplorer/src/screens/stories/exampleStory.jsx
+++ features/polyExplorer/src/screens/stories/exampleStory.jsx
@@ -5,7 +5,11 @@ import DataStory from "../../components/dataStory/dataStory.jsx";
 const ExampleStory = () => {
     return (
         <DataStory progressBarColor="#3BA6FF">
-            <OverviewTab />
+            <OverviewTab>
+                <div label="installs">1</div>
+                <div label="users">2</div>
+                <div label="partof">3</div>
+            </OverviewTab>
             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
             nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
             erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
