d0ac202b0b470727189bca0709d7023e5af4b058
mteresa-jimenez <mteresa.jn@gmail.com>
2021-08-02 15:29:56 +0200

    Make overviewTab component dynamic

--- features/polyExplorer/src/components/clusterStories/messengers/overviewTab.jsx
+++ features/polyExplorer/src/components/clusterStories/messengers/overviewTab.jsx
@@ -1,48 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import i18n from "../../../i18n.js";
 
 import "./overviewTab.css";
 
-const overviewTab = () => {
-    const tabOverview = [
-        {
-            tabName: "install",
-            tabContent: <div>1</div>,
-        },
-        {
-            tabName: "users",
-            tabContent: <div>2</div>,
-        },
-        {
-            tabName: "partof",
-            tabContent: <div>3</div>,
-        },
-    ];
+const overviewTab = ({ children }) => {
+    const [clickedTab, setClickedTab] = useState(children[0].props.label);
 
-    const onClickTab = () => {
-        console.log("click tab");
+    const overviewTabTranslations = {
+        installs: i18n.t("clusterStoriesMessengers:overview.tab.installs"),
+        users: i18n.t("clusterStoriesMessengers:overview.tab.users"),
+        partof: i18n.t("clusterStoriesMessengers:overview.tab.partof")
+    };
+
+    const onClickTab = (ev, newClickedTab) => {
+        ev.preventDefault();
+        setClickedTab(newClickedTab);
     };
 
     return (
-        <nav className="overview-tab">
-            <button
-                onClick={onClickTab}
-                className="overview-button active"
-            >
-                Installs
-            </button>
-            <button
-                onClick={onClickTab}
-                className="overview-button"
-            >
-                Users
-            </button>
-            <button
-                onClick={onClickTab}
-                className="overview-button"
-            >
-                Part of
-            </button>
-        </nav>
+        <div>
+            <div className="overview-tab">
+                {children.map((tab, index) => (
+                    <button
+                        key={index}
+                        onClick={(ev) => onClickTab(ev, tab.props.label)}
+                        className={
+                            tab.props.label === clickedTab
+                                ? "overview-button active"
+                                : "overview-button"
+                        }
+                    >
+                        {overviewTabTranslations[tab.props.label]}
+                    </button>
+                ))}
+            </div>
+            <div>
+                {children.map((tabContent, index) => {
+                    if (tabContent.props.label === clickedTab)
+                        return (
+                            <div key={index} className="overview-content">
+                                {tabContent.props.children}
+                            </div>
+                        );
+                })}
+            </div>
+        </div>
     );
 };
 
