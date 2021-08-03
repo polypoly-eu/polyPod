a6f051d1c1670f4ad872987603e6280b340fcc76
Richard Stoffels <richard.stoffels@polypoly.net>
2021-08-03 11:18:05 +0200

    Add React to project and adapt rollup to it

--- features/facebookImport/rollup.config.js
+++ features/facebookImport/rollup.config.js
@@ -1,18 +1,31 @@
 import resolve from "@rollup/plugin-node-resolve";
 import copy from "rollup-plugin-copy";
+import sucrase from "@rollup/plugin-sucrase";
+import css from "rollup-plugin-css-only";
 
 export default {
-    input: "src/components/facebook-import.js",
+    input: "src/facebookImporter.jsx",
     output: {
         file: "dist/facebook-import.js",
         format: "iife",
+        globals: {
+            react: "React",
+            "react-dom": "ReactDOM",
+        },
     },
     plugins: [
+        css({ output: "css/bundle.css" }),
+        sucrase({
+            transforms: ["jsx"],
+            production: true,
+        }),
         copy({
             targets: [
                 {
                     src: [
                         "src/static/*",
+                        "node_modules/react/umd/react.development.js",
+                        "node_modules/react-dom/umd/react-dom.development.js",
                         "node_modules/@polypoly-eu/podjs/dist/pod.js",
                         "node_modules/poly-look/dist/poly-look.bundled.js",
                     ],
@@ -22,4 +35,5 @@ export default {
         }),
         resolve(),
     ],
+    external: ["react", "react-dom"],
 };
---
+++ features/facebookImport/src/facebookImporter.jsx
@@ -0,0 +1,11 @@
+
+import React from "react";
+import * as ReactDOM from "react-dom";
+
+import "./components/facebook-import.js"
+
+const FacebookImporter = () => {
+    return <facebook-import></facebook-import>
+}
+
+ReactDOM.render(<FacebookImporter />, document.getElementById("facebook-import"));
