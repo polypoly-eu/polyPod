b386ef1b873e8ebdfef3f2507ffb3bce21be6f7b
mteresa-jimenez <mteresa.jn@gmail.com>
2021-08-03 19:05:58 +0200

    Create fakeTab component for tabs in Cluster Stories

---
+++ features/polyExplorer/src/components/clusterStories/fakeTab.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+import "./fakeTab.css";
+
+const FakeTab = ({ children, tabTranslations, onClickedOverviewTab }) => {
+    const [clickedTab, setClickedTab] = useState(children[0].props.tabId);
+
+    const onClickTab = (ev, newClickedTab) => {
+        ev.preventDefault();
+        setClickedTab(newClickedTab);
+        onClickedOverviewTab(newClickedTab);
+    };
+
+    return (
+        <div>
+            <div className="faketab">
+                {children.map((tab, index) => (
+                    <button
+                        key={index}
+                        onClick={(ev) => onClickTab(ev, tab.props.tabId)}
+                        className={
+                            tab.props.tabId === clickedTab
+                                ? "faketab-button active"
+                                : "faketab-button"
+                        }
+                    >
+                        {tabTranslations[tab.props.label]}
+                    </button>
+                ))}
+            </div>
+            <div>
+                {children.map((tabContent, index) => {
+                    if (tabContent.props.tabId === clickedTab)
+                        return (
+                            <div key={index} className="faketab-content">
+                                {tabContent.props.children}
+                            </div>
+                        );
+                })}
+            </div>
+        </div>
+    );
+};
+
+export default FakeTab;
