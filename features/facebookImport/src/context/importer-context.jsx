4ec05dfcca0f47e55601ec2c3821998a70515139
Richard Stoffels <richard.stoffels@polypoly.net>
2021-08-03 14:04:10 +0200

    Add context and router

--- features/facebookImport/src/context/importer-context.jsx
+++ features/facebookImport/src/context/importer-context.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Storage from "../model/storage.js";
 
 const ImporterContext = React.createContext();
 
+async function initPod() {
+    return await window.pod;
+}
+
 export const ContextProvider = ({ children }) => {
+    const pod = window.pod;
+    const storage = new Storage(pod);
+    storage.refreshFiles();
+    storage.changeListener();
+    const files = Object.values(storage);
+
     return (
         <ImporterContext.Provider value={{}}>
             {children}
--- features/facebookImport/src/facebookImporter.jsx
+++ features/facebookImport/src/facebookImporter.jsx
@@ -1,11 +1,73 @@
-
 import React from "react";
 import * as ReactDOM from "react-dom";
+import {
+    MemoryRouter as Router,
+    Switch,
+    Redirect,
+    Route,
+    useHistory,
+} from "react-router-dom";
 
-import "./components/facebook-import.js"
+import "./components/facebook-import.js";
+import "./components/explore-view.js";
+import "./components/import-view.js";
+import "./components/report-view.js";
+import "./components/overview-view.js";
+
+import {
+    ImporterProvider,
+    ImporterContext,
+} from "./context/importer-context.jsx";
 
 const FacebookImporter = () => {
-    return <facebook-import></facebook-import>
-}
+    const handleImportFile = () => {
+        console.log("import");
+    };
 
-ReactDOM.render(<FacebookImporter />, document.getElementById("facebook-import"));
+    const handleRemoveFile = () => {
+        console.log("remove");
+    };
+
+    const handleExploreFile = () => {
+        console.log("explore");
+    };
+
+    return (
+        <div className="facebook-importer">
+            <Switch>
+                <Route exact path="/">
+                    <Redirect
+                        to={{ pathname: "/main", state: navigationState }}
+                    />
+                </Route>
+                <Route exact path="/main">
+                    <overview-view
+                        pod={pod}
+                        files={files}
+                        import-file={handleImportFile}
+                        remove-file={handleRemoveFile}
+                        explore-file={handleExploreFile}
+                    ></overview-view>
+                </Route>
+            </Switch>
+        </div>
+    );
+};
+
+const FacebookImporterApp = () => {
+    //global history object
+    const history = useHistory();
+
+    return (
+        <Router history={history}>
+            <ImporterProvider>
+                <FacebookImporter />
+            </ImporterProvider>
+        </Router>
+    );
+};
+
+ReactDOM.render(
+    <FacebookImporterApp />,
+    document.getElementById("facebook-import")
+);
