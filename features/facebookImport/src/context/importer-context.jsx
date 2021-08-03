8190b49c16ade5b9b0c814c72c0a180c0e07cb57
Richard Stoffels <richard.stoffels@polypoly.net>
2021-08-03 14:18:34 +0200

    Add linter and fix it

--- features/facebookImport/src/context/importer-context.jsx
+++ features/facebookImport/src/context/importer-context.jsx
@@ -1,19 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 
 import Storage from "../model/storage.js";
 
 export const ImporterContext = React.createContext();
-
-async function initPod() {
-    return await window.pod;
-}
 
 export const ImporterProvider = ({ children }) => {
     const pod = window.pod;
     const storage = new Storage(pod);
     storage.refreshFiles();
     storage.changeListener();
-    const files = [];
+    let files = [];
     storage.changeListener = () => {
         files = Object.values(storage.files);
     };
