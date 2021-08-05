1950d111971579b0f155567baf92c7e71459c815
Richard Stoffels <richard.stoffels@polypoly.net>
2021-08-05 10:52:43 +0200

    Restructure pod logic as pod is already a promise

--- features/facebookImport/src/context/importer-context.jsx
+++ features/facebookImport/src/context/importer-context.jsx
@@ -15,35 +15,17 @@ const importSteps = {
 };
 const namespace = "http://polypoly.coop/schema/facebookImporter/#";
 
-async function initPod() {
-    return await window.pod;
-}
-
-function initStorage(setPod, setFiles, setStorage) {
-    initPod().then((pod) => {
-        setPod(pod);
-        const storage = new Storage(pod);
-        storage.refreshFiles();
-        storage.changeListener = () => {
-            setFiles(Object.values(storage.files));
-        };
-        setStorage(storage);
-    });
-}
-
 function updatePodNavigation(pod, history) {
-    if (pod) {
-        pod.polyNav.actions = {
-            back: () => history.goBack(),
-        };
-        history.length > 1
-            ? pod.polyNav.setActiveActions(["back"])
-            : pod.polyNav.setActiveActions([]);
-    }
+    pod.polyNav.actions = {
+        back: () => history.goBack(),
+    };
+    history.length > 1
+        ? pod.polyNav.setActiveActions(["back"])
+        : pod.polyNav.setActiveActions([]);
 }
 
 function updateTitle(pod) {
-    if (pod) pod.polyNav.setTitle(i18n.t(`common:title`));
+    pod.polyNav.setTitle(i18n.t(`common:title`));
 }
 
 //from storage
@@ -73,10 +55,13 @@ async function writeImportStatus(status) {
 }
 
 export const ImporterProvider = ({ children }) => {
-    //state
-    const [pod, setPod] = useState(null);
-    const [files, setFiles] = useState([]);
-    const [storage, setStorage] = useState(null);
+    const pod = window.pod;
+    let files = [];
+    const storage = new Storage(pod);
+    storage.refreshFiles();
+    storage.changeListener = () => {
+        files = Object.values(storage.files);
+    };
 
     // Adding an empty navigationState here since routing-changes alone don't rerender the components, which we need for title updates etc
     // Also I'm sure navigationStates will soon come up in further development (delete this comment then)
@@ -107,7 +92,7 @@ export const ImporterProvider = ({ children }) => {
     }
 
     function handleBack() {
-        if (currentPath != "/overview") {
+        if (history.length > 1) {
             history.goBack();
             if (history.location.state) {
                 changeNavigationState(history.location.state);
@@ -117,7 +102,6 @@ export const ImporterProvider = ({ children }) => {
 
     //on startup
     useEffect(() => {
-        initStorage(setPod, setFiles, setStorage);
         determineImportStatus(pod);
     }, []);
 
