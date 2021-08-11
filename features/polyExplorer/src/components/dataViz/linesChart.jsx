7f16f0c2b11129d4b80f3b0b64272a8e5a5b8d25
Adrian Ferreres Esteller <ardiadrianadri@gmail.com>
2021-08-11 17:19:55 +0200

    feat(prod4pod-653): draw lines

--- features/polyExplorer/src/components/dataViz/linesChart.jsx
+++ features/polyExplorer/src/components/dataViz/linesChart.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import * as d3 from "d3";
 
 import "./linesChart.css";
@@ -39,10 +39,74 @@ const LinesChart = ({ data }) => {
         },
     };
 
-    function _getMesures(screenSize) {
-        return {
-            half: canvasConfig[screenSize].resolution / 2,
-        };
+    const [scaleX, updateScaleX] = useState(null);
+    const [scaleY, updateScaleY] = useState(null);
+
+    // function _getMesures(screenSize) {
+    //     return {
+    //         half: canvasConfig[screenSize].resolution / 2,
+    //     };
+    // }
+
+    function _calculateScaleX(screenSize) {
+        const { resolution, leftMargin, rightMargin } = canvasConfig[
+            screenSize
+        ];
+        const listOfDates = [data.rangeDates[0]];
+
+        while (
+            listOfDates[listOfDates.length - 1].getTime() <
+            data.rangeDates[1].getTime()
+        ) {
+            const lastDate = listOfDates[listOfDates.length - 1];
+            const day = lastDate.getDate();
+            const month = lastDate.getMonth();
+            const year = lastDate.getFullYear();
+
+            listOfDates.push(new Date(year + 1, month, day));
+        }
+
+        return d3
+            .scaleTime()
+            .domain(
+                d3.extent(listOfDates, (jsDate) =>
+                    d3.timeParse("%Y-%m-%d")(
+                        `${jsDate.getFullYear()}-${
+                            jsDate.getMonth() + 1
+                        }-${jsDate.getDate()}`
+                    )
+                )
+            )
+            .range([0, resolution - leftMargin - rightMargin]);
+    }
+
+    function _calculateScaleY(screenSize) {
+        const { resolution, topMargin } = canvasConfig[screenSize];
+
+        return d3
+            .scaleLinear()
+            .domain(data.rangeY)
+            .range([resolution - topMargin, 0]);
+    }
+
+    function _getScaleX(screenSize) {
+        if (scaleX) {
+            return scaleX;
+        } else {
+            let x = _calculateScaleX(screenSize);
+            updateScaleX(x);
+            return x;
+        }
+    }
+
+    function _getScaleY(screenSize) {
+        if (scaleY) {
+            return scaleY;
+        } else {
+            let y = _calculateScaleY(screenSize);
+            updateScaleY(y);
+            return y;
+        }
     }
 
     function _getRoot(screenSize) {
@@ -75,40 +139,9 @@ const LinesChart = ({ data }) => {
     }
 
     function calculateXAxis(screenSize) {
-        const { resolution, rightMargin, leftMargin } = canvasConfig[
-            screenSize
-        ];
+        const { resolution, leftMargin } = canvasConfig[screenSize];
         const root = _getRoot(screenSize);
-        const datesRange = data.rangeDates.map(
-            (dateString) => new Date(dateString)
-        );
-        const listOfDates = [datesRange[0]];
-
-        let x;
-        while (
-            listOfDates[listOfDates.length - 1].getTime() <
-            datesRange[1].getTime()
-        ) {
-            const lastDate = listOfDates[listOfDates.length - 1];
-            const day = lastDate.getDate();
-            const month = lastDate.getMonth();
-            const year = lastDate.getFullYear();
-
-            listOfDates.push(new Date(year + 1, month, day));
-        }
-
-        x = d3
-            .scaleTime()
-            .domain(
-                d3.extent(listOfDates, (jsDate) =>
-                    d3.timeParse("%Y-%m-%d")(
-                        `${jsDate.getFullYear()}-${
-                            jsDate.getMonth() + 1
-                        }-${jsDate.getDate()}`
-                    )
-                )
-            )
-            .range([0, resolution - leftMargin - rightMargin]);
+        const x = _getScaleX(screenSize);
 
         root.append("g")
             .attr("transform", `translate(${leftMargin}, ${resolution})`)
@@ -137,17 +170,8 @@ const LinesChart = ({ data }) => {
             screenSize
         ];
         const root = _getRoot(screenSize);
-        const listYTicks = [1];
 
-        let index = 1;
-        while (listYTicks[listYTicks.length - 1] < data.rangeY[1]) {
-            listYTicks.push(Math.pow(10, index++));
-        }
-
-        const y = d3
-            .scaleLinear()
-            .domain(data.rangeY)
-            .range([resolution - topMargin, 0]);
+        const y = _getScaleY(screenSize);
 
         const yAxis = root
             .append("g")
@@ -191,11 +215,75 @@ const LinesChart = ({ data }) => {
         yAxis.select(".domain").style("visibility", "hidden");
     }
 
+    function createLinearGradient(color, groupName, screenSize) {
+        const root = _getRoot(screenSize);
+        const gradient = root
+            .append("linearGradient")
+            .attr("id", groupName)
+            .attr("x1", "0%")
+            .attr("y1", "100%")
+            .attr("x2", "0%")
+            .attr("y2", "0%");
+
+        gradient
+            .append("stop")
+            .attr("offset", "0%")
+            .style("stop-color", color)
+            .style("stop-opacity", 1);
+        gradient
+            .append("stop")
+            .attr("offset", "100%")
+            .style("stop-color", color)
+            .style("stop-opacity", 0);
+    }
+
+    function drawLine(points, color, screenSize) {
+        const { leftMargin } = canvasConfig[screenSize];
+        const root = _getRoot(screenSize);
+        const x = _getScaleX(screenSize);
+        const y = _getScaleY(screenSize);
+
+        root.append("path")
+            .datum(points)
+            .attr("fill", "none")
+            .attr("stroke", color)
+            .attr("stroke-width", 2)
+            .attr(
+                "d",
+                d3
+                    .line()
+                    .x((d) => {
+                        const date = `${d.x.getFullYear()}-${
+                            d.x.getMonth() + 1
+                        }-${d.x.getDate()}`;
+                        const d3Date = d3.timeParse("%Y-%m-%d")(date);
+                        const point = x(d3Date);
+
+                        return point + leftMargin;
+                    })
+                    .y((d) => {
+                        const point = y(d.y);
+
+                        return point;
+                    })
+            );
+    }
+
+    function drawLines(screenSize) {
+        for (const group of data.groups) {
+            createLinearGradient(group.color, group.groupName, screenSize);
+            for (const line of group.lines) {
+                drawLine(line.points, group.color, screenSize);
+            }
+        }
+    }
+
     useEffect(() => {
         const innerWidth = window.innerWidth;
         const screenSize = calculateScreenSize(innerWidth);
         calculateXAxis(screenSize);
         calculateYAxis(screenSize);
+        drawLines(screenSize);
     }, []);
     return <div className="line-chart" ref={svgCanvas}></div>;
 };
