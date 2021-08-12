4d43c16556a2571a164e251535c2bee61efb5822
Richard Stoffels <richard.stoffels@polypoly.net>
2021-08-12 13:52:24 +0200

    Add and style overview screen

--- features/facebookImport/src/i18n.js
+++ features/facebookImport/src/i18n.js
@@ -2,17 +2,21 @@ import { determineLanguage, I18n } from "@polypoly-eu/silly-i18n";
 
 import commonEn from "./locales/en/common.json";
 import importEn from "./locales/en/import.json";
+import overviewEn from "./locales/en/overview.json";
 
 import commonDe from "./locales/de/common.json";
 import importDe from "./locales/de/import.json";
+import overviewDe from "./locales/de/overview.json";
 
 export default new I18n(determineLanguage(), {
     en: {
         common: commonEn,
         import: importEn,
+        overview: overviewEn,
     },
     de: {
         common: commonDe,
         import: importDe,
+        overview: overviewDe,
     },
 });
--- features/facebookImport/src/views/overview/overview.jsx
+++ features/facebookImport/src/views/overview/overview.jsx
@@ -2,37 +2,69 @@ import React, { useContext } from "react";
 import RouteButton from "../../components/routeButton.jsx";
 import { ImporterContext } from "../../context/importer-context.jsx";
 
+import i18n from "../../i18n.js";
+
+import "./overview.css";
+
 const Overview = () => {
-    const { files, handleRemoveFile } = useContext(ImporterContext);
+    const { files, handleRemoveFile, updateImportStatus, importSteps } =
+        useContext(ImporterContext);
+
+    const facebookHeader = (
+        <div className="header">
+            <h3>Facebook</h3>
+            <p>{i18n.t("overview:fb.product")}</p>
+            <p className="gray">{i18n.t("overview:internet")}</p>
+            <div className="separator"></div>
+        </div>
+    );
 
     const fileList = (
         <>
-            <h2>Imported files</h2>
-            <ul>
-                {Object.values(files).map((file, index) => (
-                    <li key={index}>
-                        <span>
-                            ID: {file.id}. Imported: {file.time}. Size:
-                            {file.data.size} bytes
-                        </span>
-                        <button onClick={() => handleRemoveFile(file.id)}>
-                            Remove
-                        </button>
-                        <button>Explore</button>
-                    </li>
-                ))}
-            </ul>
+            <h4>{i18n.t("overview:imported.data")}</h4>
+            {Object.values(files).map((file, index) => (
+                <div className="files" key={index}>
+                    <p>ID: {file.id}</p>
+                    <p>
+                        {i18n.t("overview:imported.time")} {file.time}
+                    </p>
+                    <p>
+                        {i18n.t("overview:size")} {file.data.size} bytes
+                    </p>
+                    <div className="btn-holder">
+                        <RouteButton className="btn primary" route="/explore">
+                            {i18n.t("overview:explore")}
+                        </RouteButton>
+                        <RouteButton
+                            className="btn secondary"
+                            route="/import"
+                            onClick={() => handleRemoveFile(file.id)}
+                        >
+                            {i18n.t("overview:new.import")}
+                        </RouteButton>
+                    </div>
+                </div>
+            ))}
         </>
     );
 
     return (
         <div className="overview">
-            <h1>File overview</h1>
-            <RouteButton route="/import">Import (another) file</RouteButton>
+            {facebookHeader}
             {Object.values(files).length ? (
                 fileList
             ) : (
-                <em>No files imported</em>
+                <div className="btn-holder">
+                    <RouteButton
+                        route="/import"
+                        className="btn primary"
+                        onClick={() =>
+                            updateImportStatus(importSteps.beginning)
+                        }
+                    >
+                        {i18n.t("overview:import.data")}
+                    </RouteButton>
+                </div>
             )}
         </div>
     );
