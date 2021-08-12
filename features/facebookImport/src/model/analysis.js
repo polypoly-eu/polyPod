ed5569de658225a49d193a49f536195632c2b2da
Andrei Chis <chisvasileandrei@gmail.com>
2021-08-12 11:26:29 +0200

    Add an analysis for expected files that are missing

--- features/facebookImport/src/model/analysis.js
+++ features/facebookImport/src/model/analysis.js
@@ -2,6 +2,41 @@ import { html } from "lit";
 import * as zip from "@zip.js/zip.js";
 import allStructure from "../static/allStructure";
 
+async function jsonDataEntities(reader) {
+    const entries = await reader.getEntries();
+    const relevantEntries = entries.filter(
+        (each) =>
+            !each.filename.includes(".DS_Store") &&
+            !each.filename.includes("__MACOSX") &&
+            !each.filename.includes("/files/") && // Remove user files
+            each.filename.endsWith(".json")
+    );
+    return relevantEntries;
+}
+
+function anonymisePathSegment(pathSegment, isFileName, fullPath) {
+    let anonymizedSegment = pathSegment;
+
+    if (
+        fullPath.includes("messages") &&
+        /^[a-zA-Z0-9]+_[_a-zA-Z0-9-]{9,12}$/.test(anonymizedSegment)
+    ) {
+        anonymizedSegment = "uniqueid_hash";
+    }
+
+    return anonymizedSegment;
+}
+
+function anonymiseJsonEntityPath(entity) {
+    const fileName = entity.filename;
+    const nameParts = fileName.split("/").slice(1);
+
+    const anonymisedParts = nameParts.map((each) =>
+        anonymisePathSegment(each, false, fileName)
+    );
+    return anonymisedParts.join("/");
+}
+
 const subAnalyses = [
     class {
         get title() {
@@ -179,49 +214,14 @@ const subAnalyses = [
             return true;
         }
 
-        async _relevantEntities(reader) {
-            const entries = await reader.getEntries();
-            const relevantEntries = entries.filter(
-                (each) =>
-                    !each.filename.includes(".DS_Store") &&
-                    !each.filename.includes("__MACOSX") &&
-                    !each.filename.includes("/files/") && // Remove user files
-                    each.filename.endsWith(".json")
-            );
-            return relevantEntries;
-        }
-
-        _anonymisePathSegment(pathSegment, isFileName, fullPath) {
-            let anonymizedSegment = pathSegment;
-
-            if (
-                fullPath.includes("messages") &&
-                /^[a-zA-Z0-9]+_[_a-zA-Z0-9-]{9,12}$/.test(anonymizedSegment)
-            ) {
-                anonymizedSegment = "uniqueid_hash";
-            }
-
-            return anonymizedSegment;
-        }
-
-        _anonymiseEntityPath(entity) {
-            const fileName = entity.filename;
-            const nameParts = fileName.split("/").slice(1);
-
-            const anonymisedParts = nameParts.map((each) =>
-                this._anonymisePathSegment(each, false, fileName)
-            );
-            return anonymisedParts.join("/");
-        }
-
         async parse({ reader }) {
             this._missingEntryNames = [];
             this.active = true;
             if (!reader) return;
 
-            const relevantEntries = await this._relevantEntities(reader);
+            const relevantEntries = await jsonDataEntities(reader);
             const anonymizedPaths = relevantEntries.map((each) =>
-                this._anonymiseEntityPath(each)
+                anonymiseJsonEntityPath(each)
             );
 
             this._unknownFiles = anonymizedPaths.filter(
@@ -233,6 +233,55 @@ const subAnalyses = [
         render() {
             return html`<ul>
                 ${this._unknownFiles.map((entry) => html`<li>${entry}</li>`)}
+            </ul>`;
+        }
+    },
+    class {
+        get title() {
+            return "Missing expected JSON files";
+        }
+
+        get isForDataReport() {
+            return true;
+        }
+
+        _knownJsonFiles() {
+            const knowsJsonFiles = allStructure.filter((each) =>
+                each.endsWith(".json")
+            );
+            return knowsJsonFiles.filter(
+                (each) =>
+                    !/^(posts|photos_and_videos)\/album\/[1-9][0-9]?.json$/.test(
+                        each
+                    ) &&
+                    !/^messages\/(inbox|legacy_threads|message_requests|filtered_threads|archived_threads)\/uniqueid_hash\/message_[2-9][0-9]?.json$/.test(
+                        each
+                    )
+            );
+        }
+
+        async parse({ reader }) {
+            this._expectedMissingFiles = [];
+            this.active = true;
+            if (!reader) return;
+
+            const relevantEntries = await jsonDataEntities(reader);
+            const anonymizedPaths = relevantEntries.map((each) =>
+                anonymiseJsonEntityPath(each)
+            );
+
+            const knowsJsonFiles = this._knownJsonFiles();
+            this._expectedMissingFiles = knowsJsonFiles.filter(
+                (each) => !anonymizedPaths.includes(each)
+            );
+            this.active = this._expectedMissingFiles.length > 0;
+        }
+
+        render() {
+            return html`<ul>
+                ${this._expectedMissingFiles.map(
+                    (entry) => html`<li>${entry}</li>`
+                )}
             </ul>`;
         }
     },
