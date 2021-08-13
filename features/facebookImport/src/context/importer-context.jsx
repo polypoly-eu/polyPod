2dfeb83960b39bb8fa6b581bbc45b22654f05601
rsto <57987795+RichardSto@users.noreply.github.com>
2021-08-13 03:53:00 +0200

    PROD4POD-690: Reactify facebookImporter UI (#196)
    * Add React to project and adapt rollup to it
    
    * Add basic context!
    
    * Add context and router
    
    * Fix rollup error and get first version running
    
    * Add linter and fix it
    
    * Make pod call async
    
    * Add overview view and routeButton component
    
    * Move views to add css in folder
    
    * Rearrange context logic, fix errors
    
    * Add i18n and configurations needed to add it
    
    * Enable podNav actions and navigationState
    
    * Remove unnecessary imports and fix linting
    
    * Store importStatus
    
    * Use importStatus to determine routing
    
    * Add basic styling and fonts
    
    * Restructure pod logic as pod is already a promise
    
    * Fix importStates and add importview components
    
    * Adapt import to new design
    
    * More import styling
    
    * Small import styling advancements
    
    * Delete old quads in triple store
    
    * Add intro and advanced importStatus logic
    
    * Add scrolling behaivor
    
    * Add infoBox
    
    * Add strong tag and styles
    
    * Add strings
    
    * Add request content
    
    * Add content to body
    
    * Activate buttons and add file adding
    
    * Add icons to progress bar
    
    * Smoothen the edges and add file deletion
    
    * Fix linter
    
    * Add storage handling and fix scrollbutton issue
    
    * Add overview and fix file deletion
    
    * Bring pod to top level
    
    * Add and style overview screen
    
    * Add statechanges to make usage smoother

---
+++ features/facebookImport/src/context/importer-context.jsx
@@ -0,0 +1,153 @@
+import React, { useEffect, useState } from "react";
+
+import Storage from "../model/storage.js";
+import i18n from "../i18n.js";
+import { useHistory } from "react-router-dom";
+
+export const ImporterContext = React.createContext();
+
+const pod = window.pod;
+
+//all nav-states for checking purposes
+const navigationStates = ["importStatus"];
+const importSteps = {
+    loading: "loading",
+    beginning: "beginning",
+    request: "request",
+    download: "download",
+    import: "import",
+    explore: "explore",
+    finished: "finished",
+};
+const namespace = "http://polypoly.coop/schema/fbImport/";
+
+function updatePodNavigation(pod, history) {
+    pod.polyNav.actions = {
+        back: () => history.goBack(),
+    };
+    history.length > 1
+        ? pod.polyNav.setActiveActions(["back"])
+        : pod.polyNav.setActiveActions([]);
+}
+
+function updateTitle(pod) {
+    pod.polyNav.setTitle(i18n.t(`common:title`));
+}
+
+//from storage
+async function readImportStatus() {
+    const statusQuads = await pod.polyIn.select({
+        subject: { value: `${namespace}facebookImporter` },
+        predicate: { value: `${namespace}importStatus` },
+    });
+    let status = statusQuads[0]?.object?.value?.split(namespace)[1];
+    return status || importSteps.beginning;
+}
+
+async function writeImportStatus(status) {
+    const { dataFactory, polyIn } = pod;
+    const existingQuad = (
+        await pod.polyIn.select({
+            subject: { value: `${namespace}facebookImporter` },
+            predicate: { value: `${namespace}importStatus` },
+        })
+    )[0];
+    polyIn.delete(existingQuad);
+    const quad = dataFactory.quad(
+        dataFactory.namedNode(`${namespace}facebookImporter`),
+        dataFactory.namedNode(`${namespace}importStatus`),
+        dataFactory.namedNode(`${namespace}${status}`)
+    );
+    polyIn.add(quad);
+}
+
+export const ImporterProvider = ({ children }) => {
+    //storage
+    const storage = new Storage(pod);
+    const [files, setFiles] = useState([]);
+    storage.changeListener = () => {
+        setFiles(Object.values(storage.files));
+    };
+
+    const [navigationState, setNavigationState] = useState({
+        importStatus: importSteps.loading,
+    });
+
+    const history = useHistory();
+
+    const handleRemoveFile = (fileID) => {
+        storage.removeFile({ id: fileID });
+    };
+
+    //change the navigationState like so: changeNavigationState({<changedState>:<changedState>})
+    function changeNavigationState(changedState) {
+        if (changedState) {
+            Object.keys(changedState)?.forEach((key) => {
+                if (!navigationStates.includes(key)) {
+                    console.log(`NavigationStateError with key: ${key}`);
+                    return;
+                }
+            });
+            setNavigationState({ ...navigationState, ...changedState });
+        }
+    }
+
+    function handleBack() {
+        if (history.length > 1) {
+            history.goBack();
+            if (history.location.state) {
+                changeNavigationState(history.location.state);
+            }
+        }
+    }
+
+    function refreshFiles() {
+        storage.refreshFiles().then(() => setFiles(storage.files));
+    }
+
+    function addFile(file) {
+        storage.addFile({ data: file.size, time: new Date(), id: 1 });
+    }
+
+    function updateImportStatus(newStatus) {
+        changeNavigationState({ importStatus: newStatus });
+        writeImportStatus(newStatus);
+    }
+
+    //on startup
+    useEffect(() => {
+        readImportStatus().then((status) => {
+            if (
+                status &&
+                !(navigationState.importStatus == importSteps.explore)
+            )
+                changeNavigationState({ importStatus: status });
+        });
+        refreshFiles();
+    }, []);
+
+    //on history change
+    useEffect(() => {
+        updatePodNavigation(pod, history);
+        updateTitle(pod);
+        console.log(files);
+    });
+
+    return (
+        <ImporterContext.Provider
+            value={{
+                pod,
+                files,
+                handleRemoveFile,
+                navigationState,
+                changeNavigationState,
+                handleBack,
+                importSteps,
+                updateImportStatus,
+                addFile,
+            }}
+        >
+            {children}
+        </ImporterContext.Provider>
+    );
+};
