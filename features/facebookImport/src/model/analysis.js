1640d79626e4a947ef77bb8626aba0c9ded16ea8
Andrei Chis <chisvasileandrei@gmail.com>
2021-08-19 14:40:13 +0200

    Add a "Report Metadata" analysis

--- features/facebookImport/src/model/analysis.js
+++ features/facebookImport/src/model/analysis.js
@@ -217,6 +217,53 @@ const subAnalyses = [
 
     class {
         get title() {
+            return "Report Metadata";
+        }
+
+        get id() {
+            return "report-metadata";
+        }
+
+        get isForDataReport() {
+            return true;
+        }
+
+        get jsonReport() {
+            return {
+                id: this.id,
+                metadata: {
+                    fileSize: this._size,
+                    filesCount: this._filesCount,
+                    preferedLanguage: this._preferedLanguage,
+                },
+            };
+        }
+
+        async parse({ size, zipFile }) {
+            this.active = true;
+            this._size = size;
+
+            const entries = await zipFile.getEntries();
+            this._filesCount = entries.length;
+
+            this._preferedLanguage = "todo";
+        }
+
+        render() {
+            return (
+                <ul>
+                    <li key={1}>{`File size: ${this._size}`}</li>
+                    <li key={2}>{`Files count: ${this._filesCount}`}</li>
+                    <li
+                        key={3}
+                    >{`Preferred language: ${this._preferedLanguage}`}</li>
+                </ul>
+            );
+        }
+    },
+
+    class {
+        get title() {
             return "NoData Folders";
         }
 
