90377f97e705aa649933cc5439d68b54bfd9d0c7
Adrian Ferreres <ardiadrianadri@gmail.com>
2021-08-19 13:01:25 +0200

    feat(prod4pod-657): wip bubble chart messenger first version

--- features/polyExplorer/src/screens/stories/exampleStory.jsx
+++ features/polyExplorer/src/screens/stories/exampleStory.jsx
@@ -1,26 +1,28 @@
-import React, { useState } from "react";
-import i18n from "../../i18n.js";
-import FakeTab from "../../components/clusterStories/fakeTab.jsx";
+import React, { useEffect, useRef } from "react";
 import DataStory from "../../components/dataStory/dataStory.jsx";
+import OneMessagerBubblesChart from "../../components/clusterStories/messengers/oneMessagerBubblesChart.jsx";
+import * as d3 from "d3";
 
 import "./exampleStory.css";
 
 const ExampleStory = () => {
-    const [overviewClickedTab, setOverviewClickedTab] = useState();
-
-    const onClickedOverviewTab = (id) => {
-        if (id === "1") {
-            setOverviewClickedTab("1");
-        }
-        if (id === "2") {
-            setOverviewClickedTab("2");
-        }
-        if (id === "3") {
-            setOverviewClickedTab("3");
-        }
-    };
-
     const fakeEvent = () => {};
+    const mockData = {
+        title: "Facebook Messenger",
+        color: "#FB8A89",
+        bubbles: [5, 5, 5, 5, 5, 5, 5, 5],
+    };
+    const svgCanvas = useRef();
+
+    useEffect(() => {
+        d3.select(svgCanvas.current)
+            .attr("xmlns", "http://www.w3.org/2000/svg")
+            .attr("xmlns:xlink", "http://www.w3.org/1999/xlink")
+            .attr("xmlns:xhtml", "http://www.w3.org/1999/xhtml")
+            .attr("viewBox", "0 0 300 300")
+            .style("width", "100%")
+            .style("height", "400px");
+    }, []);
 
     return (
         <DataStory
@@ -28,29 +30,14 @@ const ExampleStory = () => {
             className="example-story"
             scrollEvent={fakeEvent}
         >
-            <h3>Tab clicked: {overviewClickedTab}</h3>
-            <FakeTab onClickedTab={onClickedOverviewTab}>
-                <div
-                    label={i18n.t(
-                        "clusterMessengerStory:overview.tab.installs"
-                    )}
-                    tabId="1"
-                >
-                    1
-                </div>
-                <div
-                    label={i18n.t("clusterMessengerStory:overview.tab.users")}
-                    tabId="2"
-                >
-                    2
-                </div>
-                <div
-                    label={i18n.t("clusterMessengerStory:overview.tab.partof")}
-                    tabId="3"
-                >
-                    3
-                </div>
-            </FakeTab>
+            <svg ref={svgCanvas}>
+                <OneMessagerBubblesChart
+                    data={mockData}
+                    width={65}
+                    height={65}
+                    coord={[0, 0]}
+                ></OneMessagerBubblesChart>
+            </svg>
             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
             nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
             erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
