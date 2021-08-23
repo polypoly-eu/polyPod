37334cdfa1fccd5088f068e8150157059629ba73
Andrei Chis <chisvasileandrei@gmail.com>
2021-08-23 17:10:29 +0200

    Basic unexpected data check for off facebook events

--- features/facebookImport/src/importer/data-importers/off-facebook-events-importer.js
+++ features/facebookImport/src/importer/data-importers/off-facebook-events-importer.js
@@ -1,11 +1,38 @@
+import { IMPORT_WARNING } from "../importer-status.js";
 import { readJSONDataArray } from "../importer-util.js";
+
+const OffFacebookEventFields = ["id", "type", "timestamp"];
 
 export default class OffFacebookEventsImporter {
     async import({ zipFile }, facebookAccount) {
-        facebookAccount.offFacebookCompanies = await readJSONDataArray(
+        const rawData = await readJSONDataArray(
             "apps_and_websites_off_of_facebook/your_off-facebook_activity.json",
             "off_facebook_activity_v2",
             zipFile
         );
+
+        let uknonwnKeys = new Set();
+        // TODO: expand this check; The current one is just a toy example
+        for (const companyRawData of rawData) {
+            for (const eventRawData of companyRawData?.events) {
+                const unexpectedKeys = Object.keys(eventRawData).filter(
+                    (each) => !OffFacebookEventFields.includes(each)
+                );
+                for (const unexpectedKey of unexpectedKeys) {
+                    uknonwnKeys.add(unexpectedKey);
+                }
+            }
+        }
+
+        facebookAccount.offFacebookCompanies = rawData;
+        if (uknonwnKeys.size > 0) {
+            return {
+                status: IMPORT_WARNING,
+                message: `Unexpected keys: ${Array.from(uknonwnKeys).join(
+                    " "
+                )}`,
+                data: { uknonwnKeys },
+            };
+        }
     }
 }
--- features/facebookImport/src/importer/importer-status.js
+++ features/facebookImport/src/importer/importer-status.js
@@ -1,2 +1,3 @@
 export const IMPORT_SUCCESS = "success";
 export const IMPORT_ERROR = "error";
+export const IMPORT_WARNING = "warning";
