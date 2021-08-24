8ceff307eab7838734f4c71809fa7b64e3442175
Oleksandr Paraska <shoniko@gmail.com>
2021-08-24 13:24:33 +0300

    Fix iOS polyNav test

--- ios/PolyPodApp/PolyPodTests/PolyNavTests.swift
+++ ios/PolyPodApp/PolyPodTests/PolyNavTests.swift
@@ -1,12 +1,13 @@
 import XCTest
+import Zip
 
 private class PolyNavDelegateStub: PolyNavDelegate {
-    var importFileResult: String?
+    var importFileResult: URL?
     func doHandleOpenUrl(url: String) {
     }
     
     func doHandleImportFile(completion: @escaping (URL?) -> Void) {
-        completion(URL.init(fileURLWithPath: "/test/file"))
+        completion(importFileResult)
     }
     
     func doHandleSetTitle(title: String) {
@@ -15,21 +16,52 @@ private class PolyNavDelegateStub: PolyNavDelegate {
     func doHandleSetActiveActions(actions: [String]) {
     }
 }
+private let testFolderPath = FileManager.default.temporaryDirectory
+    .appendingPathComponent("tests")
+private let testFilePath = testFolderPath
+    .appendingPathComponent("testFile.json")
+private let testZipFilePath = testFolderPath.appendingPathComponent("testFile.zip")
+
+private func removeTestFile() {
+    let fileManager = FileManager.default
+    if fileManager.fileExists(atPath: testFolderPath.path) {
+        try! fileManager.removeItem(at: testFolderPath)
+    }
+}
+
+private func createTestFile() {
+    try! FileManager.default.createDirectory(at: testFolderPath, withIntermediateDirectories: true, attributes: nil)
+    try! "".write(to: testFilePath, atomically: true, encoding: .utf8)
+    let zipFilePath = try! Zip.quickZipFiles([testFilePath], fileName: "testFile")
+    do {
+        try FileManager.default.moveItem(at: zipFilePath, to: testZipFilePath)
+    }
+    catch (_) {
+    
+    }
+}
 
 class PolyNavTests: XCTestCase {
     let polyNav = PolyNav()
     
+    override class func setUp() {
+        createTestFile()
+    }
+    
+    override class func tearDown() {
+        removeTestFile()
+    }
+    
     func testImportFileReturnsFileSelectedByUser() {
-        let testData = "/test/file"
         let delegateStub = PolyNavDelegateStub()
-        delegateStub.importFileResult = testData
+        delegateStub.importFileResult = testZipFilePath
         polyNav.delegate = delegateStub
         // TODO: Instead of nil, the result is supposed to be testData,
         //       make that work again. Commented it out for now because I
         //       assume this was caused by an API change I don't fully
         //       understand yet.
-        expectImportFileResult(nil)
-        //expectImportFileResult(testData)
+        //expectImportFileResult(nil)
+        expectImportFileResult("polyPod://")
     }
     
     func testImportFileReturnsNullIfUserCancelled() {
@@ -43,7 +75,14 @@ class PolyNavTests: XCTestCase {
         let expectation = XCTestExpectation()
         polyNav.importFile() { actual in
             expectation.fulfill()
-            XCTAssertEqual(expected, actual)
+            if expected == nil {
+                XCTAssertEqual(expected, actual)
+            }
+            else if let actual = actual {
+                let start = actual.startIndex
+                let end = actual.index(start, offsetBy: expected!.count)
+                XCTAssertEqual(expected, String(actual[start..<end]))
+            }
         }
         wait(for: [expectation], timeout: 10)
     }
