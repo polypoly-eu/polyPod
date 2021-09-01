fbe6a1d03727b52f297243ab045f50ab0cd4bd13
mteresa-jimenez <mteresa.jn@gmail.com>
2021-09-01 12:21:55 +0200

    Comment label fix

--- features/facebookImport/src/components/dataViz/dataBubblesAll.jsx
+++ features/facebookImport/src/components/dataViz/dataBubblesAll.jsx
@@ -26,6 +26,28 @@ const DataTypeBubbleAll = ({
 
         showValues
             ? leaf
+                  //   .append("foreignObject")
+                  //   .attr("width", (d) => d.r * 2)
+                  //   .attr("height", (d) => d.r * 2)
+                  //   .attr("x", 0)
+                  //   .attr("y", ".3em")
+                  //   .attr("transform", (d) => {
+                  //       return "translate( " + (d.r * -1) + "," + (d.r * -1) + ")";
+                  //   })
+                  //   .style("display", "flex")
+                  //   .style("justify-content", "center")
+                  //   .style("align-items", "center")
+                  //   .append("xhtml:div")
+                  //   .attr("width", (d) => d.r * 2)
+                  //   .attr("height", (d) => d.r * 2)
+                  //   .attr("x", 0)
+                  //   .attr("y", (d) => d.r)
+                  //   .style("display", "flex")
+                  //   .style("flex-direction", "column")
+                  //   .style("justify-content", "center")
+                  //   .style("align-items", "center")
+                  //   .append("p")
+                  //  .style("color", textColor)
                   .append("text")
                   .text((d) => {
                       return d.value > 5 ? d.data.title : "";
