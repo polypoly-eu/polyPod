24946e8501cf82c48c0aa725ea68bf6b55960e5d
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2021-09-01 13:04:12 +0200

    The problem is now it's not really testing this

--- core/api/pod-api/src/tests/default.test.ts
+++ core/api/pod-api/src/tests/default.test.ts
@@ -3,7 +3,6 @@ import { DefaultPod } from "../default";
 import { dataset } from "@rdfjs/dataset";
 import fetch from "node-fetch";
 import { Volume } from "memfs";
-import { Pod } from "../api";
 import { getHttpbinUrl } from "@polypoly-eu/fetch-spec";
 
 describe("Mock pod", () => {
