afade97ad9cf5a613a6ccf99ccbf57f6d5f402ce
JJ Merelo <jjmerelo@gmail.com>
2021-09-03 13:13:31 +0200

    :rotating_light:

--- features/facebookImport/src/importer/structure-checkers/check-not-html.js
+++ features/facebookImport/src/importer/structure-checkers/check-not-html.js
@@ -1,11 +1,10 @@
-function checkNotHTML( zipFile ) {
+function checkNotHTML(zipFile) {
     const profileHTMLfile = zipFile.getEntries.filter(
         (f) => f === "profile_information/profile_information.html"
     );
-    if ( profileHTMLfile.length >= 1 ) {
-        return "This zip file contains HTML, not JSON files"
+    if (profileHTMLfile.length >= 1) {
+        return "This zip file contains HTML, not JSON files";
     } else {
-        return ""
+        return "";
     }
 }
-
