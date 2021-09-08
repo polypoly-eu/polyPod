7c405a464194fb9f9303346541861bf766525f3f
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2021-09-08 08:28:14 +0200

    Adds script for basic generation of a zip test file
    This only generates *the structure*, based on the common folders found. It does
    not create any kind of reasonable content. However, it's a start and it does
    work with the files that summarize the Facebook file structure
    
    (the change to .gitignore in another folder just slipped by)

---
+++ dev-utils/fb-zip-file/scripts/generate-test-file.js
@@ -0,0 +1,25 @@
+import JSZip from "jszip";
+import fs from "fs";
+
+import { commonStructure, noDataFileName } from "../src/globals.js";
+
+let zipFile = new JSZip();
+const dataFileName = "fi-test.zip";
+let structure = commonStructure();
+
+for (let key in structure) {
+    if (structure[key] === []) {
+        zipFile.file(`${key}/${noDataFileName}`, "\n");
+    } else {
+        structure[key].forEach((element) => {
+            zipFile.file(`${key}/${element}`, "[ 'foo' ]\n");
+        });
+    }
+}
+
+zipFile
+    .generateNodeStream({ type: "nodebuffer", streamFiles: true })
+    .pipe(fs.createWriteStream(dataFileName))
+    .on("finish", function () {
+        console.log(`${dataFileName} written.`);
+    });
