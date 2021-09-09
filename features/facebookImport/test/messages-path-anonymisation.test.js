c270bdfff143c689371a3dd2bab5a3d88e102bf3
Andrei Chis <chisvasileandrei@gmail.com>
2021-09-09 13:36:19 +0200

    Add more tests

--- features/facebookImport/test/messages-path-anonymisation.test.js
+++ features/facebookImport/test/messages-path-anonymisation.test.js
@@ -25,6 +25,22 @@ test("anonymize inbox message", () => {
     expect(anonymizeJsonEntityPath(fileName)).toBe(expectedAnonymized);
 });
 
+test("anonymize message requests message", () => {
+    const fileName =
+        "facebook-username/messages/message_requests/johndoes_sk3rnqkbmg/message_1.json";
+    const expectedAnonymized =
+        "messages/message_requests/uniqueid_hash/message_1.json";
+    expect(anonymizeJsonEntityPath(fileName)).toBe(expectedAnonymized);
+});
+
+test("anonymize legacy threads message", () => {
+    const fileName =
+        "facebook-username/messages/legacy_threads/johndoes_sk3rnqkbmg/message_1.json";
+    const expectedAnonymized =
+        "messages/legacy_threads/uniqueid_hash/message_1.json";
+    expect(anonymizeJsonEntityPath(fileName)).toBe(expectedAnonymized);
+});
+
 test("anonymize second messages file", () => {
     const fileName =
         "facebook-username/messages/inbox/johndoes_sk3rnqkbmg/message_2.json";
@@ -43,5 +59,13 @@ test("anonymize with missing name message", () => {
     const fileName =
         "facebook-username/messages/inbox/jhdgfylq-w/message_1.json";
     const expectedAnonymized = "messages/inbox/uniqueid_hash/message_1.json";
+    expect(anonymizeJsonEntityPath(fileName)).toBe(expectedAnonymized);
+});
+
+test("anonymize unknown message folder", () => {
+    const fileName =
+        "facebook-username/messages/unknown_messages/johndoes_sk3rnqkbmg/message_1.json";
+    const expectedAnonymized =
+        "messages/unknown_messages/uniqueid_hash/message_1.json";
     expect(anonymizeJsonEntityPath(fileName)).toBe(expectedAnonymized);
 });
