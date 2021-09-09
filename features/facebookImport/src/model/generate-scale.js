2b60391a49eef224a51ec88d7049a836a31b65ba
Richard Stoffels <richard.stoffels@polypoly.net>
2021-09-09 17:51:51 +0200

    Add generate scale algorithm

--- features/facebookImport/src/model/generate-scale.js
+++ features/facebookImport/src/model/generate-scale.js
@@ -1,12 +1,40 @@
-/*
-    A function to generate a nice looking scale from any number
-    Returns an array (scale)
-*/
+const stepBasis = [
+    0,
+    0.2,
+    0.5,
+    "next",
+    1,
+    1,
+    1,
+    "next",
+    2,
+    "nextPow",
+    "nextPow",
+];
 
-function generateScale(aNumber) {
-    const log10 = Math.log10(aNumber);
-    const decimalNumber = aNumber / Math.pow(10, log10 - 1);
-    console.log(decimalNumber);
+/**
+ * Generate a scale with decent step number values from any number
+ * Limted to 10 decimal places and only positive numbers
+ * @param {Number} aNumber - Any number
+ * @returns {Number[]} - Scale where this number fits in
+ */
+export default function generateScale(aNumber) {
+    let log10 = Math.floor(Math.log10(aNumber));
+    const decimalNumber = aNumber / Math.pow(10, log10);
+    let nextHigherInteger = Math.ceil(decimalNumber);
+    if (stepBasis[nextHigherInteger] == "next") nextHigherInteger++;
+    if (stepBasis[nextHigherInteger] == "nextPow") {
+        nextHigherInteger = 1;
+        log10++;
+    }
+    const scale = [];
+    for (
+        let step = 0;
+        step <= nextHigherInteger;
+        step += stepBasis[nextHigherInteger]
+    ) {
+        scale.push(Math.round(step * 10) / 10);
+    }
+    if (decimalNumber < scale[scale.length - 2]) scale.pop();
+    return scale.map((val) => val * Math.pow(10, log10));
 }
-
-generateScale();
