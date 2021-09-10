9bb36a67116f14ade040bb19593b9a8a1d5a9483
Richard Stoffels <richard.stoffels@polypoly.net>
2021-09-10 10:58:41 +0200

    Fix decimal computation problems

--- features/facebookImport/src/model/generate-scale.js
+++ features/facebookImport/src/model/generate-scale.js
@@ -2,13 +2,13 @@ const stepBasis = [0, 0.2, 0.5, "next", 1, 1, 1, "next", 2, "nextPow"];
 
 /**
  * Generate a scale with decent step number values from any number
- * Limted to positive numbers
+ * Limited to positive numbers
  * @param {Number} aNumber - Any number
  * @returns {Number[]} - Scale where this number fits in
  */
 export default function generateScale(aNumber) {
     let log10 = Math.floor(Math.log10(aNumber));
-    const decimalNumber = aNumber / Math.pow(10, log10);
+    const decimalNumber = +(aNumber / Math.pow(10, log10)).toPrecision(10);
     let nextHigherInteger = Math.ceil(decimalNumber);
     if (stepBasis[nextHigherInteger] == "next") nextHigherInteger++;
     if (stepBasis[nextHigherInteger] == "nextPow") {
@@ -24,5 +24,10 @@ export default function generateScale(aNumber) {
         scale.push(Math.round(step * 10) / 10);
     }
     if (decimalNumber < scale[scale.length - 2]) scale.pop();
-    return scale.map((val) => val * Math.pow(10, log10));
+    return log10 == 0
+        ? scale
+        : scale.map(
+              (val) =>
+                  +(val * Math.pow(10, log10)).toPrecision(Math.abs(log10 + 1))
+          );
 }
