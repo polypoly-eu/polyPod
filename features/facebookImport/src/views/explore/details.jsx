42f1a8835e19d4c287175c1278d2a270704d19e3
mteresa-jimenez <mteresa.jn@gmail.com>
2021-09-10 12:41:14 +0200

    Add styles to Details screen

--- features/facebookImport/src/views/explore/details.jsx
+++ features/facebookImport/src/views/explore/details.jsx
@@ -1,11 +1,13 @@
 import React, { useContext } from "react";
 import { ImporterContext } from "../../context/importer-context.jsx";
 
+import "./details.css";
+
 const ExploreDetails = () => {
     const { activeDetails } = useContext(ImporterContext);
     return (
-        <div>
-            <h1>{activeDetails.title}</h1>
+        <div className="details-view">
+            <h1 className="details-view-title">{activeDetails.title}</h1>
             {activeDetails.renderDetails()}
         </div>
     );
