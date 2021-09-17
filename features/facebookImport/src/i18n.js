e40e57c62cb2e4ae76bb802a3f4bfcd11956dbd5
Richard Stoffels <richard.stoffels@polypoly.net>
2021-09-17 12:14:33 +0200

    Adapt i18n to that

--- features/facebookImport/src/i18n.js
+++ features/facebookImport/src/i18n.js
@@ -4,11 +4,13 @@ import commonEn from "./locales/en/common.json";
 import importEn from "./locales/en/import.json";
 import overviewEn from "./locales/en/overview.json";
 import exploreEn from "./locales/en/explore.json";
+import activitiesMinistoryEn from "./locales/en/ministories/activities.json";
 
 import commonDe from "./locales/de/common.json";
 import importDe from "./locales/de/import.json";
 import overviewDe from "./locales/de/overview.json";
 import exploreDe from "./locales/de/explore.json";
+import activitiesMinistoryDe from "./locales/de/ministories/activities.json";
 
 export default new I18n(determineLanguage(), {
     en: {
@@ -16,11 +18,13 @@ export default new I18n(determineLanguage(), {
         import: importEn,
         overview: overviewEn,
         explore: exploreEn,
+        activitiesMinistory: activitiesMinistoryEn,
     },
     de: {
         common: commonDe,
         import: importDe,
         overview: overviewDe,
         explore: exploreDe,
+        activitiesMinistory: activitiesMinistoryDe,
     },
 });
