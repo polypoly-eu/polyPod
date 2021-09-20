b519d270b3f979fc9d10497ebad3b743a92d35ba
Richard Stoffels <richard.stoffels@polypoly.net>
2021-09-20 17:12:45 +0200

    Add off facebook ministory

--- features/facebookImport/src/i18n.js
+++ features/facebookImport/src/i18n.js
@@ -6,6 +6,8 @@ import overviewEn from "./locales/en/overview.json";
 import exploreEn from "./locales/en/explore.json";
 import activitiesMiniStoryEn from "./locales/en/miniStories/activities.json";
 import messagesMiniStoryEn from "./locales/en/miniStories/messages.json";
+import advertisingValueMiniStoryEn from "./locales/en/miniStories/advertisingValue.json";
+import OffFacebookEventsMiniStoryEn from "./locales/en/miniStories/offFacebookEvents.json";
 import reportEn from "./locales/en/report.json";
 
 import commonDe from "./locales/de/common.json";
@@ -14,6 +16,8 @@ import overviewDe from "./locales/de/overview.json";
 import exploreDe from "./locales/de/explore.json";
 import activitiesMiniStoryDe from "./locales/de/miniStories/activities.json";
 import messagesMiniStoryDe from "./locales/de/miniStories/messages.json";
+import advertisingValueMiniStoryDe from "./locales/de/miniStories/advertisingValue.json";
+import OffFacebookEventsMiniStoryDe from "./locales/de/miniStories/offFacebookEvents.json";
 import reportDe from "./locales/de/report.json";
 
 export default new I18n(determineLanguage(), {
@@ -24,6 +28,8 @@ export default new I18n(determineLanguage(), {
         explore: exploreEn,
         activitiesMiniStory: activitiesMiniStoryEn,
         messagesMiniStory: messagesMiniStoryEn,
+        advertisingValueMiniStory: advertisingValueMiniStoryEn,
+        offFacebookEventsMiniStory: OffFacebookEventsMiniStoryEn,
         report: reportEn,
     },
     de: {
@@ -33,6 +39,8 @@ export default new I18n(determineLanguage(), {
         explore: exploreDe,
         activitiesMiniStory: activitiesMiniStoryDe,
         messagesMiniStory: messagesMiniStoryDe,
+        advertisingValueMiniStory: advertisingValueMiniStoryDe,
+        offFacebookEventsMiniStory: OffFacebookEventsMiniStoryDe,
         report: reportDe,
     },
 });
--- features/facebookImport/src/model/analyses/off-facebook-events-analysis.js
+++ features/facebookImport/src/model/analyses/off-facebook-events-analysis.js
@@ -1,17 +1,39 @@
 import RootAnalysis from "./root-analysis.js";
+import i18n from "../../i18n.js";
+import { groupOffFacebookEventsByType } from "./utils/off-facebook-events-utils.js";
+
+import OffFacebookMiniStory from "../../components/offFacebookMinistory/offFacebookMinistory.jsx";
 
 export default class OffFacebookEventsAnalysis extends RootAnalysis {
     get title() {
-        return "Off-Facebook Activity";
+        return i18n.t("offFacebookEventsMiniStory:title");
     }
 
     async analyze({ facebookAccount }) {
-        this._companiesCount = facebookAccount.offFacebookCompaniesCount;
-        this._eventsCount = facebookAccount.offFacebookEventsCount;
-        this.active = this._companiesCount > 0;
+        console.log(facebookAccount);
+        this._companiesCount = facebookAccount.offFacebookCompanies.length;
+        try {
+            this._eventsCount = facebookAccount.offFacebookEventsCount;
+            this.active = this._companiesCount > 0;
+            this._purchasesCount = 0;
+            if (!this.active) {
+                return;
+            }
+
+            this._purchasesCount = groupOffFacebookEventsByType(
+                facebookAccount
+            ).find((e) => e.type == "PURCHASE")?.count;
+        } catch (err) {
+            console.log(err);
+        }
     }
 
     renderSummary() {
-        return `There are ${this._eventsCount} events from ${this._companiesCount} businesses and organizations you visit off of Facebook`;
+        return (
+            <OffFacebookMiniStory
+                companiesCount={this._companiesCount}
+                purchasesCount={this._purchasesCount}
+            />
+        );
     }
 }
