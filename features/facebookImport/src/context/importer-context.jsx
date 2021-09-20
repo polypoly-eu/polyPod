fbc6e5a7b1d666b957946bdff63c0412f2079433
Felix Dahlke <felix.dahlke@polypoly.net>
2021-09-20 16:15:30 +0200

    PROD4POD-779 - Move success message to explore screen

--- features/facebookImport/src/context/importer-context.jsx
+++ features/facebookImport/src/context/importer-context.jsx
@@ -95,6 +95,7 @@ export const ImporterProvider = ({ children }) => {
     const [fileAnalysis, setFileAnalysis] = useState(null);
     const [activeDetails, setActiveDetails] = useState(null);
     const [globalError, setGlobalError] = useState(null);
+    const [reportResult, setReportResult] = useState(null);
 
     const [navigationState, setNavigationState] = useState({
         importStatus: importSteps.loading,
@@ -235,6 +236,8 @@ export const ImporterProvider = ({ children }) => {
                 globalError,
                 setGlobalError,
                 facebookAccount,
+                reportResult,
+                setReportResult,
             }}
         >
             {children}
--- features/facebookImport/src/views/explore/explore.jsx
+++ features/facebookImport/src/views/explore/explore.jsx
@@ -2,7 +2,20 @@ import React, { useContext, useEffect, useRef, useState } from "react";
 import RouteButton from "../../components/buttons/routeButton.jsx";
 import { ImporterContext } from "../../context/importer-context.jsx";
 
+import i18n from "../../i18n.js";
+
 import "./explore.css";
+
+const PopUpMessage = ({ children, handleClosePopUp }) => {
+    return (
+        <div className="pop-up">
+            <div className="pop-up-message">{children}</div>
+            <div className="close-icon" onClick={handleClosePopUp}>
+                x
+            </div>
+        </div>
+    );
+};
 
 const AnalysisCard = ({
     analysis,
@@ -43,13 +56,35 @@ const UnrecognizedCard = ({ unrecognizedData }) => {
 };
 
 const ExploreView = () => {
-    const { navigationState, fileAnalysis, setActiveDetails } =
-        useContext(ImporterContext);
+    const {
+        navigationState,
+        fileAnalysis,
+        setActiveDetails,
+        reportResult,
+        setReportResult,
+    } = useContext(ImporterContext);
 
     const [scrollingProgress, setScrollingProgress] = useState(
         navigationState.exploreScrollingProgress
     );
     const exploreRef = useRef();
+
+    const handleCloseReportResult = () => {
+        setReportResult(null);
+    };
+
+    const renderReportResult = () =>
+        reportResult !== null && (
+            <PopUpMessage handleClosePopUp={handleCloseReportResult}>
+                {reportResult ? (
+                    i18n.t("explore:report.success")
+                ) : (
+                    <span className="unsuccessfully">
+                        {i18n.t("explore:report.error")}
+                    </span>
+                )}
+            </PopUpMessage>
+        );
 
     const renderFileAnalyses = () => {
         if (!fileAnalysis) {
@@ -96,6 +131,7 @@ const ExploreView = () => {
             onScroll={saveScrollingProgress}
         >
             <h1 className="explore-view-title">Explore your data</h1>
+            {renderReportResult()}
             {renderFileAnalyses()}
         </div>
     );
