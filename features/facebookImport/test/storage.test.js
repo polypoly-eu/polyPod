f070eca6f8417fe6ae692d98ec70ab361620ec1f
JJ Merelo <jjmerelo@gmail.com>
2021-09-20 11:02:54 +0200

    [skip ci] Checking in to move elsewhere

--- features/facebookImport/test/storage.test.js
+++ features/facebookImport/test/storage.test.js
@@ -1,38 +1,18 @@
-import JSZip from "jszip";
-import { createWriteStream, readFileSync } from "fs";
+import "@polypoly-eu/podjs";
 
 import Storage from "../src/model/storage.js";
 import { expect } from "@jest/globals";
 
-const noDataFileName = "no-data.txt";
-const dataFileName = "src/static/commonStructure.json";
-const zipFileName = `/tmp/test-${process.pid}.zip`;
-let testStream;
 let storage;
+let pod;
 
 beforeAll((done) => {
-    const zipFile = new JSZip();
-    const structure = JSON.parse(readFileSync(dataFileName));
-
-    for (let key in structure) {
-        if (structure[key] === []) {
-            zipFile.file(`${key}/${noDataFileName}`, "\n");
-        } else {
-            structure[key].forEach((element) => {
-                zipFile.file(`${key}/${element}`, "[ 'foo' ]\n");
-            });
-        }
-    }
-    testStream = zipFile
-        .generateNodeStream({ type: "nodebuffer" })
-        .pipe(createWriteStream(zipFileName))
-        .on("finish", () => {
-            done();
-        });
+    pod = await window.pod;
+    storage = new Storage( pod );
 });
 
 describe("Tests file storage", () => {
     it("Adds and removes a file correctly", (done) => {
-        console.log("Fake test");
+        expect(storage).toBeDefined();
     });
 });
