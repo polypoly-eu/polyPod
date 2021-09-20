fcd0e9cfe8777ab5d3126d3f7e3ddbb0c8fa1fb8
JJ Merelo <jjmerelo@gmail.com>
2021-09-20 08:01:20 +0200

    First structure checker

---
+++ features/facebookImport/src/importer/structure-checkers/facebook-checks.js
@@ -0,0 +1,17 @@
+export default class FacebookChecks {
+    constructor(filesInZip) {
+        this._entries = filesInZip;
+        this._files = filesInZip.map((f) => f.filename);
+    }
+
+    get files() {
+        return Object.values(this._files);
+    }
+
+    isItHTMLExport() {
+        const profileHTMLfile = this._files.filter(
+            (f) => f === "profile_information/profile_information.html"
+        );
+        return profileHTMLfile.length >= 1;
+    }
+}
