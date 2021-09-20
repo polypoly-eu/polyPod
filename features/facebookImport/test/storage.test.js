3e3b3106c5e2886d4b8cd1e605f0946e450f1082
JJ Merelo <jjmerelo@gmail.com>
2021-09-20 11:41:04 +0200

    :white_check_mark: for very initial tests

--- features/facebookImport/test/storage.test.js
+++ features/facebookImport/test/storage.test.js
@@ -1,18 +1,22 @@
-import "@polypoly-eu/podjs";
-
+import { DefaultPod } from "@polypoly-eu/pod-api";
+import { dataset } from "@rdfjs/dataset";
+import fetch from "node-fetch";
+import { Volume } from "memfs";
 import Storage from "../src/model/storage.js";
 import { expect } from "@jest/globals";
 
 let storage;
 let pod;
 
-beforeAll((done) => {
-    pod = await window.pod;
+beforeAll(() => {
+    const aDataset = dataset();
+    const volume = new Volume().promises;
+    pod = new DefaultPod(aDataset, volume, fetch);
     storage = new Storage( pod );
 });
 
 describe("Tests file storage", () => {
-    it("Adds and removes a file correctly", (done) => {
-        expect(storage).toBeDefined();
+    it("Adds and removes a file correctly", () => {
+        expect(pod).toBeDefined();
     });
 });
