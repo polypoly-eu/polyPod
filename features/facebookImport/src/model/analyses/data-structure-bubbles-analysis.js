1c22504c045bd823ff417bcd6360502f4dccf6b4
mteresa-jimenez <mteresa.jn@gmail.com>
2021-09-21 16:34:03 +0200

    Delete unused analysis code

--- features/facebookImport/src/model/analyses/data-structure-bubbles-analysis.js
+++ features/facebookImport/src/model/analyses/data-structure-bubbles-analysis.js
@@ -8,25 +8,10 @@ export default class DataStructureBubblesAnalysis extends RootAnalysis {
         return i18n.t("dataStructureMiniStory:title");
     }
 
-    _computeScaleFactor(bubblesData) {
-        let maximumSize = Math.max(...bubblesData.map(({ count }) => count));
-        if (maximumSize < 100) maximumSize = 100;
-        let currentFactor = 1;
-        while (!(maximumSize / currentFactor < 100)) {
-            currentFactor = currentFactor + 10;
-        }
-        return currentFactor;
-    }
-
     async analyze({ facebookAccount }) {
         this._bubblesData = facebookAccount.dataGroups.filter(
             ({ count }) => count > 0
         );
-
-        const scaleFactor = this._computeScaleFactor(this._bubblesData);
-        this._bubblesDataScaled = this._bubblesData.map(({ count, title }) => {
-            return { title, count: count / scaleFactor };
-        });
 
         this.active = this._bubblesData.length > 0;
     }
