4ceb36805701658ee748a59d40c8f2bf0c650833
rsto <57987795+RichardSto@users.noreply.github.com>
2021-09-21 09:29:35 +0200

    Prod4 pod 803/fb knows (#297)
    * Add off facebook ministory
    
    * Add react to scope
    
    * Make it bold
    
    * Remove testing stuff
    
    * Remove eventsCount as not needed
    
    * Make purchases default to 0
    
    * Remove merge mistakes and fix typo
    
    * Fix path typo
    
    * Simplify puchasesCount

--- features/facebookImport/src/i18n.js
+++ features/facebookImport/src/i18n.js
@@ -6,6 +6,7 @@ import overviewEn from "./locales/en/overview.json";
 import exploreEn from "./locales/en/explore.json";
 import activitiesMiniStoryEn from "./locales/en/miniStories/activities.json";
 import messagesMiniStoryEn from "./locales/en/miniStories/messages.json";
+import offFacebookEventsMiniStoryEn from "./locales/en/miniStories/offFacebookEvents.json";
 import advertisingValueMiniStoryEn from "./locales/en/miniStories/advertisingValue.json";
 import reportEn from "./locales/en/report.json";
 
@@ -15,6 +16,7 @@ import overviewDe from "./locales/de/overview.json";
 import exploreDe from "./locales/de/explore.json";
 import activitiesMiniStoryDe from "./locales/de/miniStories/activities.json";
 import messagesMiniStoryDe from "./locales/de/miniStories/messages.json";
+import offFacebookEventsMiniStoryDe from "./locales/de/miniStories/offFacebookEvents.json";
 import advertisingValueMiniStoryDe from "./locales/de/miniStories/advertisingValue.json";
 import reportDe from "./locales/de/report.json";
 
@@ -26,6 +28,7 @@ export default new I18n(determineLanguage(), {
         explore: exploreEn,
         activitiesMiniStory: activitiesMiniStoryEn,
         messagesMiniStory: messagesMiniStoryEn,
+        offFacebookEventsMiniStory: offFacebookEventsMiniStoryEn,
         advertisingValueMiniStory: advertisingValueMiniStoryEn,
         report: reportEn,
     },
@@ -36,6 +39,7 @@ export default new I18n(determineLanguage(), {
         explore: exploreDe,
         activitiesMiniStory: activitiesMiniStoryDe,
         messagesMiniStory: messagesMiniStoryDe,
+        offFacebookEventsMiniStory: offFacebookEventsMiniStoryDe,
         advertisingValueMiniStory: advertisingValueMiniStoryDe,
         report: reportDe,
     },
