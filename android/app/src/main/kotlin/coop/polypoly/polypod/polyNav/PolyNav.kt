37de82686841c75d600153dd51ee4e5906b0854b
Oleksandr Paraska <oleksandr@shoniko.com>
2021-09-27 15:51:01 +0200

    Fix Android black screen when importing (#325)
    * Unzip in a coroutine
    
    * Remove size from import dialog
    
    * Change the import error message
    
    * Suspend instead of starting a new coroutine
    
    * Bring back the imported size

--- android/app/src/main/kotlin/coop/polypoly/polypod/polyNav/PolyNav.kt
+++ android/app/src/main/kotlin/coop/polypoly/polypod/polyNav/PolyNav.kt
@@ -8,9 +8,12 @@ import android.webkit.WebMessage
 import android.webkit.WebView
 import coop.polypoly.polypod.Preferences
 import coop.polypoly.polypod.polyOut.PolyOut
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.async
 import java.io.File
 import java.util.UUID
 import kotlin.collections.HashSet
+import kotlin.coroutines.EmptyCoroutineContext
 
 open class PolyNav(
     private val webView: WebView,
@@ -19,6 +22,7 @@ open class PolyNav(
 ) {
     private val registeredActions = HashSet<String>()
     private val fsPrefix = "polypod://"
+    private val coroutineScope = CoroutineScope(EmptyCoroutineContext)
 
     open fun setActiveActions(actions: Array<String>) {
         registeredActions.clear()
@@ -66,16 +70,18 @@ open class PolyNav(
                     )
             }
         }
-        contentResolver?.openInputStream(importedUrl).use { inputStream ->
-            if (inputStream == null) {
-                throw Error("File copy error")
+        coroutineScope.async {
+            contentResolver?.openInputStream(importedUrl).use { inputStream ->
+                if (inputStream == null) {
+                    throw Error("File import error")
+                }
+                val newId = UUID.randomUUID().toString()
+                val fs = Preferences.getFileSystem(context).toMutableMap()
+                fs[fsPrefix + newId] = fileName
+                Preferences.setFileSystem(context, fs)
+                ZipTools.unzipAndEncrypt(inputStream, context, newId)
             }
-            val newId = UUID.randomUUID().toString()
-            ZipTools.unzipAndEncrypt(inputStream, context, newId)
-            val fs = Preferences.getFileSystem(context).toMutableMap()
-            fs[fsPrefix + newId] = fileName
-            Preferences.setFileSystem(context, fs)
-        }
+        }.await()
         return importedUrl
     }
 
