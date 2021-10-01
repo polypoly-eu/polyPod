e4f39b09d6f4a4fa1340759defd1134b49dbbefc
mteresa-jimenez <mteresa.jn@gmail.com>
2021-10-01 12:37:50 +0200

    PROD4POD-866/Create InfoButton component

---
+++ features/facebookImport/src/components/buttons/infoButton/infoButton.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import RouteButton from "../routeButton.jsx";
+import "./infoButton.css";
+
+const InfoButton = ({
+    // route,
+    // saveActiveIndex,
+    stateChange = null,
+}) => (
+    <div
+    // onClick={saveActiveIndex}
+    >
+        <RouteButton
+            className="info-button"
+            // route={route}
+            stateChange={stateChange}
+        >
+            <img src="./images/question.svg"></img>
+        </RouteButton>
+    </div>
+);
+
+export default InfoButton;
