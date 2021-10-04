fa224f9f462a43b5b88fb556e6b706a90432757a
mteresa-jimenez <mteresa.jn@gmail.com>
2021-10-04 15:58:28 +0200

    Delete commented out code

--- features/facebookImport/src/components/buttons/infoButton/infoButton.jsx
+++ features/facebookImport/src/components/buttons/infoButton/infoButton.jsx
@@ -2,22 +2,14 @@ import React from "react";
 import RouteButton from "../routeButton.jsx";
 import "./infoButton.css";
 
-const InfoButton = ({
-    route,
-    // saveActiveIndex,
-    stateChange = null,
-}) => (
-    <div
-    // onClick={saveActiveIndex}
+const InfoButton = ({ route, stateChange = null }) => (
+    <RouteButton
+        className="info-button"
+        route={route}
+        stateChange={stateChange}
     >
-        <RouteButton
-            className="info-button"
-            route={route}
-            stateChange={stateChange}
-        >
-            <img src="./images/question.svg"></img>
-        </RouteButton>
-    </div>
+        <img src="./images/question.svg"></img>
+    </RouteButton>
 );
 
 export default InfoButton;
