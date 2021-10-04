03554c967922ee22588e9c9562353ebb1900f435
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2021-10-04 07:28:04 +0200

    Finishes :recycle

--- features/facebookImport/test/utf8-encoding-decoding.test.js
+++ features/facebookImport/test/utf8-encoding-decoding.test.js
@@ -5,24 +5,26 @@ import { jsonStringifyWithUtfEscape } from "../src/importer/json-encoding";
 const jsonData1 = "🦊";
 const jsonData2 = "🦊🤗😍";
 const decodedData1 = '"\\u00f0\\u009f\\u00a6\\u008a"';
-test("JSON encode single emoji", () => {
-    expect(jsonStringifyWithUtfEscape(jsonData1)).toBe(decodedData1);
+const decodedData2 =
+    '"\\u00f0\\u009f\\u00a6\\u008a\\u00f0\\u009f\\u00a4\\u0097\\u00f0\\u009f\\u0098\\u008d"';
+
+describe("JSON encode", () => {
+    it("Encodes single emoji", () => {
+        expect(jsonStringifyWithUtfEscape(jsonData1)).toBe(decodedData1);
+    });
+    it("Encodes multiple emojis", () => {
+        expect(jsonStringifyWithUtfEscape(jsonData2)).toBe(decodedData2);
+    });
 });
 
-test("JSON encode multiple emojis", () => {
-    expect(jsonStringifyWithUtfEscape(jsonData2)).toBe(
-        '"\\u00f0\\u009f\\u00a6\\u008a\\u00f0\\u009f\\u00a4\\u0097\\u00f0\\u009f\\u0098\\u008d"'
-    );
-});
+describe("Decodes", () => {
+    it("With single emoji", () => {
+        const parsedJson = JSON.parse(decodedData1);
+        expect(decodeURIComponent(escape(parsedJson))).toBe(jsonData1);
+    });
 
-test("Decode single emoji", () => {
-    const parsedJson = JSON.parse(decodedData1);
-    expect(decodeURIComponent(escape(parsedJson))).toBe(jsonData1);
-});
-
-test("Decode multiple emojis", () => {
-    const parsedJson = JSON.parse(
-        '"\\u00f0\\u009f\\u00a6\\u008a\\u00f0\\u009f\\u00a4\\u0097\\u00f0\\u009f\\u0098\\u008d"'
-    );
-    expect(decodeURIComponent(escape(parsedJson))).toBe(jsonData2);
+    it("With multiple emojis", () => {
+        const parsedJson = JSON.parse(decodedData2);
+        expect(decodeURIComponent(escape(parsedJson))).toBe(jsonData2);
+    });
 });
