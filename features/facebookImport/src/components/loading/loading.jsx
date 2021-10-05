111b0725432d49ed5cb4e08af974060339d97a15
Richard Stoffels <richard.stoffels@polypoly.net>
2021-10-05 16:15:51 +0200

    Revert "Try using an overlay to capture mouse events"
    This reverts commit 4c628c33e320cea7e7eca42426a79ad62b87555d.

--- features/facebookImport/src/components/loading/loading.jsx
+++ features/facebookImport/src/components/loading/loading.jsx
@@ -5,12 +5,10 @@ import i18n from "../../i18n.js";
 import "./loading.css";
 
 const Loading = ({ message }) => (
-    <div className="loading-overlay">
-        <div className="loading">
-            <img src="./images/loading.gif" />
-            <p>{message} ...</p>
-            <p>{i18n.t("common:loading.closing")}</p>
-        </div>
+    <div className="loading">
+        <img src="./images/loading.gif" />
+        <p>{message} ...</p>
+        <p>{i18n.t("common:loading.closing")}</p>
     </div>
 );
 
