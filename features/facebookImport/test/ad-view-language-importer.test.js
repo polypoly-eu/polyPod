4ac9bff8ff1cb2117d5dab13cdda7fcd66e10946
Andrei Chis <chisvasileandrei@gmail.com>
2021-10-12 14:22:39 +0200

    Version that passes the datast to test.each.

--- features/facebookImport/test/ad-view-language-importer.test.js
+++ features/facebookImport/test/ad-view-language-importer.test.js
@@ -1,17 +1,98 @@
 "use strict";
 
-import { RECENTLY_VIEWED_FILE_PATH } from "../src/model/importers/recently-viewed-ads-importer";
 import {
+    createDanishAdViewsData,
     createEnglishAdViewsData,
     createGermanAdViewsData,
 } from "./datasets/ad-views-data";
-import { ZipFileMock } from "./mocks/zipfile-mock";
-import { runRecentlyViewedAdsImporter } from "./utils/data-importing";
+import { runAdsImportForDataset } from "./utils/data-importing";
 import { expectImportSuccess } from "./utils/importer-assertions";
 
+const datasets = [
+    ["English", createEnglishAdViewsData()],
+    ["German", createGermanAdViewsData()],
+    ["Danish", createDanishAdViewsData()],
+];
+
+describe("Import ad views", () => {
+    test.each(datasets)("from %s dataset", async (language, dataset) => {
+        const importingResult = await runAdsImportForDataset(dataset);
+        const result = importingResult.result;
+        const relatedAccounts = importingResult.relatedAccounts;
+        const [firstRelatedAccount, secondRelatedAccount] =
+            relatedAccounts.items;
+
+        //returns success status
+        expectImportSuccess(result);
+
+        //has two related accounts
+        expect(relatedAccounts.count).toBe(2);
+
+        // has three ads
+        expect(relatedAccounts.adsCount).toBe(3);
+
+        //has four ad views
+        expect(relatedAccounts.adViewsCount).toBe(4);
+
+        //has correct related account ids for first account
+        expect(firstRelatedAccount.rawId).toBeUndefined();
+        expect(firstRelatedAccount.urlId).toBe("companyx.com");
+
+        //has correct related account ids for second account
+        expect(secondRelatedAccount.rawId).toBe("99888776543223");
+        expect(secondRelatedAccount.urlId).toBeUndefined();
+
+        //has correct related accourl URLs
+        expect(firstRelatedAccount.url).toBe(
+            "https://www.facebook.com/companyx.com"
+        );
+        expect(secondRelatedAccount.url).toBe(
+            "https://www.facebook.com/99888776543223"
+        );
+
+        //has correct related account names
+        expect(firstRelatedAccount.displayName).toBe("Company X");
+        expect(secondRelatedAccount.displayName).toBe("Company Y");
+
+        //has correct ads count in accounts
+        expect(firstRelatedAccount.adsCount).toBe(2);
+        expect(secondRelatedAccount.adsCount).toBe(1);
+
+        //has correct ad views count in accounts
+        expect(firstRelatedAccount.adViewsCount).toBe(3);
+        expect(secondRelatedAccount.adViewsCount).toBe(1);
+
+        //has correct url for ads
+        const [firstAd, secondAd] = firstRelatedAccount.relatedPosts;
+        const [thirdAd] = secondRelatedAccount.relatedPosts;
+        expect(firstAd.url).toBe(
+            "https://www.facebook.com/companyx.com/posts/1112223344556677"
+        );
+        expect(secondAd.url).toBe(
+            "https://www.facebook.com/companyx.com/posts/2233445566678"
+        );
+        expect(thirdAd.url).toBe(
+            "https://www.facebook.com/permalink.php?story_fbid=12345678889&id=99888776543223"
+        );
+
+        //has correct viewed timestamps for ads
+        expect(firstAd.viewedTimestamps).toStrictEqual([
+            1630875618, 1631975618,
+        ]);
+        expect(secondAd.viewedTimestamps).toStrictEqual([1671975618]);
+        expect(thirdAd.viewedTimestamps).toStrictEqual([1693455618]);
+
+        //has correct view counts for ads
+        expect(firstAd.viewsCount).toBe(2);
+        expect(secondAd.viewsCount).toBe(1);
+        expect(thirdAd.viewsCount).toBe(1);
+    });
+});
+
+/*
 let testInputs = null;
 
-beforeAll( () => {
+beforeAll(() => {
     const datasets = [
         { language: "English", dataset: createEnglishAdViewsData() },
         { language: "German", dataset: createGermanAdViewsData() },
@@ -69,3 +150,4 @@ describe("Import ad views", () => {
         }
     );
 });
+*/
