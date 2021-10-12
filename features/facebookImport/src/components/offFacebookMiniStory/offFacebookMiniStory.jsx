777a1896352a1c79bc82ea359e8ed47f5f70a61a
Richard Stoffels <richard.stoffels@polypoly.net>
2021-10-12 18:31:23 +0200

    Add texts and legend to ministory graphs

--- features/facebookImport/src/components/offFacebookMiniStory/offFacebookMiniStory.jsx
+++ features/facebookImport/src/components/offFacebookMiniStory/offFacebookMiniStory.jsx
@@ -4,6 +4,26 @@ import i18n from "../../i18n";
 
 import "./offFacebookMiniStory.css";
 
+const legend = (
+    <div className="legend">
+        <div>
+            <img
+                className="on-bars"
+                src="./images/legend-bars-on.svg"
+                alt="legend-on-facebook-bars"
+            />
+            <p>{i18n.t("offFacebookEventsMiniStory:on.fb.events")}</p>
+        </div>
+        <div>
+            <img
+                src="./images/legend-bars-off.svg"
+                alt="legend-off-facebook-bars"
+            />
+            <p>{i18n.t("offFacebookEventsMiniStory:off.fb.events")}</p>
+        </div>
+    </div>
+);
+
 export const OffFacebookMiniStorySummary = ({
     companiesCount,
     purchasesCount,
@@ -28,18 +48,29 @@ export const OffFacebookMiniStorySummary = ({
 
 export const OffFacebookMiniStoryDetails = ({ displayData }) => {
     return (
-        <div className="ministory-details">
+        <div className="off-facebook-events-ministory-details">
+            <p>{i18n.t("offFacebookEventsMiniStory:details.text.1")}</p>
+            {legend}
             {Object.entries(displayData).map(([companyName, data], i) => (
                 <div key={i}>
-                    <p>{companyName}</p>
+                    <div className="divide-y">
+                        <p>{companyName}</p>
+                        <p className="source">
+                            {i18n.t("offFacebookEventsMiniStory:number.events")}
+                        </p>
+                    </div>
                     <MirroredBarChart
                         data={data}
                         colors={{ upperBar: "#EB6561", lowerBar: "#F7FAFC" }}
                         width="400"
                         height="200"
                     />
+                    <p className="source">
+                        {i18n.t("offFacebookEventsMiniStory:last.90.days")}
+                    </p>
                 </div>
             ))}
+            <p>{i18n.t("offFacebookEventsMiniStory:details.text.2")}</p>
         </div>
     );
 };
