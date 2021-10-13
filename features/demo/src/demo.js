c415832761274e075f2e29ed9828b3b970d05746
Felix Dahlke <felix.dahlke@polypoly.net>
2021-10-13 14:39:22 +0200

    Add unhandled rejection trigger to demo feature

--- features/demo/src/demo.js
+++ features/demo/src/demo.js
@@ -1,7 +1,17 @@
 window.addEventListener("DOMContentLoaded", () => {
-    document.querySelector(".error-message").addEventListener("click", () => {
+    document.querySelector(".unhandled-error").addEventListener("click", () => {
         const error = new Error("An error has been provoked");
         error.cause = new Error("This is the cause");
         throw error;
     });
+
+    document
+        .querySelector(".unhandled-rejection")
+        .addEventListener("click", () => {
+            new Promise((_, reject) => {
+                const error = new Error("A rejection has been provoked");
+                error.cause = new Error("This is the cause");
+                reject(error);
+            });
+        });
 });
