14f299425d36f2a967bda200a9b8397838c4bdea
Felix Dahlke <felix.dahlke@polypoly.net>
2021-10-13 13:49:54 +0200

    PROD4POD-875 - Add more details to JS errors on iOS

--- ios/PolyPodApp/FeatureContainer/handleErrors.js
+++ ios/PolyPodApp/FeatureContainer/handleErrors.js
@@ -1,9 +1,24 @@
-window.onerror = (message, url, line, column, error) => {
+function errorToString(error) {
+    if (typeof error === "object") {
+        let message = `${error.name || "Unknown error"}: `;
+        if (error.message) message += error.message;
+        if (error.stack) message += `\n${error.stack}`;
+        if (error.cause) message += `\n\nCause:\n${errorToString(error.cause)}`;
+        return message;
+    }
+    return error;
+}
+
+window.addEventListener("error", ({ error }) => {
     window.webkit.messageHandlers.error.postMessage({
-        message,
-        url,
-        line,
-        column,
-        error: JSON.stringify(error)
+        message: "Unhandled error:\n\n" + errorToString(error)
     });
-};
+    return true;
+});
+
+window.addEventListener("unhandledrejection", (event) => {
+    window.webkit.messageHandlers.error.postMessage({
+        message: "Unhandled rejection:\n\n" + errorToString(event.reason)
+    });
+    event.preventDefault();
+});
