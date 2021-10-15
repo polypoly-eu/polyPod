19b5843dbe9617c45e9ae41a0a048e140a799f31
Andrei Chis <chisvasileandrei@gmail.com>
2021-10-15 16:07:32 +0200

    Add babel-jest dependencies

--- features/facebookImport/babel.config.cjs
+++ features/facebookImport/babel.config.cjs
@@ -1,3 +1,3 @@
 module.exports = {
-  presets: [['@babel/preset-env', {targets: {node: 'current'}}]],
+  presets: [["@babel/preset-react", {targets: {node: 'current'}}], ['@babel/preset-env', {targets: {node: 'current'}}]],
 };
