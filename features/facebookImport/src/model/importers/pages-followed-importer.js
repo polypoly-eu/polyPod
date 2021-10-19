a37b80ec3ae4e7e98b17bc09507b080a48c3fc4e
Andrei Chiș <chisvasileandrei@gmail.com>
2021-10-19 17:28:17 +0200

    More importer tests (#393)
    * Add utilities for creating mocked zips
    
    * Use utilities for creating mocks
    
    * Add friend importer tests
    
    * Fixes zipWithWrongDatasetKey
    
    * Fix calls to createMockedZip
    
    * Add tests for liked pages
    
    * Extract constants for testing
    
    * Add test data for page events
    
    * Expand tests for importing page events

--- features/facebookImport/src/model/importers/pages-followed-importer.js
+++ features/facebookImport/src/model/importers/pages-followed-importer.js
@@ -1,11 +1,15 @@
 import DirectKeyDataImporter from "./direct-key-data-importer.js";
 
+export const FOLLOWED_PAGES_FILE_PATH = "pages/pages_you_follow.json";
+export const FOLLOWED_PAGES_DATA_KEY = "pages_followed_v2";
+export const FOLLOWED_PAGES_STORAGE_KEY = "followedPages";
+
 export default class FollowedPagesImporter extends DirectKeyDataImporter {
     constructor() {
         super(
-            "pages/pages_you_follow.json",
-            "pages_followed_v2",
-            "followedPages"
+            FOLLOWED_PAGES_FILE_PATH,
+            FOLLOWED_PAGES_DATA_KEY,
+            FOLLOWED_PAGES_STORAGE_KEY
         );
     }
 }
