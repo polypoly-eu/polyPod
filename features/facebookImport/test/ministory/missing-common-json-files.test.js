5bed3176b8e287218132758549e4c49487e14328
Andrei Chiș <chisvasileandrei@gmail.com>
2021-10-21 10:28:41 +0200

    Fix for missing common files analysis (#386)
    * More fined-graind control of enriched data in mock
    
    * Expose the importing of a zipFile as a function.
    
    * Expose runAnalysis tp be used from tests
    
    * Improve test data for off-facebook events.
    
    * Add utility functions for testing analysis.
    
    * Add babel-jest dependencies
    
    * Test analysis for off-facebook events
    
    * Fix for MissingCommonJSONFilesAnalysis
    
    * Slightly improve naming.
    
    * Add expectInactiveAnalysis
    
    * Add DATASET_EXPECTED_VALUES
    
    * Use DATASET_EXPECTED_VALUES in ministories
    
    * Add tests
    
    * Introduce a constant for the number of companies
    
    * Do no anonymize paths as it is not needed
    
    * Removed unused import

---
+++ features/facebookImport/test/ministory/missing-common-json-files.test.js
@@ -0,0 +1,54 @@
+import MissingCommonJSONFilesAnalysis from "../../src/model/analyses/report/missing-common-json-files";
+import commonStructure from "../../src/static/commonStructure";
+import { ZipFileMock } from "../mocks/zipfile-mock";
+import { runAnalysisForExport } from "../utils/analyses-execution";
+import {
+    expectActiveAnalysis,
+    expectAnalysisSuccessStatus,
+    expectInactiveAnalysis,
+} from "../utils/analysis-assertions";
+
+describe("Missing common JSON files analysis for empty zip", () => {
+    let analysis = null;
+    let status = null;
+
+    beforeAll(async () => {
+        let zipFile = new ZipFileMock();
+        ({ analysis, status } = await runAnalysisForExport(
+            MissingCommonJSONFilesAnalysis,
+            zipFile
+        ));
+    });
+
+    it("has success status", async () => {
+        expectAnalysisSuccessStatus(status);
+    });
+
+    it("is active", async () => {
+        expectActiveAnalysis(analysis);
+    });
+});
+
+describe("Missing common JSON files analysis for zip with no missing common files", () => {
+    let analysis = null;
+    let status = null;
+
+    beforeAll(async () => {
+        let zipFile = new ZipFileMock();
+        commonStructure
+            .filter((each) => each.endsWith(".json"))
+            .forEach((each) => zipFile.addJsonEntry(each.substring(1), {}));
+        ({ analysis, status } = await runAnalysisForExport(
+            MissingCommonJSONFilesAnalysis,
+            zipFile
+        ));
+    });
+
+    it("has success status", async () => {
+        expectAnalysisSuccessStatus(status);
+    });
+
+    it("is not active", async () => {
+        expectInactiveAnalysis(analysis);
+    });
+});
