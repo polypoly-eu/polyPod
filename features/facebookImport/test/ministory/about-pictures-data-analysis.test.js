deb1d4d048c47acd514b4ec536a911d062f6f8eb
Andrei Chis <chisvasileandrei@gmail.com>
2021-10-26 16:32:45 +0200

    Test about pictures ministory

---
+++ features/facebookImport/test/ministory/about-pictures-data-analysis.test.js
@@ -0,0 +1,60 @@
+import AboutPicturesDataAnalysis from "../../src/model/analyses/ministories/about-pictures-data-analysis";
+import { ZipFileMock } from "../mocks/zipfile-mock";
+import { runAnalysisForExport } from "../utils/analyses-execution";
+import {
+    expectInactiveAnalysis,
+    expectAnalysisSuccessStatus,
+    expectActiveAnalysis,
+} from "../utils/analysis-assertions";
+
+describe("Pictures ministory on export with no JPEG pictures in correct locations", () => {
+    let analysis = null;
+    let status = null;
+
+    beforeAll(async () => {
+        let zipFile = new ZipFileMock();
+        zipFile.addNamedEntry("photos_and_videos/emptyalbum.png", "");
+        zipFile.addNamedEntry(
+            "messages/inbox/user_2j57v4wtoa/photos/14567839_1208576379157271_232556476_n_21487783899157271.jpg",
+            ""
+        );
+        ({ analysis, status } = await runAnalysisForExport(
+            AboutPicturesDataAnalysis,
+            zipFile
+        ));
+    });
+
+    it("has success status", async () => {
+        expectAnalysisSuccessStatus(status);
+    });
+
+    it("is not active", async () => {
+        expectInactiveAnalysis(analysis);
+    });
+});
+
+describe("Pictures ministory on export with JPEG pictures in correct location", () => {
+    let analysis = null;
+    let status = null;
+
+    beforeAll(async () => {
+        let zipFile = new ZipFileMock();
+        zipFile.addNamedEntry(
+            "photos_and_videos/photos/14567839_1208576379157271_232556476_n_21487783899157271.jpg",
+            ""
+        );
+
+        ({ analysis, status } = await runAnalysisForExport(
+            AboutPicturesDataAnalysis,
+            zipFile
+        ));
+    });
+
+    it("has success status", async () => {
+        expectAnalysisSuccessStatus(status);
+    });
+
+    it("is active", async () => {
+        expectActiveAnalysis(analysis);
+    });
+});
