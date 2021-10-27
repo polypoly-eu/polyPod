1350fa31a1dcd13442e2aebf41e075510634de20
Andrei Chis <chisvasileandrei@gmail.com>
2021-10-27 10:13:15 +0200

    Make constant usage more uniform among importers.

--- features/facebookImport/src/model/importers/name-importer.js
+++ features/facebookImport/src/model/importers/name-importer.js
@@ -1,10 +1,16 @@
 import { readJSONDataObject } from "./utils/importer-util.js";
 
-const fileName = "profile_information/profile_information.json";
+export const PROFILE_INFORMATION_FILE_PATH =
+    "profile_information/profile_information.json";
+export const PROFILE_INFORMATION_DATA_KEY = "profile_v2";
 
 export default class NameImporter {
     async _readLanguageData(zipFile) {
-        return readJSONDataObject(fileName, "profile_v2", zipFile);
+        return readJSONDataObject(
+            PROFILE_INFORMATION_FILE_PATH,
+            PROFILE_INFORMATION_DATA_KEY,
+            zipFile
+        );
     }
 
     async import({ zipFile, facebookAccount }) {
