f5e229e5631031a5a0ac545881f4b16b3a2ca90a
rsto <57987795+RichardSto@users.noreply.github.com>
2021-10-28 10:06:03 +0200

    Fix activities bar chart (#411)
    * PROD4POD-937/Add missing label
    
    * Fix CSS
    
    * PROD4POD-937/Remove axis lines
    
    * Create function to style bar chart axis
    
    * Add styles to axis
    
    * Fix bars width
    
    * Add value labels - WIP
    
    * WIP: Add texts
    
    * Fix texts above bar
    
    * Add delay to texts transition to make it cooler
    
    * Add gridlines
    
    * Add numbers to the front and add in between gridline
    
    Co-authored-by: mteresa-jimenez <mteresa.jn@gmail.com>

--- core/utils/poly-look/src/incubate/visualisations/VerticalBarChart.jsx
+++ core/utils/poly-look/src/incubate/visualisations/VerticalBarChart.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useRef } from "react";
 
 import * as d3 from "d3";
 
+import "./verticalBarChart.css";
+
 /**
  * Visualizes data as a cluster of bubbles where the value of the bubble is represented as the radius.
  *
@@ -13,14 +15,18 @@ import * as d3 from "d3";
  * @param {number} data[].value - The value of the bubble, which corresponds to it's radius
  * @param {number = 400} [width] - The width of the svg
  * @param {number = 300} [height] - The height of the svg
+ * @param {number = adaptive} [barWidth] - The width of the bars
  * @param {string|callback = "blue"} [barColor] - The color of the bar (callbacks receive event and data)
+ * @param {string = null} [barValueColor] - The color the values are shown in (default = no values shown)
  * @returns {jsx-div with svg attached}
  */
 export const VerticalBarChart = ({
   data,
   barColor = "blue",
   width = 400,
-  height = 300,
+  height = 200,
+  barWidth,
+  barValueColor,
 }) => {
   const barChartRef = useRef();
 
@@ -33,7 +39,10 @@ export const VerticalBarChart = ({
     },
     chartHeight = height - margin.bottom - margin.top,
     chartWidth = width - margin.left - margin.right,
-    initializingBarHeight = 2;
+    initializingBarHeight = 2,
+    barValueMargin = 4,
+    numberTicksY = 3,
+    gridXMargin = 12;
 
   const xScale = d3.scaleBand().range([0, chartWidth]).padding(0.2),
     yScale = d3.scaleLinear().range([chartHeight, margin.bottom]);
@@ -66,9 +75,9 @@ export const VerticalBarChart = ({
         d3
           .axisLeft(yScale)
           .tickFormat((d) => d)
-          .ticks(5)
+          .ticks(numberTicksY)
       )
-      .attr("class", "y-axis")
+      .attr("class", "y-axis axis")
       .append("text")
       .attr("y", 6)
       .attr("dy", "0.71em")
@@ -77,7 +86,7 @@ export const VerticalBarChart = ({
 
     barChart
       .append("g")
-      .attr("class", "x-axis")
+      .attr("class", "x-axis axis")
       .call(d3.axisBottom(xScale))
       .attr("transform", `translate(0, ${chartHeight})`);
   }
@@ -93,7 +102,7 @@ export const VerticalBarChart = ({
           .tickFormat((d) => d)
           .ticks(5)
       )
-      .attr("class", "y-axis")
+      .attr("class", "y-axis axis")
       .append("text")
       .attr("y", 6)
       .attr("dy", "0.71em")
@@ -113,10 +122,13 @@ export const VerticalBarChart = ({
       .duration(750)
       .attr("y", chartHeight - initializingBarHeight)
       .attr("height", initializingBarHeight)
-      .attr("x", (d) => xScale(d.title))
-      .attr("width", xScale.bandwidth())
+      .attr("x", (d) =>
+        barWidth
+          ? xScale(d.title) + (xScale.bandwidth() - barWidth) / 2
+          : xScale(d.title)
+      )
+      .attr("width", barWidth || xScale.bandwidth())
       .attr("fill", barColor)
-      .attr("class", "bar")
       .transition()
       .duration(750)
       .attr("y", (d) => yScale(d.value))
@@ -129,10 +141,14 @@ export const VerticalBarChart = ({
       .append("rect")
       .attr("y", chartHeight - initializingBarHeight)
       .attr("height", initializingBarHeight)
-      .attr("x", (d) => xScale(d.title))
-      .attr("width", xScale.bandwidth())
-      .attr("fill", barColor)
       .attr("class", "bar")
+      .attr("x", (d) =>
+        barWidth
+          ? xScale(d.title) + (xScale.bandwidth() - barWidth) / 2
+          : xScale(d.title)
+      )
+      .attr("width", barWidth || xScale.bandwidth())
+      .attr("fill", barColor)
       .transition()
       .duration(750)
       .delay((_, i) => i * 50)
@@ -140,11 +156,64 @@ export const VerticalBarChart = ({
       .attr("height", (d) => chartHeight - yScale(d.value));
   }
 
+  function addEnteringBarValues(barValues) {
+    barValues
+      .enter()
+      .append("text")
+      .attr("x", (d) => xScale(d.title) + xScale.bandwidth() / 2)
+      .attr("class", "bar-value")
+      .attr("text-anchor", "middle")
+      .attr("y", (d) => yScale(d.value) - barValueMargin)
+      .text((d) => d.value)
+      .attr("fill", "transparent")
+      .style("font-size", "10px")
+      .transition()
+      .delay(1000)
+      .duration(500)
+      .attr("fill", barValueColor);
+  }
+
+  function updateExistingBarValues(barValues) {
+    barValues
+      .attr("fill", "transparent")
+      .attr("y", (d) => yScale(d.value) - barValueMargin)
+      .text((d) => d.value)
+      .raise()
+      .transition()
+      .delay(1500)
+      .duration(500)
+      .attr("fill", barValueColor);
+  }
+
+  function addYAxisGrid(barChart) {
+    barChart.select(".axis-grid").remove();
+    barChart
+      .append("g")
+      .attr("class", "axis-grid")
+      .call(
+        d3
+          .axisLeft(yScale)
+          .tickSize(-chartWidth + gridXMargin)
+          .tickFormat("")
+          .ticks(numberTicksY * 2)
+      )
+      .attr("transform", `translate(${gridXMargin / 2}, 0)`);
+  }
+
   function displayBars(barChart) {
     const bars = barChart.selectAll(".bar").data(data, (d) => d.title);
     bars.exit().remove();
     updateExistingBars(bars);
     addEnteringBars(bars);
+  }
+
+  function displayValues(barChart) {
+    const barValues = barChart
+      .selectAll(".bar-value")
+      .data(data, (d) => d.title);
+    barValues.exit().remove();
+    updateExistingBarValues(barValues);
+    addEnteringBarValues(barValues);
   }
 
   useEffect(() => {
@@ -155,8 +224,12 @@ export const VerticalBarChart = ({
 
     if (barChart.select(".x-axis").empty()) addAxis(barChart);
     else transitionAxis(barChart);
+
     displayBars(barChart);
+    addYAxisGrid(barChart);
+    if (barValueColor) displayValues(barChart);
+    else barChart.selectAll(".bar-value").remove();
   });
 
-  return <div className="bar-chart" ref={barChartRef}></div>;
+  return <div className="bar-chart vertical-bar-chart" ref={barChartRef}></div>;
 };
