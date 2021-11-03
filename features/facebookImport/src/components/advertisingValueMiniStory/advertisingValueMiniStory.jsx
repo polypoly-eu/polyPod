1cb07e843e2bf30d0c96d829b8efdf19279b1073
lenalaupoly <lena.lau@polypoly.eu>
2021-11-03 16:30:31 +0100

    PROD4POD-907: fix style of Preview of AdvertisingMiniStory

--- features/facebookImport/src/components/advertisingValueMiniStory/advertisingValueMiniStory.jsx
+++ features/facebookImport/src/components/advertisingValueMiniStory/advertisingValueMiniStory.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useRef } from "react";
 import i18n from "../../i18n";
 
 import "./advertisingValueMiniStory.css";
 
+const calculateFontSize = (text, maxWidth) => {
+    // TODO: Extract text size affecting styles from target element
+    const minFontSize = 14;
+    const maxFontSize = 34;
+    const canvas = document.createElement("canvas");
+    const context = canvas.getContext("2d");
+    for (let fontSize = maxFontSize; fontSize > minFontSize; fontSize--) {
+        context.font = `${fontSize}px Jost`;
+        if (context.measureText(text).width <= maxWidth) return fontSize;
+    }
+    return minFontSize;
+};
+
 const AdvertisingValueMiniStory = ({ randomAdInterests, numberInterests }) => {
+    // following takes the three interests, splits them into single words and picks the longest word
+    let splitInterests = [].concat.apply(
+        [],
+        randomAdInterests.map((int) => int.split(" "))
+    );
+    let longestInterestWord = splitInterests.sort(function (a, b) {
+        return b.length - a.length;
+    })[0];
+
     return (
         <div className="advertising-value-mini-story">
             <p
@@ -14,9 +36,26 @@ const AdvertisingValueMiniStory = ({ randomAdInterests, numberInterests }) => {
                 }}
             />
             <ul>
-                {randomAdInterests.map((interest, index) => (
-                    <li key={index}>{interest}</li>
-                ))}
+                {randomAdInterests.map((interest, index) => {
+                    const refWidth = useRef(0);
+                    const fontSize = calculateFontSize(
+                        longestInterestWord,
+                        refWidth.current.clientWidth
+                    );
+
+                    return (
+                        <li key={index}>
+                            <p
+                                style={{
+                                    fontSize: fontSize,
+                                }}
+                                ref={refWidth}
+                            >
+                                {interest}
+                            </p>
+                        </li>
+                    );
+                })}
             </ul>
             <p>{i18n.t("advertisingValueMiniStory:end.text")}</p>
             <p className="source">
