112945dc637874d868944152be8f71830220c871
Felix Dahlke <felix.dahlke@polypoly.net>
2021-11-04 19:25:01 +0100

    Expand UpdateNotification test cases

--- android/app/src/test/kotlin/coop/polypoly/polypod/updatenotification/UpdateNotificationTest.kt
+++ android/app/src/test/kotlin/coop/polypoly/polypod/updatenotification/UpdateNotificationTest.kt
@@ -1,6 +1,5 @@
 package coop.polypoly.polypod.updatenotification
 
-import android.content.Context
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.truth.Truth.assertThat
@@ -13,57 +12,76 @@ import org.robolectric.annotation.LooperMode
 @RunWith(AndroidJUnit4::class)
 @Config(sdk = [Config.OLDEST_SDK])
 class UpdateNotificationTest {
-    private val context: Context = ApplicationProvider.getApplicationContext()
-
     @Test
     fun `notification with id = 0 considered seen`() {
-        UpdateNotification.mockData.id = 0
-        val notification = UpdateNotification(context)
+        val notification = loadNotification(0)
         assertThat(notification.shouldShowInAppNotification()).isFalse()
         assertThat(notification.shouldShowPushNotification()).isFalse()
     }
 
     @Test
     fun `first ever notification considered not seen`() {
-        UpdateNotification.mockData.id = 1
-        val notification = UpdateNotification(context)
+        val notification = loadNotification(1)
         assertThat(notification.shouldShowInAppNotification()).isTrue()
         assertThat(notification.shouldShowPushNotification()).isTrue()
     }
 
     @Test
     fun `previously shown notification considered seen`() {
-        UpdateNotification.mockData.id = 1
-        val notification = UpdateNotification(context)
+        val notification = loadNotification(1)
         notification.onInAppNotificationSeen()
-        notification.onPushNotificationSeen()
-        assertThat(notification.shouldShowInAppNotification()).isFalse()
-        assertThat(notification.shouldShowPushNotification()).isFalse()
+
+        val secondNotification = loadNotification(1)
+        assertThat(secondNotification.shouldShowInAppNotification()).isFalse()
+        assertThat(secondNotification.shouldShowPushNotification()).isFalse()
     }
 
     @Test
     fun `additional notification with different id considered not seen`() {
-        UpdateNotification.mockData.id = 1
-        var notification = UpdateNotification(context)
+        val notification = loadNotification(1)
         notification.onInAppNotificationSeen()
-        notification.onPushNotificationSeen()
 
-        UpdateNotification.mockData.id = 2
-        notification = UpdateNotification(context)
-        assertThat(notification.shouldShowInAppNotification()).isTrue()
-        assertThat(notification.shouldShowPushNotification()).isTrue()
+        val secondNotification = loadNotification(2)
+        assertThat(secondNotification.shouldShowInAppNotification()).isTrue()
+        assertThat(secondNotification.shouldShowPushNotification()).isTrue()
     }
 
     @Test
     fun `additional notification with lower id considered not seen`() {
-        UpdateNotification.mockData.id = 2
-        var notification = UpdateNotification(context)
+        val notification = loadNotification(2)
         notification.onInAppNotificationSeen()
         notification.onPushNotificationSeen()
 
-        UpdateNotification.mockData.id = 1
-        notification = UpdateNotification(context)
+        val secondNotification = loadNotification(1)
+        assertThat(secondNotification.shouldShowInAppNotification()).isTrue()
+        assertThat(secondNotification.shouldShowPushNotification()).isTrue()
+    }
+
+    @Test
+    fun `push notification considered seen after startup`() {
+        val notification = loadNotification(1)
+        notification.onStartup()
+        assertThat(notification.shouldShowPushNotification()).isFalse()
+    }
+
+    @Test
+    fun `in app notification considered not seen after push was seen`() {
+        val notification = loadNotification(1)
+        notification.onPushNotificationSeen()
+        assertThat(notification.shouldShowPushNotification()).isFalse()
         assertThat(notification.shouldShowInAppNotification()).isTrue()
-        assertThat(notification.shouldShowPushNotification()).isTrue()
+    }
+
+    @Test
+    fun `every notification considered seen on first run`() {
+        val notification = loadNotification(1)
+        notification.onFirstRun()
+        assertThat(notification.shouldShowPushNotification()).isFalse()
+        assertThat(notification.shouldShowInAppNotification()).isFalse()
+    }
+
+    private fun loadNotification(id: Int): UpdateNotification {
+        UpdateNotification.mockData.id = id
+        return UpdateNotification(ApplicationProvider.getApplicationContext())
     }
 }
