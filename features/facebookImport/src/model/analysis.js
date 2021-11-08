c634b568ced57b54e56064e5aa9680522847ebdc
Andrei Chis <chisvasileandrei@gmail.com>
2021-11-08 17:52:33 +0100

    Add dedicated function for analysing a zip

--- features/facebookImport/src/model/analysis.js
+++ features/facebookImport/src/model/analysis.js
@@ -217,9 +217,8 @@ export async function runAnalysis(analysisClass, enrichedData) {
     }
 }
 
-export async function analyzeFile(file, facebookAccount) {
-    const zipFile = new ZipFile(file, window.pod);
-    const enrichedData = { ...file, zipFile, facebookAccount };
+export async function analyzeZip(zipData, zipFile, facebookAccount) {
+    const enrichedData = { ...zipData, zipFile, facebookAccount };
     const analysesResults = await Promise.all(
         subAnalyses.map(async (subAnalysisClass) => {
             return runAnalysis(subAnalysisClass, enrichedData);
@@ -237,4 +236,9 @@ export async function analyzeFile(file, facebookAccount) {
         analyses: activeGlobalAnalyses,
         unrecognizedData: new UnrecognizedData(analysesResults),
     };
+}
+
+export async function analyzeFile(zipData, facebookAccount) {
+    const zipFile = new ZipFile(zipData, window.pod);
+    return await analyzeZip(zipData, zipFile, facebookAccount);
 }
