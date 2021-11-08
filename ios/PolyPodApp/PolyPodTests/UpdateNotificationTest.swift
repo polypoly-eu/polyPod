e9ff91c2553e71fffd984b13ea6b2688dcbda095
Felix Dahlke <felix.dahlke@polypoly.net>
2021-11-08 10:07:24 +0100

    Add UpdateNotification unit tests

--- ios/PolyPodApp/PolyPodTests/UpdateNotificationTest.swift
+++ ios/PolyPodApp/PolyPodTests/UpdateNotificationTest.swift
@@ -1,5 +1,73 @@
 import XCTest
 
 class UpdateNotificationTest: XCTestCase {
-    // TODO: Add unit test coverage similar to what's on Android
+    override func setUp() {
+        UserDefaults.standard.removeObject(
+            forKey: UserDefaults.Keys.lastUpdateNotificationId.rawValue
+        )
+    }
+    
+    func testNotificationWithId0Seen() {
+        let notification = loadNotification(0)
+        XCTAssertFalse(notification.showInApp)
+        XCTAssertFalse(notification.showPush)
+    }
+    
+    func testFirstNotificationNotSeen() {
+        let notification = loadNotification(1)
+        XCTAssertTrue(notification.showInApp)
+        XCTAssertTrue(notification.showPush)
+    }
+    
+    func testPreviouslySeenNotificationSeen() {
+        let notification = loadNotification(1)
+        notification.onInAppSeen()
+        XCTAssertFalse(notification.showInApp)
+        XCTAssertFalse(notification.showPush)
+    }
+    
+    func testAdditionalNotificationNotSeen() {
+        let notification = loadNotification(1)
+        notification.onInAppSeen()
+        
+        let secondNotification = loadNotification(2)
+        XCTAssertTrue(secondNotification.showInApp)
+        XCTAssertTrue(secondNotification.showPush)
+    }
+    
+    func testAdditionalNotificationWithLowerIdNotSeen() {
+        let notification = loadNotification(2)
+        notification.onInAppSeen()
+        
+        let secondNotification = loadNotification(1)
+        XCTAssertTrue(secondNotification.showInApp)
+        XCTAssertTrue(secondNotification.showPush)
+    }
+    
+    func testPushNotificationSeenAfterStartup() {
+        let notification = loadNotification(1)
+        notification.onStartup()
+        XCTAssertFalse(notification.showPush)
+    }
+    
+    func testInAppNotSeenAfterPushSeen() {
+        let notification = loadNotification(1)
+        notification.onPushSeen()
+        XCTAssertFalse(notification.showPush)
+    }
+    
+    func testAllSeenAfterFirstRun() {
+        let notification = loadNotification(1)
+        notification.onFirstRun()
+        XCTAssertFalse(notification.showInApp)
+        XCTAssertFalse(notification.showPush)
+    }
+    
+    private func loadNotification(_ id: Int) -> UpdateNotification {
+        UserDefaults.standard.set(
+            id,
+            forKey: UserDefaults.Keys.updateNotificationMockId.rawValue
+        )
+        return UpdateNotification()
+    }
 }
