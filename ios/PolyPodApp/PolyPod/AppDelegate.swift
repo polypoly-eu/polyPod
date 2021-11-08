d8512291b80105eecea0b962d88e2018044b5924
Felix Dahlke <felix.dahlke@polypoly.net>
2021-11-08 18:42:21 +0100

    PROD4POD-386 - Add push notifications on iOS
    This doesn't seem to actually work yet - at least I couldn't produce a
    notification in my tests yet.
    
    _Showing_ the notification works, it's just the background task
    scheduling that I haven't been able to get to work - or I haven't waited
    long enough, given how iOS calls background tasks kinda arbitrarily.

--- ios/PolyPodApp/PolyPod/AppDelegate.swift
+++ ios/PolyPodApp/PolyPod/AppDelegate.swift
@@ -1,8 +1,11 @@
+import BackgroundTasks
 import UIKit
 import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    private static let updateNotificationCheckIdentifier = "coop.polypoly.polypod.updateNotificationCheck"
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
@@ -23,7 +26,44 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let count = try! managedContext.count(for: fetchRequest)
         print("Number of quads in Core Data:", count)
         
+        self.registerUpdateNotificationCheck()
+        
         return true
+    }
+    
+    private func registerUpdateNotificationCheck() {
+        BGTaskScheduler.shared.register(forTaskWithIdentifier: AppDelegate.updateNotificationCheckIdentifier, using: nil) { task in
+            self.handleUpdateNotificationCheck(task)
+        }
+    }
+    
+    private func handleUpdateNotificationCheck(_ task: BGTask) {
+        let notification = UpdateNotification()
+        if notification.showPush {
+            notification.onPushSeen()
+            showUpdateNotification()
+        }
+        task.setTaskCompleted(success: true)
+        scheduleUpdateNotificationCheck()
+    }
+    
+    private func showUpdateNotification() {
+        let identifier = UUID().uuidString
+        
+        let content = UNMutableNotificationContent()
+        let notification = UpdateNotification()
+        content.title = notification.title
+        content.body = notification.text
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: TimeInterval(notification.pushDelay), repeats: false)
+        let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+        
+        let notificationCenter = UNUserNotificationCenter.current()
+        notificationCenter.add(request) { error in
+            if error != nil {
+                print("Error showing update notification: \(error!.localizedDescription)")
+            }
+        }
     }
     
     // MARK: UISceneSession Lifecycle
@@ -98,6 +138,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 let nserror = error as NSError
                 fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
             }
+        }
+    }
+    
+    func scheduleUpdateNotificationCheck() {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert]) {_, _ in }
+        
+        let task = BGProcessingTaskRequest(identifier: AppDelegate.updateNotificationCheckIdentifier)
+        task.earliestBeginDate = Date(timeIntervalSinceNow: 60)
+        task.requiresExternalPower = false
+        task.requiresNetworkConnectivity = false
+        do {
+            try BGTaskScheduler.shared.submit(task)
+        } catch {
+            print("Failed to schedule task \(AppDelegate.updateNotificationCheckIdentifier): \(error.localizedDescription)")
         }
     }
 }
