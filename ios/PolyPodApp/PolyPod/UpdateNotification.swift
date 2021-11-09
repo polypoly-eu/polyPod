755aeca530bae9192a8e7fc6bcec61b710930719
Felix Dahlke <felix.dahlke@polypoly.net>
2021-11-09 10:55:02 +0100

    Read iOS notifications from file
    While at it, I also set it to the same content we currently have on
    Android.
    
    In the future, we could move update-notification.json to /assets, and
    link to it from both iOS and Android (replacing the current string
    resource file used there).

--- ios/PolyPodApp/PolyPod/UpdateNotification.swift
+++ ios/PolyPodApp/PolyPod/UpdateNotification.swift
@@ -1,4 +1,4 @@
-import Foundation
+import UIKit
 
 private class LastUpdateNotification {
     private static let idKey = UserDefaults.Keys
@@ -54,6 +54,62 @@ class UpdateNotification {
         }
     }
     
+    private struct Data {
+        private static let invalid = Data(
+            id: 0,
+            pushDelay: 0,
+            title: [String: String](),
+            text: [String: String]()
+        )
+        
+        static func load() -> Data {
+            guard let asset = NSDataAsset(
+                name: "UpdateNotification",
+                bundle: Bundle.main
+            ) else {
+                return invalid
+            }
+            
+            guard let json = try? JSONSerialization.jsonObject(
+                with: asset.data
+            ) as? [String: Any] else {
+                return invalid
+            }
+            
+            guard let id = json["id"] as? Int,
+                  let pushDelay = json["pushDelay"] as? Int,
+                  let title = json["title"] as? [String: String],
+                  let text = json["text"] as? [String: String]
+            else {
+                return invalid
+            }
+            return Data(id: id, pushDelay: pushDelay, title: title, text: text)
+        }
+        
+        private static func readLocalizedValue(
+            _ values: [String: String]
+        ) -> String {
+            let defaultLanguage = "en"
+            let userLanguage = Locale.current.languageCode ?? defaultLanguage
+            return values[userLanguage] ?? values[defaultLanguage]!
+        }
+        
+        let id: Int
+        let pushDelay: Int
+        let title: [String: String]
+        let text: [String: String]
+        
+        var localizedTitle: String {
+            return Data.readLocalizedValue(self.title)
+        }
+        
+        var localizedText: String {
+            return Data.readLocalizedValue(self.text)
+        }
+    }
+    
+    private static let notificationData = Data.load()
+        
     private static func loadLastState(_ id: Int) -> State {
         if id == 0 {
             return .ALL_SEEN
@@ -74,15 +130,10 @@ class UpdateNotification {
         return State.parse(lastState) ?? .ALL_SEEN
     }
     
-    // TODO: Don't hard code the following
-    let id = UpdateNotificationMockId.read() ?? 79205050
-    let pushDelay = 120
-    let title = "Notification system test"
-    let text = """
-This is a test of the update notification system - if you see it,
-please tell fhd about it - and try to remember whether this was a fresh
-installation of the app, or if you previously had it installed.
-"""
+    let id = UpdateNotificationMockId.read() ?? notificationData.id
+    let pushDelay = notificationData.pushDelay
+    let title = notificationData.localizedTitle
+    let text = notificationData.localizedText
     
     private var cachedState: State
     private var state: State {
