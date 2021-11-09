0cd7a95c6b51718573101b94d7bffb91e3bc78f6
Andrei Chiș <chisvasileandrei@gmail.com>
2021-11-09 17:21:49 +0100

    Basic tests for the report (#448)
    * Fix bug when calling importZip
    
    * Add dedicated function for analysing a zip
    
    * Expand tests for Missing common JSON files to test report
    
    * Inject the pod into features.
    
    * Add a test for the entire report
    
    * Test the report metadata ministory
    
    * Test unknown top-level folders analysis
    
    * Export the number of importers and analyses
    
    * Fix naming
    
    * Add an utility method for running all analyses
    
    * Test MinistoriesStatusAnalysis
    
    * Test DataImportingStatusAnalysis
    
    * Fix naming
    
    * Fix naming
    
    * Small fixes

---
+++ features/facebookImport/test/report-creation.test.js
@@ -0,0 +1,34 @@
+import { zipFileWithOffFacebookEvents } from "./datasets/off-facebook-events-data";
+import { runAnalysesForZip } from "./utils/analyses-execution";
+import { expectActiveAnalysis } from "./utils/analysis-assertions";
+
+export const NUMBER_OF_REPORT_ANALYSES = 8;
+
+describe("Report creation for export", () => {
+    let unrecognizedData = null;
+    let jsonReport = null;
+
+    beforeAll(async () => {
+        let zipFile = zipFileWithOffFacebookEvents();
+        zipFile.addJsonEntry("unknow_folder/unknown_file.json", '""');
+        ({ unrecognizedData } = await runAnalysesForZip(zipFile));
+
+        jsonReport = unrecognizedData.jsonReport;
+    });
+
+    it("is report", async () => {
+        expectActiveAnalysis(unrecognizedData);
+    });
+
+    it("has correct number of report analysis", async () => {
+        expect(unrecognizedData.reportAnalyses.length).toBe(
+            NUMBER_OF_REPORT_ANALYSES
+        );
+    });
+
+    it("has correct number of analysis in JSON report", async () => {
+        expect(jsonReport.reportAnalyses_v1.length).toBe(
+            NUMBER_OF_REPORT_ANALYSES
+        );
+    });
+});
