0f248e351750936453bdfda98752d460abd8eb9f
JJ Merelo <jjmerelo@gmail.com>
2021-11-15 10:31:10 +0100

    All swiper based content has vanished

--- features/polyExplorer/cypress/helpers/navigation.js
+++ features/polyExplorer/cypress/helpers/navigation.js
@@ -9,18 +9,6 @@ export function startWithCompanies() {
             });
 }
 
-export function startMeUp () {
-    cy.visit(INDEX_ROUTE);
-    cy.get(".button-container button")
-    .click()
-    .then(() => {
-        return cy
-            .get(
-                ".swiper-slide.swiper-slide-active .data-sharing-section.companies-shared .data-sharing-gauge"
-            )
-            .click();
-    });
-}
 export function navigation(index, stopCondition, next) {
     cy.get(".swiper-slide-active").then(($activeSwiper) => {
         if (stopCondition($activeSwiper)) {
