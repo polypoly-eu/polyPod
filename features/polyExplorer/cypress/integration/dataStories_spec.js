d75b1aec9ebac9cb666beb2b91bf698ad2bbbe59
JJ Merelo <jjmerelo@gmail.com>
2021-11-15 10:29:20 +0100

    Eliminates non-functioning test

--- features/polyExplorer/cypress/integration/dataStories_spec.js
+++ features/polyExplorer/cypress/integration/dataStories_spec.js
@@ -8,16 +8,4 @@ describe("Data stories", () => {
         });
     });
 
-    it("should fail if swiping in the companies screen does not work", () => {
-        const numberSwipes = 8;
-        cy.visit(INDEX_ROUTE);
-        cy.get(".button-container button").click();
-
-        for (let i = 1; i < numberSwipes; i++) {
-            cy.get(".featured-company-holder")
-                .trigger("pointerdown", { which: 1 })
-                .trigger("pointermove", "left")
-                .trigger("pointerup", { force: true });
-        }
-    });
 });
