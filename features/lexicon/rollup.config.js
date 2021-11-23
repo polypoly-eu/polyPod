b1b2ce5257c6d96f96c9aff285c214c9ba3ecc8e
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2021-11-23 09:53:05 +0100

    Eliminate also from configuration

--- features/lexicon/rollup.config.js
+++ features/lexicon/rollup.config.js
@@ -2,7 +2,6 @@ import svelte from "rollup-plugin-svelte";
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import livereload from "rollup-plugin-livereload";
-import { terser } from "rollup-plugin-terser";
 import css from "rollup-plugin-css-only";
 import json from "@rollup/plugin-json";
 import sucrase from "@rollup/plugin-sucrase";
@@ -71,7 +70,7 @@ export default {
                 {
                     src: [
                         "node_modules/@polypoly-eu/podjs/dist/pod.js",
-                        "node_modules/@polypoly-eu/poly-look/dist/css/poly-look.css"
+                        "node_modules/@polypoly-eu/poly-look/dist/css/poly-look.css",
                     ],
                     dest: "public",
                 },
@@ -88,7 +87,7 @@ export default {
 
         // If we're building for production (npm run build
         // instead of npm run dev), minify
-        production && terser(),
+        production,
     ],
     watch: {
         clearScreen: false,
