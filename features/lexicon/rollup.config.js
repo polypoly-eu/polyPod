6a075d51a59989eb910b3fa2a15e1ac83c63924d
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2021-11-23 11:06:12 +0100

    Eliminate last sentence, which only minified

--- features/lexicon/rollup.config.js
+++ features/lexicon/rollup.config.js
@@ -84,10 +84,6 @@ export default {
         // Watch the `public` directory and refresh the
         // browser on changes when not in production
         !production && livereload("public"),
-
-        // If we're building for production (npm run build
-        // instead of npm run dev), minify
-        production,
     ],
     watch: {
         clearScreen: false,
