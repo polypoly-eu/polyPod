6c1c1191117bfb8782bb76f5e9700a3c0195b546
mteresa-jimenez <mteresa.jn@gmail.com>
2021-11-24 12:19:10 +0100

    Fix duplicated file

--- features/facebookImport/src/components/messagesMiniStory/messagesMiniStory.jsx
+++ features/facebookImport/src/components/messagesMiniStory/messagesMiniStory.jsx
@@ -5,7 +5,7 @@ import i18n from "../../i18n";
 import BarChart from "../dataViz/barChart.jsx";
 import InfoButton from "../buttons/infoButton/infoButton.jsx";
 
-import "./messagesThreadsMiniStory.css";
+import "./messagesMiniStory.css";
 
 export const MessagesMiniStorySummary = ({
     messagesCount,
