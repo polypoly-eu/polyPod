e8cd5cbb36925340fa1205ae1e0f6070d0ca945b
Felix Dahlke <felix.dahlke@polypoly.net>
2021-11-25 09:48:06 +0100

    PROD4POD-1123 - Fix missing variables in message story details
    It appears the new translations used variables that don't exist, and
    ultimately really just want the same text that's shown on the story's
    summary.

--- features/facebookImport/src/components/messagesMiniStory/messagesMinistory.jsx
+++ features/facebookImport/src/components/messagesMiniStory/messagesMinistory.jsx
@@ -7,6 +7,20 @@ import InfoButton from "../buttons/infoButton/infoButton.jsx";
 
 import "./messagesMinistory.css";
 
+const SummaryText = ({
+    messagesCount,
+    messagesThreadsData,
+    totalUsernamesCount,
+}) => (
+    <p>
+        {i18n.t("explore:messages.summary", {
+            messages: messagesCount,
+            threads: messagesThreadsData.length,
+            people: totalUsernamesCount,
+        })}
+    </p>
+);
+
 export const MessagesMiniStorySummary = ({
     messagesCount,
     messagesThreadsData,
@@ -17,28 +31,27 @@ export const MessagesMiniStorySummary = ({
             <p className="highlighted-number">
                 {messagesCount.toLocaleString("de-DE")}
             </p>
-            <p>
-                {i18n.t("explore:messages.summary", {
-                    messages: messagesCount,
-                    threads: messagesThreadsData.length,
-                    people: totalUsernamesCount,
-                })}
-            </p>
+            <SummaryText
+                messagesCount={messagesCount}
+                messagesThreadsData={messagesThreadsData}
+                totalUsernamesCount={totalUsernamesCount}
+            />
         </div>
     );
 };
 
 export const MessagesMiniStoryDetails = ({
-    totalUsernamesCount,
+    messagesCount,
     messagesThreadsData,
+    totalUsernamesCount,
 }) => {
     return (
         <>
-            <p>
-                {i18n.t("messagesMiniStory:number.chats", {
-                    number_chats: totalUsernamesCount,
-                })}
-            </p>
+            <SummaryText
+                messagesCount={messagesCount}
+                messagesThreadsData={messagesThreadsData}
+                totalUsernamesCount={totalUsernamesCount}
+            />
             <p> {i18n.t("messagesMiniStory:chart.title")}</p>
             <BarChart
                 data={messagesThreadsData}
