f2c0b4ecfb26dc2b9bf135203bb9299be36c11da
Richard Stoffels <richard.stoffels@polypoly.net>
2021-11-26 14:15:36 +0100

    Remove numbers

--- features/facebookImport/src/components/progressBar/progressBar.jsx
+++ features/facebookImport/src/components/progressBar/progressBar.jsx
@@ -9,35 +9,6 @@ const ProgressBar = ({
     importSections,
     file,
 }) => {
-    const { startRequest } = useContext(ImporterContext);
-
-    function requestCheckIcon() {
-        if (startRequest || file) {
-            return <img src={`./images/request-done.svg`} />;
-        } else {
-            return <div className={`number request-number`}>1</div>;
-        }
-    }
-
-    function checkForIcon(section) {
-        if (
-            section !== "request" &&
-            importSections.indexOf(importStatus) >
-                importSections.indexOf(section)
-        ) {
-            return <img src={`./images/${section}-done.svg`} />;
-        }
-        if (
-            importSections.indexOf(importStatus) ===
-            importSections.indexOf(section)
-        )
-            return (
-                <div className={`number ${section}-number`}>
-                    {importSections.indexOf(importStatus) + 1}
-                </div>
-            );
-    }
-
     return (
         <div className="progress-bar" id="progress-bar">
             {importSections.map((section, index) => (
@@ -47,9 +18,9 @@ const ProgressBar = ({
                     className={`section`}
                 >
                     <div className={`line ${section}-progress`}>
-                        {section === "request"
-                            ? requestCheckIcon()
-                            : checkForIcon(section)}
+                        <div className={`number ${section}-number`}>
+                            {index + 1}
+                        </div>
                     </div>
                 </div>
             ))}
