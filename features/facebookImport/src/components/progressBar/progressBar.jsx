644df6b71941fa9570ac47d512a9e442de0145cb
Richard Stoffels <57987795+RichardSto@users.noreply.github.com>
2021-11-26 16:53:44 +0100

    Prod4 pod 1132/remove numbers (#515)
    * Remove numbers
    
    * Remove unused imports

--- features/facebookImport/src/components/progressBar/progressBar.jsx
+++ features/facebookImport/src/components/progressBar/progressBar.jsx
@@ -1,43 +1,8 @@
-import React, { useContext } from "react";
-import { ImporterContext } from "../../context/importer-context.jsx";
+import React from "react";
 
 import "./progressBar.css";
 
-const ProgressBar = ({
-    onUpdateImportStatus,
-    importStatus,
-    importSections,
-    file,
-}) => {
-    const { startRequest } = useContext(ImporterContext);
-
-    function requestCheckIcon() {
-        if (startRequest || file) {
-            return <img src={`./images/request-done.svg`} />;
-        } else {
-            return <div className={`number request-number`}>1</div>;
-        }
-    }
-
-    function checkForIcon(section) {
-        if (
-            section !== "request" &&
-            importSections.indexOf(importStatus) >
-                importSections.indexOf(section)
-        ) {
-            return <img src={`./images/${section}-done.svg`} />;
-        }
-        if (
-            importSections.indexOf(importStatus) ===
-            importSections.indexOf(section)
-        )
-            return (
-                <div className={`number ${section}-number`}>
-                    {importSections.indexOf(importStatus) + 1}
-                </div>
-            );
-    }
-
+const ProgressBar = ({ onUpdateImportStatus, importSections }) => {
     return (
         <div className="progress-bar" id="progress-bar">
             {importSections.map((section, index) => (
@@ -47,9 +12,9 @@ const ProgressBar = ({
                     className={`section`}
                 >
                     <div className={`line ${section}-progress`}>
-                        {section === "request"
-                            ? requestCheckIcon()
-                            : checkForIcon(section)}
+                        <div className={`number ${section}-number`}>
+                            {index + 1}
+                        </div>
                     </div>
                 </div>
             ))}
