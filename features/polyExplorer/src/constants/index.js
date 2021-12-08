d7318b8f771f68db2769fa16b5e95e46c087f386
Richard Stoffels <richard.stoffels@polypoly.net>
2021-12-08 18:22:06 +0100

    Readd needed constants

---
+++ features/polyExplorer/src/constants/index.js
@@ -0,0 +1,22 @@
+export const ANIMATION_TIME = {
+    LARGE: 1000,
+    SHORT: 500,
+};
+
+export const OPACITY_RANGE = {
+    TRANSPARENT: 0,
+    SEMI_TRANSPARENT: 0.15,
+    NOT_TRANSPARENT: 1,
+};
+
+export const DIAGRAMS = {
+    FLAT: "flat",
+    INDUSTRY_HIGHLIGHT: "industryHighlight",
+    COMPANY_HIGHLIGHT: "companyHighlight",
+    ALL_INDUSTRIES: "allIndustries",
+};
+
+export const INTRO_ANIMATIONS = {
+    FORWARD: "forward",
+    BACKWARD: "backward",
+};
