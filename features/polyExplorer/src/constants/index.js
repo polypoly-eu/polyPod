ea4ce91950f956ac21d85ae4ed0d7b4a2f56870d
Richard Stoffels <richard.stoffels@polypoly.net>
2021-12-09 08:45:52 +0100

    Readd the needed constants

--- features/polyExplorer/src/constants/index.js
+++ features/polyExplorer/src/constants/index.js
@@ -16,7 +16,14 @@ export const DIAGRAMS = {
     ALL_INDUSTRIES: "allIndustries",
 };
 
-export const INTRO_ANIMATIONS = {
-    FORWARD: "forward",
-    BACKWARD: "backward",
+export const DONUT_CHART = {
+    DEFAULT_GROUP: "default",
+    DEFAULT_COLOR: "#3BA6FF",
+    FACEBOOK_GROUP: "Facebook",
+    FACEBOOK_COLOR: "#3749A9",
+};
+
+export const BUBBLES_SPEECH_SIZES = {
+    BIG: "big",
+    SMALL: "small",
 };
