e70882af601a28fe2bf7699dbb8ee06d31ef8f47
Richard Stoffels <richard.stoffels@polypoly.net>
2021-12-20 10:51:31 +0100

    Remove default theme for cascading

--- core/utils/poly-look/src/react/chips/chips.jsx
+++ core/utils/poly-look/src/react/chips/chips.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 import "./chips.css";
 
-const Chips = ({ chipsContent, activeChip, onChipClick, theme = "light" }) => {
+const Chips = ({ chipsContent, activeChip, onChipClick, theme }) => {
   return (
-    <div className={`poly-theme-${theme} chips-container`}>
+    <div className={`${theme ? `poly-theme-${theme}` : ""} poly-chips`}>
       {chipsContent.map((e) => {
         const title = e.title || e.id || e;
         return (
