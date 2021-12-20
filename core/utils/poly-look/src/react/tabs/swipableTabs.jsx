ce0307e1a56ff080cd8b1c935e7f7081f4ae22ac
Richard Stoffels <richard.stoffels@polypoly.net>
2021-12-20 11:21:16 +0100

    Fix lint

--- core/utils/poly-look/src/react/tabs/swipableTabs.jsx
+++ core/utils/poly-look/src/react/tabs/swipableTabs.jsx
@@ -5,44 +5,42 @@ import Tab from "./tab.jsx";
 import "./tabs.css";
 
 const SwipableTabs = ({ children }) => {
-    const [activeTab, setActiveTab] = useState(children[0].props.label);
+  const [activeTab, setActiveTab] = useState(children[0].props.label);
 
-    const onTabClick = (ev, newActiveTabId) => {
-        ev.preventDefault();
-        setActiveTab(newActiveTabId);
-    };
+  const onTabClick = (ev, newActiveTabId) => {
+    ev.preventDefault();
+    setActiveTab(newActiveTabId);
+  };
 
-    return (
-        <div className="tabs swipable-tabs">
-            <div className="tab-buttons">
-                {children.map((tab, index) => (
-                    <button
-                        key={index}
-                        onClick={(ev) => onTabClick(ev, tab.props.label)}
-                        className={
-                            tab.props.label === activeTab
-                                ? "tab-button active"
-                                : "tab-button"
-                        }
-                    >
-                        {tab.props.label}
-                    </button>
-                ))}
-            </div>
-            <Swiper
-                spaceBetween={1}
-                slidesPerView={1}
-                initialSlide={0}
-                onSlideChange={(swiper) =>
-                    setActiveTab(children[swiper.activeIndex].props.label)
-                }
-            >
-                {children.map((tab, index) => (
-                    <SwiperSlide key={index}>{tab.props.children}</SwiperSlide>
-                ))}
-            </Swiper>
-        </div>
-    );
+  return (
+    <div className="tabs swipable-tabs">
+      <div className="tab-buttons">
+        {children.map((tab, index) => (
+          <button
+            key={index}
+            onClick={(ev) => onTabClick(ev, tab.props.label)}
+            className={
+              tab.props.label === activeTab ? "tab-button active" : "tab-button"
+            }
+          >
+            {tab.props.label}
+          </button>
+        ))}
+      </div>
+      <Swiper
+        spaceBetween={1}
+        slidesPerView={1}
+        initialSlide={0}
+        onSlideChange={(swiper) =>
+          setActiveTab(children[swiper.activeIndex].props.label)
+        }
+      >
+        {children.map((tab, index) => (
+          <SwiperSlide key={index}>{tab.props.children}</SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
 };
 
 export { SwipableTabs, Tab };
--- core/utils/poly-look/src/react/tabs/tabs.jsx
+++ core/utils/poly-look/src/react/tabs/tabs.jsx
@@ -4,38 +4,33 @@ import Tab from "./tab.jsx";
 import "./tabs.css";
 
 const Tabs = ({ children }) => {
-    const [activeTab, setActiveTab] = useState(children[0].props.label);
+  const [activeTab, setActiveTab] = useState(children[0].props.label);
 
-    const onTabClick = (ev, newActiveTabId) => {
-        ev.preventDefault();
-        setActiveTab(newActiveTabId);
-    };
+  const onTabClick = (ev, newActiveTabId) => {
+    ev.preventDefault();
+    setActiveTab(newActiveTabId);
+  };
 
-    return (
-        <div className="tabs">
-            <div className="tab-buttons">
-                {children.map((tab, index) => (
-                    <button
-                        key={index}
-                        onClick={(ev) => onTabClick(ev, tab.props.label)}
-                        className={
-                            tab.props.label === activeTab
-                                ? "tab-button active"
-                                : "tab-button"
-                        }
-                    >
-                        {tab.props.label}
-                    </button>
-                ))}
-            </div>
-            <div className="tab-content">
-                {
-                    children.find((tab) => tab.props.label == activeTab).props
-                        .children
-                }
-            </div>
-        </div>
-    );
+  return (
+    <div className="tabs">
+      <div className="tab-buttons">
+        {children.map((tab, index) => (
+          <button
+            key={index}
+            onClick={(ev) => onTabClick(ev, tab.props.label)}
+            className={
+              tab.props.label === activeTab ? "tab-button active" : "tab-button"
+            }
+          >
+            {tab.props.label}
+          </button>
+        ))}
+      </div>
+      <div className="tab-content">
+        {children.find((tab) => tab.props.label == activeTab).props.children}
+      </div>
+    </div>
+  );
 };
 
 export { Tabs, Tab };
