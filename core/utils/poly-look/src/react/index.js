6ff9f2eb87aaba7df33e30287cf3374f00df8f66
Richard Stoffels <richard.stoffels@polypoly.net>
2021-12-21 11:25:49 +0100

    Fix export statement

--- core/utils/poly-look/src/react/index.js
+++ core/utils/poly-look/src/react/index.js
@@ -1,6 +1,5 @@
 import Chips from "./chips/chips.jsx";
-export { Chips };
 import Tab from "./tabs/tab.jsx";
 import { Tabs } from "./tabs/tabs.jsx";
 
-export { Tab, Tabs };
+export { Tab, Tabs, Chips };
