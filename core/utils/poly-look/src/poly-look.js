cd1b34273ee574837efb94b1db12b71f534a3a57
Richard Stoffels <57987795+RichardSto@users.noreply.github.com>
2021-12-21 11:46:28 +0100

    Add chips and themes to poly-look (#563)
    * Add chips and themes to poly-look
    
    * Add way to just add an array or have it named title or id
    
    * Add colors to poly-look to change place of color definition to global
    
    * Remove default theme for cascading
    
    * Add default theme
    
    * Add theme to fbImport
    
    * Fix export statement

--- core/utils/poly-look/src/poly-look.js
+++ core/utils/poly-look/src/poly-look.js
@@ -1,11 +1,14 @@
 import "./styles.css";
+import "./themes.css";
 import "./colors.css";
+
 import {
   BubbleCluster,
   VerticalBarChart,
   MirroredBarChart,
   SankeyDiagram,
 } from "./visualisations/charts";
+import { Chips } from "./react/index";
 import { PolyChart } from "./visualisations/wrappers/react/polyChart.jsx";
 import { Tab, Tabs } from "./react";
 export {
@@ -14,6 +17,7 @@ export {
   MirroredBarChart,
   SankeyDiagram,
   PolyChart,
+  Chips,
   Tab,
   Tabs,
 };
