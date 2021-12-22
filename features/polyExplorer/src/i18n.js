4271e493abcb34669d994622838e23ef0f9ac322
Felix Dahlke <felix.dahlke@polypoly.net>
2021-12-22 18:07:41 +0100

    Eliminate notification area

--- features/polyExplorer/src/i18n.js
+++ features/polyExplorer/src/i18n.js
@@ -19,7 +19,6 @@ import explorationPurposeInfoScreenEn from "./locales/en/explorationPurposeInfoS
 import explorationCompaniesInfoScreenEn from "./locales/en/explorationCompaniesInfoScreen.json";
 import explorationJurisdictionInfoScreenEn from "./locales/en/explorationJurisdictionInfoScreen.json";
 import infographicEn from "./locales/en/infographic.json";
-import storiesPreviewScreenEn from "./locales/en/stories/storiesPreviewScreen.json";
 import clusterStoriesPreviewEn from "./locales/en/stories/clusterStoriesPreview.json";
 import clusterMessengerStoryEn from "./locales/en/stories/clusterMessengerStory.json";
 import clusterDigitalGiantsStoryEn from "./locales/en/stories/clusterDigitalGiants.json";
@@ -45,7 +44,6 @@ import explorationPurposeInfoScreenDe from "./locales/de/explorationPurposeInfoS
 import explorationCompaniesInfoScreenDe from "./locales/de/explorationCompaniesInfoScreen.json";
 import explorationJurisdictionInfoScreenDe from "./locales/de/explorationJurisdictionInfoScreen.json";
 import infographicDe from "./locales/de/infographic.json";
-import storiesPreviewScreenDe from "./locales/de/stories/storiesPreviewScreen.json";
 import clusterStoriesPreviewDe from "./locales/de/stories/clusterStoriesPreview.json";
 import clusterMessengerStoryDe from "./locales/de/stories/clusterMessengerStory.json";
 import clusterDigitalGiantsStoryDe from "./locales/de/stories/clusterDigitalGiants.json";
@@ -73,7 +71,6 @@ export default new I18n(determineLanguage(), {
         explorationCompaniesInfoScreen: explorationCompaniesInfoScreenEn,
         explorationJurisdictionInfoScreen: explorationJurisdictionInfoScreenEn,
         infographic: infographicEn,
-        storiesPreviewScreen: storiesPreviewScreenEn,
         clusterStoriesPreview: clusterStoriesPreviewEn,
         clusterMessengerStory: clusterMessengerStoryEn,
         clusterDigitalGiantsStory: clusterDigitalGiantsStoryEn,
@@ -100,7 +97,6 @@ export default new I18n(determineLanguage(), {
         explorationCompaniesInfoScreen: explorationCompaniesInfoScreenDe,
         explorationJurisdictionInfoScreen: explorationJurisdictionInfoScreenDe,
         infographic: infographicDe,
-        storiesPreviewScreen: storiesPreviewScreenDe,
         clusterStoriesPreview: clusterStoriesPreviewDe,
         clusterMessengerStory: clusterMessengerStoryDe,
         clusterDigitalGiantsStory: clusterDigitalGiantsStoryDe,
