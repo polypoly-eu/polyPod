fe6b442789df5a7d32e4cfea42b4056011062245
Richard Stoffels <richard.stoffels@polypoly.net>
2022-01-06 15:42:27 +0100

    Add cypress and write first generic test

---
+++ core/utils/poly-look/cypress/integration/react/chips.spec.js
@@ -0,0 +1,24 @@
+import { Chips } from "../../../src/react";
+import React from "react";
+import { mount } from "@cypress/react";
+
+const chipsContent = [
+  { id: chip1, translation: "Chip 1" },
+  { id: chip2, translation: "Chip 2" },
+  { id: chip3, translation: "Chip 3" },
+];
+
+const onChipClick = (id, activeChips) => console.log(id, activeChips);
+
+describe("renders Chips", () => {
+  it("renders correctly", () => {
+    mount(
+      <Chips
+        chipsContent={chipsContent}
+        defaultActiveChips={[chip1]}
+        onChipClick={onChipClick}
+      />
+    );
+    cy.get(".poly-chips").should("exist");
+  });
+});
