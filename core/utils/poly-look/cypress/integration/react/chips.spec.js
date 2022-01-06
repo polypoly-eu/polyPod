4afdc2f07b7eae68a7943e9c9ebd38fe174ede58
Richard Stoffels <richard.stoffels@polypoly.net>
2022-01-06 15:53:23 +0100

    Fix linting in generated cypress code

--- core/utils/poly-look/cypress/integration/react/chips.spec.js
+++ core/utils/poly-look/cypress/integration/react/chips.spec.js
@@ -3,9 +3,9 @@ import React from "react";
 import { mount } from "@cypress/react";
 
 const chipsContent = [
-  { id: chip1, translation: "Chip 1" },
-  { id: chip2, translation: "Chip 2" },
-  { id: chip3, translation: "Chip 3" },
+  { id: "chip1", translation: "Chip 1" },
+  { id: "chip2", translation: "Chip 2" },
+  { id: "chip3", translation: "Chip 3" },
 ];
 
 const onChipClick = (id, activeChips) => console.log(id, activeChips);
