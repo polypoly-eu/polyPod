f377529a4abf9466d709812438b9fc235e1177dd
JJ Merelo <jjmerelo@gmail.com>
2022-01-10 09:05:55 +0100

    Extending to PoC

--- features/polyPreview/src/i18n.js
+++ features/polyPreview/src/i18n.js
@@ -1,14 +1,2 @@
-import { determineLanguage, I18n } from "@polypoly-eu/silly-i18n";
-
-import commonEn from "./locales/en/common.json";
-
-import commonDe from "./locales/de/common.json";
-
-export default new I18n(determineLanguage(), {
-    en: {
-        common: commonEn,
-    },
-    de: {
-        common: commonDe,
-    },
-});
+import { createI18n } from "@polypoly-eu/silly-i18n";
+export default createI18n( ['common'], ['en','de' ], "./locales" );
