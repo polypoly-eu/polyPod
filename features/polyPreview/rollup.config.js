0557cc5ff22d996bff85fa50d9bfed3058c8a698
JJ Merelo <jjmerelo@gmail.com>
2022-01-11 14:03:40 +0100

    This... works

--- features/polyPreview/rollup.config.js
+++ features/polyPreview/rollup.config.js
@@ -8,7 +8,7 @@ export default {
     input: "src/index.js",
     output: {
         file: "dist/index.js",
-        format: "iife",
+        format: "esm",
     },
     plugins: [
         css({ output: "css/bundle.css" }),
