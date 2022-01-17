8c52d16a0f2b6c37d8f2181b89027fbed6877b32
Lena Lau <lena.lau@polypoly.eu>
2022-01-17 13:20:58 +0100

    P4P-1228: add source to infoButton and adapt figma styling

--- features/polyExplorer/src/components/buttons/infoButton/infoButton.jsx
+++ features/polyExplorer/src/components/buttons/infoButton/infoButton.jsx
@@ -1,26 +1,23 @@
 import React from "react";
 import LinkButton from "../linkButton/linkButton.jsx";
 import "./infoButton.css";
+import i18n from "../../../i18n.js";
 
 const InfoButton = ({
+    source,
     light = false,
     route,
     saveActiveIndex,
     stateChange = null,
 }) => (
-    <div onClick={saveActiveIndex}>
+    <div onClick={saveActiveIndex} className="source-info-container">
+        <p className="source">{source}</p>
         <LinkButton
             className={light ? "info-button-light" : "info-button"}
             route={route}
             stateChange={stateChange}
         >
-            <img
-                src={
-                    light
-                        ? "images/question-light.svg"
-                        : "images/question-dark.svg"
-                }
-            ></img>
+            <img src="images/question-dark.svg"></img>
         </LinkButton>
     </div>
 );
