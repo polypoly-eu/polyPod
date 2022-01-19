def5551544cebf3ff4acd993c8793df64b878b8a
mteresa-jimenez <mteresa.jn@gmail.com>
2022-01-19 10:23:02 +0100

    Implement chart with example

--- features/polyExplorer/src/components/clusterStories/purposes.jsx
+++ features/polyExplorer/src/components/clusterStories/purposes.jsx
@@ -1,9 +1,28 @@
 import React from "react";
 import { Tabs, Tab } from "@polypoly-eu/poly-look";
-
+import PurposesBarChart from "../dataViz/purposesBarChart.jsx";
 import i18n from "../../i18n.js";
 
 export default function Purposes() {
+    const data = {
+        mostMentioned: [
+            { title: "Example A", value: 1079999 },
+            { title: "Example B", value: 31 },
+            { title: "Example C", value: 63588 },
+            { title: "Example for a Longer Label", value: 6400000 },
+        ],
+        multipleMentions: [
+            { title: "Example", value: 1999 },
+            { title: "Exa", value: 310 },
+            { title: "Example C", value: 638 },
+            { title: "Example for a", value: 40000 },
+        ],
+    };
+
+    const handleAnimation = () => {
+        return true;
+    };
+
     return (
         <div className="purposes">
             <Tabs>
@@ -13,7 +32,10 @@ export default function Purposes() {
                         "clusterStoryCommon:label.purposes.mostMentioned"
                     )}
                 >
-                    <p>TODO</p>
+                    <PurposesBarChart
+                        data={data.mostMentioned}
+                        animation={handleAnimation()}
+                    />
                 </Tab>
                 <Tab
                     id="multiple-mentions"
@@ -21,7 +43,10 @@ export default function Purposes() {
                         "clusterStoryCommon:label.purposes.multipleMentions"
                     )}
                 >
-                    <p>TODO</p>
+                    <PurposesBarChart
+                        data={data.multipleMentions}
+                        animation={handleAnimation()}
+                    />
                 </Tab>
             </Tabs>
         </div>
