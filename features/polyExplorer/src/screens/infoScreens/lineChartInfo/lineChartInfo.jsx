dcabd3a5a7f5260d416453688efe39432bbe312e
mteresa-jimenez <mteresa.jn@gmail.com>
2022-01-19 13:19:50 +0100

    Create new Info component

---
+++ features/polyExplorer/src/screens/infoScreens/lineChartInfo/lineChartInfo.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+// import i18n from "../../../i18n.js";
+import StoriesInfoScreen from "../../../components/clusterStories/storiesInfoScreen.jsx";
+
+const LineChartInfo = () => {
+    return <StoriesInfoScreen className="line-chart-info" />;
+};
+
+export default LineChartInfo;
