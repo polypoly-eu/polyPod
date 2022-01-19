c8026b9f5d393ba04e76f42751966a42ceb92f5c
mteresa-jimenez <mteresa.jn@gmail.com>
2022-01-19 10:30:19 +0100

    Create new component

---
+++ features/polyExplorer/src/components/clusterStories/storiesInfoScreen.jsx
@@ -0,0 +1,34 @@
+import React, { useContext } from "react";
+import { ExplorerContext } from "../../context/explorer-context.jsx";
+
+import i18n from "../../i18n.js";
+import Screen from "../screen/screen.jsx";
+
+import "./baseInfoScreen.css";
+
+const BaseInfoScreen = ({ className, infoChildren }) => {
+    const { handleBack } = useContext(ExplorerContext);
+    return (
+        <Screen className={className} light={true}>
+            <div className="base-info">
+                {infoChildren.map((infoChild, i) => (
+                    <div key={i}>
+                        <div className="info-title">
+                            <div className="line"></div>
+                            <h1>{i18n.t(`baseInfoScreen:title${i + 1}`)}</h1>
+                            <div className="line"></div>
+                        </div>
+                        {infoChild}
+                    </div>
+                ))}
+                <div className="base-info-padding">
+                    <button onClick={() => handleBack()}>
+                        {i18n.t("common:button.ok")}
+                    </button>
+                </div>
+            </div>
+        </Screen>
+    );
+};
+
+export default BaseInfoScreen;
