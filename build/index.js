4dda072630edad31fcc46d43f0f4a26240af9145
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2022-01-26 08:27:37 +0100

    Splitting yet over again

--- build/index.js
+++ build/index.js
@@ -17,69 +17,7 @@ const {
 
 const { logMain, logDetail, logDependencies, logSuccess } = require("./log.js");
 const { parseCommandLine, showUsage, parseManifest } = require("./cli.js");
-
-function extractDependencies(manifest) {
-    const allDependencies = {
-        ...manifest.dependencies,
-        ...manifest.devDependencies,
-    };
-    const localDependencies = [];
-    const remoteDependencies = [];
-    for (let [name, url] of Object.entries(allDependencies)) {
-        const group = url.startsWith("file:")
-            ? localDependencies
-            : remoteDependencies;
-        group.push(name);
-    }
-    return { localDependencies, remoteDependencies };
-}
-
-function createPackageData(path) {
-    const manifest = parseManifest(`${path}/package.json`);
-    const { localDependencies, remoteDependencies } =
-        extractDependencies(manifest);
-    return {
-        path,
-        name: manifest.name,
-        localDependencies,
-        remoteDependencies,
-        scripts: Object.keys(manifest.scripts || {}),
-    };
-}
-
-function createPackageTree(metaManifest) {
-    return Object.fromEntries(
-        metaManifest.packages
-            .map((path) => createPackageData(path))
-            .map((data) => [data.name, data])
-    );
-}
-
-function collectDependentPackages(name, packageTree) {
-    const dependents = Object.keys(packageTree).filter((key) =>
-        packageTree[key].localDependencies.includes(name)
-    );
-    let transitiveDependents = [];
-    for (let dependent of dependents)
-        transitiveDependents = transitiveDependents.concat(
-            collectDependentPackages(dependent, packageTree)
-        );
-    return dependents.concat(transitiveDependents);
-}
-
-function skipPackages(packageTree, start) {
-    if (!Object.keys(packageTree).includes(start))
-        throw `Start package with name '${start}' not found - did you use the path?`;
-
-    logMain(`Starting at package '${start}'`);
-
-    const packagesToKeep = new Set([
-        start,
-        ...collectDependentPackages(start, packageTree),
-    ]);
-    for (let [name, pkg] of Object.entries(packageTree))
-        if (!packagesToKeep.has(name)) pkg.processed = true;
-}
+const { createPackageTree, skipPackages } = require("./deps.js");
 
 function executeProcess(executable, args, env = process.env) {
     const cmd = platformize(executable);
