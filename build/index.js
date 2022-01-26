d6a9f9bc383086f8f5cd1d87a8a3e0fdd2d4cadb
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2022-01-26 08:50:32 +0100

    (possibly) last module

--- build/index.js
+++ build/index.js
@@ -18,50 +18,7 @@ const {
 const { logMain, logDetail, logDependencies, logSuccess } = require("./log.js");
 const { parseCommandLine, showUsage, parseManifest } = require("./cli.js");
 const { createPackageTree, skipPackages } = require("./deps.js");
-
-function executeProcess(executable, args, env = process.env) {
-    const cmd = platformize(executable);
-    const spawnedProcess = spawn(cmd, args, { env: env });
-    spawnedProcess.stdout.on("data", (data) => {
-        console.log(data.toString());
-    });
-
-    spawnedProcess.stderr.on("data", (data) => {
-        console.error(data.toString());
-    });
-
-    return new Promise((resolve, reject) => {
-        spawnedProcess.on("exit", (code) => {
-            if (code === 0) resolve();
-            else reject(`Process exited with ${code}`);
-        });
-    });
-}
-
-const npm = async (...args) => {
-    const start = new Date();
-    await executeProcess(
-        "npm",
-        ["--no-update-notifier", "--no-fund", ...args],
-        { ...process.env, FORCE_COLOR: 1 }
-    );
-    const elapsed = new Date() - start;
-    const realCommand = args[args.length - 1];
-    logDetail(` ${ANSIInvert("npm " + realCommand)} finished in ${elapsed} ms`);
-};
-
-async function npmInstall(name) {
-    logDetail(`${name}: Installing dependencies ...`);
-    await npm("--no-audit", "--prefer-offline", "ci");
-}
-
-async function npmRun(script, pkg) {
-    if (!pkg.scripts.includes(script)) return false;
-
-    logDetail(`${pkg.name}: Executing ${script} script ...`);
-    await npm("run", script);
-    return true;
-}
+const { executeProcess, npm, npmInstall, npmRun } = require("./npm.js");
 
 async function cleanPackage(pkg) {
     if (await npmRun("clean", pkg)) return;
