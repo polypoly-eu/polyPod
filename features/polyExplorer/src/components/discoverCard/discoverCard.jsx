1cd47a1627947bd863ce283c7cc5b882a6f6356f
Alex Farhat <alex.farhat@polypoly.net>
2022-01-27 16:06:00 +0200

    story accessible by clicking on image, text or button

--- features/polyExplorer/src/components/discoverCard/discoverCard.jsx
+++ features/polyExplorer/src/components/discoverCard/discoverCard.jsx
@@ -8,14 +8,14 @@ import { I18nSection } from "@polypoly-eu/silly-i18n";
 const i18nP = new I18nSection(i18n, "clusterStoriesPreview");
 const DiscoverCard = ({ story }) => {
     return (
-        <div className="discover-container">
+        <LinkButton route={story.route} className="discover-container">
             <img src={story.img.src} alt={i18nP.t(story.img.alt)} />
             <h3>{i18nP.t(story.title)}</h3>
             <p>{i18nP.t(story.previewText)}</p>
-            <LinkButton route={story.route} className="discover-button">
+            <div className="discover-button">
                 {i18n.t("clusterStoriesPreview:story.button.discover")}
-            </LinkButton>
-        </div>
+            </div>
+        </LinkButton>
     );
 };
 
