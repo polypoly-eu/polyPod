44f643fda9a34d2c2cfc07c334c40489915e927f
Lena Lau <lena.lau@polypoly.eu>
2022-01-31 15:55:55 +0100

    Implement newer StoriesInfoScreen to adapt correct title

--- features/polyExplorer/src/screens/featuredEntityInfo/featuredEntityInfo.jsx
+++ features/polyExplorer/src/screens/featuredEntityInfo/featuredEntityInfo.jsx
@@ -1,37 +1,37 @@
 import React from "react";
 
 import i18n from "../../i18n.js";
-import BaseInfoScreen from "../../components/baseInfoScreen/baseInfoScreen.jsx";
+import StoriesInfoScreen from "../../components/clusterStories/storiesInfoScreen.jsx";
 import Infographic from "../../components/infographic/infographic.jsx";
 
 const FeaturedEntityInfoScreen = ({ onClose }) => {
+    const FeaturedEntityInfoScreenContent = [
+        <div className="base-info-padding">
+            <p>{i18n.t("featuredEntityInfoScreen:text.main")}</p>
+
+            <Infographic
+                type="featuredEntity"
+                texts={{
+                    highlighted1: i18n.t(
+                        "infographic:featuredEntity.highlighted1"
+                    ),
+                    text1: i18n.t("infographic:featuredEntity.text1"),
+                    text2: i18n.t("infographic:featuredEntity.text2"),
+                    text3: i18n.t("infographic:featuredEntity.text3"),
+                }}
+            />
+
+            <h4>{i18n.t("featuredEntityInfoScreen:headline.average")}</h4>
+            <p>{i18n.t("featuredEntityInfoScreen:text.average")}</p>
+            <h4>{i18n.t("featuredEntityInfoScreen:headline.total")}</h4>
+            <p>{i18n.t("featuredEntityInfoScreen:text.total")}</p>
+        </div>,
+    ];
     return (
-        <BaseInfoScreen
+        <StoriesInfoScreen
             className="featured-entity-info-screen"
-            headline={i18n.t("featuredEntityInfoScreen:headline.main")}
-            onClose={onClose}
-        >
-            <div className="base-info-padding">
-                <p>{i18n.t("featuredEntityInfoScreen:text.main")}</p>
-
-                <Infographic
-                    type="featuredEntity"
-                    texts={{
-                        highlighted1: i18n.t(
-                            "infographic:featuredEntity.highlighted1"
-                        ),
-                        text1: i18n.t("infographic:featuredEntity.text1"),
-                        text2: i18n.t("infographic:featuredEntity.text2"),
-                        text3: i18n.t("infographic:featuredEntity.text3"),
-                    }}
-                />
-
-                <h2>{i18n.t("featuredEntityInfoScreen:headline.average")}</h2>
-                <p>{i18n.t("featuredEntityInfoScreen:text.average")}</p>
-                <h2>{i18n.t("featuredEntityInfoScreen:headline.total")}</h2>
-                <p>{i18n.t("featuredEntityInfoScreen:text.total")}</p>
-            </div>
-        </BaseInfoScreen>
+            infoChildren={FeaturedEntityInfoScreenContent}
+        />
     );
 };
 
