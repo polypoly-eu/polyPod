1a64ae4b4cc78302d3531f54517c814c5aca17cf
Alex Farhat <alex.farhat@polypoly.net>
2022-02-04 11:00:40 +0100

    Variable font size to be readable for all bubble sizes

--- features/polyExplorer/src/components/dataViz/dataTypeBubbleAll.jsx
+++ features/polyExplorer/src/components/dataViz/dataTypeBubbleAll.jsx
@@ -2,13 +2,27 @@ import React from "react";
 import utils from "./utils.js";
 import "./dataViz.css";
 import DataTypeBubbles from "./dataTypeBubbles.jsx";
-//import { text } from "d3";
+
+const smallBubblesFontSize = "10px";
+const bigBubblesFontSize = "20px";
+const bigBubblesRadius = 50;
+const mediumBubblesFontSize = "16px";
+const smallBubblesRadius = 20;
+const smallBubbleThreshold = 10;
 
 /*
     Component to visualize data in a non-ordered bubble-diagram
     Takes in width and height of the output svg as well as
     data object: [{dataType, value},{},..]
 */
+
+const bubbleFontSize = (d) => {
+        if (d.r < smallBubblesRadius) return smallBubblesFontSize;
+        if (d.r > bigBubblesRadius) return bigBubblesFontSize;
+        return mediumBubblesFontSize;
+    };
+    const filterText = (d) =>
+        d.r > smallBubbleThreshold ? Math.round(d.value) : "";
 
 const DataTypeBubbleAll = ({
     data,
@@ -20,11 +34,6 @@ const DataTypeBubbleAll = ({
     showValues = true,
     highlight = null,
 }) => {
-    //this is needed for the font-size calculations
-    let highestValue = 0;
-    //This is necessary because later d.count is a function
-
-    //data.sort((a, b) => b.value - a.value);
 
     // d3 svg bubble-diagram drawing function
     const drawBubblesLeafs = (leaf, bubbleContainer) => {
@@ -37,18 +46,11 @@ const DataTypeBubbleAll = ({
         showValues
             ? leaf
                   .append("text")
-                  .text((d) => {
-                      if (highestValue < 2000) return d.value.toString();
-                      else return d.value > 25 ? d.value : "";
-                  })
+                  .text((d) => filterText(d))
                   .attr("text-anchor", "middle")
                   .attr("y", ".3em")
                   .style("fill", textColor)
-                  .style("font-size", (d) => {
-                      return (
-                          (8 + d.value / (highestValue / 4)).toString() + "px"
-                      );
-                  })
+                  .style("font-size", (d) => bubbleFontSize(d))
                   .style("font-family", "Jost Medium")
                   .style("font-weight", "500")
             : null;
