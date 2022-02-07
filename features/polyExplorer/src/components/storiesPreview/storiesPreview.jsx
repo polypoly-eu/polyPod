a408f52264ff90d12253bab4bfcd4857843080f3
Alex Farhat <alex.farhat@polypoly.net>
2022-02-07 18:53:55 +0100

    placed main in tabs component, css styling and refactoring needed

--- features/polyExplorer/src/components/storiesPreview/storiesPreview.jsx
+++ features/polyExplorer/src/components/storiesPreview/storiesPreview.jsx
@@ -5,7 +5,7 @@ import "./storiesPreview.css";
 
 const StoriesPreview = ({ storiesMetadata }) => {
     return (
-        <div className="stories-preview">
+        <div className="stories-preview poly-theme-light">
             <div className="preview-scrollable">
                 {Object.values(storiesMetadata).map((story, index) => (
                     <DiscoverCard key={index} story={story} />
--- features/polyExplorer/src/screens/main/main.jsx
+++ features/polyExplorer/src/screens/main/main.jsx
@@ -7,44 +7,30 @@ import StoriesPreview from "../../components/storiesPreview/storiesPreview.jsx";
 
 import "./main.css";
 import { ExplorerContext } from "../../context/explorer-context.jsx";
+import { Tab, Tabs } from "@polypoly-eu/poly-look";
 
 const MainScreen = () => {
-    const { navigationState, routeTo, entities, storiesMetadata } =
-        useContext(ExplorerContext);
-    let showClusters = navigationState.showClusters;
-
+    const { storiesMetadata } = useContext(ExplorerContext);
     return (
         <Screen
             noScroll
             className="main-screen"
             topShadow={false}
-            theme={showClusters ? "poly-theme-light" : "poly-theme-dark"}
+            theme="poly-theme-dark"
         >
             <div className="nav-button-container poly-nav-bar-separator-bottom">
-                <button
-                    onClick={() => routeTo("main", { showClusters: true })}
-                    className={
-                        showClusters ? "nav-button active" : "nav-button"
-                    }
-                >
-                    {i18n.t("mainScreen:tab.discover")}
-                </button>
-                <button
-                    onClick={() => routeTo("main", { showClusters: false })}
-                    className={
-                        showClusters ? "nav-button" : "nav-button active"
-                    }
-                >
-                    {i18n.t("mainScreen:tab.explore", {
-                        total: Object.keys(entities).length,
-                    })}
-                </button>
+                <Tabs swipe={true}>
+                    <Tab
+                        id="discover"
+                        label={i18n.t("mainScreen:tab.discover")}
+                    >
+                        <StoriesPreview storiesMetadata={storiesMetadata} />
+                    </Tab>
+                    <Tab id="explore" label={i18n.t("mainScreen:tab.explore")}>
+                        <FilteredEntityList />
+                    </Tab>
+                </Tabs>
             </div>
-            {showClusters ? (
-                <StoriesPreview storiesMetadata={storiesMetadata} />
-            ) : (
-                <FilteredEntityList />
-            )}
         </Screen>
     );
 };
