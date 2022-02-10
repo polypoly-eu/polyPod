6b7888243b70893d6b1f95d53aa053f6c6590685
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2022-02-10 08:28:59 +0100

    Splits locale functions to new file

--- core/utils/silly-i18n/src/locale.js
+++ core/utils/silly-i18n/src/locale.js
@@ -4,9 +4,20 @@ import {
     NonExistingSectionError,
 } from "./errors.js";
 
-import { determineLocale, determineLanguage } from "./locale.js";
+/**
+ * Determines the environment locale in a platform-independent way
+ *
+ * @returns a standard name for the language, such as 'en-US'. The first code corresponds to the language
+ */
+export const determineLocale = () =>
+    Intl.DateTimeFormat().resolvedOptions().locale;
 
-export { determineLocale, determineLanguage };
+/**
+ * Determines the environment language
+ *
+ * @returns a two-character standard name for the language, such as 'en'
+ */
+export const determineLanguage = () => determineLocale().split("-")[0];
 
 /**
  * Simple class for performing string translations, with simple templating capabilities
