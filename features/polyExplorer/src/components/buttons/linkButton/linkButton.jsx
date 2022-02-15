polypoly-eu/polyPod

commit e0bd82c727e869badb7533fcc62b81fafa73dd61

Add scrolling progress memory to stories (#696)
* Add scrolling progress memory to stories:

* Undo unrelated async change

* Make component so it is understandable what happens. Also fix major bug where a ref was passed instead of destructuring... omg

* Fix Nits

features/polyExplorer/src/components/buttons/linkButton/linkButton.jsx
@@ -12,7 +12,7 @@ const LinkButton = ({
     const { navigationState, changeNavigationState, handleBack } =
         useContext(ExplorerContext);
     const history = useHistory();
-    let changedNavigationState = navigationState;
+    let changedNavigationState = { ...navigationState };
 
     const onClickButton = () => {
         onClick();

features/polyExplorer/src/components/clusterStory/clusterStory.jsx
@@ -1,9 +1,8 @@
-import React, { useRef } from "react";
+import React from "react";
 import Screen from "../screen/screen.jsx";
 
 import "./clusterStory.css";
 
-const defaultProgressBarColor = "#3BA6FF";
 const fadingTopBackgroundDefaultDistance = "600px";
 
 /* This component is used as a base for the different Cluster Stories screens. In order to style the screen,
@@ -21,42 +20,19 @@ const fadingTopBackgroundDefaultDistance = "600px";
 
 const ClusterStory = ({
     children,
-    progressBarColor = defaultProgressBarColor,
     className,
     primaryColor,
     fadingTopBackground,
+    setScrollingRef,
 }) => {
-    const scrollRef = useRef();
-    const progressRef = useRef();
-
-    const handleScrollUp = () => {
-        scrollRef.current.scrollTo(0, 0);
-    };
-
-    const handleProgress = () => {
-        progressRef.current.style.width = `${
-            (scrollRef.current.scrollTop /
-                (scrollRef.current.scrollHeight -
-                    scrollRef.current.offsetHeight)) *
-            100
-        }%`;
-    };
-
     return (
         <Screen
             className={`cluster-story ${className}`}
             theme={"poly-theme-light"}
+            setScrollingRef={setScrollingRef}
         >
             <div
-                className="progress-bar"
-                style={{ backgroundColor: progressBarColor }}
-                ref={progressRef}
-            ></div>
-            <div className="scroll-up-btn" onClick={handleScrollUp}></div>
-            <div
                 className="content"
-                ref={scrollRef}
-                onScroll={() => handleProgress()}
                 style={{
                     "--story-color-primary": primaryColor,
                     background: fadingTopBackground

features/polyExplorer/src/components/screen/screen.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 import "./screen.css";
 
@@ -8,11 +8,19 @@ const Screen = ({
     topShadow = true,
     children,
     noScroll = false,
+    setScrollingRef,
 }) => {
+    const scrollingRef = useRef();
+
+    useEffect(() => {
+        setScrollingRef && setScrollingRef(scrollingRef.current);
+    }, []);
+
     return (
         <div className={`${theme || ""} explorer-container`}>
             {topShadow && <div className="poly-nav-bar-separator-overlay" />}
             <div
+                ref={scrollingRef}
                 className={`${
                     noScroll && "noScroll"
                 } screen-content ${className}`}

features/polyExplorer/src/components/scrollingProgressTracker/scrollingProgressTracker.jsx
@@ -1,0 +1,33 @@
+import React, { useEffect } from "react";
+
+import { useHistory } from "react-router-dom";
+
+/*
+    This component is used for saving a scrolling progression in a screen when a child of it is clicked.
+    The problem this solves is that the child's onclick has priority to the parent's one. So when the child's
+    onClick moves forward in the history the scrolling progress needs to be saved in the previous history entry.
+ */
+const ScrollingProgressTracker = ({ children, scrollingRef }) => {
+    const history = useHistory();
+
+    const handleExitStory = () => {
+        history.entries[
+            history.entries.length - 2
+        ].state.storyScrollingProgress = scrollingRef.scrollTop;
+    };
+
+    useEffect(() => {
+        scrollingRef?.scrollTo(
+            0,
+            history.location.state.storyScrollingProgress || 0
+        );
+    });
+
+    return (
+        <div className="scrolling-progress-tracker" onClick={handleExitStory}>
+            {children}
+        </div>
+    );
+};
+
+export default ScrollingProgressTracker;

features/polyExplorer/src/context/explorer-context.jsx
@@ -100,6 +100,7 @@ export const ExplorerProvider = ({ children }) => {
         "showClusters",
         "selectedEntity",
         "explorationState",
+        "storyScrollingProgress",
     ];
     const [navigationState, setNavigationState] = useState({
         firstRun: false,
@@ -110,6 +111,7 @@ export const ExplorerProvider = ({ children }) => {
             index: null,
             category: null,
         },
+        storyScrollingProgress: null,
     });
     const [activeFilters, setActiveFilters] = useState(new EntityFilter());
     const [popUp, setPopUp] = useState(null);

features/polyExplorer/src/screens/stories/digitalGiantsStory.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 
 import ClusterStory from "../../components/clusterStory/clusterStory.jsx";
 import GradientCircleList from "../../components/gradientCircleList/gradientCircleList.jsx";
@@ -13,6 +13,7 @@ import EmbeddedSankey from "../../components/embeddedSankey/embeddedSankey.jsx";
 import EntityList from "../../components/entityList/entityList.jsx";
 import SourceInfoButton from "../../components/sourceInfoButton/sourceInfoButton.jsx";
 import LinkButton from "../../components/buttons/linkButton/linkButton.jsx";
+import ScrollingProgressTracker from "../../components/scrollingProgressTracker/scrollingProgressTracker.jsx";
 
 const i18nHeader = "clusterDigitalGiantsStory";
 const i18nHeaderCommon = "clusterStoryCommon";
@@ -23,6 +24,8 @@ const bigSixNames = ["Amazon", "Apple", "Google", "Meta", "PayPal", "TikTok"];
 const DigitalGiantsStory = () => {
     const { featuredEntities, entityJurisdictionByPpid, createPopUp } =
         useContext(ExplorerContext);
+
+    const [scrollingRef, setScrollingRef] = useState(null);
 
     const bigSix = bigSixNames.map((n) => {
         const entity = featuredEntities
@@ -55,6 +58,7 @@ const DigitalGiantsStory = () => {
             fadingTopBackground={{
                 distance: "600px",
             }}
+            setScrollingRef={setScrollingRef}
         >
             <h1 className="cluster-story-main-title">
                 {i18n.t(`${i18nHeader}:title`)}
@@ -129,7 +133,9 @@ const DigitalGiantsStory = () => {
             <p className="big-first-letter">
                 {i18n.t(`${i18nHeader}:explore.further.p.1`)}
             </p>
-            <EntityList entities={bigSix} expand={true} />
+            <ScrollingProgressTracker scrollingRef={scrollingRef}>
+                <EntityList entities={bigSix} expand={true} />
+            </ScrollingProgressTracker>
             <LinkButton route={"back"} className="poly-button margin-top">
                 {i18n.t(`${i18nHeaderCommon}:discover.other.topics`)}
             </LinkButton>

features/polyExplorer/src/screens/stories/messengerStory.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 
 import ClusterStory from "../../components/clusterStory/clusterStory.jsx";
 import { ExplorerContext } from "../../context/explorer-context.jsx";
@@ -18,6 +18,7 @@ import GradientCircleList from "../../components/gradientCircleList/gradientCirc
 import MessengerTreeMap from "../../components/clusterStories/messengerTreeMap.jsx";
 import LinkButton from "../../components/buttons/linkButton/linkButton.jsx";
 import MessengerMauChart from "../../components/clusterStories/messengerMauChart.jsx";
+import ScrollingProgressTracker from "../../components/scrollingProgressTracker/scrollingProgressTracker.jsx";
 
 const i18nHeader = "clusterMessengerStory";
 const i18nHeaderCommon = "clusterStoryCommon";
@@ -31,6 +32,8 @@ const MessengerStory = () => {
         entityObjectByPpid,
         createPopUp,
     } = useContext(ExplorerContext);
+
+    const [scrollingRef, setScrollingRef] = useState(null);
 
     const listOfMessengerApps = [
         "Facebook Messenger",
@@ -115,6 +118,7 @@ const MessengerStory = () => {
             fadingTopBackground={{
                 distance: "600px",
             }}
+            setScrollingRef={setScrollingRef}
         >
             <div className="messenger-intro-background"></div>
             <h1 className="cluster-story-main-title">
@@ -254,7 +258,12 @@ const MessengerStory = () => {
             <p className="big-first-letter">
                 {i18n.t(`${i18nHeader}:explore.further.p.1`)}
             </p>
-            <EntityList entities={Object.values(products)} expand={true} />
+            <ScrollingProgressTracker
+                key="messenger"
+                scrollingRef={scrollingRef}
+            >
+                <EntityList entities={Object.values(products)} expand={true} />
+            </ScrollingProgressTracker>
             <LinkButton route={"back"} className="poly-button margin-top">
                 {i18n.t(`${i18nHeaderCommon}:discover.other.topics`)}
             </LinkButton>

commit 6a62487bda0c15459c33a512b8ec9821e680239f

PROD4POD-1380 - Revert "Add scrolling progress memory to stories"
This reverts commit e0bd82c727e869badb7533fcc62b81fafa73dd61, which
introduced a regression.

features/polyExplorer/src/components/buttons/linkButton/linkButton.jsx
@@ -12,7 +12,7 @@ const LinkButton = ({
     const { navigationState, changeNavigationState, handleBack } =
         useContext(ExplorerContext);
     const history = useHistory();
-    let changedNavigationState = { ...navigationState };
+    let changedNavigationState = navigationState;
 
     const onClickButton = () => {
         onClick();

features/polyExplorer/src/components/clusterStory/clusterStory.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useRef } from "react";
 import Screen from "../screen/screen.jsx";
 
 import "./clusterStory.css";
 
+const defaultProgressBarColor = "#3BA6FF";
 const fadingTopBackgroundDefaultDistance = "600px";
 
 /* This component is used as a base for the different Cluster Stories screens. In order to style the screen,
@@ -20,19 +21,42 @@ const fadingTopBackgroundDefaultDistance = "600px";
 
 const ClusterStory = ({
     children,
+    progressBarColor = defaultProgressBarColor,
     className,
     primaryColor,
     fadingTopBackground,
-    setScrollingRef,
 }) => {
+    const scrollRef = useRef();
+    const progressRef = useRef();
+
+    const handleScrollUp = () => {
+        scrollRef.current.scrollTo(0, 0);
+    };
+
+    const handleProgress = () => {
+        progressRef.current.style.width = `${
+            (scrollRef.current.scrollTop /
+                (scrollRef.current.scrollHeight -
+                    scrollRef.current.offsetHeight)) *
+            100
+        }%`;
+    };
+
     return (
         <Screen
             className={`cluster-story ${className}`}
             theme={"poly-theme-light"}
-            setScrollingRef={setScrollingRef}
         >
             <div
+                className="progress-bar"
+                style={{ backgroundColor: progressBarColor }}
+                ref={progressRef}
+            ></div>
+            <div className="scroll-up-btn" onClick={handleScrollUp}></div>
+            <div
                 className="content"
+                ref={scrollRef}
+                onScroll={() => handleProgress()}
                 style={{
                     "--story-color-primary": primaryColor,
                     background: fadingTopBackground

features/polyExplorer/src/components/screen/screen.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 
 import "./screen.css";
 
@@ -8,19 +8,11 @@ const Screen = ({
     topShadow = true,
     children,
     noScroll = false,
-    setScrollingRef,
 }) => {
-    const scrollingRef = useRef();
-
-    useEffect(() => {
-        setScrollingRef && setScrollingRef(scrollingRef.current);
-    }, []);
-
     return (
         <div className={`${theme || ""} explorer-container`}>
             {topShadow && <div className="poly-nav-bar-separator-overlay" />}
             <div
-                ref={scrollingRef}
                 className={`${
                     noScroll && "noScroll"
                 } screen-content ${className}`}

features/polyExplorer/src/components/scrollingProgressTracker/scrollingProgressTracker.jsx
@@ -1,33 +1,0 @@
-import React, { useEffect } from "react";
-
-import { useHistory } from "react-router-dom";
-
-/*
-    This component is used for saving a scrolling progression in a screen when a child of it is clicked.
-    The problem this solves is that the child's onclick has priority to the parent's one. So when the child's
-    onClick moves forward in the history the scrolling progress needs to be saved in the previous history entry.
- */
-const ScrollingProgressTracker = ({ children, scrollingRef }) => {
-    const history = useHistory();
-
-    const handleExitStory = () => {
-        history.entries[
-            history.entries.length - 2
-        ].state.storyScrollingProgress = scrollingRef.scrollTop;
-    };
-
-    useEffect(() => {
-        scrollingRef?.scrollTo(
-            0,
-            history.location.state.storyScrollingProgress || 0
-        );
-    });
-
-    return (
-        <div className="scrolling-progress-tracker" onClick={handleExitStory}>
-            {children}
-        </div>
-    );
-};
-
-export default ScrollingProgressTracker;

features/polyExplorer/src/context/explorer-context.jsx
@@ -100,7 +100,6 @@ export const ExplorerProvider = ({ children }) => {
         "showClusters",
         "selectedEntity",
         "explorationState",
-        "storyScrollingProgress",
     ];
     const [navigationState, setNavigationState] = useState({
         firstRun: false,
@@ -111,7 +110,6 @@ export const ExplorerProvider = ({ children }) => {
             index: null,
             category: null,
         },
-        storyScrollingProgress: null,
     });
     const [activeFilters, setActiveFilters] = useState(new EntityFilter());
     const [popUp, setPopUp] = useState(null);

features/polyExplorer/src/screens/stories/digitalGiantsStory.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 
 import ClusterStory from "../../components/clusterStory/clusterStory.jsx";
 import GradientCircleList from "../../components/gradientCircleList/gradientCircleList.jsx";
@@ -13,7 +13,6 @@ import EmbeddedSankey from "../../components/embeddedSankey/embeddedSankey.jsx";
 import EntityList from "../../components/entityList/entityList.jsx";
 import SourceInfoButton from "../../components/sourceInfoButton/sourceInfoButton.jsx";
 import LinkButton from "../../components/buttons/linkButton/linkButton.jsx";
-import ScrollingProgressTracker from "../../components/scrollingProgressTracker/scrollingProgressTracker.jsx";
 
 const i18nHeader = "clusterDigitalGiantsStory";
 const i18nHeaderCommon = "clusterStoryCommon";
@@ -24,8 +23,6 @@ const bigSixNames = ["Amazon", "Apple", "Google", "Meta", "PayPal", "TikTok"];
 const DigitalGiantsStory = () => {
     const { featuredEntities, entityJurisdictionByPpid, createPopUp } =
         useContext(ExplorerContext);
-
-    const [scrollingRef, setScrollingRef] = useState(null);
 
     const bigSix = bigSixNames.map((n) => {
         const entity = featuredEntities
@@ -58,7 +55,6 @@ const DigitalGiantsStory = () => {
             fadingTopBackground={{
                 distance: "600px",
             }}
-            setScrollingRef={setScrollingRef}
         >
             <h1 className="cluster-story-main-title">
                 {i18n.t(`${i18nHeader}:title`)}
@@ -133,9 +129,7 @@ const DigitalGiantsStory = () => {
             <p className="big-first-letter">
                 {i18n.t(`${i18nHeader}:explore.further.p.1`)}
             </p>
-            <ScrollingProgressTracker scrollingRef={scrollingRef}>
-                <EntityList entities={bigSix} expand={true} />
-            </ScrollingProgressTracker>
+            <EntityList entities={bigSix} expand={true} />
             <LinkButton route={"back"} className="poly-button margin-top">
                 {i18n.t(`${i18nHeaderCommon}:discover.other.topics`)}
             </LinkButton>

features/polyExplorer/src/screens/stories/messengerStory.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 
 import ClusterStory from "../../components/clusterStory/clusterStory.jsx";
 import { ExplorerContext } from "../../context/explorer-context.jsx";
@@ -18,7 +18,6 @@ import GradientCircleList from "../../components/gradientCircleList/gradientCirc
 import MessengerTreeMap from "../../components/clusterStories/messengerTreeMap.jsx";
 import LinkButton from "../../components/buttons/linkButton/linkButton.jsx";
 import MessengerMauChart from "../../components/clusterStories/messengerMauChart.jsx";
-import ScrollingProgressTracker from "../../components/scrollingProgressTracker/scrollingProgressTracker.jsx";
 
 const i18nHeader = "clusterMessengerStory";
 const i18nHeaderCommon = "clusterStoryCommon";
@@ -32,8 +31,6 @@ const MessengerStory = () => {
         entityObjectByPpid,
         createPopUp,
     } = useContext(ExplorerContext);
-
-    const [scrollingRef, setScrollingRef] = useState(null);
 
     const listOfMessengerApps = [
         "Facebook Messenger",
@@ -118,7 +115,6 @@ const MessengerStory = () => {
             fadingTopBackground={{
                 distance: "600px",
             }}
-            setScrollingRef={setScrollingRef}
         >
             <div className="messenger-intro-background"></div>
             <h1 className="cluster-story-main-title">
@@ -258,12 +254,7 @@ const MessengerStory = () => {
             <p className="big-first-letter">
                 {i18n.t(`${i18nHeader}:explore.further.p.1`)}
             </p>
-            <ScrollingProgressTracker
-                key="messenger"
-                scrollingRef={scrollingRef}
-            >
-                <EntityList entities={Object.values(products)} expand={true} />
-            </ScrollingProgressTracker>
+            <EntityList entities={Object.values(products)} expand={true} />
             <LinkButton route={"back"} className="poly-button margin-top">
                 {i18n.t(`${i18nHeaderCommon}:discover.other.topics`)}
             </LinkButton>