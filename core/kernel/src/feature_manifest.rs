60211f887817f75b5105f0124d2c86c664fb91f7
Ghenadie Vasiliev-Pusca <ghenadie.pusca@polypoly.net>
2022-02-18 09:23:09 +0200

    Parse full feature manifest

--- core/kernel/src/feature_manifest.rs
+++ core/kernel/src/feature_manifest.rs
@@ -1,6 +1,9 @@
 use std::collections::HashMap;
+use serde::{Deserialize};
 
+#[derive(Deserialize)]
 pub struct FeatureManifest {
+    // Everything is Option? interesting
     name: Option<String>,
     author: Option<String>,
     version: Option<String>,
@@ -9,4 +12,27 @@ pub struct FeatureManifest {
     thumbnail_color: Option<String>,
     primary_color: Option<String>,
     links: Option<HashMap<String, String>>
+}
+
+#[derive(Deserialize)]
+struct FullFeatureManifest {
+    name: Option<String>,
+    author: Option<String>,
+    version: Option<String>,
+    description: Option<String>,
+    thumbnail: Option<String>,
+    thumbnail_color: Option<String>,
+    primary_color: Option<String>,
+    links: Option<HashMap<String, String>>,
+    translations: Option<HashMap<String, FeatureManifest>>
+}
+
+type JSONString = str;
+
+impl TryFrom<&JSONString> for FullFeatureManifest {
+    type Error = String;
+
+    fn try_from(value: &JSONString) -> Result<Self, Self::Error> {
+        serde_json::from_str(value).map_err(|err| err.to_string())
+    }
 }
