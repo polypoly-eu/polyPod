b05dd1b7c6406df895c916a77623f71aac2f51f8
Ghenadie Vasiliev-Pusca <ghenadie.pusca@polypoly.net>
2022-02-18 09:34:27 +0200

    Empty JSON parsing

--- core/kernel/src/feature_manifest.rs
+++ core/kernel/src/feature_manifest.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 use serde::{Deserialize};
 
-#[derive(Deserialize)]
+#[derive(Deserialize, PartialEq, Debug)]
 pub struct FeatureManifest {
     // Everything is Option? interesting
     name: Option<String>,
@@ -27,12 +27,52 @@ struct FullFeatureManifest {
     translations: Option<HashMap<String, FeatureManifest>>
 }
 
-type JSONString = str;
+// Alias for str, probably will be moved to a centralized place to be reused
+pub type JSONString = str;
+
+impl FeatureManifest {
+    // Hmm, maybe add typed error so the caler can decide based on error. 
+    fn parse(json: &JSONString, locale: &str) -> FeatureManifest {
+        FullFeatureManifest::try_from(json)
+        .map(FeatureManifest::build_feature_manifest)
+        .unwrap_or(FeatureManifest::default())
+    }
+
+    fn default() -> FeatureManifest {
+        FeatureManifest {
+            name: None,
+            author: None,
+            version: None,
+            description: None,
+            thumbnail: None,
+            thumbnail_color: None,
+            primary_color: None,
+            links: None
+        }
+    }
+
+    fn build_feature_manifest(full_manifest: FullFeatureManifest) -> FeatureManifest {
+        FeatureManifest::default()
+    }
+}
+
 
 impl TryFrom<&JSONString> for FullFeatureManifest {
     type Error = String;
 
     fn try_from(value: &JSONString) -> Result<Self, Self::Error> {
         serde_json::from_str(value).map_err(|err| err.to_string())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_empty_json() {
+        let parsed = FeatureManifest::parse("", "");
+
+        assert_eq!(parsed, FeatureManifest::default(), "Expected to return default manifest if json is empty")
     }
 }
