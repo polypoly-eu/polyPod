89da708c6bb1bf5ec03ccca9fd47fc49f0f472d4
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2022-02-19 20:08:23 +0100

    Adds another needed library

--- core/utils/poly-look/test/react-components/tabs.test.js
+++ core/utils/poly-look/test/react-components/tabs.test.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
 import { Tab, Tabs } from "../../src/react-components/tabs/tabs";
 
 /**
@@ -12,17 +13,24 @@ const tabArray = Array(3)
   .map((i) => <Tab id={`tab${i}`} label={`label${i}`} key={`k${i}`} />);
 
 it("Creates an empty Tab component", () => {
-  expect(render(singleTab)).toBeTruthy();
+  const renderedTab = render(singleTab);
+  console.log(renderedTab);
+  expect(renderedTab.container).toBeTruthy();
 });
 
 it("Creates a Tabs component", () => {
-  expect(
-    render(
-      <Tabs id="tabsID" label="tabsLabel" key="t">
-        {tabArray}
-      </Tabs>
-    )
-  ).toBeTruthy();
+  const { findByLabelText, getByText, asFragment } = render(
+    <Tabs id="tabsID" label="tabsLabel" key="t">
+      {tabArray}
+    </Tabs>
+  );
+  expect(findByLabelText("tabsLabel")).toBeInTheDocument();
+  console.log(asFragment());
+  const firstRender = asFragment();
+  tabArray.forEach((i) => {
+    console.log(i);
+    fireEvent.click(getByText(i.label));
+  });
 });
 
 it("Creates a Tabs component with swipe", () => {
