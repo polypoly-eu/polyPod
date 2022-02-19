706418b3d139b6474126e610366de32af1a047ed
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2022-02-19 19:31:58 +0100

    Testing also a group of Tabs

--- core/utils/poly-look/test/react-components/tabs.test.js
+++ core/utils/poly-look/test/react-components/tabs.test.js
@@ -1,11 +1,25 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import { Tab } from "../../src/react-components/tabs/tabs";
+import { Tab, Tabs } from "../../src/react-components/tabs/tabs";
 
 /**
  * @jest-environment jsdom
  */
-
+const singleTab = <Tab id="tabID" label="tabLabel" key="t" />;
 it("Creates an empty Tab component", () => {
-  expect(render(<Tab id="tabID" label="tabLabel" key="t" />)).toBeTruthy();
+  expect(render(singleTab)).toBeTruthy();
+});
+
+it("Creates a Tabs component", () => {
+  let tabArray = Array(3)
+    .fill(1)
+    .map((i, j) => i + j)
+    .map((i) => <Tab id={`tab${i}`} label={`label${i}`} key={`k${i}`} />);
+  expect(
+    render(
+      <Tabs id="tabsID" label="tabsLabel" key="t">
+        {tabArray}
+      </Tabs>
+    )
+  ).toBeTruthy();
 });
