79d52f4146d1c6b1dd14ec0a2adad476810a38d8
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2022-02-20 12:34:47 +0100

    :white_check_mark: with 100% coverage

--- core/utils/poly-look/test/react-components/tabs.test.js
+++ core/utils/poly-look/test/react-components/tabs.test.js
@@ -12,6 +12,11 @@ const tabArray = Array(3)
   .map((i, j) => i + j)
   .map((i) => <Tab id={`tab${i}`} label={`label${i}`} key={`k${i}`} />);
 
+let tabChangedTo;
+const onTabChange = (newTab) => {
+  tabChangedTo = newTab;
+};
+
 it("Creates an empty Tab component", () => {
   const renderedTab = render(singleTab);
   expect(renderedTab.container).toBeTruthy();
@@ -30,5 +35,18 @@ it("Creates Tabs components", () => {
       expect(getByText(i.props.label)).toHaveClass("tab-button active");
     });
     unmount();
+  });
+});
+
+it("Checks onTabChange is called", () => {
+  const { getByText, unmount } = render(
+    <Tabs id="tabsID" label="tabsLabel" key="t" onTabChange={onTabChange}>
+      {tabArray}
+    </Tabs>
+  );
+  tabArray.forEach((i) => {
+    expect(getByText(i.props.label)).toBeInTheDocument();
+    fireEvent.click(getByText(i.props.label));
+    expect(tabChangedTo).toBe(i.props.id);
   });
 });
