7124cf31c77184e6bc2f1d397685058ca57e31a1
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2022-02-21 15:11:48 +0100

    :recycle: to use the new/old file

--- features/polyExplorer/test/company.js
+++ features/polyExplorer/test/company.js
@@ -1,16 +1,13 @@
 "use strict";
-import { readFileSync } from "fs";
 
 import assert from "assert";
 import * as company from "../src/model/company.js";
 import testGlobalJSON from "./testGlobal.js";
+import oneCompany from "./testCompanyData.js";
 
 const i18n = {
     language: "en",
 };
-
-const testCompanyJSON = JSON.parse(readFileSync("src/data/companies.json"));
-const oneCompany = testCompanyJSON.filter((c) => c.ppid === "Apple (US)")[0];
 
 describe("Company", function () {
     const testCompany = new company.Company(oneCompany, testGlobalJSON, i18n);
