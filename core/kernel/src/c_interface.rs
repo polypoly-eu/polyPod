4f83c96dffcf47c4999d2b3822839ceb67a368f2
Ghenadie Vasiliev-Pusca <ghenadie.pusca@polypoly.net>
2022-02-22 12:52:03 +0200

    Some docs refinement

--- core/kernel/src/c_interface.rs
+++ core/kernel/src/c_interface.rs
@@ -15,7 +15,7 @@ use std::{
 ///
 /// Bootstrap the kernel with the given configuration:
 /// - language_code: User's locale language code
-/// Returns the result JSON of either success or failure to bootstrap kernel
+/// Returns the result JSON of either success or failure to bootstrap the kernel
 #[no_mangle]
 pub unsafe extern "C" fn kernel_bootstrap(language_code: *const c_char) -> *const c_char {
     unsafe fn bootstrap(language_code: *const c_char) -> Result<(), KernelError> {
@@ -33,10 +33,13 @@ pub unsafe extern "C" fn kernel_bootstrap(language_code: *const c_char) -> *cons
 /// Returns the JSON with the parsed feature manifest by taking into account the user's locale language code.
 ///
 /// This is unfortunate at the moment that this API gets a JSON and responds back with a JSON. Ideally the kernel
-/// would be able to read all features from the disk on its own and return a JSON with all of the features.
+/// would be able to read all the features from the disk on its own and return back a JSON with all of the features.
 /// It was choosen to return back to platform code a JSON, as composing complex C model is not nice. For example
-/// for feature manifest we would have to implement/bring in the functionality of the HashMap. The JSON would allow
-/// to easily represent any complex data model in a standard way.
+/// Rust's HashMap cannot be directly mapped to C, and instead of working with Opaque types and pointers to access data from a hashmap - use the JSON.
+/// The JSON would allow to easily represent any complex data model in a standard way.
+///
+/// This API would be an example of any further API that will be added. That is - the API would respond back with a JSON
+/// containing the result of the operation with any meaningfull payload or error code/message.
 #[no_mangle]
 pub unsafe extern "C" fn parse_feature_manifest(json: *const c_char) -> *const c_char {
     unsafe fn parse_manifest(json: *const c_char) -> Result<FeatureManifest, KernelError> {
