fa0b5502166e82a814858aad07a8f940dcef95e0
Ghenadie Vasiliev-Pusca <ghenadie.pusca@polypoly.net>
2022-02-25 12:11:53 +0200

    Use flatbuffers to create the models

--- core/kernel/src/lib.rs
+++ core/kernel/src/lib.rs
@@ -1,3 +1,9 @@
 pub mod c_interface;
 mod feature_manifest;
 pub mod kernel;
+#[allow(unused_imports)]
+mod feature_manifest_generated;
+#[allow(unused_imports)]
+mod feature_manifest_response_generated;
+#[allow(unused_imports)]
+mod failure_generated;
