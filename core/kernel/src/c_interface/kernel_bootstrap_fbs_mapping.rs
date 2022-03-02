05b54f6377e1a8deeefa29b8207ef6b44e050180
Ghenadie Vasiliev-Pusca <ghenadie.pusca@polypoly.net>
2022-03-02 11:30:45 +0200

    Test kernel boostrap success response

--- core/kernel/src/c_interface/kernel_bootstrap_fbs_mapping.rs
+++ core/kernel/src/c_interface/kernel_bootstrap_fbs_mapping.rs
@@ -21,3 +21,20 @@ pub fn build_kernel_bootstrap_response(result: Result<(), KernelFailure>) -> Vec
     finish_kernel_bootstrap_response_buffer(&mut fbb, response);
     fbb.finished_data().to_owned()
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::flatbuffers_generated::kernel_bootstrap_response_generated::kernel_bootstrap_response::root_as_kernel_bootstrap_response;
+
+    use super::*;
+
+    #[test]
+    fn test_build() {
+        let byte_response = build_kernel_bootstrap_response(Ok(()));
+        let parsed = root_as_kernel_bootstrap_response(&byte_response);
+        assert!(parsed.is_ok(), "Expected response parsing to be successfull");
+        let response = parsed.unwrap();
+
+        assert!(response.failure().is_none(), "Expected response to not contain failure")
+    }
+}
