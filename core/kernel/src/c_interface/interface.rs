755b56d4a469a74e32eb194c9255fe461ad8c835
Ghenadie Vasiliev-Pusca <ghenadie.pusca@polypoly.net>
2022-03-02 14:08:28 +0200

    Refactor kernel

--- core/kernel/src/c_interface/interface.rs
+++ core/kernel/src/c_interface/interface.rs
@@ -4,8 +4,7 @@ use crate::{
         kernel_bootstrap_fbs_mapping::build_kernel_bootstrap_response, utils::cstring_to_str,
     },
     feature_manifest_parsing::FeatureManifest,
-    kernel::Kernel,
-    kernel::KERNEL,
+    kernel::{bootstrap, parse_feature_manifest},
     kernel_failure::KernelFailure,
 };
 use std::os::raw::c_char;
@@ -17,33 +16,29 @@ use std::os::raw::c_char;
 ///           Also, most likely, it will be required to expose an API to deallocate the byte buffer after parsing.
 ///
 /// Bootstrap the kernel with the given configuration:
-/// - language_code: User's locale language code
-/// Returns a flatbuffer byte array
+/// - language_code: User's locale language code.
+/// Returns a flatbuffer byte array with kernel_bootstrap_response.
 #[no_mangle]
 pub unsafe extern "C" fn kernel_bootstrap(language_code: *const c_char) -> *const u8 {
-    unsafe fn bootstrap(language_code: *const c_char) -> Result<(), KernelFailure> {
+    unsafe fn kernel_bootstrap_result(language_code: *const c_char) -> Result<(), KernelFailure> {
         let language_code = cstring_to_str(&language_code).map(String::from)?;
-        Kernel::bootstrap(language_code)
+        bootstrap(language_code)
     }
 
-    build_kernel_bootstrap_response(bootstrap(language_code)).as_ptr()
+    build_kernel_bootstrap_response(kernel_bootstrap_result(language_code)).as_ptr()
 }
 
 /// # Safety
 /// This function can be unsafe if the json pointer is null or the string is in wrong format.
+///
+/// Parse the given feature maniest json.
+/// - json: Feature manifest json string to be parsed.
+/// Returns a flatbuffer byte array with feature_manifest_response.
 #[no_mangle]
-pub unsafe extern "C" fn parse_feature_manifest(json: *const c_char) -> *const u8 {
-    unsafe fn parse_manifest(json: *const c_char) -> Result<FeatureManifest, KernelFailure> {
-        let kernel = get_kernel()?;
+pub unsafe extern "C" fn parse_feature_manifest_from_json(json: *const c_char) -> *const u8 {
+    unsafe fn parse_manifest_result(json: *const c_char) -> Result<FeatureManifest, KernelFailure> {
         let json = cstring_to_str(&json)?;
-        kernel.parse_feature_manifest(json)
+        parse_feature_manifest(json)
     }
-    build_feature_manifest_parsing_response(parse_manifest(json)).as_ptr()
-}
-
-fn get_kernel() -> Result<&'static Kernel, KernelFailure> {
-    match KERNEL.get() {
-        Some(kernel) => Ok(kernel),
-        None => Err(KernelFailure::kernel_not_bootstraped()),
-    }
+    build_feature_manifest_parsing_response(parse_manifest_result(json)).as_ptr()
 }
--- core/kernel/src/kernel.rs
+++ core/kernel/src/kernel.rs
@@ -4,28 +4,34 @@ use crate::{
 };
 use once_cell::sync::OnceCell;
 
-/// Kernel is held as a singleton.
-pub static KERNEL: OnceCell<Kernel> = OnceCell::new();
+// Kernel is held as a singleton.
+static KERNEL: OnceCell<Kernel> = OnceCell::new();
 
-/// The Kernel would act as a composition root, containing any global configuration
-/// to be shared between components, as well managing components life time.
-pub struct Kernel {
+// The Kernel would act as a composition root, containing any global configuration
+// to be shared between components, as well managing components life time.
+struct Kernel {
     language_code: String,
 }
 
-impl Kernel {
-    pub fn bootstrap(language_code: String) -> Result<(), KernelFailure> {
-        if KERNEL.get().is_some() {
-            return Err(KernelFailure::kernel_bootstrap_failed());
-        }
-
-        let kernel = Kernel { language_code };
-
-        let _ = KERNEL.set(kernel);
-        Ok(())
+pub fn bootstrap(language_code: String) -> Result<(), KernelFailure> {
+    if KERNEL.get().is_some() {
+        return Err(KernelFailure::kernel_bootstrap_failed());
     }
 
-    pub fn parse_feature_manifest(&self, json: &JSONStr) -> Result<FeatureManifest, KernelFailure> {
-        FeatureManifest::parse(json, &self.language_code)
+    let kernel = Kernel { language_code };
+
+    let _ = KERNEL.set(kernel);
+    Ok(())
+}
+
+pub fn parse_feature_manifest(json: &JSONStr) -> Result<FeatureManifest, KernelFailure> {
+    let kernel = get_instance()?;
+    FeatureManifest::parse(json, &kernel.language_code)
+}
+
+fn get_instance() -> Result<&'static Kernel, KernelFailure> {
+    match KERNEL.get() {
+        Some(kernel) => Ok(kernel),
+        None => Err(KernelFailure::kernel_not_bootstraped()),
     }
 }
