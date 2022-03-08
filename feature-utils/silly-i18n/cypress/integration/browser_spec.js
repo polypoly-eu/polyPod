108f9c9483d9b1ea9f40c08042d697082e4824f4
Juan Julián Merelo Guervós <jjmerelo@gmail.com>
2022-03-08 11:27:19 +0100

    [Task] {PROD4POD-122} Add a real l12n module (#729)
    * Creates initial versions of translator
    
    * Completed initial error checking
    
    * Add test for translation
    
    * Add integration tests
    
    * Unit-test also same languages :white_check_mark:
    
    * Spin off common code :recycle:
    
    * :recycle: common code
    
    * :white_check_mark: improve coverage
    
    * Try to create more specific regexes
    
    * Spin off test utilities
    
    + include L12n in i18n
    
    * Adapting tests to yes, undocumented private instance var
    
    * :white_check_mark: total coverage
    
    * Fix docs
    
    * :white_check_mark: adds dates and tests
    
    * :white_check_mark: for Dates

--- feature-utils/silly-i18n/cypress/integration/browser_spec.js
+++ feature-utils/silly-i18n/cypress/integration/browser_spec.js
@@ -1,5 +1,13 @@
 import { determineLanguage, determineLocale } from "../../src/locale.js";
 import { I18n } from "../../src/index.js";
+import { L12n } from "../../src/l12n.js";
+
+import {
+    bigNumber,
+    numberPairs,
+    date,
+    datePairs,
+} from "../../test/test-utils.js";
 
 const LANGUAGE = "foo";
 let i18n;
@@ -17,21 +25,32 @@ describe("Test language determination", () => {
     });
 
     it("is able to create the object in a web environment", () => {
-        expect(i18n).to.have.keys(["_locale", "language", "_translations"]);
+        expect(i18n).to.have.keys(["_l12n", "language", "_translations"]);
         expect(i18n.language).to.equal(LANGUAGE);
+    });
+});
+
+describe("Test locale options correctly in its module", () => {
+    it("Converts big numbers to locale format", () => {
+        for (const [locale, l12nString] of Object.entries(numberPairs)) {
+            const localeHere = new L12n(locale);
+            expect(localeHere.locale).to.equal(locale);
+            expect(localeHere.t(bigNumber)).to.equal(l12nString);
+        }
+    });
+
+    it("Converts dates to locale format", () => {
+        for (const [locale, l12nDate] of Object.entries(datePairs)) {
+            const localeHere = new L12n(locale);
+            expect(localeHere.t(date)).to.equal(l12nDate);
+        }
     });
 });
 
 describe("Test locale numeric options correctly", () => {
     it("Converts big numbers to locale format", () => {
-        const bigNumber = "1000000.33";
-        const localePairs = {
-            "de-DE": "1.000.000,33",
-            "es-ES": "1.000.000,33",
-            "en-GB": "1,000,000.33",
-        };
-        for (const [locale, l8nString] of Object.entries(localePairs)) {
-            i18n._locale = locale;
+        for (const [locale, l8nString] of Object.entries(numberPairs)) {
+            i18n._l12n = new L12n(locale);
             expect(i18n.locale).to.equal(locale);
             expect(i18n.t("options:opt", { opt: bigNumber })).to.equal(
                 l8nString
