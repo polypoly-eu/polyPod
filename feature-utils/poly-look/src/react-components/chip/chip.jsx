4933812aa52249ffbf7f7e7529479550a6c4f965
mteresa-jimenez <mteresa.jn@gmail.com>
2022-03-08 16:10:35 +0100

    Add parameters description

--- feature-utils/poly-look/src/react-components/chip/chip.jsx
+++ feature-utils/poly-look/src/react-components/chip/chip.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import "./chip.css";
 
+/**
+ * Individual chip
+ * @param {string} [id] - Text in chip's label, which is also acting as an if for the chip.
+ * @param {string} [translation] - Translation for the text in chip's label.
+ * @param {callback} handleClick - Chip's handleClick function (id of clicked chip).
+ * @param {boolean} active - Indicates whether the chip is selected (active) or not.
+ */
+
 const Chip = ({ id, translation, handleClick, active }) => {
   return (
     <button
