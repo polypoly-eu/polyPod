23bbf888ef716f27249c91fc847c1bef235c2ec2
mteresa-jimenez <mteresa.jn@gmail.com>
2022-03-08 17:15:34 +0100

    Fix description

--- feature-utils/poly-look/src/react-components/chip/chip.jsx
+++ feature-utils/poly-look/src/react-components/chip/chip.jsx
@@ -5,8 +5,8 @@ import "./chip.css";
  * Individual chip
  * @param {string} [id] - Text in chip's label, which is also acting as an if for the chip.
  * @param {string} [translation] - Translation for the text in chip's label.
- * @param {callback} handleClick - Chip's handleClick function (id of clicked chip).
- * @param {boolean} active - Indicates whether the chip is selected (active) or not.
+ * @param {callback} [handleClick] - Chip's handleClick function (id of clicked chip).
+ * @param {boolean} [active] - Indicates whether the chip is selected (active) or not.
  */
 
 const Chip = ({ id, translation, handleClick, active }) => {
