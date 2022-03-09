0b91e2b64c2de324a9bd56c77a4ef4e0f703c3d3
Georgiana Piscoi <georgiana.piscoi@polypoly.net>
2022-03-09 18:25:13 +0200

    properly restructured imports

--- feature-utils/poly-look/storybook/.storybook/lit/server.mjs
+++ feature-utils/poly-look/storybook/.storybook/lit/server.mjs
@@ -1,7 +1,6 @@
 import { storybookPlugin } from "@web/dev-server-storybook";
 import baseConfig from "../../web-dev-server.config.mjs";
-import { fromRollup } from "@web/dev-server-rollup";
-import { nodeResolve } from "@web/dev-server-rollup";
+import { fromRollup, nodeResolve } from "@web/dev-server-rollup";
 import svg from "rollup-plugin-svg";
 
 const svgForWebServer = fromRollup(svg);
