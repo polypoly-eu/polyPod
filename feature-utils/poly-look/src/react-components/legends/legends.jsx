1b3f92be6c431a8b71fde016399dd2abda6e0ef6
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2022-03-09 10:27:03 +0100

    :recycle: to undry

---
+++ feature-utils/poly-look/src/react-components/legends/legends.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import "./legends.css";
+
+const legendComponent = ( type, legend ) => {
+  return (
+    <div className={`${type}-legend`}>
+      {legend.map((content, index) => (
+        <div key={index} className="legend-entry">
+          <div style={{ backgroundColor: content.color }}>
+          {content.description}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const BlockLegend = ({ legend }) => legendComponent( "block", legend );
+const LineLegend = ({ legend }) => legendComponent( "type", legend );
+export { BlockLegend, LineLegend };
---
+++ feature-utils/poly-look/test/react-components/legends.test.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import {
+  BlockLegend,
+  LineLegend,
+} from "../../src/react-components/legends/legends.jsx";
+
+/**
+ * @jest-environment jsdom
+ */
+const legendValues = [
+  ["No color", "#000000"],
+  ["Gray", "#888888"],
+  ["All colors", "#ffffff"],
+];
+const legendEntries = legendValues.map((l) => {
+  return { description: l[0], color: l[1] };
+});
+const blockLegend = <BlockLegend legend={legendEntries} />;
+const lineLegend = <LineLegend legend={legendEntries} />;
+
+describe("Basic tests BlockLegend", () => {
+  it("Creates a BlockLegend component", () => {
+    const renderedBlockLegend = render(blockLegend);
+    expect(renderedBlockLegend.container).toBeTruthy();
+    expect(renderedBlockLegend.getByText("No color")).toBeInTheDocument();
+  });
+});
+
+describe("Basic tests LineLegend", () => {
+  it("Creates a LineLegend component", () => {
+    const renderedLineLegend = render(lineLegend);
+    expect(renderedLineLegend.container).toBeTruthy();
+    expect(renderedLineLegend.getByText("No color")).toBeInTheDocument();
+  });
+});
