75788346c943f59a1f44579a9941afe15bd2c4ad
mteresa-jimenez <mteresa.jn@gmail.com>
2022-03-18 12:12:11 +0100

    Fix test in Chip component

--- feature-utils/poly-look/test/react-components/chip.test.js
+++ feature-utils/poly-look/test/react-components/chip.test.js
@@ -1,11 +1,15 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Chip from "../../src/react-components/chip/chip.jsx";
 
-const chip = <Chip id="chipId" />;
+const mockedHandleClick = jest.fn();
 
-test('renders "Chip"', () => {
-  const renderedChip = render(chip);
-  expect(renderedChip.container).toBeTruthy();
+const chip = <Chip id="chipId" handleClick={mockedHandleClick} />;
+
+it("renders Chip", () => {
+  const { container, getByRole } = render(chip);
+  expect(container).toBeTruthy();
+  fireEvent.click(getByRole("button"), mockedHandleClick);
+  expect(mockedHandleClick).toHaveBeenCalled();
 });
