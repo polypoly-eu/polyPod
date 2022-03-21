5183e7388caf79979d6988e4958e23a9f261c842
Felix Dahlke <felix.dahlke@polypoly.net>
2022-03-21 15:59:06 +0100

    Revert "Try ignoring UpdateNotificationTest"
    This reverts commit 8f96c9b3df87bf66663b71be167ae4e5d9d1a58c.

--- platform/android/app/src/test/kotlin/coop/polypoly/polypod/updatenotification/UpdateNotificationTest.kt
+++ platform/android/app/src/test/kotlin/coop/polypoly/polypod/updatenotification/UpdateNotificationTest.kt
@@ -4,7 +4,6 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.truth.Truth.assertThat
 import coop.polypoly.polypod.core.UpdateNotification
-import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.annotation.Config
@@ -13,7 +12,6 @@ import org.robolectric.annotation.LooperMode
 @LooperMode(LooperMode.Mode.PAUSED)
 @RunWith(AndroidJUnit4::class)
 @Config(sdk = [Config.OLDEST_SDK])
-@Ignore("Not working on GitHub Actions")
 class UpdateNotificationTest {
     @Test
     fun `notification with id = 0 considered seen`() {
