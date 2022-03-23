ad9319be2b16b2dae42e002755a7885b68aff10b
Ghenadie Vasiliev-Pusca <ghenadie.pusca@polypoly.net>
2022-03-23 10:31:22 +0200

    Add basic bootsrap API
    - Made sure that the API is called and nothing wrong happens :)

--- platform/ios/PolyPodApp/Core/NewFeatureManifest.swift
+++ platform/ios/PolyPodApp/Core/NewFeatureManifest.swift
@@ -1,1 +1,16 @@
 import PolyPodCore
+import Foundation
+
+class Core {
+    static let instance = Core()
+    
+    private let language_code: UnsafePointer<CChar>
+    init() {
+        self.language_code = NSString(string: Language.current).utf8String!
+    }
+    
+    func bootstrap() {
+        // TODO handle flatbuffer response
+        _ = core_bootstrap(language_code)
+    }
+}
--- platform/ios/PolyPodApp/PolyPod/AppDelegate.swift
+++ platform/ios/PolyPodApp/PolyPod/AppDelegate.swift
@@ -9,6 +9,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
+        Core.instance.bootstrap()
         Log.bootstrap()
         Log.info("Application initialized")
         
