599ec9f50ed9f5204befcf90cea8350ddf3067f4
mteresa-jimenez <mteresa.jn@gmail.com>
2022-03-29 13:20:24 +0200

    Fix import in Chip test

--- feature-utils/poly-look/test/react-components/chip.test.js
+++ feature-utils/poly-look/test/react-components/chip.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import Chip from "../../src/react-components/chip/chip.jsx";
+import Chip from "../../src/react-components/filterChips/chip.jsx";
 
 const mockedHandleClick = jest.fn();
 
