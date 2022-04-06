28ebb5ae6a771c92347417498261fccff9a8bbd4
Richard Stoffels <richard.stoffels@polypoly.net>
2022-04-06 17:41:28 +0200

    Push first tests

--- feature-utils/poly-look/test/react-components/cardList.test.js
+++ feature-utils/poly-look/test/react-components/cardList.test.js
@@ -1,6 +1,23 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import "@testing-library/jest-dom";
-import { Card, CardList } from "../../src/poly-look";
+import { Card, CardList } from "../../src/react-components";
 
-describe("Card works as expected", () => {});
+const testH1 = "TestH1";
+
+const singleBasicCard = (
+  <Card>
+    <h1>{testH1}</h1>
+  </Card>
+);
+
+describe("Card works as expected", () => {
+  it("renders the card correctly", () => {
+    const renderedCard = render(singleBasicCard);
+    expect(renderedCard.container).toBeTruthy();
+  });
+
+  it("renders the children correctly", () => {
+    const { getByText } = render(singleBasicCard);
+    expect(getByText(testH1)).toBeInTheDocument();
+  });
+});
