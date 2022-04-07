92efd09ddf0e1104ab699f3accf70705290d3804
irstavr <irini.stavrakantonaki@polypoly.net>
2022-04-07 09:37:51 +0200

    setup rollup to inject json import when window.manifestData

--- platform/podjs/rollup.config.js
+++ platform/podjs/rollup.config.js
@@ -1,20 +1,34 @@
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import sucrase from "@rollup/plugin-sucrase";
-import inject from "@rollup/plugin-inject";
 import json from "@rollup/plugin-json";
+
+import inject from "@rollup/plugin-inject";
+import path from "path";
+
+const externalManifestFile = path.resolve(
+    __dirname,
+    "src/static/manifest.json"
+);
+
+const globals = {
+    [externalManifestFile]: "$inject_window_manifestData",
+};
 
 export default [
     {
         input: "src/index.ts",
+        external: [externalManifestFile, "$inject_window_manifestData"],
         output: [
             {
                 file: "dist/index.es.js",
                 format: "esm",
+                globals,
             },
             {
                 file: "dist/index.js",
                 format: "cjs",
+                globals,
             },
         ],
         plugins: [
@@ -25,17 +39,21 @@ export default [
                 transforms: ["typescript"],
             }),
             inject({
-                manifestPath: path.resolve("static/manifest.json"),
+                "window.manifestData": externalManifestFile,
+                dest: "dist",
+                preventAssignment: true,
             }),
         ],
         context: "window",
     },
     {
         input: "src/pod.ts",
+        external: [externalManifestFile, "$inject_window_manifestData"],
         output: [
             {
                 file: "dist/pod.js",
                 format: "iife",
+                globals,
             },
         ],
         plugins: [
@@ -45,6 +63,11 @@ export default [
             sucrase({
                 exclude: ["node_modules/**"],
                 transforms: ["typescript"],
+            }),
+            inject({
+                "window.manifestData": externalManifestFile,
+                dest: "dist",
+                preventAssignment: true,
             }),
         ],
         context: "window",
