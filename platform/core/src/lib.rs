cc4683723f5ab91b517dd8fe9796ebaf70d97ad2
Ghenadie Vasiliev-Pusca <ghenadie.pusca@polypoly.net>
2022-04-07 18:29:16 +0300

    Core - iOS integration with feature manifest (#784)
    * Disable bitcode
    
    - For now disable bitcode, as Rust code cannot yet be compiled with bitcode.
    
    * Add local dependencies folder
    
    - Will be hosed to hold references to local dependencies
    
    * Group without folder
    
    - Will use links to point to dependencies
    
    * Link to core framework
    
    * C Header name has to be in quotes
    
    * Add framework search paths to point to core
    
    * Add basic bootsrap API
    
    - Made sure that the API is called and nothing wrong happens :)
    
    * Group together flatbuffers related files
    
    * Run scripts with `sh`
    
    * Update Core README
    
    * Remove unrelated change
    
    * Fail if unsupported OS is set
    
    * `sh` not working properly on Linux
    
    * Update README
    
    * Link to local flatbuffers depedency
    
    * Send a byte buffer over the C API
    
    - To correctly read the bytes starting with a pointer it is needed to have the length.
    
    * Generate flatbuffers for Swift
    
    * Simple flatbuffer integration, having successful build!!!
    
    * Remove redundant faltbuffer namespaces
    
    * Put Packages in a better place
    
    * Add api to deallocated flatbuffer bytes after being used
    
    * Small refactoring
    
    * Made some fields mandatory
    
    * Fix some tests
    
    * Fix some linting issues
    
    * Describe the steps to build_ios_xcframework in makefile
    
    * Add build_core to ios makefile
    
    * Build core only once in the workflow
    
    * Update README
    
    * Revert unwanted change
    
    * Build core in test workflow
    
    * Minor code improvements
    
    * Add CoreModule package
    
    - Should encapsulate everything related to Core
    
    * Link xcframework with new package instead
    
    * Encpsulate swift in core in a SwiftPackage
    
    * Link PolyPodCore xcframework
    
    * Encapsulate Rust Core in a swift package
    
    * Small naming and docs update
    
    * Lower Swift-tools-version to 5.5
    
    * Try to run with XCode 13.3 and macos-12
    
    * Forgot to commit macos-12 change...
    
    * Small change to trigger CI
    
    * No luck with macos-12
    
    * For Xcode 13.2.1 the binary target has to be in the same folder as the package
    
    * Make link fields required
    
    * Review followup
    
    * Additional gitignore
    
    * Small fix for cleanup
    
    * Minor failures update
    
    * Consolidate failure messaging
    
    * Rename build_core to core
    
    * Fix typos
    
    * Remove empty line

--- platform/core/src/lib.rs
+++ platform/core/src/lib.rs
@@ -2,8 +2,10 @@ mod c_interface;
 mod core;
 mod core_failure;
 mod feature_manifest_parsing;
+// Disable any checks agains the generated code
 #[rustfmt::skip]
 #[allow(clippy::all)]
 #[allow(unused_imports)]
+#[allow(dead_code)]
 /// cbindgen:ignore
 mod flatbuffers_generated;
