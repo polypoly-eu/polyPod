5c83382d937c28e6695623e800a82a8c8b036f26
Ghenadie Vasiliev-Pusca <ghenadie.pusca@polypoly.net>
2022-04-11 18:32:50 +0300

    Download provisioning profile from AppStoreConnect

---
+++ platform/AppStoreConnect/Sources/AppStoreConnect/ProvisioningProfiles.swift
@@ -0,0 +1,30 @@
+import Foundation
+import AppStoreConnect_Swift_SDK
+
+extension AppStoreConnect {
+    
+    /// Retrieves the provisioning profile content for a given name
+    /// - Parameter name: The name of the provisioning profile to get the content for
+    /// - Returns: The  content of the provisioning profile
+    public func getProfileContent(forProvisioningProfile name: String) async throws -> String {
+        let profile = try await getProvisioningProfile(withName: name)
+        guard let encodedContent = profile.attributes?.profileContent else {
+            throw AppStoreConnectError.provisioningProfileContentIsMissing
+        }
+        
+        return encodedContent
+    }
+    
+    func getProvisioningProfile(withName name: String) async throws -> Profile {
+        NSLog("Retrieving provisioning profile with name \(name)")
+        let endpoint = APIEndpoint.listProfiles(filter: [.name([name])],
+                                                include: [.certificates])
+        let result = try await apiProvider.request(endpoint)
+        if let profile = result.data.first {
+            NSLog("Succesfully downloaded provisioning profile with name \(name)")
+            return profile
+        }
+        NSLog("Failed to download provisioning profile with name \(name)")
+        throw AppStoreConnectError.provisioningProfileNotFound
+    }
+}
