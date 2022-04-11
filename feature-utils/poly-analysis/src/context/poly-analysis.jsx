fb91cdf1f43a2d5de9cb2d39ca45d9b6761e2fce
Alex Farhat <alex.farhat@polypoly.net>
2022-04-11 11:11:45 +0200

    minor js doc fix

--- feature-utils/poly-analysis/src/context/poly-analysis.jsx
+++ feature-utils/poly-analysis/src/context/poly-analysis.jsx
@@ -4,7 +4,7 @@ import { analyzeFile } from "../analysis";
 /**
  * Poly Analysis Context - This context is responsible for maintaining the fileAnalysis up to date with the latest analyses based on how dataAccount changes.
  * @param {AnalysisClass[]} [subAnalysis] - Analysis class that extend RootAnalysis. Usually found in the anlysis.js file that is held in a specific feature
- * @param {context} [parentContext] - Context that holds variables that this context and other modules should react to.
+ * @param {context} [parentContext] - Reference to context that holds variables that this context and other modules should react to.
  */
 
 export const PolyAnalysisContext = createContext();
