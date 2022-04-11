152cad80d57e0e67df62deb149742a1ae2d993c6
Richard Stoffels <richard.stoffels@polypoly.net>
2022-04-11 16:21:27 +0200

    Test babel config hack

--- feature-utils/poly-look/babel.config.js
+++ feature-utils/poly-look/babel.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   presets: ["@babel/preset-env", "@babel/preset-react"],
+  env: {
+    test: {
+      presets: [["@babel/preset-env", { targets: { node: "current" } }]],
+    },
+  },
 };
