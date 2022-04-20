30f191bb75ddd2df3247be30ebc22ed7977d215a
Felix Dahlke <felix.dahlke@polypoly.net>
2022-04-20 18:43:43 +0200

    Remove redundant arrow function

--- features/polyExplorer/cypress/helpers/indexed-db.js
+++ features/polyExplorer/cypress/helpers/indexed-db.js
@@ -1,6 +1,4 @@
 export async function clearIndexedDB() {
     const databases = await indexedDB.databases();
-    await Promise.all(
-        databases.map((database) => indexedDB.deleteDatabase(database))
-    );
+    await Promise.all(databases.map(indexedDB.deleteDatabase));
 }
