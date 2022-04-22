6058b4a261a4586267db1be00810bda4bc5151c5
mteresa-jimenez <mteresa.jn@gmail.com>
2022-04-22 13:40:30 +0200

    Fix Chip test - comments in GitHub

--- feature-utils/poly-look/test/react-components/chip.test.js
+++ feature-utils/poly-look/test/react-components/chip.test.js
@@ -6,23 +6,26 @@ import Chip from "../../src/react-components/chip.jsx";
 const chipId = "chipId";
 const mockedHandleClick = jest.fn();
 
-const chip = <Chip id={chipId} handleClick={mockedHandleClick} active />;
-
 it("Chip can be clicked", () => {
-  const { container, getByText } = render(chip);
+  const { getByText, container } = render(
+    <Chip id={chipId} handleClick={mockedHandleClick} />
+  );
+  console.log(render);
   expect(container).toBeTruthy();
   fireEvent.click(getByText(chipId), mockedHandleClick);
   expect(mockedHandleClick).toHaveBeenCalled();
 });
 
 it("Chip has been selected", () => {
-  const { getByText } = render(chip);
-  const chipElement = getByText(chipId);
-  if (chip.props.active) {
-    expect(chip.props.active).toBeTruthy();
-    expect(chipElement).toHaveClass("chip selected");
-  } else {
-    expect(chip.props.active).toBeFalsy();
-    expect(chipElement).not.toHaveClass("chip selected");
-  }
+  const active = render(
+    <Chip id={chipId} handleClick={mockedHandleClick} active />
+  ).getByText(chipId);
+  expect(active).toHaveClass("chip selected");
+});
+
+it("Chip has not been selected", () => {
+  const inactive = render(
+    <Chip id={chipId} handleClick={mockedHandleClick} />
+  ).getByText(chipId);
+  expect(inactive).not.toHaveClass("chip selected");
 });
