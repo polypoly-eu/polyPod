039f31fb1b0f7ecedcd01d689e1d0f8fdcdd38e5
irstavr <irini.stavrakantonaki@polypoly.net>
2022-04-22 19:09:07 +0200

    setup for rollup loadManifest plugin

--- platform/podjs/rollup.config.js
+++ platform/podjs/rollup.config.js
@@ -1,19 +1,17 @@
 import commonjs from "@rollup/plugin-commonjs";
-// import sucrase from "@rollup/plugin-sucrase";
+import sucrase from "@rollup/plugin-sucrase";
 import json from "@rollup/plugin-json";
-import typescript from "rollup-plugin-typescript2";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import merge from "deepmerge";
 import { createBasicConfig } from "@open-wc/building-rollup";
 import path from "path";
 
-const myCopyPlugin = require("./rollup.plugin.loadManifest.js");
+const genPodJsPlugin = require("./rollup.plugin.loadManifest.js");
 
 const baseConfig = createBasicConfig();
 
 const pathResolve = (loc) => path.resolve(__dirname, loc);
 
-const externalManifestFile = "manifest.json";
 const nodeModules = "node_modules/**";
 
 export default (commandLineArgs) =>
@@ -30,22 +28,13 @@ export default (commandLineArgs) =>
                     format: "cjs",
                 },
             ],
-            // external: [externalManifestFile],
             plugins: [
-                nodeResolve(),
-                //new
-                typescript(),
-                // new
-                commonjs({
-                    transformMixedEsModules: true,
-                    include: nodeModules,
-                    extensions: [".js", ".ts", ".mjs", ".cjs"],
-                }),
                 json(),
-                // sucrase({
-                //     exclude: [nodeModules],
-                //     transforms: ["typescript"],
-                // }),
+                nodeResolve(),
+                sucrase({
+                    exclude: [nodeModules],
+                    transforms: ["typescript"],
+                }),
             ],
             context: "window",
         },
@@ -56,38 +45,20 @@ export default (commandLineArgs) =>
                     file: pathResolve("dist/pod.js"),
                     format: "iife",
                 },
-                // {
-                //     file: pathResolve("dist/pod.es.js"),
-                //     format: "esm",
-                // },
             ],
-            // external: [externalManifestFile],
             plugins: [
-                nodeResolve({
-                    browser: true,
-                }),
-                // new
-                typescript(),
-                // replaceManifest({
-                //     __DYNAMIC_IMPORT_MANIFEST__: externalManifestFile,
-                // }),
-                commonjs({
-                    // new
-                    transformMixedEsModules: true,
-                    include: nodeModules,
-                    extensions: [".js", ".ts", ".mjs", ".cjs"],
-                }),
                 json(),
+                nodeResolve(),
+                commonjs({}),
+                sucrase({
+                    exclude: [nodeModules],
+                    transforms: ["typescript"],
+                }),
                 commandLineArgs.loadManifest
-                    ? myCopyPlugin.loadManifest({
-                          src: commandLineArgs.manifestJson,
-                          dest: "/dist",
+                    ? genPodJsPlugin.loadManifest({
+                          manifestJsonPath: commandLineArgs.manifestJson,
                       })
                     : null,
-                // sucrase({
-                //     exclude: [nodeModules],
-                //     transforms: ["typescript"],
-                // }),
             ],
             context: "window",
         },
