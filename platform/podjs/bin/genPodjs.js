09c99735257b557c8692be0f099f9d6df99337f4
irstavr <irini.stavrakantonaki@polypoly.net>
2022-04-27 11:23:01 +0200

    refactor genPodJs - eliminate magicString

--- platform/podjs/bin/genPodjs.js
+++ platform/podjs/bin/genPodjs.js
@@ -1,45 +1,16 @@
-const MagicString = require("magic-string");
 const fs = require("fs");
-
-function replaceManifestData(code, manifestData) {
-    const magicString = new MagicString(code);
-    const pattern = new RegExp("window.manifestData", "g");
-
-    let hasReplacements = false;
-    let match;
-
-    while ((match = pattern.exec(code))) {
-        hasReplacements = true;
-
-        const start = match.index;
-        const end = start + match[0].length;
-
-        const replacement = String(manifestData);
-
-        magicString.overwrite(start, end, replacement);
-    }
-
-    if (!hasReplacements) {
-        return null;
-    }
-
-    return magicString.toString();
-}
 
 function executeReplacement(podJs, manifestJsonPath) {
     try {
         const manifestData = fs.readFileSync(manifestJsonPath, "utf8");
         const podJsCode = fs.readFileSync(podJs, "utf8");
 
-        const replacedCode = replaceManifestData(podJsCode, manifestData);
-        if (!replacedCode) {
-            console.log("Nothing to replace into ", podJs);
-            return;
-        }
+        const pattern = new RegExp("window.manifestData", "g");
+        const replacedCode = podJsCode.replace(pattern, manifestData);
 
         fs.writeFileSync(podJs, replacedCode);
     } catch (err) {
-        console.error("Error during replacing code", err);
+        console.error("Error while replacing code", err);
     }
 }
 
