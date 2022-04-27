ef813d73793599a45ce0335de4e1d1ac7acc2cd6
Georgiana Piscoi <georgiana.piscoi@polypoly.net>
2022-04-27 14:23:46 +0300

    Simplify poly-look.js

--- feature-utils/poly-look/src/poly-look.js
+++ feature-utils/poly-look/src/poly-look.js
@@ -1,54 +1,6 @@
 import { cssBundler } from "./css";
 cssBundler();
-import {
-  BubbleCluster,
-  VerticalBarChart,
-  MirroredBarChart,
-  SankeyDiagram,
-  TreeMap,
-} from "./visualisations/charts";
-import { PolyChart } from "./visualisations/wrappers/react/polyChart.jsx";
-import {
-  Tab,
-  Tabs,
-  Chip,
-  FilterChips,
-  BlockLegend,
-  LineLegend,
-  ErrorPopup,
-  Card,
-  ClickableCard,
-  RoutingCard,
-  List,
-  SideSwiper,
-  PolyImportProvider,
-  PolyAnalysisProvider,
-  PolyImportContext,
-  PolyAnalysisContext,
-} from "./react-components";
-import { INITIAL_HISTORY_STATE } from "./constants";
-export {
-  BubbleCluster,
-  VerticalBarChart,
-  MirroredBarChart,
-  SankeyDiagram,
-  TreeMap,
-  PolyChart,
-  Chip,
-  FilterChips,
-  Tab,
-  Tabs,
-  BlockLegend,
-  LineLegend,
-  ErrorPopup,
-  Card,
-  ClickableCard,
-  RoutingCard,
-  List,
-  PolyImportProvider,
-  PolyAnalysisProvider,
-  PolyImportContext,
-  PolyAnalysisContext,
-  INITIAL_HISTORY_STATE,
-  SideSwiper,
-};
+
+export { PolyChart } from "./visualisations/wrappers/react/polyChart.jsx";
+export * from "./react-components";
+export * from "./constants";
