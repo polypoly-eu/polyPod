23dd42632e757eb12de2a68f3e782be0b60f9f62
Richard Stoffels <richard.stoffels@polypoly.net>
2022-04-28 17:52:21 +0200

    Add tests really

---
+++ feature-utils/poly-look/test/react-components/overlays/base.test.js
@@ -0,0 +1,31 @@
+import React from "react";
+import BaseOverlay from "../../../src/react-components/overlays/base";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+const testClassName = "test1";
+const testId = "testId";
+const testComponent = (
+  <BaseOverlay className={testClassName}>
+    <div data-testid={testId}></div>
+  </BaseOverlay>
+);
+
+const centeredTestComponent = (
+  <BaseOverlay className={testClassName} centered={true}></BaseOverlay>
+);
+
+it("renders correctly", () => {
+  const { container, getByTestId } = render(testComponent);
+  expect(container).toBeTruthy();
+  const baseOverlay = getByTestId("base-overlay-test");
+  expect(baseOverlay.className).toContain("base-overlay");
+  expect(baseOverlay.className).toContain(testClassName);
+  expect(baseOverlay.className).not.toContain("centered");
+  expect(getByTestId(testId)).toBeTruthy();
+});
+
+it("adds centered class when specified", () => {
+  const { container, getByTestId } = render(centeredTestComponent);
+  expect(getByTestId("base-overlay-test").className).toContain("centered");
+});
---
+++ feature-utils/poly-look/test/react-components/overlays/loading.test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { LoadingOverlay } from "../../../src/react-components";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+const testMessage = "testMessage";
+const testGifPath = "test/test.gif";
+const testLoading = (
+  <LoadingOverlay message={testMessage} loadingGif={testGifPath} />
+);
+
+it("renders correctly", () => {
+  const { container, getByTestId } = render(testLoading);
+  expect(container).toBeTruthy();
+  expect(getByTestId("base-overlay-test").className).toContain("loading");
+  expect(getByTestId("message-test").innerHTML).toBe(testMessage);
+  expect(getByTestId("img-test").getAttribute("src")).toBe(testGifPath);
+});
