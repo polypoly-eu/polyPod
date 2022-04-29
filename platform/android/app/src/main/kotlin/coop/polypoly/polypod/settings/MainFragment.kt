15ec5ab5599396914ea3f21f4b77836eaee0dd32
irstavr <irini.stavrakantonaki@polypoly.net>
2022-04-29 19:55:01 +0200

    handle authentication preference in main fragment

--- platform/android/app/src/main/kotlin/coop/polypoly/polypod/settings/MainFragment.kt
+++ platform/android/app/src/main/kotlin/coop/polypoly/polypod/settings/MainFragment.kt
@@ -4,16 +4,52 @@ import android.os.Bundle
 import androidx.navigation.findNavController
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import coop.polypoly.polypod.Authentication
+import coop.polypoly.polypod.Preferences
 import coop.polypoly.polypod.R
 import coop.polypoly.polypod.RuntimeInfo
 
 class MainFragment : PreferenceFragmentCompat() {
+
     override fun onCreatePreferences(
         savedInstanceState: Bundle?,
         rootKey: String?
     ) {
         setPreferencesFromResource(R.xml.settings, rootKey)
         findPreference<Preference>("version")?.summary = RuntimeInfo.VERSION
+
+        findPreference<Preference>("authentication")?.onPreferenceChangeListener =
+            Preference.OnPreferenceChangeListener { preference, newValue ->
+
+                var status = newValue as Boolean;
+                println( "Pref " + preference.key + " changed to " + status.toString())
+
+                if (status) {
+                    Authentication.setUp(requireActivity()) { success ->
+                        if (!success) {
+                            println("setup auth failed")
+                            false
+                        } else {
+                            Preferences.setAuthentication(requireContext(), status)
+                            println("Enabled auth")
+                        }
+                    }
+                } else {
+                    println(requireActivity())
+                    Authentication.disable(requireActivity()) { success ->
+
+                        if (!success) {
+                            println("disable auth failed")
+                            false
+                        } else {
+                            Preferences.setAuthentication(requireContext(), status)
+                            println("Disabled auth")
+                        }
+                    }
+                }
+
+                true
+            }
 
         findPreference<Preference>("imprint")?.onPreferenceClickListener =
             Preference.OnPreferenceClickListener {
