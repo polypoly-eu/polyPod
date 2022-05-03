d5df88e7ad6b5176c7f464710ffe62fce000ef80
Richard Stoffels <richard.stoffels@polypoly.net>
2022-05-03 13:01:00 +0200

    Add negativ test case and nit

--- feature-utils/poly-look/test/react-components/overlays/screen.test.js
+++ feature-utils/poly-look/test/react-components/overlays/screen.test.js
@@ -5,11 +5,15 @@ import { Screen } from "../../../src/react-components";
 
 const testClass = "testClass";
 const testLayout = "testLayout";
+const testChild = <div data-testid="testChild"></div>;
 const testScreen = (
   <Screen className={testClass} layout={testLayout}>
     <div data-testid="testChild"></div>
+    {testChild}
   </Screen>
 );
+
+const noPropsTestScreen = <Screen></Screen>;
 
 describe("screen", () => {
   it("renders correctly", () => {
@@ -18,5 +22,12 @@ describe("screen", () => {
     expect(screen.className).toContain(testClass);
     expect(screen.className).toContain(testLayout);
     expect(getByTestId("testChild")).toBeTruthy();
+  });
+
+  it("also renders without props", () => {
+    const { container } = render(noPropsTestScreen);
+    const screen = container.querySelector(".screen");
+    expect(screen.className.trim()).toBe("screen");
+    expect(screen.children.length).toBe(0);
   });
 });
