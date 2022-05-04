b74e35f153fa2382027ee3529ba30384de502f23
irstavr <irini.stavrakantonaki@polypoly.net>
2022-05-04 13:41:17 +0200

    delete setAuthentication as unused

--- platform/android/app/src/main/kotlin/coop/polypoly/polypod/Preferences.kt
+++ platform/android/app/src/main/kotlin/coop/polypoly/polypod/Preferences.kt
@@ -6,7 +6,6 @@ import org.json.JSONObject
 
 class Preferences {
     companion object {
-        private const val authenticationKey = "authenticationKey"
         private const val firstRunKey = "firstRun"
         private const val lastNotificationIdKey = "lastNotificationId"
         private const val lastNotificationStateKey = "lastNotificationState"
@@ -37,12 +36,6 @@ class Preferences {
             val edit = getPrefs(context).edit()
             edit.putInt(lastNotificationIdKey, id)
             edit.putString(lastNotificationStateKey, state)
-            edit.commit()
-        }
-
-        fun setAuthentication(context: Context, shouldCheck: Boolean) {
-            val edit = getPrefs(context).edit()
-            edit.putBoolean(authenticationKey, shouldCheck)
             edit.commit()
         }
 
