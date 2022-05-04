da7b4c4ee4ecc07f3b4677f5ec3c50203c709f1c
Alex Farhat <alex.farhat@polypoly.net>
2022-05-04 11:15:53 +0200

    grouped Place visits analysis

--- features/google/src/model/analyses/analysisKeys.js
+++ features/google/src/model/analyses/analysisKeys.js
@@ -1,3 +1,4 @@
 export default {
     groupedActivityTypes: "groupedActivityTypes",
+    groupedPlaceVisits: "groupedPlaceVisits",
 };
---
+++ features/google/src/model/analyses/ministories/place-visits-analysis.js.js
@@ -0,0 +1,17 @@
+import { RootAnalysis } from "@polypoly-eu/poly-analysis";
+import analysisKeys from "../analysisKeys";
+
+export default class ActivitySegmentsAnalysis extends RootAnalysis {
+    async analyze({ dataAccount: googleAccount }) {
+        const placeVisits = googleAccount.placeVisits;
+        const groupedPlaceVisits = {};
+        placeVisits.forEach((place) => {
+            if (!groupedPlaceVisits[place.locationName])
+                groupedPlaceVisits[place.locationName] = 1;
+            else groupedPlaceVisits[place.locationName] += 1;
+        });
+        if (Object.keys(groupedPlaceVisits).length > 0)
+            googleAccount.analyses[analysisKeys.groupedPlaceVisits] =
+                groupedPlaceVisits;
+    }
+}
