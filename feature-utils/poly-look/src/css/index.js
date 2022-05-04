aa8186487274d7438a658bd12170eb8f53bc6ae1
Richard Stoffels <57987795+RichardSto@users.noreply.github.com>
2022-05-04 11:27:14 +0200

    Add screen and test (#848)
    * Add screen and test
    
    * Also add the export import file
    
    * Add negative test case and nit
    
    * Revert "Add negative test case and nit"
    
    This reverts commit c99b8b304b570ee91bf0ca3638ac57e55df03de7.
    
    * Add negativ test case and nit
    
    * Remove leftover merge
    
    * Add poly-prefix and layout to index
    
    * Fix tests
    
    * Remove unnecessary import

--- feature-utils/poly-look/src/css/index.js
+++ feature-utils/poly-look/src/css/index.js
@@ -2,6 +2,7 @@ import "./styles.css";
 import "./colors.css";
 import "./themes.css";
 import "./font.css";
+import "./layout.css";
 
 /**
  * Doesn't really do anything except being a place where css files are referenced so that they're bundled
---
+++ feature-utils/poly-look/test/react-components/overlays/screen.test.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { Screen } from "../../../src/react-components";
+
+const testClass = "testClass";
+const testLayout = "testLayout";
+const testChild = <div data-testid="testChild"></div>;
+const testScreen = (
+  <Screen className={testClass} layout={testLayout}>
+    {testChild}
+  </Screen>
+);
+
+const noPropsTestScreen = <Screen></Screen>;
+
+describe("screen", () => {
+  it("renders correctly", () => {
+    const { getByTestId, container } = render(testScreen);
+    const screen = container.querySelector(".poly-screen");
+    expect(screen.className).toContain(testClass);
+    expect(screen.className).toContain(testLayout);
+    expect(getByTestId("testChild")).toBeTruthy();
+  });
+
+  it("also renders without props", () => {
+    const { container } = render(noPropsTestScreen);
+    const screen = container.querySelector(".poly-screen");
+    expect(screen.className.trim()).toBe("poly-screen");
+    expect(screen.children.length).toBe(0);
+  });
+});
