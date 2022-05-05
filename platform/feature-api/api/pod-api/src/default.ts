353844bf9d8b6e848b5cb2853b9fddccb2c93496
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2022-05-05 14:02:18 +0200

    Fix paths

--- platform/feature-api/api/pod-api/src/default.ts
+++ platform/feature-api/api/pod-api/src/default.ts
@@ -8,7 +8,7 @@
  */
 
 import * as RDF from "rdf-js";
-import { dataFactory } from "@polypoly-eu/rdf";
+import { dataFactory } from "@polypoly-eu/api";
 import { Pod, PolyIn, PolyOut, PolyNav, Info, Endpoint } from "./api";
 import { EncodingOptions, FS, Stats } from "./fs";
 import { Entry } from ".";
