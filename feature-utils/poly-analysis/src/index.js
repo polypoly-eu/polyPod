6d3bd856469ec28b6f1a3179be1a7fd25bee00d2
Richard Stoffels <richard.stoffels@polypoly.net>
2022-05-10 10:05:34 +0200

    Fix all merge problems

--- feature-utils/poly-analysis/src/index.js
+++ feature-utils/poly-analysis/src/index.js
@@ -3,14 +3,11 @@ import MinistoriesStatusReport from "./report/ministories-status-report";
 
 import ReportAnalysis from "./report/report-analysis";
 import RootAnalysis from "./analysis/root-analysis";
-import {
-    jsonDataEntities,
-    relevantZipEntries,
-} from "./analysis/utils/analysis-util";
+import { jsonDataEntities, relevantZipEntries } from "./utils/analysis-util";
 import ReportStories from "./report/report-stories";
-import genericAnalyses from "./analysis/generic-analyses/generic-analyses";
-import DataImportingStatusAnalysis from "./analysis/generic-analyses/importing-status-analysis";
-import DataStructureBubblesAnalysis from "./analysis/generic-analyses/data-structure-bubbles-analysis";
+import genericAnalyses from "./generic-analyses";
+import DataImportingStatusAnalysis from "./report/data-importing-status-report";
+import DataStructureBubblesAnalysis from "./analysis/data-structure-bubbles-analysis";
 
 export {
     analyzeZip,
