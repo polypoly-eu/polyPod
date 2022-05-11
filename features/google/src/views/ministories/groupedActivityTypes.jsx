bd3ffd27ae5e937e38ba136aa9e15819cc9dca99
Richard Stoffels <richard.stoffels@polypoly.net>
2022-05-11 14:11:12 +0200

    Add placevisits

--- features/google/src/views/ministories/groupedActivityTypes.jsx
+++ features/google/src/views/ministories/groupedActivityTypes.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { SingleDataStory } from "./singleDataStory.jsx";
 import analysisKeys from "../../model/analyses/analysisKeys";
 import FilterChipBubbles from "../../components/filterChipBubbles/filterChipBubbles.jsx";
+import { mapToChartDataArray } from "@polypoly-eu/poly-look";
 
 class GroupedActivityTypesStory extends SingleDataStory {
     constructor(props) {
@@ -9,15 +10,13 @@ class GroupedActivityTypesStory extends SingleDataStory {
     }
 
     _renderSummary() {
-        const data = Object.entries(this.analysisData).map(([key, value]) => ({
-            title: key,
-            value,
-        }));
         return (
-            <div>
+            <>
                 <h1>Grouped Activity Types</h1>
-                <FilterChipBubbles data={data} />
-            </div>
+                <FilterChipBubbles
+                    data={mapToChartDataArray(this.analysisData)}
+                />
+            </>
         );
     }
 }
