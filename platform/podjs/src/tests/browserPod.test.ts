2a819814d71a1d928e29cca40811126de36d8986
Tim Palade <palade.timotei@gmail.com>
2022-05-11 15:32:59 +0300

    Also read the contents to make sure you can unarchive successfully

--- platform/podjs/src/tests/browserPod.test.ts
+++ platform/podjs/src/tests/browserPod.test.ts
@@ -1,13 +1,14 @@
 import { IDBPolyOut } from "../browserPod";
 
-test("Importing Archive", async () => {
+test("Simple Archive Import", async () => {
     const polyOut = new IDBPolyOut();
     try {
         const zipPath =
-            "https://github.com/timoteipalade/test-files/blob/main/testZip.zip";
-        const result = await polyOut.importArchive(zipPath);
-        expect(result).not.toEqual("");
             "https://github.com/timoteipalade/test-files/raw/main/testZip.zip";
+        const zipId = await polyOut.importArchive(zipPath);
+        expect(zipId).not.toEqual("");
+        const contents = await polyOut.readDir(zipId);
+        expect(contents.length).toBeGreaterThan(0);
     } catch (err) {
         expect(err).toBeUndefined();
     }
