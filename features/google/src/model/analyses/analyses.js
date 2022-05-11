138f36d31ad1e619da617a5532c4653ccf60731d
Richard Stoffels <richard.stoffels@polypoly.net>
2022-05-11 15:06:38 +0200

    Fix errors from merge

--- features/google/src/model/analyses/analyses.js
+++ features/google/src/model/analyses/analyses.js
@@ -1,13 +1,4 @@
 import PlaceVisitsAnalysis from "./ministories/place-visits-analysis.js";
 import ActivitySegmentsAnalysis from "./ministories/activity-type-analysis";
 
-<<<<<<< HEAD
-export const specificAnalyses = [
-    PlaceVisitsAnalysis,
-    ActivitySegmentsAnalysis,
-    ActivityByProductAnalysis,
-];
-
-=======
 export const specificAnalyses = [PlaceVisitsAnalysis, ActivitySegmentsAnalysis];
->>>>>>> 4fd1d47d4 (Revert "Add placevisits")
