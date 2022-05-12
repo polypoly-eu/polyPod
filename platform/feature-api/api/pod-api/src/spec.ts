499fbe0abd8d85691ab011674906c84391f698ca
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2022-05-12 12:52:38 +0200

    Moving tests so that they actually test BrowserPod
    Moving them out of the spec makes easier to only test what's needed, and not the whole lot of Pod implementations out there (which would need to be tested sooner or later)

--- platform/feature-api/api/pod-api/src/spec.ts
+++ platform/feature-api/api/pod-api/src/spec.ts
@@ -148,49 +148,6 @@ export class PodSpec {
                     );
                 });
 
-                describe("Import Archive", () => {
-                    it("only one archive", async () => {
-                        const zipPath =
-                            "https://github.com/timoteipalade/test-files/raw/main/testZip.zip";
-                        const zipId = await polyOut.importArchive(zipPath);
-                        assert.isNotEmpty(zipId);
-                        const contents = await polyOut.readDir(zipId);
-                        assert.isAtLeast(contents.length, 1);
-                    });
-
-                    it("multiple archives", async () => {
-                        const zipPath1 =
-                            "https://github.com/timoteipalade/test-files/raw/main/multipleZips1.zip";
-                        const zipPath2 =
-                            "https://github.com/timoteipalade/test-files/raw/main/multipleZips2.zip";
-
-                        const zipId1 = await polyOut.importArchive(zipPath1);
-                        assert.isNotEmpty(zipId1);
-                        const zipId2 = await polyOut.importArchive(zipPath2, zipId1);
-                        assert.equal(zipId1, zipId2);
-                        const contents = await polyOut.readDir(zipId1);
-                        const filenames = contents.map((entry) => {
-                            return entry.path;
-                        });
-                        const containsFile1 =
-                            filenames.filter((name) => {
-                                return name === "multipleZips1/file1.rtf";
-                            }).length > 0;
-                        const containsFile2 =
-                            filenames.filter((name) => {
-                                return name === "multipleZips2/file2.rtf";
-                            }).length > 0;
-                        assert.isTrue(
-                            containsFile1,
-                            `Does not contain file1. Contents are: ${JSON.stringify(contents)}`
-                        );
-                        assert.isTrue(
-                            containsFile2,
-                            `Does not contain file2. Contents are: ${JSON.stringify(contents)}`
-                        );
-                    });
-                });
-
                 it("stat/read", async () => {
                     await fc.assert(
                         fc.asyncProperty(pathGen, async (path) => {
--- platform/podjs/cypress/integration/pod_spec.js
+++ platform/podjs/cypress/integration/pod_spec.js
@@ -4,3 +4,46 @@ import "../../dist/pod.js";
 describe("pod.js satisfies spec", () => {
     podSpec(window.pod, "");
 });
+
+describe("Import Archive", () => {
+    it("only one archive", async () => {
+        const zipPath =
+            "https://github.com/timoteipalade/test-files/raw/main/testZip.zip";
+        const zipId = await window.pod.polyOut.importArchive(zipPath);
+        assert.isNotEmpty(zipId);
+        const contents = await window.pod.polyOut.readDir(zipId);
+        assert.isAtLeast(contents.length, 1);
+    });
+
+    it("multiple archives", async () => {
+        const zipPath1 =
+            "https://github.com/timoteipalade/test-files/raw/main/multipleZips1.zip";
+        const zipPath2 =
+            "https://github.com/timoteipalade/test-files/raw/main/multipleZips2.zip";
+
+        const zipId1 = await window.pod.polyOut.importArchive(zipPath1);
+        assert.isNotEmpty(zipId1);
+        const zipId2 = await window.pod.polyOut.importArchive(zipPath2, zipId1);
+        assert.equal(zipId1, zipId2);
+        const contents = await window.pod.polyOut.readDir(zipId1);
+        const filenames = contents.map((entry) => {
+            return entry.path;
+        });
+        const containsFile1 =
+            filenames.filter((name) => {
+                return name === "multipleZips1/file1.rtf";
+            }).length > 0;
+        const containsFile2 =
+            filenames.filter((name) => {
+                return name === "multipleZips2/file2.rtf";
+            }).length > 0;
+        assert.isTrue(
+            containsFile1,
+            `Does not contain file1. Contents are: ${JSON.stringify(contents)}`
+        );
+        assert.isTrue(
+            containsFile2,
+            `Does not contain file2. Contents are: ${JSON.stringify(contents)}`
+        );
+    });
+});
