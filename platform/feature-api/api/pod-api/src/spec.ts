a872d98742da4a59d32c9b01071eeafdcfeafbc7
Tim Palade <palade.timotei@gmail.com>
2022-05-12 12:40:04 +0300

    Add import archive test for multiple zips

--- platform/feature-api/api/pod-api/src/spec.ts
+++ platform/feature-api/api/pod-api/src/spec.ts
@@ -148,17 +148,41 @@ export class PodSpec {
                     );
                 });
 
-                it("importArchive", async () => {
-                    try {
+                describe("Import Archive", () => {
+                    it("only one archive", async () => {
                         const zipPath =
                             "https://github.com/timoteipalade/test-files/raw/main/testZip.zip";
                         const zipId = await polyOut.importArchive(zipPath);
-                        chai.assert.isNotEmpty(zipId);
+                        assert.isNotEmpty(zipId);
                         const contents = await polyOut.readDir(zipId);
-                        chai.assert.isAtLeast(contents.length, 1);
-                    } catch (err) {
-                        chai.assert.isUndefined(err);
-                    }
+                        assert.isAtLeast(contents.length, 1);
+                    });
+
+                    it("multiple archives", async () => {
+                        const zipPath1 =
+                            "https://github.com/timoteipalade/test-files/raw/main/multipleZips1.zip";
+                        const zipPath2 =
+                            "https://github.com/timoteipalade/test-files/raw/main/multipleZips2.zip";
+
+                        const zipId1 = await polyOut.importArchive(zipPath1);
+                        assert.isNotEmpty(zipId1);
+                        const zipId2 = await polyOut.importArchive(zipPath2, zipId1);
+                        assert.equal(zipId1, zipId2);
+                        const contents = await polyOut.readDir(zipId1);
+                        const filenames = contents.map((entry) => {
+                            return entry.path;
+                        });
+                        const containsFile1 =
+                            filenames.filter((name) => {
+                                return name === "multipleZips1/file1.rtf";
+                            }).length > 0;
+                        const containsFile2 =
+                            filenames.filter((name) => {
+                                return name === "multipleZips2/file2.rtf";
+                            }).length > 0;
+                        assert.isTrue(containsFile1, "Does not contain file1");
+                        assert.isTrue(containsFile2, "Does not contain file1");
+                    });
                 });
 
                 it("stat/read", async () => {
