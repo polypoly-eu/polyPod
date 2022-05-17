a18570ee925cdf1c86602eb77046ba23bf79e733
Tim Palade <palade.timotei@gmail.com>
2022-05-17 11:38:41 +0300

    Fix linter issues

--- platform/android/app/src/test/kotlin/coop/polypoly/polypod/polyout/PolyOutTest.kt
+++ platform/android/app/src/test/kotlin/coop/polypoly/polypod/polyout/PolyOutTest.kt
@@ -18,10 +18,17 @@ import org.robolectric.annotation.Config
 import org.robolectric.annotation.LooperMode
 import java.io.InputStream
 import java.io.OutputStream
-import java.security.*
+import java.security.Key
+import java.security.KeyStore
+import java.security.KeyStoreSpi
+import java.security.Provider
+import java.security.SecureRandom
+import java.security.Security
 import java.security.cert.Certificate
 import java.security.spec.AlgorithmParameterSpec
-import java.util.*
+import java.util.Collections
+import java.util.Date
+import java.util.Enumeration
 import javax.crypto.KeyGenerator
 import javax.crypto.KeyGeneratorSpi
 import javax.crypto.SecretKey
@@ -33,7 +40,9 @@ class PolyOutTest {
     private var polyOut: PolyOut? = null
 
     init {
-        polyOut = PolyOut(context = InstrumentationRegistry.getInstrumentation().targetContext)
+        polyOut = PolyOut(
+            context = InstrumentationRegistry.getInstrumentation().targetContext
+        )
     }
 
     class MockFeature(
@@ -52,52 +61,101 @@ class PolyOutTest {
 
     @Suppress("TooManyFunctions")
     class FakeKeyStore : KeyStoreSpi() {
-        override fun engineIsKeyEntry(alias: String?): Boolean = wrapped.isKeyEntry(alias)
+        override fun engineIsKeyEntry(
+            alias: String?
+        ): Boolean = wrapped.isKeyEntry(alias)
 
-        override fun engineIsCertificateEntry(alias: String?): Boolean = wrapped.isCertificateEntry(alias)
+        override fun engineIsCertificateEntry(
+            alias: String?
+        ): Boolean = wrapped.isCertificateEntry(alias)
 
-        override fun engineGetCertificate(alias: String?): Certificate = wrapped.getCertificate(alias)
+        override fun engineGetCertificate(
+            alias: String?
+        ): Certificate = wrapped.getCertificate(alias)
 
-        override fun engineGetCreationDate(alias: String?): Date = wrapped.getCreationDate(alias)
+        override fun engineGetCreationDate(
+            alias: String?
+        ): Date = wrapped.getCreationDate(alias)
 
         override fun engineDeleteEntry(alias: String?) {
             storedKeys.remove(alias)
         }
 
-        override fun engineSetKeyEntry(alias: String?, key: Key?, password: CharArray?, chain: Array<out Certificate>?) =
+        override fun engineSetKeyEntry(
+            alias: String?,
+            key: Key?,
+            password: CharArray?,
+            chain: Array<out Certificate>?
+        ) =
             wrapped.setKeyEntry(alias, key, password, chain)
 
-        override fun engineSetKeyEntry(alias: String?, key: ByteArray?, chain: Array<out Certificate>?) = wrapped.setKeyEntry(alias, key, chain)
+        override fun engineSetKeyEntry(
+            alias: String?,
+            key: ByteArray?,
+            chain: Array<out Certificate>?
+        ) = wrapped.setKeyEntry(alias, key, chain)
 
-        override fun engineStore(stream: OutputStream?, password: CharArray?) = wrapped.store(stream, password)
+        override fun engineStore(
+            stream: OutputStream?,
+            password: CharArray?
+        ) = wrapped.store(stream, password)
 
         override fun engineSize(): Int = wrapped.size()
 
-        override fun engineAliases(): Enumeration<String> = Collections.enumeration(storedKeys.keys)
+        override fun engineAliases(): Enumeration<String> = Collections.enumeration(storedKeys.keys) // ktlint-disable max-line-length
 
-        override fun engineContainsAlias(alias: String?): Boolean = storedKeys.containsKey(alias)
+        override fun engineContainsAlias(
+            alias: String?
+        ): Boolean = storedKeys.containsKey(alias)
 
-        override fun engineLoad(stream: InputStream?, password: CharArray?) = wrapped.load(stream, password)
+        override fun engineLoad(
+            stream: InputStream?,
+            password: CharArray?
+        ) = wrapped.load(stream, password)
 
-        override fun engineGetCertificateChain(alias: String?): Array<Certificate>? = wrapped.getCertificateChain(alias)
+        override fun engineGetCertificateChain(
+            alias: String?
+        ): Array<Certificate>? = wrapped.getCertificateChain(alias)
 
-        override fun engineSetCertificateEntry(alias: String?, cert: Certificate?) = wrapped.setCertificateEntry(alias, cert)
+        override fun engineSetCertificateEntry(
+            alias: String?,
+            cert: Certificate?
+        ) = wrapped.setCertificateEntry(alias, cert)
 
-        override fun engineGetCertificateAlias(cert: Certificate?): String? = wrapped.getCertificateAlias(cert)
+        override fun engineGetCertificateAlias(
+            cert: Certificate?
+        ): String? = wrapped.getCertificateAlias(cert)
 
-        override fun engineGetKey(alias: String?, password: CharArray?): Key? = (storedKeys[alias] as? KeyStore.SecretKeyEntry)?.secretKey
+        override fun engineGetKey(
+            alias: String?,
+            password: CharArray?
+        ): Key? = (storedKeys[alias] as? KeyStore.SecretKeyEntry)?.secretKey
 
-        override fun engineGetEntry(p0: String, p1: KeyStore.ProtectionParameter?): KeyStore.Entry? = storedKeys[p0]
+        override fun engineGetEntry(
+            p0: String,
+            p1: KeyStore.ProtectionParameter?
+        ): KeyStore.Entry? = storedKeys[p0]
 
-        override fun engineSetEntry(p0: String, p1: KeyStore.Entry, p2: KeyStore.ProtectionParameter?) {
+        override fun engineSetEntry(
+            p0: String,
+            p1: KeyStore.Entry,
+            p2: KeyStore.ProtectionParameter?
+        ) {
             storedKeys[p0] = p1
         }
 
-        override fun engineLoad(p0: KeyStore.LoadStoreParameter?) = wrapped.load(p0)
+        override fun engineLoad(
+            p0: KeyStore.LoadStoreParameter?
+        ) = wrapped.load(p0)
 
-        override fun engineStore(p0: KeyStore.LoadStoreParameter?) = wrapped.store(p0)
+        override fun engineStore(
+            p0: KeyStore.LoadStoreParameter?
+        ) = wrapped.store(p0)
 
-        override fun engineEntryInstanceOf(p0: String?, p1: Class<out KeyStore.Entry>?) = wrapped.entryInstanceOf(p0, p1)
+        override fun engineEntryInstanceOf(
+            p0: String?,
+            p1: Class<out KeyStore.Entry>?
+        ) = wrapped.entryInstanceOf(p0, p1)
 
         companion object {
             private val wrapped = KeyStore.getInstance("BKS", "BC")
@@ -111,11 +169,17 @@ class PolyOutTest {
 
         override fun engineInit(random: SecureRandom?) = wrapped.init(random)
 
-        override fun engineInit(params: AlgorithmParameterSpec?, random: SecureRandom?) = wrapped.init(random).also {
+        override fun engineInit(
+            params: AlgorithmParameterSpec?,
+            random: SecureRandom?
+        ) = wrapped.init(random).also {
             lastSpec = params
         }
 
-        override fun engineInit(keysize: Int, random: SecureRandom?) = wrapped.init(keysize, random)
+        override fun engineInit(
+            keysize: Int,
+            random: SecureRandom?
+        ) = wrapped.init(keysize, random)
 
         override fun engineGenerateKey(): SecretKey = wrapped.generateKey()
     }
@@ -144,10 +208,19 @@ class PolyOutTest {
             description = "",
             primaryColor = 0,
             thumbnailColor = 0,
-            thumbnail = null)
+            thumbnail = null
+        )
 
-        val url = InstrumentationRegistry.getInstrumentation().javaClass.classLoader!!.getResource("testZip.zip").toString()
-        val inputStream = InstrumentationRegistry.getInstrumentation().javaClass.classLoader!!.getResource("testZip.zip").openStream()
+        val url = InstrumentationRegistry
+            .getInstrumentation()
+            .javaClass.classLoader!!
+            .getResource("testZip.zip")
+            .toString()
+        val inputStream = InstrumentationRegistry
+            .getInstrumentation()
+            .javaClass.classLoader!!
+            .getResource("testZip.zip")
+            .openStream()
 
         val context = InstrumentationRegistry.getInstrumentation().targetContext
         val resolver = Shadows.shadowOf(context.contentResolver)
