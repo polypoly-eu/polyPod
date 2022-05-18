dbfc020e598796c4e2c522be81752c4efa9df546
Alex Farhat <95639770+alexandrosFarhat@users.noreply.github.com>
2022-05-18 09:41:17 +0100

    Prod4 pod 1567 google reports (#882)
    * moved report structure to google, need to resolve route button
    
    * reports working, but stylinig is off
    
    * styles almost there
    
    * fixed last style
    
    * Facebook -> google

---
+++ features/google/src/views/report/reportWrapper.jsx
@@ -0,0 +1,48 @@
+import {
+    MinistoriesStatusReport,
+    ReportStories,
+} from "@polypoly-eu/poly-analysis";
+import { PolyImportContext } from "@polypoly-eu/poly-look";
+import React, { useContext } from "react";
+import { Route } from "react-router-dom";
+import { ministories } from "../ministories/ministories.js";
+import { reports } from "../ministories/reports.js";
+import ReportDetails from "./details.jsx";
+import ReportView from "./report.jsx";
+
+const ReportWrapper = () => {
+    const { account } = useContext(PolyImportContext);
+
+    const computedReportStoriesList = reports.map(
+        (reportClass) => new reportClass({ account })
+    );
+
+    const computedMinistories = ministories.map(
+        (ministoryClass) => new ministoryClass({ account })
+    );
+    const activeReportStories = computedReportStoriesList.filter(
+        (reportStory) => reportStory.active
+    );
+    const statusReport = new MinistoriesStatusReport([
+        ...computedReportStoriesList,
+        ...computedMinistories,
+    ]);
+
+    const computedReportStories = new ReportStories([
+        ...activeReportStories,
+        statusReport,
+    ]);
+
+    return (
+        <>
+            <Route exact path="/report">
+                <ReportView reportStories={computedReportStories} />
+            </Route>
+            <Route exact path="/report/details">
+                <ReportDetails reportStories={computedReportStories} />
+            </Route>
+        </>
+    );
+};
+
+export default ReportWrapper;
