e3bbfcb16871adcf1a02d545848b1436cfa7c748
Tim Palade <palade.timotei@gmail.com>
2022-05-18 17:32:26 +0300

    Make iOS tests more readable

--- platform/ios/PolyPodApp/PolyPodTests/PolyOutTests.swift
+++ platform/ios/PolyPodApp/PolyPodTests/PolyOutTests.swift
@@ -147,6 +147,30 @@ class PolyOutTests: XCTestCase {
         wait(for: [expectation], timeout: 1.0)
     }
     
+    func importArchive(url: String, destUrl: String? = nil, polyOut: PolyOut) -> String? {
+            let expectation = expectation(description: "Exp")
+            var newURL: String?
+            polyOut.importArchive(url: url, destUrl: destUrl) { newUrl in
+                newURL = newUrl
+                expectation.fulfill()
+            }
+            wait(for: [expectation], timeout: 10.0)
+            return newURL
+    }
+    
+    func readDir(url: String, polyOut: PolyOut) -> ([[String: String]]?, Error?) {
+        let expectation = expectation(description: "Exp")
+        var _stuff: [[String: String]]?
+        var _error: Error?
+        polyOut.readDir(url: url) { stuff, error in
+            _stuff = stuff
+            _error = error
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 10.0)
+        return (_stuff, _error)
+    }
+    
     func testImportOneArchive() {
         let polyOut = PolyOut(session: NetworkSessionMock())
         
@@ -155,17 +179,13 @@ class PolyOutTests: XCTestCase {
         
         let url = bundle.url(forResource: "testZip", withExtension: "zip")!
         
-        let expectation = XCTestExpectation(description: "Exp")
-        polyOut.importArchive(url: url.absoluteString, destUrl: nil) { newUrl in
-            XCTAssertTrue(newUrl != nil && newUrl != "", "newUrl is nil or empty")
-            polyOut.readDir(url: newUrl!) { stuff, error in
-                XCTAssertNil(error, "error is not nil")
-                XCTAssertTrue(stuff != nil && !stuff!.isEmpty, "no files were found")
-                XCTAssertTrue(stuff!.filter{ $0["path"] == "testZip/testfile.rtf" }.count > 0, "file not found")
-                expectation.fulfill()
-            }
-        }
-        wait(for: [expectation], timeout: 5.0)
+        let newUrl = importArchive(url: url.absoluteString, destUrl: nil, polyOut: polyOut)
+        XCTAssertTrue(newUrl != nil && newUrl != "", "newUrl is nil or empty")
+        
+        let (stuff, error) = readDir(url: newUrl!, polyOut: polyOut)
+        XCTAssertNil(error, "error is not nil")
+        XCTAssertTrue(stuff != nil && !stuff!.isEmpty, "no files were found")
+        XCTAssertTrue(stuff!.filter{ $0["path"] == "testZip/testfile.rtf" }.count > 0, "file not found")
     }
     
     func testImportMultipleArchives() {
@@ -177,23 +197,17 @@ class PolyOutTests: XCTestCase {
         let url1 = bundle.url(forResource: "multipleZips1", withExtension: "zip")!
         let url2 = bundle.url(forResource: "multipleZips2", withExtension: "zip")!
         
-        let expectation = XCTestExpectation(description: "Exp")
-        polyOut.importArchive(url: url1.absoluteString, destUrl: nil) { newUrl1 in
-            XCTAssertTrue(newUrl1 != nil && newUrl1 != "", "newUrl1 is nil or empty")
-            
-            polyOut.importArchive(url: url2.absoluteString, destUrl: newUrl1!) { newUrl2 in
-                XCTAssertTrue(newUrl2 != nil && newUrl2 != "", "newUrl2 is nil or empty")
-                XCTAssertTrue(newUrl1 == newUrl2)
-                
-                polyOut.readDir(url: newUrl1!) { stuff, error in
-                    XCTAssertNil(error, "error is not nil")
-                    XCTAssertTrue(stuff != nil && !stuff!.isEmpty, "no files were found")
-                    XCTAssertTrue(stuff!.filter{ $0["path"] == "multipleZips1/file1.rtf" }.count > 0, "file1  not found")
-                    XCTAssertTrue(stuff!.filter{ $0["path"] == "multipleZips2/file2.rtf" }.count > 0, "file2  not found")
-                    expectation.fulfill()
-                }
-            }
-        }
-        wait(for: [expectation], timeout: 20.0)
+        let newUrl1 = importArchive(url: url1.absoluteString, destUrl: nil, polyOut: polyOut)
+        XCTAssertTrue(newUrl1 != nil && newUrl1 != "", "newUrl1 is nil or empty")
+        
+        let newUrl2 = importArchive(url: url2.absoluteString, destUrl: newUrl1, polyOut: polyOut)
+        XCTAssertTrue(newUrl2 != nil && newUrl2 != "", "newUrl2 is nil or empty")
+        XCTAssertTrue(newUrl1 == newUrl2)
+        
+        let (stuff, error) = readDir(url: newUrl1!, polyOut: polyOut)
+        XCTAssertNil(error, "error is not nil")
+        XCTAssertTrue(stuff != nil && !stuff!.isEmpty, "no files were found")
+        XCTAssertTrue(stuff!.filter{ $0["path"] == "multipleZips1/file1.rtf" }.count > 0, "file1  not found")
+        XCTAssertTrue(stuff!.filter{ $0["path"] == "multipleZips2/file2.rtf" }.count > 0, "file2  not found")
     }
 }
