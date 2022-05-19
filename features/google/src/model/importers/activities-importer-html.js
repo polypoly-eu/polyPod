69e661cd63d051f9b4b60d138a8c09d26e2e13d6
Alex Farhat <alex.farhat@polypoly.net>
2022-05-19 12:04:50 +0200

    made the two activities importers separate files

--- features/google/src/model/importers/activities-importer-html.js
+++ features/google/src/model/importers/activities-importer-html.js
@@ -1,11 +1,9 @@
 import UserActivity from "../entities/user-activity";
 
 const activityHtmlRegex = /\/My Activity\/.*\.html$/;
-const activityJsonRegex = /\/My Activity\/.*\.json$/;
 
-class ActivityParser {
-    constructor(isHtml) {
-        this._isHtml = isHtml;
+class ActivityHtmlParser {
+    constructor() {
         this._iframe = document.createElement("iframe");
         this._iframe.style.display = "none";
         document.body.appendChild(this._iframe);
@@ -28,13 +26,6 @@ class ActivityParser {
 
     async parse(entry) {
         const content = await entry.getContent();
-        if (!this._isHtml) {
-            return this._parseJson(content, entry);
-        }
-        return await this._parseHtml(content, entry);
-    }
-
-    async _parseHtml(content, entry) {
         const text = await new TextDecoder("utf-8").decode(content);
         const { contentDocument } = this._iframe;
         contentDocument.write(text);
@@ -45,40 +36,26 @@ class ActivityParser {
         return this._scrapeTimestamps(contentDocument, productName);
     }
 
-    async _parseJson(content, entry) {
-        const text = await new TextDecoder("utf-8").decode(content);
-        const jsonObj = JSON.parse(text);
-        const pathParts = entry.path.split("/");
-        const productName = pathParts[pathParts.length - 2];
-        return jsonObj.map(
-            (entry) =>
-                new UserActivity({
-                    timestamp: new Date(entry.time),
-                    productName,
-                })
-        );
-    }
-
     release() {
         document.body.removeChild(this._iframe);
         this._iframe = null;
     }
 }
 
-export default class ActivitiesImporter {
+export default class ActivitiesHtmlImporter {
     async import({ zipFile, facebookAccount: googleAccount }) {
         const entries = await zipFile.getEntries();
-        const activityEntries = entries.filter(
-            ({ path }) =>
-                activityHtmlRegex.test(path) || activityJsonRegex.test(path)
+        const activityEntries = entries.filter(({ path }) =>
+            activityHtmlRegex.test(path)
         );
-        const isHtml = activityHtmlRegex.test(activityEntries[0].path);
-        const parser = new ActivityParser(isHtml);
-        googleAccount.activities = (
-            await Promise.all(
-                activityEntries.map((entry) => parser.parse(entry))
+        const parser = new ActivityHtmlParser();
+        googleAccount.activities
+            .push(
+                await Promise.all(
+                    activityEntries.map((entry) => parser.parse(entry))
+                )
             )
-        ).flat();
+            .flat();
         parser.release();
     }
 }
