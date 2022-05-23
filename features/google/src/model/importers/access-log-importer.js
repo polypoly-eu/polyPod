31382f686919ff621a8189be8c3c0be43f2d096c
Alex Farhat <alex.farhat@polypoly.net>
2022-05-23 19:04:09 +0200

    merge conflict

--- features/google/src/model/importers/access-log-importer.js
+++ features/google/src/model/importers/access-log-importer.js
@@ -1,34 +1,17 @@
 import AccessLogEntry from "../entities/access-log-entry";
-<<<<<<< HEAD
-=======
 import { readCsvFromText } from "./utils/importer-utils";
->>>>>>> main
 
 const accessLogRegex = /\/Access Log Activity\/.*\.csv$/;
 
 class AccessLogParser {
     constructor() {}
 
-<<<<<<< HEAD
-    _csvToJson(csvText) {
-        const rows = csvText.split("\n");
-        const headers = rows.shift().replaceAll('"', "").split(",");
-
-        const keysEnum = {};
-        headers.forEach((key, index) => (keysEnum[key] = index));
-
-        const data = rows.map((row) => {
-            const rowData = row.split(",");
-            const productName = rowData[keysEnum["Product Name"]];
-            const date = rowData[keysEnum["Activity Timestamp"]];
-=======
     _dataFromCsv(csvText) {
         const { rows, headersEnum } = readCsvFromText(csvText);
         const data = rows.map((row) => {
             const rowData = row.split(",");
             const productName = rowData[headersEnum["Product Name"]];
             const date = rowData[headersEnum["Activity Timestamp"]];
->>>>>>> main
             return new AccessLogEntry({
                 timestamp: new Date(date),
                 productName,
@@ -40,13 +23,8 @@ class AccessLogParser {
     async parse(entry) {
         const content = await entry.getContent();
         const text = await new TextDecoder("utf-8").decode(content);
-<<<<<<< HEAD
-        const jsonData = this._csvToJson(text);
-        return jsonData;
-=======
         const data = this._dataFromCsv(text);
         return data;
->>>>>>> main
     }
 }
 
