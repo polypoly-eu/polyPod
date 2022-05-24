6c7ede83d82bacab4467181f7c782b9c0a1b1c0d
Alex Farhat <alex.farhat@polypoly.net>
2022-05-24 19:50:06 +0200

    forgot to save the file

--- features/google/src/model/importer.js
+++ features/google/src/model/importer.js
@@ -5,10 +5,6 @@ import SemanticLocationsImporter from "./importers/semantic-locations-importer";
 
 export const dataImporters = [
     ActivitiesHtmlImporter,
-<<<<<<< HEAD
-=======
-    SemanticLocationsImporter,
->>>>>>> 3e4e08abeb22d7c25fa4be986b918add29bc42ed
     ActivitiesJsonImporter,
     AccessLogImporter,
     SemanticLocationsImporter,
