a169b34c8430ecae8146a05b6a23034afce50726
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2022-05-26 12:27:35 +0200

    :white_check_mark: includes check of equal sections
    It warns if there are differences in the sections in different languages

--- feature-utils/silly-i18n/rollup-plugin.js
+++ feature-utils/silly-i18n/rollup-plugin.js
@@ -8,6 +8,18 @@ const FILEEXT = ".json";
 const PLUGIN = "silly-i18n";
 const SUFFIX = `?${PLUGIN}`;
 
+function symmetricDifference(setA, setB) {
+    let _difference = new Set(setA);
+    for (let elem of setB) {
+        if (_difference.has(elem)) {
+            _difference.delete(elem);
+        } else {
+            _difference.add(elem);
+        }
+    }
+    return _difference;
+}
+
 export default function (options) {
     return {
         name: PLUGIN,
@@ -36,12 +48,43 @@ export default function (options) {
                 for (const file of await util.promisify(glob)(pattern)) {
                     const lang = langRegex.exec(file)[1];
                     const section = path.basename(file, FILEEXT);
-
                     (translations[lang] ||= {})[section] = JSON.parse(
                         await fs.promises.readFile(file, { encoding: "utf-8" })
                     );
                 }
-
+                const allLanguages = Object.keys(translations);
+                let sectionSets = {};
+                allLanguages.forEach(
+                    (key) =>
+                        (sectionSets[key] = new Set(
+                            Object.keys(translations[key])
+                        ))
+                );
+                let languageCombos = [];
+                for (let i = 0; i < allLanguages.length; i++) {
+                    for (let j = i + 1; j < allLanguages.length; j++) {
+                        languageCombos.push([allLanguages[i], allLanguages[j]]);
+                    }
+                }
+                languageCombos.forEach((pair) => {
+                    const difference = symmetricDifference(
+                        sectionSets[pair[0]],
+                        sectionSets[pair[1]]
+                    );
+                    if (difference) {
+                        difference.forEach((section) => {
+                            if (sectionSets[pair[0]].has(section)) {
+                                console.warn(
+                                    `⚠️ ${section} is included in ${pair[0]}, but not in ${pair[1]}`
+                                );
+                            } else {
+                                console.warn(
+                                    `⚠️ ${section} is included in ${pair[0]}, but not in ${pair[1]}`
+                                );
+                            }
+                        });
+                    }
+                });
                 const { language } = options || {};
                 const [importExtra, languageArg] = language
                     ? ["", JSON.stringify(language)]
