2f3954b94a7a4ff679ba9046c30af5f5d90f5f1a
Richard Stoffels <richard.stoffels@polypoly.net>
2022-05-27 16:03:17 +0200

    Add google endpoint

--- platform/utils/endpoints-generator/endpoints.js
+++ platform/utils/endpoints-generator/endpoints.js
@@ -1,7 +1,19 @@
+const fallbackURL = "https://localhost:8000";
+const fallbackAuth = "";
+
 export default {
-    polyPediaReport: {
-        url: process.env.POLYPOD_POLYPEDIA_REPORT_URL,
-        auth: process.env.POLYPOD_POLYPEDIA_REPORT_AUTHORIZATION || "",
+    "polyPediaReport/facebookImporter": {
+        url:
+            process.env.POLYPOD_POLYPEDIA_REPORT_URL ||
+            fallbackURL + "/facebookImporter",
+        auth:
+            process.env.POLYPOD_POLYPEDIA_REPORT_AUTHORIZATION || fallbackAuth,
+    },
+    "polyPediaReport/google": {
+        url:
+            process.env.POLYPOD_POLYPEDIA_REPORT_URL || fallbackURL + "/google",
+        auth:
+            process.env.POLYPOD_POLYPEDIA_REPORT_AUTHORIZATION || fallbackAuth,
     },
     demoTest: {
         url: "",
