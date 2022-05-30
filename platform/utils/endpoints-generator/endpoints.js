42f99f314844eafc8d9fa8228447ea736774f683
Richard Stoffels <richard.stoffels@polypoly.net>
2022-05-30 14:37:55 +0200

    Add nit

--- platform/utils/endpoints-generator/endpoints.js
+++ platform/utils/endpoints-generator/endpoints.js
@@ -1,19 +1,17 @@
+const polyPediaReportUrl = process.env.POLYPOD_POLYPEDIA_REPORT_URL;
+const polyPediaReportAuth = process.env.POLYPOD_POLYPEDIA_REPORT_AUTHORIZATION;
+
 const fallbackURL = "https://localhost:8000";
 const fallbackAuth = "";
 
 export default {
     "polyPediaReport/facebookImporter": {
-        url:
-            process.env.POLYPOD_POLYPEDIA_REPORT_URL ||
-            fallbackURL + "/facebookImporter",
-        auth:
-            process.env.POLYPOD_POLYPEDIA_REPORT_AUTHORIZATION || fallbackAuth,
+        url: polyPediaReportUrl || fallbackURL + "/facebookImporter",
+        auth: polyPediaReportAuth || fallbackAuth,
     },
     "polyPediaReport/google": {
-        url:
-            process.env.POLYPOD_POLYPEDIA_REPORT_URL || fallbackURL + "/google",
-        auth:
-            process.env.POLYPOD_POLYPEDIA_REPORT_AUTHORIZATION || fallbackAuth,
+        url: polyPediaReportUrl || fallbackURL + "/google",
+        auth: polyPediaReportAuth || fallbackAuth,
     },
     demoTest: {
         url: "",
