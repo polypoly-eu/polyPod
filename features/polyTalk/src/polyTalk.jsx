0155159e62fb2be2c917eaed48fe9ffb949f3845
Richard Stoffels <richard.stoffels@polypoly.net>
2022-06-01 17:10:58 +0200

    Add react app with pod.js and poly-look

---
+++ features/polyTalk/src/polyTalk.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import * as ReactDOM from "react-dom";
+import {
+    MemoryRouter as Router,
+    Switch,
+    Redirect,
+    Route,
+    useHistory,
+} from "react-router-dom";
+import Home from "./views/home.jsx";
+import "./styles.css";
+
+const PolyTalk = () => {
+    return (
+        <div className="poly-talk poly-theme poly-theme-dark">
+            <Switch>
+                <Route exact path="/">
+                    <Redirect to={{ pathname: "/home" }} />
+                </Route>
+                <Route exact path="/home">
+                    <Home />
+                </Route>
+            </Switch>
+        </div>
+    );
+};
+
+//Router and context
+const PolyTalkApp = () => {
+    //global history object
+    const history = useHistory();
+
+    return (
+        <Router history={history}>
+            <div className="poly-nav-bar-separator-overlay" />
+            <PolyTalk />
+        </Router>
+    );
+};
+
+//render to html
+ReactDOM.render(<PolyTalkApp />, document.getElementById("feature"));
---
+++ features/polyTalk/src/views/home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Screen } from "@polypoly-eu/poly-look";
+
+const Home = () => {
+    return <Screen>hello</Screen>;
+};
+
+export default Home;
