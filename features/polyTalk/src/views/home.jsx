f6df7211c8d661d2a4c6fc1b5000e47d6743a861
Richard Stoffels <richard.stoffels@polypoly.net>
2022-06-03 14:00:45 +0200

    Add stylings to screen

--- features/polyTalk/src/views/home.jsx
+++ features/polyTalk/src/views/home.jsx
@@ -1,7 +1,11 @@
 import React, { useContext } from "react";
 import { Screen } from "@polypoly-eu/poly-look";
 import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
-import { ConversationList, Conversation } from "@chatscope/chat-ui-kit-react";
+import {
+    ConversationList,
+    Conversation,
+    Avatar,
+} from "@chatscope/chat-ui-kit-react";
 import { MessagesContext } from "../context/messages.jsx";
 
 const Home = () => {
@@ -10,7 +14,7 @@ const Home = () => {
     return (
         <Screen className="poly-theme-light">
             <ConversationList>
-                {Object.entries(rooms).map(([ name, room ], i) => (
+                {Object.entries(rooms).map(([name, room], i) => (
                     <Conversation
                         key={i}
                         name={name}
