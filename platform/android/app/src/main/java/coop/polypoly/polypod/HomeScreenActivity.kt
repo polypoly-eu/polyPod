c8f96db07923f6fe1dc98ae5db3ffc4aa300c093
Ghenadie Vasiliev-Pusca <ghenadie.pusca@polypoly.net>
2022-06-07 14:21:43 +0300

    Small card draft

--- platform/android/app/src/main/java/coop/polypoly/polypod/HomeScreenActivity.kt
+++ platform/android/app/src/main/java/coop/polypoly/polypod/HomeScreenActivity.kt
@@ -1,13 +1,22 @@
 package coop.polypoly.polypod
 
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -25,26 +34,27 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!", modifier = Modifier.height(50.dp))
 }
 
+@Composable
+fun SmallCardView() {
+    Column(modifier = Modifier
+        .padding(8.dp)
+        .requiredSize(width = 130.dp, height = 130.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween) {
+
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(0.7f),
+            contentScale = ContentScale.Fit,
+            alignment = Alignment.Center
+        )
+        Text(text = "Some text")
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    Column {
-        Row {
-            Text(
-                text = "Hello Tim!", modifier = Modifier
-                    .height(100.dp)
-                    .width(300.dp)
-            )
-            Column {
-                Text(text = "Hello Tim!", modifier = Modifier.height(50.dp))
-                Text(text = "Hello Tim!", modifier = Modifier.height(50.dp))
-            }
-        }
-
-        Row {
-            Text(text = "Hello Tim!", modifier = Modifier.height(50.dp))
-            Text(text = "Hello Tim!", modifier = Modifier.height(50.dp))
-            Text(text = "Hello Tim!", modifier = Modifier.height(50.dp))
-        }
-    }
+    SmallCardView()
 }
