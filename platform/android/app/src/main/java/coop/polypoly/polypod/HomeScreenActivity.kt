a870b28b1290f631d268467bbd53f602f5698e81
Tim Palade <palade.timotei@gmail.com>
2022-06-08 14:06:20 +0300

    Fix lint error

--- platform/android/app/src/main/java/coop/polypoly/polypod/HomeScreenActivity.kt
+++ platform/android/app/src/main/java/coop/polypoly/polypod/HomeScreenActivity.kt
@@ -5,7 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Text
