17c3479cb62e4dc3d5689593ad81e099a9c710e6
Ghenadie Vasiliev-Pusca <ghenadie.pusca@polypoly.net>
2022-06-08 10:57:34 +0300

    Adjust image size

--- platform/android/app/src/main/java/coop/polypoly/polypod/HomeScreenActivity.kt
+++ platform/android/app/src/main/java/coop/polypoly/polypod/HomeScreenActivity.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -16,15 +18,19 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 class HomeScreenActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-                Greeting("Android")
+            Greeting("Android")
         }
     }
 }
@@ -35,26 +41,42 @@ fun Greeting(name: String) {
 }
 
 @Composable
-fun SmallCardView() {
-    Column(modifier = Modifier
-        .padding(8.dp)
-        .requiredSize(width = 130.dp, height = 130.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.SpaceBetween) {
+fun SmallCardView(size: Dp) {
 
-        Image(
-            painter = painterResource(id = R.drawable.ic_launcher),
-            contentDescription = null,
-            modifier = Modifier.fillMaxSize(0.7f),
-            contentScale = ContentScale.Fit,
-            alignment = Alignment.Center
-        )
-        Text(text = "Some text")
+    Card(modifier = Modifier
+        .width(size)
+        .height(size),
+    shape = RoundedCornerShape(8.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher),
+                contentDescription = null,
+                modifier = Modifier.weight(0.9f),
+                contentScale = ContentScale.Fit,
+                alignment = Alignment.Center
+            )
+            Text(text = "Facebook Import", textAlign = TextAlign.Center)
+        }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    SmallCardView()
+    val configuration = LocalConfiguration.current
+    val interItemSpacing = 8
+    val horizontalPadding = 8
+    val size: Dp = (configuration.screenWidthDp / 3 - 2*interItemSpacing - 2*horizontalPadding).dp
+
+    Row(modifier = Modifier.padding(horizontalPadding.dp), horizontalArrangement = Arrangement.spacedBy(interItemSpacing.dp)) {
+        SmallCardView(size = size)
+        SmallCardView(size = size)
+        SmallCardView(size = size)
+    }
 }
