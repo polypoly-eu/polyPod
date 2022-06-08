cde1a8904eb991bb79465991ed744e7ea4645943
Tim Palade <palade.timotei@gmail.com>
2022-06-08 11:29:21 +0300

    Small card view

--- platform/android/app/src/main/java/coop/polypoly/polypod/HomeScreenActivity.kt
+++ platform/android/app/src/main/java/coop/polypoly/polypod/HomeScreenActivity.kt
@@ -15,11 +15,9 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.constraintlayout.compose.ConstraintLayout
 
 class HomeScreenActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,27 +34,47 @@ fun Greeting(name: String) {
 }
 
 @Composable
-fun SmallCardView(size: Dp) {
-
-    Card(modifier = Modifier
-        .width(size)
-        .height(size),
-    shape = RoundedCornerShape(8.dp)
+fun SmallCardView(
+    size: Dp,
+    topPadding: Dp,
+    otherPadding: Dp,
+    verticalSpacing: Dp,
+    cornerRadius: Dp
+) {
+    Card(
+        modifier = Modifier
+            .width(size)
+            .height(size),
+        shape = RoundedCornerShape(cornerRadius)
     ) {
         Column(
             modifier = Modifier
-                .padding(8.dp),
+                .padding(
+                    top = topPadding,
+                    start = otherPadding,
+                    end = otherPadding,
+                    bottom = otherPadding
+                ),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.SpaceBetween
         ) {
             Image(
                 painter = painterResource(id = R.drawable.ic_launcher),
                 contentDescription = null,
-                modifier = Modifier.weight(0.9f),
+                // Takes all the height left after the text is placed
+                modifier = Modifier.weight(1.0f),
                 contentScale = ContentScale.Fit,
                 alignment = Alignment.Center
             )
-            Text(text = "Facebook Import", textAlign = TextAlign.Center)
+            Spacer(
+                modifier = Modifier.defaultMinSize(
+                    minWidth = size, minHeight = verticalSpacing
+                )
+            )
+            Text(
+                text = "Facebook Import",
+                textAlign = TextAlign.Center
+            )
         }
     }
 }
@@ -67,11 +85,39 @@ fun DefaultPreview() {
     val configuration = LocalConfiguration.current
     val interItemSpacing = 8
     val horizontalPadding = 8
-    val size: Dp = (configuration.screenWidthDp / 3 - 2*interItemSpacing - 2*horizontalPadding).dp
 
-    Row(modifier = Modifier.padding(horizontalPadding.dp), horizontalArrangement = Arrangement.spacedBy(interItemSpacing.dp)) {
-        SmallCardView(size = size)
-        SmallCardView(size = size)
-        SmallCardView(size = size)
+    // small card
+    val smallCardVerticalSpacing = 8.dp
+    val smallCardTopPadding = 8.dp
+    val smallCardOtherPadding = 8.dp
+    val smallCardCornerRadius = 8.dp
+
+    val size: Dp = (configuration.screenWidthDp / 3 - 2 * interItemSpacing - 2 * horizontalPadding).dp // ktlint-disable max-line-length
+
+    Row(
+        modifier = Modifier.padding(horizontalPadding.dp),
+        horizontalArrangement = Arrangement.spacedBy(interItemSpacing.dp)
+    ) {
+        SmallCardView(
+            size = size,
+            topPadding = smallCardTopPadding,
+            otherPadding = smallCardOtherPadding,
+            verticalSpacing = smallCardVerticalSpacing,
+            cornerRadius = smallCardCornerRadius
+        )
+        SmallCardView(
+            size = size,
+            topPadding = smallCardTopPadding,
+            otherPadding = smallCardOtherPadding,
+            verticalSpacing = smallCardVerticalSpacing,
+            cornerRadius = smallCardCornerRadius
+        )
+        SmallCardView(
+            size = size,
+            topPadding = smallCardTopPadding,
+            otherPadding = smallCardOtherPadding,
+            verticalSpacing = smallCardVerticalSpacing,
+            cornerRadius = smallCardCornerRadius
+        )
     }
 }
