14737ae31005ca9cca716574f3426d42b21b6e23
Juan Julián Merelo Guervós <jjmerelo@gmail.com>
2022-06-09 10:09:24 +0200

    [Fix] {PROD4POD-1641} Show imported date (#963)
    * Set localized date
    
    * Adds localized date to import
    
    As a matter of fact, this could be added to Facebook too...
    
    * :rotating_light:

--- features/google/src/views/overview/overview.jsx
+++ features/google/src/views/overview/overview.jsx
@@ -8,6 +8,8 @@ import {
     PolyChart,
     INITIAL_HISTORY_STATE,
 } from "@polypoly-eu/poly-look";
+import { L12n } from "@polypoly-eu/silly-i18n/src/l12n";
+
 import { useHistory } from "react-router-dom";
 import i18n from "!silly-i18n";
 import { analyzeFile } from "@polypoly-eu/poly-analysis";
@@ -18,6 +20,8 @@ import {
     BUBBLE_LIGHT_COLOR,
 } from "../../constants/bubbleViz";
 import { GoogleContext } from "../../context/google-context.jsx";
+
+const l12n = new L12n();
 
 const Overview = () => {
     const { account, handleRemoveFile, files, refreshFiles } =
@@ -98,6 +102,9 @@ const Overview = () => {
                         {i18n.t("overview:imported.file")} {files[0].name}
                         <br />
                         {i18n.t("overview:size")} {formatSize(files[0].size)}
+                        <br />
+                        {i18n.t("overview:imported.time")}{" "}
+                        {l12n.t(new Date(files[0].time))}
                     </p>
                 </>
             )}
