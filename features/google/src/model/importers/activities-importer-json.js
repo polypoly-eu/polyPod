d0948774433d36db6c3df984671af875de283ff4
Alex Farhat <95639770+alexandrosFarhat@users.noreply.github.com>
2022-06-09 15:59:52 +0200

    Prod4 pod 1659 read relevant entries (#968)
    * converted to relevantZipEntries
    
    * correct import
    
    * save

--- features/google/src/model/importers/activities-importer-json.js
+++ features/google/src/model/importers/activities-importer-json.js
@@ -1,3 +1,4 @@
+import { relevantZipEntries } from "@polypoly-eu/poly-analysis";
 import UserActivity from "../entities/user-activity";
 import { matchRegex } from "./utils/lang-constants";
 
@@ -21,7 +22,7 @@ class ActivityJsonParser {
 
 export default class ActivitiesJsonImporter {
     async import({ zipFile, facebookAccount: googleAccount }) {
-        const entries = await zipFile.getEntries();
+        const entries = await relevantZipEntries(zipFile);
         const activityEntries = entries.filter(({ path }) =>
             matchRegex(path, this)
         );
