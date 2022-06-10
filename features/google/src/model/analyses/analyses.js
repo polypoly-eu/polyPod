e057e4b7a4ffb3cac63bb62d3a8b5a30f5054f25
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2022-06-10 08:38:04 +0200

    Adds to analysis

--- features/google/src/model/analyses/analyses.js
+++ features/google/src/model/analyses/analyses.js
@@ -2,10 +2,12 @@ import PlaceVisitsAnalysis from "./ministories/place-visits-analysis.js";
 import ActivitySegmentsAnalysis from "./ministories/activity-type-analysis";
 import ActivityByProductAnalysis from "./ministories/activity-by-products.js";
 import ActivitiesOverTimeAnalysis from "./ministories/activities-over-time.js";
+import ReportMetadataAnalysis from "./ministories/report-metadata.js";
 
 export const specificAnalyses = [
     PlaceVisitsAnalysis,
     ActivitySegmentsAnalysis,
     ActivityByProductAnalysis,
     ActivitiesOverTimeAnalysis,
+    ReportMetadataAnalysis,
 ];
