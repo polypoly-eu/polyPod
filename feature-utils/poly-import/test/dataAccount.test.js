9fab1b7e9712f8a94b0e9f70b2c0ca2512af1205
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2022-06-14 12:17:24 +0200

    :white_check_mark: add test for data types

---
+++ feature-utils/poly-import/test/dataAccount.test.js
@@ -0,0 +1,15 @@
+import { DataAccount } from "../src";
+
+describe("DataAccount has the correct attributes", () => {
+    const dataAccount = new DataAccount();
+    it("Attributes have the correct type", () => {
+        [
+            "importingResults",
+            "importedFileNames",
+            "preferredLanguage",
+            "analysesExecutionResults",
+        ].forEach((attribute) => {
+            expect(dataAccount[attribute]).toBeInstanceOf(Array);
+        });
+    });
+});
