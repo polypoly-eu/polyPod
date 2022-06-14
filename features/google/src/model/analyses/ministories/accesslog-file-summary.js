29127e72e11d0f17d2a4d8aaa8b4c7319507e413
alexandrosFarhat <alex.farhat@polypoly.net>
2022-06-14 11:01:24 +0200

    added file size unit converter

--- features/google/src/model/analyses/ministories/accesslog-file-summary.js
+++ features/google/src/model/analyses/ministories/accesslog-file-summary.js
@@ -1,14 +1,14 @@
 import { ReportAnalysis } from "@polypoly-eu/poly-analysis";
 import analysisKeys from "../analysisKeys";
+import { convertFileSizeUnit } from "../utils/analysis-tools";
 
 export default class AccessLogSummaryAnalysis extends ReportAnalysis {
     async analyze({ dataAccount: googleAccount }) {
         const accessLogSummary = googleAccount.accessLogSummary;
         const allAccessLogsSummary = {
             fileName: accessLogSummary[0].fileName,
-            fileSize: accessLogSummary.reduce(
-                (total, next) => total + next.size,
-                0
+            fileSize: convertFileSizeUnit(
+                accessLogSummary.reduce((total, next) => total + next.size, 0)
             ),
         };
 
