c9683093a70a86c1c271de4de5943340fd747deb
Ghenadie Vasiliev-Pusca <ghenadie.pusca@polypoly.net>
2022-06-14 13:33:17 +0300

    Add iOS pod unlock screen

---
+++ platform/ios/PolyPodApp/PolyPod/UnlockPolyPod.swift
@@ -0,0 +1,34 @@
+// Please remove this line and the empty one after it
+
+import SwiftUI
+
+struct UnlockPolyPod: View {
+    var onCompleted: () -> Void = {}
+
+    var body: some View {
+        VStack(alignment: .center, spacing: 32) {
+            Spacer()
+            Image("NavIconPolyPodLogo")
+            Spacer()
+            PrimaryButton(title: "Unlock", onAction: authenticate)
+        }
+        .padding(.vertical, 32)
+        .onAppear(perform: authenticate)
+    }
+    
+    private func authenticate() {
+        Authentication.shared.authenticate { success in
+            if success {
+                DispatchQueue.main.async {
+                    onCompleted()
+                }
+            }
+        }
+    }
+}
+
+struct UnlockPolyPod_Previews: PreviewProvider {
+    static var previews: some View {
+        UnlockPolyPod()
+    }
+}
