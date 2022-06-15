a8bff546034dba1c700ee7e258ccd5d20a681544
Felix Dahlke <felix.dahlke@polypoly.net>
2022-06-15 09:50:55 +0200

    Disable tech demos

--- features/google/src/model/analyses/analyses.js
+++ features/google/src/model/analyses/analyses.js
@@ -1,13 +1,13 @@
-import PlaceVisitsAnalysis from "./ministories/place-visits-analysis.js";
-import ActivitySegmentsAnalysis from "./ministories/activity-type-analysis";
-import ActivityByProductAnalysis from "./ministories/activity-by-products.js";
+//import PlaceVisitsAnalysis from "./ministories/place-visits-analysis.js";
+//import ActivitySegmentsAnalysis from "./ministories/activity-type-analysis";
+//import ActivityByProductAnalysis from "./ministories/activity-by-products.js";
 import ActivitiesOverTimeAnalysis from "./ministories/activities-over-time.js";
 import ReportMetadataAnalysis from "./ministories/report-metadata.js";
 
 export const specificAnalyses = [
-    PlaceVisitsAnalysis,
-    ActivitySegmentsAnalysis,
-    ActivityByProductAnalysis,
+    //PlaceVisitsAnalysis,
+    //ActivitySegmentsAnalysis,
+    //ActivityByProductAnalysis,
     ActivitiesOverTimeAnalysis,
     ReportMetadataAnalysis,
 ];
