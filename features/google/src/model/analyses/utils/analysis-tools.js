34aedfbec508c548a6eebbf200a2e7dbea57fba3
Alex Farhat <95639770+alexandrosFarhat@users.noreply.github.com>
2022-06-17 18:35:28 +0200

    Prod4 pod 1653 accesslog file summary (#979)
    * accessLog summary added - fileSize is still in bytes
    
    * added file size unit converter
    
    * remove bit conversion
    
    * revert changes from other branch

--- features/google/src/model/analyses/utils/analysis-tools.js
+++ features/google/src/model/analyses/utils/analysis-tools.js
@@ -7,3 +7,26 @@ export function groupAnalysisByKey(analysisEntries, keyGroup) {
     });
     return groupedAnalysisByKey;
 }
+
+export function convertFileSizeUnit(byteLength) {
+    let power = 0;
+    let temp_size = byteLength;
+    while (temp_size != 0) {
+        temp_size = Math.floor(byteLength / Math.pow(1024, power));
+        power++;
+    }
+    power = power - 2;
+    const unit = (power) => {
+        switch (power) {
+            case 0:
+                return "B";
+            case 1:
+                return "KB";
+            case 2:
+                return "MB";
+            default:
+                return "GB";
+        }
+    };
+    return `${Math.round(byteLength / Math.pow(1024, power))} ${unit(power)}`;
+}
--- features/google/src/views/ministories/reports.js
+++ features/google/src/views/ministories/reports.js
@@ -1,6 +1,11 @@
+import AccessLogSummaryReport from "./accessLogSummary.jsx";
 import DataImportingStatusReport from "./dataImportingStatus.jsx";
 import ReportMetadataReport from "./reportMetadata.jsx";
 
-export const reports = [DataImportingStatusReport, ReportMetadataReport];
+export const reports = [
+    DataImportingStatusReport,
+    ReportMetadataReport,
+    AccessLogSummaryReport,
+];
 
 export default reports;
