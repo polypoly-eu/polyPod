cb79ca11b48fa5915fc05e9368a0d28922999948
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2022-06-20 07:29:11 +0200

    Eliminate temporary and use constant for literal

--- dev-utils/poly-cli/templates/package.js
+++ dev-utils/poly-cli/templates/package.js
@@ -1,21 +1,25 @@
+const INDENTING_SPACES = 4;
+
 // template returns a string that can be written to a file
 export function template(name, version, description, main, author, license) {
-    let t = {
-        name: name,
-        version: version,
-        description: description,
-        main: main,
-        scripts: {
-            build: "rollup -c",
-            test: 'echo "Error: no test specified" && exit 1',
+    return JSON.stringify(
+        {
+            name: name,
+            version: version,
+            description: description,
+            main: main,
+            scripts: {
+                build: "rollup -c",
+                test: 'echo "Error: no test specified" && exit 1',
+            },
+            devDependencies: {
+                "@polypoly-eu/rollup-plugin-copy-watch":
+                    "file:../../dev-utils/rollup-plugin-copy-watch",
+            },
+            author: author,
+            license: license,
         },
-        devDependencies: {
-            "@polypoly-eu/rollup-plugin-copy-watch":
-                "file:../../dev-utils/rollup-plugin-copy-watch",
-        },
-        author: author,
-        license: license,
-    };
-
-    return JSON.stringify(t, null, 2);
+        null,
+        INDENTING_SPACES
+    );
 }
