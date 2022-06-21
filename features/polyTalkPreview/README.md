3a10987df55e8702d3e6b83148967ac36b462d01
Tim Palade <palade.timotei@gmail.com>
2022-06-21 12:31:37 +0300

    Create folder structure: Generated with Poly-CLI

---
+++ features/polyTalkPreview/README.md
@@ -0,0 +1,13 @@
+
+# polyTalkPreview
+
+Generated from poly-cli; use your own here
+
+## Build project
+
+`npm run build`
+
+## Run Feature 
+
+Open `dist/index.html` in your favorite browser.
+    
---
+++ features/polyTalkPreview/rollup.config.mjs
@@ -0,0 +1,50 @@
+import copy from "@polypoly-eu/rollup-plugin-copy-watch";
+import css from "rollup-plugin-css-only";
+import sillyI18n from "@polypoly-eu/silly-i18n/rollup-plugin.js";
+import genPodjs from "@polypoly-eu/podjs/rollup-plugin-gen-podjs/genPodjs.js";
+
+const externalPackages = {
+    "@polypoly-eu/poly-look": "polyLook",
+    react: "React",
+    "react-dom": "ReactDOM",
+};
+
+export default {
+    input: "src/index.js",
+    output: {
+        file: "dist/index.js",
+        format: "iife",
+        globals: externalPackages,
+    },
+    plugins: [
+        sillyI18n(),
+        css({ output: "css/bundle.css" }),
+        genPodjs({
+            build_dir: "./dist",
+            manifestPath: "./src/static/manifest.json",
+        }),
+        copy({
+            targets: [
+                {
+                    src: ["./src/static/*"],
+                    dest: "dist",
+                },
+                {
+                    src: [
+                        "node_modules/@polypoly-eu/poly-look/dist/css/poly-look.css",
+                    ],
+                    dest: "dist/css",
+                },
+                {
+                    src: [
+                        "node_modules/react/umd/react.development.js",
+                        "node_modules/react-dom/umd/react-dom.development.js",
+                        "node_modules/@polypoly-eu/poly-look/dist/poly-look.js",
+                    ],
+                    dest: "dist",
+                },
+            ],
+            verbose: true,
+        }),
+    ],
+};
