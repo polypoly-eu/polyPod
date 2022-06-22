45b9f20df543b01fc31dd57fab941e0cf95776a9
Georgiana Piscoi <georgiana.piscoi@polypoly.net>
2022-06-22 16:04:24 +0300

    finished the slideshow comp

--- feature-utils/poly-look/src/react-components/slideshow/slideshow.jsx
+++ feature-utils/poly-look/src/react-components/slideshow/slideshow.jsx
@@ -4,11 +4,17 @@ import SwiperCore, { Pagination } from "swiper";
 
 import "./slideshow.css";
 
+/** Slideshow used to display images.
+ * @param {Object} props
+ * @param {Array[string]} [props.images] - Array that contains the urls of the
+ * images that will be displayed.
+ * @returns {JSX.Element}
+ */
 const Slideshow = ({ images }) => {
   SwiperCore.use([Pagination]);
   return (
     <div className="poly-slideshow">
-      <Swiper pagination={true} spaceBetween={0} slidesPerView={1}>
+      <Swiper pagination={true} spaceBetween={100} slidesPerView={1}>
         {images.map((image, index) => (
           <SwiperSlide key={index}>
             <img src={image} />
