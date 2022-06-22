d31cae306e733e37b04605726afa5d66e85b1559
Tim Palade <palade.timotei@gmail.com>
2022-06-22 16:13:18 +0300

    Add images

--- features/polyTalkPreview/dist/index.js
+++ features/polyTalkPreview/dist/index.js
@@ -24,7 +24,7 @@
                 title: "Section 1",
                 description:
                     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                images: [],
+                images: ["./images/test.jpeg", "./images/test.jpeg"],
             },
             {
                 title: "Section 2",
