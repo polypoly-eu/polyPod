d5186f1656a2ee082600d09bf5b365ea8838bfe5
Tim Palade <palade.timotei@gmail.com>
2022-06-22 15:17:34 +0300

    Create the UI structure

--- features/polyTalkPreview/dist/index.js
+++ features/polyTalkPreview/dist/index.js
@@ -1,4 +1,4 @@
-(function (React, client) {
+(function (React, client, polyLook) {
     'use strict';
 
     function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
@@ -6,9 +6,57 @@
     var React__default = /*#__PURE__*/_interopDefaultLegacy(React);
 
     const App = () => {
+        const Section = (props) => {
+            return (
+                React__default["default"].createElement('div', null
+                    , React__default["default"].createElement('h3', null, props.model.title)
+                    , React__default["default"].createElement('p', null, props.model.description)
+                    , props.model.images.length > 0 && (
+                        React__default["default"].createElement(polyLook.Slideshow, { images: props.model.images,} )
+                    )
+                )
+            );
+        };
+
+        let title = "TEST Title";
+        let sections = [
+            {
+                title: "Section 1",
+                description:
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                images: [],
+            },
+            {
+                title: "Section 2",
+                description:
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                images: [],
+            },
+            {
+                title: "Section 3",
+                description:
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                images: [],
+            },
+            {
+                title: "Section 4",
+                description:
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                images: [],
+            },
+        ];
+
         return (
             React__default["default"].createElement('div', null
-                , React__default["default"].createElement('h1', null, "What's next?" )
+                , React__default["default"].createElement('h1', null, title)
+                , React__default["default"].createElement('div', null
+                    , sections.map((s, i) => (
+                        React__default["default"].createElement(Section, { key: i, model: s,} )
+                    ))
+                )
+                , React__default["default"].createElement('div', null
+                    , React__default["default"].createElement('h2', null, "FOOTER")
+                )
             )
         );
     };
@@ -16,4 +64,4 @@
     const root = client.createRoot(document.getElementById("feature"));
     root.render(React__default["default"].createElement(App, null ));
 
-})(React, ReactDOM);
+})(React, ReactDOM, polyLook);
--- features/polyTalkPreview/src/index.jsx
+++ features/polyTalkPreview/src/index.jsx
@@ -1,12 +1,61 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { Slideshow } from "@polypoly-eu/poly-look";
 
 import "./styles.css";
 
 const App = () => {
+    const Section = (props) => {
+        return (
+            <div>
+                <h3>{props.model.title}</h3>
+                <p>{props.model.description}</p>
+                {props.model.images.length > 0 && (
+                    <Slideshow images={props.model.images} />
+                )}
+            </div>
+        );
+    };
+
+    let title = "TEST Title";
+    let sections = [
+        {
+            title: "Section 1",
+            description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            images: [],
+        },
+        {
+            title: "Section 2",
+            description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            images: [],
+        },
+        {
+            title: "Section 3",
+            description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            images: [],
+        },
+        {
+            title: "Section 4",
+            description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            images: [],
+        },
+    ];
+
     return (
         <div>
-            <h1>What's next?</h1>
+            <h1>{title}</h1>
+            <div>
+                {sections.map((s, i) => (
+                    <Section key={i} model={s} />
+                ))}
+            </div>
+            <div>
+                <h2>FOOTER</h2>
+            </div>
         </div>
     );
 };
