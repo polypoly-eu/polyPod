4dd32b95aa212f52b71b527155010349a4f56af3
Ghenadie Vasiliev-Pusca <ghenadie.pusca@polypoly.net>
2022-06-22 12:10:29 +0300

    ktlint fixes

--- platform/android/app/src/main/kotlin/coop/polypoly/polypod/homescreen/HomeScreenFragment.kt
+++ platform/android/app/src/main/kotlin/coop/polypoly/polypod/homescreen/HomeScreenFragment.kt
@@ -6,19 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.* // ktlint-disable no-wildcard-imports
 import androidx.compose.foundation.layout.* // ktlint-disable no-wildcard-imports
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.* // ktlint-disable no-wildcard-imports
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
--- platform/android/app/src/main/kotlin/coop/polypoly/polypod/homescreen/HomeScreenTiles.kt
+++ platform/android/app/src/main/kotlin/coop/polypoly/polypod/homescreen/HomeScreenTiles.kt
@@ -3,7 +3,7 @@ package coop.polypoly.polypod.homescreen
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.* // ktlint-disable no-wildcard-imports
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
