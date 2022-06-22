ecdb6fc73cab01f155552a3f4d008cd2ab1abdc1
Tim Palade <palade.timotei@gmail.com>
2022-06-22 16:43:54 +0300

    Add footer

--- features/polyTalkPreview/dist/index.js
+++ features/polyTalkPreview/dist/index.js
@@ -18,45 +18,71 @@
             );
         };
 
-        let title = "TEST Title";
-        let sections = [
-            {
-                title: "Section 1",
+        const Footer = (props) => {
+            return (
+                React__default["default"].createElement(polyLook.ClickableCard, {
+                    buttonText: props.model.buttonTitle,
+                    onlyButtonClickEvent: true,
+                    onClick: () => {},}
+                
+                    , React__default["default"].createElement('h3', null, props.model.title)
+                    , React__default["default"].createElement('p', null, props.model.description)
+                    , image !== undefined && image !== "" && (
+                        React__default["default"].createElement('img', { src: props.model.image,} )
+                    )
+                )
+            );
+        };
+
+        let data = {
+            title: "TEST Title",
+            sections: [
+                {
+                    title: "Section 1",
+                    description:
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                    images: ["./images/test.jpeg", "./images/test.jpeg"],
+                },
+                {
+                    title: "Section 2",
+                    description:
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                    images: ["./images/test.jpeg"],
+                },
+                {
+                    title: "Section 3",
+                    description:
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                    images: ["./images/test.jpeg"],
+                },
+                {
+                    title: "Section 4",
+                    description:
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                    images: [],
+                },
+            ],
+            footer: {
+                title: "Footer title",
                 description:
                     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                images: ["./images/test.jpeg", "./images/test.jpeg"],
+                image: "./images/test.jpeg",
+                buttonTitle: "Learn More",
+                buttonLink: "https://polypoly.org/en-gb/",
             },
-            {
-                title: "Section 2",
-                description:
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                images: ["./images/test.jpeg"],
-            },
-            {
-                title: "Section 3",
-                description:
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                images: ["./images/test.jpeg"],
-            },
-            {
-                title: "Section 4",
-                description:
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                images: [],
-            },
-        ];
+        };
 
         return (
             React__default["default"].createElement(polyLook.Screen, { className: "poly-theme-light", layout: "poly-standard-layout",}
                 , React__default["default"].createElement('div', null
-                    , React__default["default"].createElement('h1', null, title)
+                    , React__default["default"].createElement('h1', null, data.title)
                     , React__default["default"].createElement('div', null
-                        , sections.map((s, i) => (
+                        , data.sections.map((s, i) => (
                             React__default["default"].createElement(Section, { key: i, model: s,} )
                         ))
                     )
                     , React__default["default"].createElement('div', null
-                        , React__default["default"].createElement('h2', null, "FOOTER")
+                        , React__default["default"].createElement(Footer, { model: data.footer,} )
                     )
                 )
             )
--- features/polyTalkPreview/src/index.jsx
+++ features/polyTalkPreview/src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { Slideshow, Screen } from "@polypoly-eu/poly-look";
+import { Slideshow, Screen, ClickableCard } from "@polypoly-eu/poly-look";
 
 import "./styles.css";
 
@@ -17,45 +17,71 @@ const App = () => {
         );
     };
 
-    let title = "TEST Title";
-    let sections = [
-        {
-            title: "Section 1",
+    const Footer = (props) => {
+        return (
+            <ClickableCard
+                buttonText={props.model.buttonTitle}
+                onlyButtonClickEvent={true}
+                onClick={() => {}}
+            >
+                <h3>{props.model.title}</h3>
+                <p>{props.model.description}</p>
+                {image !== undefined && image !== "" && (
+                    <img src={props.model.image} />
+                )}
+            </ClickableCard>
+        );
+    };
+
+    let data = {
+        title: "TEST Title",
+        sections: [
+            {
+                title: "Section 1",
+                description:
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                images: ["./images/test.jpeg", "./images/test.jpeg"],
+            },
+            {
+                title: "Section 2",
+                description:
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                images: ["./images/test.jpeg"],
+            },
+            {
+                title: "Section 3",
+                description:
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                images: ["./images/test.jpeg"],
+            },
+            {
+                title: "Section 4",
+                description:
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                images: [],
+            },
+        ],
+        footer: {
+            title: "Footer title",
             description:
                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            images: ["./images/test.jpeg", "./images/test.jpeg"],
+            image: "./images/test.jpeg",
+            buttonTitle: "Learn More",
+            buttonLink: "https://polypoly.org/en-gb/",
         },
-        {
-            title: "Section 2",
-            description:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            images: ["./images/test.jpeg"],
-        },
-        {
-            title: "Section 3",
-            description:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            images: ["./images/test.jpeg"],
-        },
-        {
-            title: "Section 4",
-            description:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            images: [],
-        },
-    ];
+    };
 
     return (
         <Screen className="poly-theme-light" layout="poly-standard-layout">
             <div>
-                <h1>{title}</h1>
+                <h1>{data.title}</h1>
                 <div>
-                    {sections.map((s, i) => (
+                    {data.sections.map((s, i) => (
                         <Section key={i} model={s} />
                     ))}
                 </div>
                 <div>
-                    <h2>FOOTER</h2>
+                    <Footer model={data.footer} />
                 </div>
             </div>
         </Screen>
