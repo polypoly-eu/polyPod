c7b4b9d2155bc7563a78cfedc92542375569c1be
Georgiana Piscoi <georgiana.piscoi@polypoly.net>
2022-06-22 21:28:24 +0300

    styling

--- features/polyTalkPreview/src/index.jsx
+++ features/polyTalkPreview/src/index.jsx
@@ -8,8 +8,8 @@ import "./styles.css";
 
 const Section = (props) => {
     return (
-        <div>
-            <h3>{i18n.t(props.model.title)}</h3>
+        <div className="section">
+            <h3 className="section-title">{i18n.t(props.model.title)}</h3>
             <p>{i18n.t(props.model.description)}</p>
             {props.model.images.length > 0 && (
                 <Slideshow images={props.model.images} />
@@ -20,26 +20,27 @@ const Section = (props) => {
 
 const Footer = (props) => {
     return (
-        <ClickableCard
-            buttonText={i18n.t(props.model.buttonTitle)}
-            onlyButtonClickEvent={true}
-            onClick={() => {
-                // TODO: Open link
-            }}
-        >
-            <h3>{i18n.t(props.model.title)}</h3>
-            <p>{i18n.t(props.model.description)}</p>
-            {props.model.image !== undefined && props.model.image !== "" && (
-                <img src={props.model.image} />
-            )}
-        </ClickableCard>
+        <div className="footer">
+            <ClickableCard
+                buttonText={i18n.t(props.model.buttonTitle)}
+                onlyButtonClickEvent={true}
+                onClick={() => {
+                    // TODO: Open link
+                }}
+            >
+                <h3>{i18n.t(props.model.title)}</h3>
+                <p>{i18n.t(props.model.description)}</p>
+                {props.model.image !== undefined &&
+                    props.model.image !== "" && <img src={props.model.image} />}
+            </ClickableCard>
+        </div>
     );
 };
 
 const App = () => {
     return (
         <Screen className="poly-theme-light" layout="poly-standard-layout">
-            <div>
+            <div className="preview">
                 <h1>{i18n.t(content.title)}</h1>
                 <div>
                     {content.sections.map((s, i) => (
