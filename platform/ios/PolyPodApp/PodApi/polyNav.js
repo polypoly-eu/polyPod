819c9ea8a741318a1a249928ff382bda67fd7505
Ghenadie Vasiliev-Pusca <ghenadie.pusca@polypoly.net>
2022-06-23 18:16:28 +0300

    Revert origin check for now (#1004)

--- platform/ios/PolyPodApp/PodApi/polyNav.js
+++ platform/ios/PolyPodApp/PodApi/polyNav.js
@@ -1,10 +1,4 @@
-window.addEventListener(
-    "message",
-    ({ data: { command, action }, origin }) => {
-        if (window.location.href.indexOf(origin) === 0) {
-            return;
-        }
-        if (command === "triggerPolyNavAction") pod.polyNav.actions[action]();
-    },
-    false
-);
+window.addEventListener("message", ({ data: { command, action } }) => {
+    if (command === "triggerPolyNavAction")
+        pod.polyNav.actions[action]();
+}, false);
