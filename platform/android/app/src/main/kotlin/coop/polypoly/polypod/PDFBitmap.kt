97d6bb79ebccbb6b54f3ae263eedbd5dac6fb95f
Ghenadie Vasiliev-Pusca <ghenadie.pusca@polypoly.net>
2022-06-24 14:30:09 +0300

    New home screen android (#999)
    * Initial jetpack compose commit
    
    * Small card draft
    
    * [WIP] Figure out how to do layout
    
    * Adjust image size
    
    * Small card view
    
    * Rename to small tile
    
    * Introduce Tile and Tile config
    
    * Row Container View
    
    * Add calculations and Big Tile View
    
    * Add paddings and spacers
    
    * Add foreground color and background color for tiles
    
    * Fix lint error
    
    * Large Left Container View
    
    * Add LargeRightContainerView
    
    * MyDataSectionView
    
    * DataKnowHowSectionView
    
    * Introduce Section Model
    
    * Introduce Config and Layout
    
    * Remove greeting
    
    * Rework the connection between the UI and the data. Let the data create the UI. Decouple things.
    
    * MediumTileView + handle text overflow
    
    * Tools + Data Know How sections
    
    * Put all the sections together to form the screen
    
    * Small refactoring
    
    * Create data for Footer
    
    * [WIP] Oh Lord have mercy on this Footer
    
    * Mr. Footer
    
    * Poly in Style :sunglasses:
    
    * Linters are my nemesis
    
    * Down the rabbit hole: Refactored the way styles are assembled
    
    * Fix linter issues
    
    * Fix nasty bug
    
    * Configure layout using PolyStyle
    
    * Fonts :checkmark:
    
    * Small improvements to the code
    
    * Add spacings for container, section and screen
    
    * Refactor init
    
    * Add top bar
    
    * Add full navigation
    
    * Add back preview
    
    * Render pdfs
    
    * ktlint fixes
    
    * Pull out more code from HomeScreenFragment
    
    * ktlint fixes
    
    * Add missing function refactoring
    
    * Correct logger config
    
    * Cleanup dependencies
    
    * Fix wrong name
    
    * Filter out categories without features
    
    * Disable test due to JNI incompatibility
    
    * [android] Add borders on home screen tiles - PROD4POD-1697 (#1003)
    
    * set borders on UI Tiles of homeScreen
    
    * add borderWidth in TileLayout
    
    * add borderColor in TileModel
    
    * refactor - set variable as private, return of onCreateView() as no-null
    
    * Provide Swiftlint correct config path
    
    * move swiftlint script to separate file
    
    * Review followup
    
    * make linter happy
    
    * Add missing assignment
    
    Co-authored-by: Tim Palade <palade.timotei@gmail.com>
    Co-authored-by: irstavr <irini.stavrakantonaki@polypoly.net>

---
+++ platform/android/app/src/main/kotlin/coop/polypoly/polypod/PDFBitmap.kt
@@ -0,0 +1,52 @@
+package coop.polypoly.polypod
+
+import android.graphics.Bitmap
+import android.graphics.pdf.PdfRenderer
+import android.os.ParcelFileDescriptor
+import java.io.File
+import java.io.InputStream
+
+class PDFBitmap {
+    companion object {
+        fun bitmapFromPDF(
+            pdfInputStream: InputStream,
+            densityDpi: Int
+        ): Bitmap {
+            val tempPdf = File(kotlin.io.path.createTempFile().toString())
+            pdfInputStream.use { input ->
+                tempPdf.outputStream().use { output ->
+                    input.copyTo(output)
+                }
+            }
+
+            val fileDescriptor = ParcelFileDescriptor.open(
+                tempPdf,
+                ParcelFileDescriptor.MODE_READ_ONLY
+            )
+            val pdfRenderer = PdfRenderer(fileDescriptor)
+            val page = pdfRenderer.openPage(0)
+
+            // Create the bitmap to host the actual image.
+            // Scales the image to device screen dpi.
+            // Value of 72 corresponds to `render` docs:
+            // "transform the content from page coordinates which are in points
+            //  (1/72") to bitmap coordinates which are in pixels"
+            val bitmap = Bitmap.createBitmap(
+                densityDpi * page.width / 72,
+                densityDpi * page.height / 72,
+                Bitmap.Config.ARGB_8888
+            )
+            page.render(
+                bitmap,
+                null,
+                null,
+                PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY
+            )
+
+            page.close()
+            pdfRenderer.close()
+
+            return bitmap
+        }
+    }
+}
