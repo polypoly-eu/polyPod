ddad877218483081961548ed208fbc091f0ff113
Tim Palade <palade.timotei@gmail.com>
2022-06-27 15:16:29 +0300

    Simplify manifest template generation

--- dev-utils/poly-cli/src/templates/manifest.js
+++ dev-utils/poly-cli/src/templates/manifest.js
@@ -1,38 +1,24 @@
 export function manifestTemplate(feature_name, author, feature_type) {
-    let t;
+    let base = {
+        name: feature_name,
+        author: author,
+        description: "",
+        thumbnail: "",
+        primaryColor: "#ffffff",
+        links: {},
+        translations: {
+            de: {
+                author: "",
+                description: "",
+            },
+        },
+    };
+
     if (feature_type === "preview") {
-        t = {
-            name: feature_name,
-            author: author,
-            description: "",
-            thumbnail: "",
-            primaryColor: "#ffffff",
-            links: {
-                "learn-more": "https://polypoly.org",
-            },
-            translations: {
-                de: {
-                    author: "",
-                    description: "",
-                },
-            },
-        };
-    } else {
-        t = {
-            name: feature_name,
-            author: author,
-            description: "",
-            thumbnail: "",
-            primaryColor: "#ffffff",
-            links: {},
-            translations: {
-                de: {
-                    author: "",
-                    description: "",
-                },
-            },
+        base.links = {
+            "learn-more": "https://polypoly.org",
         };
     }
 
-    return JSON.stringify(t, null, 2);
+    return JSON.stringify(base, null, 2);
 }
