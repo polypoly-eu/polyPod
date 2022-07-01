95bdc63a595a216a8d88e16ccb3ad22711267442
mteresa-jimenez <mteresa.jn@gmail.com>
2022-07-01 18:17:03 +0200

    Fix tests

--- feature-utils/poly-look/test/react-components/banners/banner.test.js
+++ feature-utils/poly-look/test/react-components/banners/banner.test.js
@@ -1,37 +1,30 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import { HistoryStub } from "../../utils/history-stub";
 import { Banner } from "../../../src/react-components";
 
 /**
  * @jest-environment jsdom
  */
-describe("Banner", () => {
-  it("renders correctly", () => {
-    const { container, getByText, getByRole } = render(
-      <Banner
-        title="Title"
-        description="Description"
-        icon="icon.svg"
-        button={{ label: "Label" }}
-      />
-    );
+it("Banner renders correctly", () => {
+  const { container, getByText, getByRole } = render(
+    <Banner title="Title" description="Description" icon="icon.svg" />
+  );
 
-    expect(container).toBeTruthy();
-    expect(getByText("Title")).toBeTruthy();
-    expect(getByText("Description")).toBeTruthy();
-    expect(getByRole("img")).toBeTruthy();
-  });
+  expect(container).toBeTruthy();
+  expect(getByText("Title")).toBeTruthy();
+  expect(getByText("Description")).toBeTruthy();
+  expect(getByRole("img")).toBeTruthy();
+});
 
-  // it("has a functional button", () => {
-  //   const mockedClickEvent = jest.fn();
+it("Clicking the button leads to the correct route", () => {
+  const history = new HistoryStub();
 
-  //   const { getByText } = render(
-  //     <Banner button={{ label: "Label", route: "/route" }} />
-  //   );
-  //   const buttonElement = getByText("/route");
-  //   console.log(buttonElement);
-  //   fireEvent.click(buttonElement, mockedClickEvent);
-  //   expect(mockedClickEvent).toHaveBeenCalled();
-  // });
+  const { getByText } = render(
+    <Banner button={{ label: "Label", route: "/route", history: history }} />
+  );
+  const buttonElement = getByText("Label");
+  fireEvent.click(buttonElement);
+  expect(history.route).toBe("/route");
 });
