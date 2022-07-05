1434844c729fb818acdf21ae7814b4bcba186510
Juan Julián Merelo Guervós <jjmerelo@gmail.com>
2022-07-05 10:29:53 +0200

    Task generator improvements (#1031)
    * Adds section* to preview.json
    
    * Leaves non-numbered section
    
    * Adds name to translations.de
    
    * Include default for thumbnail
    
    * Add thumbnail to manifest in preview
    
    * Font size changed to medium
    
    * Add thumbnail to the templates
    
    * Use medium font weight
    
    Co-authored-by: Tim Palade <palade.timotei@gmail.com>

--- dev-utils/poly-cli/src/templates/manifest.js
+++ dev-utils/poly-cli/src/templates/manifest.js
@@ -17,6 +17,7 @@ export function manifestTemplate(feature_name, author, version, feature_type) {
         links: {},
         translations: {
             de: {
+                name: `${feature_name} auf Deutsch`,
                 author: "",
                 description: "",
             },
@@ -24,6 +25,7 @@ export function manifestTemplate(feature_name, author, version, feature_type) {
     };
 
     if (feature_type === "preview") {
+        base.thumbnail = "images/thumbnail.pdf";
         base.links = {
             "learn-more": "https://polypoly.org",
         };
