b36d265afab3e9ea0442eb25bcabf0231c37434b
Tim Palade <palade.timotei@gmail.com>
2022-07-06 13:16:55 +0300

    Fix: Large right does shows small tile and big tile when only 2 are provided

--- platform/android/app/src/main/kotlin/coop/polypoly/polypod/homescreen/HomeScreenViewModel.kt
+++ platform/android/app/src/main/kotlin/coop/polypoly/polypod/homescreen/HomeScreenViewModel.kt
@@ -11,7 +11,7 @@ import coop.polypoly.polypod.features.FeatureStorage
 data class SectionModel(
     val title: String,
     val type: SectionType,
-    val tiles: List<TileModel>,
+    val tiles: List<TileModel>
 )
 
 enum class SectionType {
@@ -39,13 +39,13 @@ data class TileModel(
     val backgroundColor: Color,
     val borderColor: Color,
     val tileTextColor: Color,
-    val onSelection: () -> Unit,
+    val onSelection: () -> Unit
 )
 
 data class FooterModel(
     val title: String,
     val description: String,
-    val buttonTitle: String,
+    val buttonTitle: String
 ) {
     fun buttonOpenUri(context: Context): Uri {
         return Uri.parse(
@@ -118,7 +118,7 @@ class HomeScreenViewModel {
                         feature.thumbnail,
                         Color(feature.thumbnailColor),
                         Color(feature.borderColor),
-                        Color(feature.tileTextColor),
+                        Color(feature.tileTextColor)
                     ) {
                         onFeatureSelected(feature.id)
                     }
