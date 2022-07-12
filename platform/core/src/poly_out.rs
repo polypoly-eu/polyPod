9b715631cc38de3e88649ab451aa3df895017bca
Tim Palade <palade.timotei@gmail.com>
2022-07-12 20:53:55 +0300

    Add list function + expand write + rename importArchive to unzip + rename remove

--- platform/core/src/poly_out.rs
+++ platform/core/src/poly_out.rs
@@ -1,14 +1,20 @@
 trait PolyOut {
+  // list all the resource ids
+  fn list() -> Result<[ResourceId], &str>;
+  fn stat(resource_id: ResourceId) -> Result<Stats, &str>;
   // metadata replaces readDir. The metadata of a folder resource will contain the name of the files and folders inside that folder.
   fn metadata(resource_id: ResourceId) -> Result<Metadata, &str>;
   // read will return the content of a resource
   // if you read a file, it will return the content
   // if you read a dir, it will return an error, telling you that you should read each file individually
   fn read(resource_id: ResourceId) -> Result<Content, &str>;
-  fn write(resource_id: String, content: Content) -> Result<Void, &str>;
-  fn stat(resource_id: ResourceId) -> Result<Stats, &str>;
-  fn importArchive(url: String, dest_resource_id: ResourceId) -> Result<ResourceId, &str>;
-  fn removeArchive(resource_id: ResourceId) -> Result<Void, &str>;
+  // You don't need a resource id if you want to write. It will return you a new resource id if you don't pass one
+  // If you do, it will write to that resource you specified.
+  // We might need some options like appending to the end of a file.
+  // write is a combination of create + update
+  fn write(content: Content, dest_resource_id: Optional<ResourceId>) -> Result<ResourceId, &str>;
+  fn unzip(url: String, dest_resource_id: ResourceId) -> Result<ResourceId, &str>;
+  fn remove(resource_id: ResourceId) -> Result<Void, &str>;
 }
 
 trait Stats {}
@@ -19,6 +25,8 @@ enum ResourceType {
 }
 struct ResourceId {
   res_type: ResourceType,
+  uuid: String, 
+  name: String
 }
 
 trait Metadata {}
