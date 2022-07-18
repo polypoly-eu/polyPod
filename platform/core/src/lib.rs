6ef26708054e3193664b81f738b8e81cce7a4391
Tim Palade <palade.timotei@gmail.com>
2022-07-18 18:29:32 +0300

    [WIP] Figured out the structure + Working out the filesystem and unzipping

--- platform/core/src/lib.rs
+++ platform/core/src/lib.rs
@@ -3,3 +3,4 @@ pub mod core_failure;
 pub mod feature_manifest_parsing;
 #[cfg(any(target_os = "ios", target_os = "android"))]
 pub mod ffi;
+pub mod poly_out;
--- platform/core/src/poly_out.rs
+++ platform/core/src/poly_out.rs
@@ -1,28 +1,112 @@
+use std::fs::DirBuilder;
+use std::path::Path;
+use uuid::Uuid;
+
+type ResourceId = String;
+type FsId = String;
+
+struct Metadata {}
+struct Content {}
+
 trait PolyOutTrait {
-  fn metadata(resource_id: ResourceId) -> Result<Metadata, &str>;
-  fn read(resource_id: ResourceId) -> Result<Content, &str>;
-  fn remove(resource_id: ResourceId) -> Result<Void, &str>;
-  // import will decide if it needs to unzip a file. The contents of the url will be extracted and inserted into a database.
-  fn import(url: String, dest_resource_id: Optional<ResourceId>) -> Result<ResourceId, &str>;
+    fn metadata(resource_id: ResourceId) -> Result<Metadata, String>;
+    fn read(resource_id: ResourceId) -> Result<Content, String>;
+    fn remove(resource_id: ResourceId) -> Result<(), String>;
+    // import will decide if it needs to unzip a file. The contents of the url will be extracted and inserted into a database.
+    fn import(
+        url: String,
+        feature_name: String,
+        dest_resource_id: Option<ResourceId>,
+    ) -> Result<ResourceId, String>;
 }
 
-impl PolyOut for PolyOutTrait {
+struct PolyOut {}
 
+impl PolyOut {
+    fn fs_id_from_resource_id(resource_id: ResourceId) -> FsId {
+        // TODO
+        return "".to_string();
+    }
+
+    fn resource_id_from_fs_id(fs_id: FsId) -> ResourceId {
+        // TODO
+        return "".to_string();
+    }
+
+    fn file_system_url_from_fs_id(fs_id: FsId) -> String {
+        // TODO
+        return "".to_string();
+    }
+
+    fn feature_files_path(feature_name: String) -> String {
+        // TODO: Create a path that will work on iOS, Android and Web
+        let path = format!("~/Documents/Developer/Tests/PolyPoly/{}", feature_name);
+        return path;
+    }
+
+    fn make_sure_feature_files_dir_exists(feature_name: String) -> Result<(), String> {
+        let files_path = PolyOut::feature_files_path(feature_name);
+        if !Path::new(&files_path).exists() {
+            DirBuilder::new()
+                .recursive(true)
+                .create(files_path)
+                .map_err(|err| err.to_string())?;
+        }
+        Ok(())
+    }
 }
 
-enum ResourceType {
-  File,
-  Folder,
+impl PolyOutTrait for PolyOut {
+    fn import(
+        url: String,
+        feature_name: String,
+        dest_resource_id: Option<ResourceId>,
+    ) -> Result<ResourceId, String> {
+        PolyOut::make_sure_feature_files_dir_exists(feature_name)?;
+
+        let fs_id = match dest_resource_id {
+            Some(res_id) => PolyOut::fs_id_from_resource_id(res_id),
+            None => Uuid::new_v4().to_string(),
+        };
+
+        let target = PolyOut::file_system_url_from_fs_id(fs_id.to_string());
+
+        //TODO: unzip in target
+
+        //TODO: write metadata for this resource id - add the files that are part of this
+        //resource id.
+
+        let resource_id = PolyOut::resource_id_from_fs_id(fs_id.to_string());
+        return Ok(resource_id);
+    }
+
+    fn metadata(resource_id: ResourceId) -> Result<Metadata, String> {
+        Err("mda".to_string())
+    }
+    fn read(resource_id: ResourceId) -> Result<Content, String> {
+        Err("mda".to_string())
+    }
+    fn remove(resource_id: ResourceId) -> Result<(), String> {
+        Err("mda".to_string())
+    }
 }
 
-struct ResourceId {
-  res_type: ResourceType,
-  uuid: String, 
-  name: String,
-  // associated feature with this resource - backlink ?
-  assoc_feature: String,
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_fs_id_from_resource_id() {
+        assert_eq!("", "a");
+    }
+
+    #[test]
+    fn test_resource_id_from_fs_id() {
+        assert_eq!("", "a");
+    }
+
+    #[test]
+    fn test_file_system_url_from_fs_id() {
+        assert_eq!("", "a");
+    }
 }
-
-trait Metadata {}
-
-trait Content {}
