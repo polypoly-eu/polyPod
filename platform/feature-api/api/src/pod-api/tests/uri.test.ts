b853499a054929fdabc70010dd2efdd09463c53b
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2022-07-25 09:03:17 +0200

    :white_check_mark: for the rest of the functions

--- platform/feature-api/api/src/pod-api/tests/uri.test.ts
+++ platform/feature-api/api/src/pod-api/tests/uri.test.ts
@@ -4,4 +4,9 @@ describe("Test functions", () => {
     it("generates and UUID in the required format", () => {
         expect(createUUID()).toMatch(/^\w{8}-\w{4}-4\w{3}-\w{4}-\w{12}$/);
     });
+    it("recognizes good and bad URIs", () => {
+        const polyUri: PolyUri = new PolyUri();
+        expect(isPolypodUri(polyUri.toString())).toBe(true);
+        expect(isPolypodUri("foobargaz")).toBe(false);
+    });
 });
