5df5e8ee0daa92b64be28d45fd1ea4f98c2aea48
Juan Julián Merelo Guervós <jjmerelo@gmail.com>
2022-07-26 11:26:08 +0200

    Update platform/feature-api/api/src/pod-api/tests/uri.test.ts
    Thanks!
    
    Co-authored-by: irstavr <irini.stavrakantonaki@polypoly.net>

--- platform/feature-api/api/src/pod-api/tests/uri.test.ts
+++ platform/feature-api/api/src/pod-api/tests/uri.test.ts
@@ -1,7 +1,7 @@
 import { createUUID, isPolypodUri, PolyUri } from "../uri";
 
 describe("Test functions", () => {
-    it("generates and UUID in the required format", () => {
+    it("should generate an UUID in the required format", () => {
         expect(createUUID()).toMatch(/^\w{8}-\w{4}-4\w{3}-\w{4}-\w{12}$/);
     });
     
