70f3ea79029133137466619ef4e9e401f2cd21c1
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2022-07-26 10:38:53 +0200

    Adds crypto-secure UUID and refactors to use it

--- platform/feature-api/api/src/pod-api/uri.ts
+++ platform/feature-api/api/src/pod-api/uri.ts
@@ -1,17 +1,12 @@
+import { v4 as uuidv4 } from "uuid";
+
 /**
  * Creates a random UUID string with a random hexadecimal value for each character in the string
  * 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx', and returns the result.
  * @returns a string in UUID format
  */
 export function createUUID(): string {
-    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
-        /[xy]/g,
-        function (c) {
-            const r = (Math.random() * 16) | 0,
-                v = c == "x" ? r : (r & 0x3) | 0x8;
-            return v.toString(16);
-        }
-    );
+    return uuidv4();
 }
 
 export function isPolypodUri(uri: string): boolean {
