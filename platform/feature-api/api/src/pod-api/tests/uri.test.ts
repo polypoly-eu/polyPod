d70f0a156fa49fe2602cfd70599531b19133bacd
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2022-07-26 08:14:03 +0200

    Generates tests for different UUIDs

--- platform/feature-api/api/src/pod-api/tests/uri.test.ts
+++ platform/feature-api/api/src/pod-api/tests/uri.test.ts
@@ -4,6 +4,14 @@ describe("Test functions", () => {
     it("generates and UUID in the required format", () => {
         expect(createUUID()).toMatch(/^\w{8}-\w{4}-4\w{3}-\w{4}-\w{12}$/);
     });
+    it("generates different UUIDs", () => {
+        let lastUUID = createUUID();
+        for (let i = 0; i < 100; i++) {
+            const thisUUID = createUUID();
+            expect(thisUUID).not.toEqual(lastUUID);
+            lastUUID = thisUUID;
+        }
+    });
     it("recognizes good and bad URIs", () => {
         const polyUri: PolyUri = new PolyUri();
         expect(isPolypodUri(polyUri.toString())).toBe(true);
