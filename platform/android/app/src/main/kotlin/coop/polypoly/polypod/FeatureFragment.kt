15e9c408adb24691ebbb026c08c59c701f0917eb
Richard Stoffels <richard.stoffels@polypoly.net>
2022-07-28 14:41:06 +0200

    Remove unused import

--- platform/android/app/src/main/kotlin/coop/polypoly/polypod/FeatureFragment.kt
+++ platform/android/app/src/main/kotlin/coop/polypoly/polypod/FeatureFragment.kt
@@ -19,7 +19,6 @@ import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-import coop.polypoly.core.Core
 import coop.polypoly.core.Feature
 import coop.polypoly.polypod.features.FeatureStorage
 import coop.polypoly.polypod.logging.LoggerFactory
