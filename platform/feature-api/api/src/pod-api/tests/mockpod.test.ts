5f9701bc925242c066f2fb7ab44a7c7d4c036057
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2022-07-28 10:57:12 +0200

    Checking that it throws correctly

--- platform/feature-api/api/src/pod-api/tests/mockpod.test.ts
+++ platform/feature-api/api/src/pod-api/tests/mockpod.test.ts
@@ -1,13 +1,18 @@
 import { podSpec } from "../spec";
 import { MockPod, MockPolyOut } from "../mock-pod";
 import { FS } from "../fs";
-
+import { PolyPodUriError } from "../uri";
 import { Volume } from "memfs";
 
 describe("Mock polyOut", () => {
-    let mockPolyOut;
+    let mockPolyOut: MockPolyOut;
     beforeAll(() => {
         mockPolyOut = new MockPolyOut();
+    });
+    it("should fail with bad URIs", async () => {
+        await expect(
+            mockPolyOut.importArchive("foo", "bar")
+        ).rejects.toThrowError(PolyPodUriError);
     });
 });
 
