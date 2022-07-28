966e7ac7cf544a2e44a52b0838714218d376425a
mteresa-jimenez <mteresa.jn@gmail.com>
2022-07-28 15:46:35 +0200

    Fix screen overlap in FeatureStorePreview feature

--- features/featureStorePreview/src/index.jsx
+++ features/featureStorePreview/src/index.jsx
@@ -118,7 +118,7 @@ const App = () => {
     }, []);
 
     return (
-        <>
+        <div className="feature-store-preview">
             <div className="poly-nav-bar-separator-overlay" />
             <Screen
                 className="poly-theme-light background-white"
@@ -154,7 +154,7 @@ const App = () => {
                     )}
                 </div>
             </Screen>
-        </>
+        </div>
     );
 };
 
