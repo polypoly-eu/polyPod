19b3a1a65adfbf244399dd62c3f4512f48a282b2
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2022-07-28 08:17:10 +0200

    Testing new feature

--- platform/feature-api/api/src/pod-api/tests/mockpod.test.ts
+++ platform/feature-api/api/src/pod-api/tests/mockpod.test.ts
@@ -1,6 +1,14 @@
 import { podSpec } from "../spec";
 import { MockPod } from "../mock-pod";
+import { FS } from "../fs";
 
-describe("Mock pod", () => {
+import { Volume } from "memfs";
+
+describe("Mock pod with default values", () => {
     podSpec(new MockPod(), "/");
+});
+
+describe("Mock pod with existing fs", () => {
+    const fs = new Volume().promises as unknown as FS;
+    podSpec(new MockPod(fs), "/");
 });
