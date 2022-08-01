00e50e6638495dd1d8e720c62839afc2250fc828
Richard Stoffels <richard.stoffels@polypoly.net>
2022-08-01 17:02:12 +0200

    Fix issues with imoporting store after changes to polyOut

--- platform/feature-api/api/src/pod-api/tests/default.test.ts
+++ platform/feature-api/api/src/pod-api/tests/default.test.ts
@@ -3,15 +3,10 @@ import { DefaultPod } from "../default";
 import { dataset } from "@rdfjs/dataset";
 import { Volume } from "memfs";
 import { FS } from "../fs";
-import oxigraph from "../../../node_modules/oxigraph/node.js";
 
 describe("Mock pod", () => {
     podSpec(
-        new DefaultPod(
-            dataset(),
-            new Volume().promises as unknown as FS,
-            new oxigraph.Store()
-        ),
+        new DefaultPod(dataset(), new Volume().promises as unknown as FS),
         "/"
     );
 });
