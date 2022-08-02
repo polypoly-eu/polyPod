a8c513cfce44034adc2eb6c3596f3143a0892b4e
irstavr <irini.stavrakantonaki@polypoly.net>
2022-08-02 13:32:06 +0200

    refactor(ios): use endpointInfoFromId instead of environment var
    in errorUploader

--- platform/ios/PolyPodApp/PolyPod/ErrorUploader.swift
+++ platform/ios/PolyPodApp/PolyPod/ErrorUploader.swift
@@ -4,44 +4,43 @@ import MessagePack
 class ErrorUploader {
     static let shared = ErrorUploader()
 
-    func uploadToServer(_ errorMsg: String, 
-        completionHandler: @escaping (MessagePackValue?, MessagePackValue?) -> Void
+    func uploadToServer(_ errorMsg: String,
+                        completionHandler: @escaping (MessagePackValue?, MessagePackValue?) -> Void
     ) {
         let endpointId = "polyApiErrorReport"
-        let authToken = getEnvironmentVar("POLYPOD_ERROR_REPORT_AUTHORIZATION")
+        
+        guard let endpointInfo = PodApi.shared.endpoint.endpointInfoFromId(endpointId: endpointId) else {
+            Log.error("uploadToServer failed: No endpoint found for: \(endpointId)")
+            completionHandler("uploadToServer", MessagePackValue("\(#function): No endpoint found for \(endpointId)"))
+            return
+        }
         let payload = jsonStringify(["errorMsg"])
 
         PodApi.shared.endpoint.send(
             endpointId: endpointId,
             payload: payload,
             contentType: "application/json",
-            authToken: authToken
+            authToken: endpointInfo.auth
         ) { error in
-            print("error in POST error=", error?.localizedDescription)
+            Log.debug("uploadToServer(): error in POST to \(endpointId)")
+            
             completionHandler(
                 .nil,
                 error == nil ? nil :
-                MessagePackValue("\(#function): \(String(describing: error?.localizedDescription))")
+                    MessagePackValue("\(#function): \(String(describing: error?.localizedDescription))")
             )
-        
         }
     }
-    
+
     private func jsonStringify(_ objectToConvert: Any) -> String {
         var resultJson = ""
-        if let json = try? JSONSerialization.data(withJSONObject: objectToConvert)
-        {
+        if let json = try? JSONSerialization.data(withJSONObject: objectToConvert) {
             if let content = String(data: json, encoding: String.Encoding.utf8) {
                 // here `content` is the JSON dictionary containing the String
                 resultJson = content
             }
         }
         return resultJson
-    }
-
-    private func getEnvironmentVar(_ name: String) -> String? {
-        guard let rawValue = getenv(name) else { return nil }
-        return String(utf8String: rawValue)
     }
 
 }
