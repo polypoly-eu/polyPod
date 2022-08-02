117998feb6a0bb38bc3cd32ec722449eb0edde36
irstavr <irini.stavrakantonaki@polypoly.net>
2022-08-02 13:31:19 +0200

    refactor(ios): set endpointInfoFromId() public + approveEndpointFetch() priv
    iOS: remove duplication of code of these two funcs inside Endpoint

--- platform/ios/PolyPodApp/PodApi/Endpoint/Endpoint.swift
+++ platform/ios/PolyPodApp/PodApi/Endpoint/Endpoint.swift
@@ -3,23 +3,23 @@ import MessagePack
 
 protocol EndpointProtocol: AnyObject {
     func send(
-        endpointId: String, 
-        payload: String, 
-        contentType: String?, 
-        authToken: String?, 
+        endpointId: String,
+        payload: String,
+        contentType: String?,
+        authToken: String?,
         completionHandler: @escaping (Error?) -> Void
     )
     func get(
-        endpointId: String, 
+        endpointId: String,
         contentType: String?,
-        authToken: String?, 
+        authToken: String?,
         completionHandler: @escaping (String?, Error?) -> Void
     )
 }
 
 protocol EndpointDelegate: AnyObject {
     func doHandleApproveEndpointFetch(
-        endpointId: String, 
+        endpointId: String,
         completion: @escaping (Bool) -> Void
     )
 }
@@ -31,40 +31,25 @@ struct EndpointInfo: Decodable {
 }
 
 final class Endpoint: EndpointProtocol {
-    
+
     init() {
         delegate = nil
     }
-    
+
     let network: Network = Network()
     var delegate: EndpointDelegate?
-    
-    func approveEndpointFetch(endpointId: String, completion: @escaping (Bool) -> Void) {
+
+    private func approveEndpointFetch(endpointId: String, completion: @escaping (Bool) -> Void) {
         delegate?.doHandleApproveEndpointFetch(endpointId: endpointId, completion: completion)
     }
-    
-    private func endpointInfoFromId(endpointId: String) -> EndpointInfo? {
-        let endpointsPath = 
-            Bundle.main.bundleURL.appendingPathComponent("config/endpoints.json")
-        guard let endpointsJsonData = (try? Data(contentsOf: endpointsPath)) else { return nil }
-        guard let endpointsJson = (
-            try? JSONDecoder().decode(
-                Dictionary<String, EndpointInfo>.self, 
-                from: endpointsJsonData
-                )
-            ) else { 
-                return nil 
-            }
-        return endpointsJson[endpointId]
-    }
-    
+
     func send(
-        endpointId: String, 
-        payload: String, 
-        contentType: String?, 
-        authToken: String?, 
+        endpointId: String,
+        payload: String,
+        contentType: String?,
+        authToken: String?,
         completionHandler: @escaping (Error?) -> Void
-        ) {
+    ) {
         approveEndpointFetch(endpointId: endpointId) { approved in
             guard approved else {
                 Log.error("endpoint.send failed: Permission for endpoint \(endpointId) denied")
@@ -79,10 +64,10 @@ final class Endpoint: EndpointProtocol {
             }
 
             let response = self.network.httpPost(
-                url: endpointInfo.url, 
-                body: payload, 
-                contentType: contentType, 
-                authToken: endpointInfo.auth, 
+                url: endpointInfo.url,
+                body: payload,
+                contentType: contentType,
+                authToken: endpointInfo.auth,
                 allowInsecure: endpointInfo.allowInsecure
             )
             switch response {
@@ -94,13 +79,13 @@ final class Endpoint: EndpointProtocol {
             }
         }
     }
-    
+
     func get(
-        endpointId: String, 
-        contentType: String?, 
-        authToken: String?, 
+        endpointId: String,
+        contentType: String?,
+        authToken: String?,
         completionHandler: @escaping (String?, Error?) -> Void
-        ) {
+    ) {
         approveEndpointFetch(endpointId: endpointId) { approved in
             guard approved else {
                 Log.error("endpoint.get failed: Permission for endpoint \(endpointId) denied")
@@ -115,9 +100,9 @@ final class Endpoint: EndpointProtocol {
             }
 
             let response = self.network.httpGet(
-                url: endpointInfo.url, 
-                contentType: contentType, 
-                authToken: endpointInfo.auth, 
+                url: endpointInfo.url,
+                contentType: contentType,
+                authToken: endpointInfo.auth,
                 allowInsecure: endpointInfo.allowInsecure
             )
             switch response {
@@ -130,15 +115,18 @@ final class Endpoint: EndpointProtocol {
         }
     }
 
-    private func approveEndpointFetch(endpointId: String, completion: @escaping (Bool) -> Void) -> Void {
-        delegate?.doHandleApproveEndpointFetch(endpointId: endpointId, completion: completion)
-    }
-
-    private func endpointInfoFromId(endpointId: String) -> EndpointInfo? {
-        let endpointsPath = Bundle.main.bundleURL
-            .appendingPathComponent("config/endpoints.json")
+    func endpointInfoFromId(endpointId: String) -> EndpointInfo? {
+        let endpointsPath =
+            Bundle.main.bundleURL.appendingPathComponent("config/endpoints.json")
         guard let endpointsJsonData = (try? Data(contentsOf: endpointsPath)) else { return nil }
-        guard let endpointsJson = (try? JSONDecoder().decode(Dictionary<String, EndpointInfo>.self, from: endpointsJsonData)) else { return nil }
+        guard let endpointsJson = (
+            try? JSONDecoder().decode(
+                Dictionary<String, EndpointInfo>.self,
+                from: endpointsJsonData
+            )
+        ) else {
+            return nil
+        }
         return endpointsJson[endpointId]
     }
 
