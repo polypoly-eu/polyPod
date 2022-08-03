b559c187318338c5cd191f18bf49f0fa742c8ccd
Richard Stoffels <richard.stoffels@polypoly.net>
2022-08-03 11:34:34 +0200

    Change queryResult to json string

--- platform/core/src/rdf.rs
+++ platform/core/src/rdf.rs
@@ -1,37 +1,11 @@
-use std::collections::HashMap;
 use oxigraph::store::{Store, StorageError};
+use sparesults::{QueryResultsFormat};
 use spargebra::{Query, Update};
-use serde::Deserialize;
-use serde::Serialize;
+use std::str;
 
 use crate::rdf_failure::RdfFailure;
 
 pub type SPARQLQuery = String;
-
-/// A simplified serializable replica of oxigraph::sparql::QuerySolution
-#[derive(Deserialize, Serialize, PartialEq, Debug, Clone)]
-pub struct QuerySolution {
-    values: HashMap<String, String>,
-}
-
-impl QuerySolution {
-    fn new(values: HashMap<String, String>) -> Self {
-        Self { values }
-    }
-}
-
-/// A simplified serializable replica of oxigraph::sparql::QueryResults
-/// Only Solutions for SELECT queries are currently supported
-#[derive(Deserialize, Serialize, PartialEq, Debug, Clone)]
-pub enum QueryResults {
-    /// Results of a [SELECT](https://www.w3.org/TR/sparql11-query/#select) query.
-    Solutions(Vec<QuerySolution>),
-    // TODO: Implement those when needed
-    // Result of a [ASK](https://www.w3.org/TR/sparql11-query/#ask) query.
-    // Boolean(bool),
-    // Results of a [CONSTRUCT](https://www.w3.org/TR/sparql11-query/#construct) or [DESCRIBE](https://www.w3.org/TR/sparql11-query/#describe) query.
-    // Graph(QueryTripleIter),
-}
 
 fn init_store(app_path: String) -> Result<Store, StorageError> {
     Store::open(app_path + env!("RDF_DB_PATH"))
@@ -51,45 +25,39 @@ fn check_update(query: SPARQLQuery) -> Result<Update, RdfFailure> {
     };
 }
 
-pub fn rdf_query(query: SPARQLQuery, app_path: String) -> Result<QueryResults, RdfFailure> {
+pub fn rdf_query(query: SPARQLQuery, app_path: String) -> Result<String, RdfFailure> {
     let store = init_store(app_path).map_err(RdfFailure::failed_to_initialize_store)?;
     match check_query(query.to_string()) {
         Ok(_) => {
             let query_results = store.query(&query).map_err( RdfFailure::map_evaluation_error)?;
-            return to_serializable_format(query_results)
+            bytes_to_string(to_json_bytes(query_results)?)
         },
-        Err(error) => return Err(error)
+        Err(error) => Err(error)
     }
 }
 
 pub fn rdf_update(query: SPARQLQuery, app_path: String) -> Result<(), RdfFailure> {
     let store = init_store(app_path).map_err(RdfFailure::failed_to_initialize_store)?;
     match check_update(query.to_string()) {
-        Ok(_) => return store.update(&query).map_err(RdfFailure::map_evaluation_error),
-        Err(error) => return Err(error)
+        Ok(_) => store.update(&query).map_err(RdfFailure::map_evaluation_error),
+        Err(error) => Err(error)
     }
 }
 
-fn to_serializable_format(query_results: oxigraph::sparql::QueryResults) -> Result<QueryResults, RdfFailure> {
-    let mut solutions: Vec<QuerySolution> = Vec::new();
-    if let oxigraph::sparql::QueryResults::Solutions(query_solutions) = query_results {
-        for query_solution_result in query_solutions {
-            let mut map: HashMap<String, String> = HashMap::new();
-            let query_solution = match query_solution_result {
-                Ok(val) => val,
-                Err(err) => return Err(RdfFailure::result_serialization_failed(err))
-            };
-            for var in query_solution.variables().iter() {
-                let query_solution_value = match query_solution.get(var) {
-                    Some(val) => val,
-                    None => break
-                };
-                map.insert(var.to_string(), query_solution_value.to_string());
-            }
-            solutions.push(QuerySolution::new(map))
-        }
+fn to_json_bytes(query_results: oxigraph::sparql::QueryResults) -> Result<Vec<u8>, RdfFailure> {
+    let mut results = Vec::new();
+    match query_results.write(&mut results, QueryResultsFormat::Json) {
+        Ok(_) => Ok(results),
+        Err(error) => Err(RdfFailure::map_evaluation_error(error))
     }
-    Ok(QueryResults::Solutions(solutions))
+}
+
+fn bytes_to_string(utf8_byte_array: Vec<u8>) -> Result<String, RdfFailure> {
+    match str::from_utf8(&utf8_byte_array) {
+        Ok(result) => Ok(result.to_string()),
+        Err(error) => Err(RdfFailure::map_utf8_error(error))
+    }
+
 }
 
 
@@ -108,13 +76,7 @@ mod tests {
             }
         "), String::from(".")).unwrap();
 
-        let test_map = HashMap::from([
-            (String::from("?s"), String::from("<http://example.com/you>")),
-            (String::from("?p"), String::from("<http://example.com/are>")),
-            (String::from("?o"), String::from("\"great\"")),
-        ]);
-        let test_result = QueryResults::Solutions(vec![QuerySolution::new(test_map)]);
-        
-        assert_eq!(query_result, test_result)
+        print!("{:?}", query_result);
+        assert!(false)
     }
 }
