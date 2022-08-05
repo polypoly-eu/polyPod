2d9df68e3eabb60f0755bdcbf52f49438dd20f9e
Tim Palade <palade.timotei@gmail.com>
2022-08-05 13:26:29 +0300

    Undo comment

--- platform/core/src/lib.rs
+++ platform/core/src/lib.rs
@@ -6,6 +6,6 @@ pub mod core;
 pub mod core_failure;
 pub mod feature_categories;
 pub mod feature_file_system;
-// #[cfg(any(target_os = "ios", target_os = "android"))]
+#[cfg(any(target_os = "ios", target_os = "android"))]
 pub mod ffi;
 mod io;
