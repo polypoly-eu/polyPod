a23141cfb1e8eeee337fae27ee86c325b545bd49
Tim Palade <palade.timotei@gmail.com>
2022-08-14 23:07:52 +0300

    Add tests + Move encoding to one function for single container, because Swift enters the generic method even for simple types, where a method is already defined

--- platform/core/PolyPodCoreSwift/Tests/PolyPodCoreSwiftTests/CodableTests/MessagePackEncoderTests.swift
+++ platform/core/PolyPodCoreSwift/Tests/PolyPodCoreSwiftTests/CodableTests/MessagePackEncoderTests.swift
@@ -6,30 +6,23 @@
 //
 
 import XCTest
+import MessagePack
+
+@testable import PolyPodCoreSwift
 
 class MessagePackEncoderTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testNil() throws {
+        let value: String? = nil
+        XCTAssertEqual(try MessagePackEncoder().encode(value), MessagePackValue.nil)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testString() throws {
+        let value: String = ""
+        XCTAssertEqual(try MessagePackEncoder().encode(value), MessagePackValue.string(""))
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testOptionalString() throws {
+        let value: String? = ""
+        XCTAssertEqual(try MessagePackEncoder().encode(value), MessagePackValue.string(""))
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
