ab2654af694f880bee25d6e4edad2614cfa6293e
Sebastian Noack <sebastian.noack@polypoly.net>
2022-08-15 13:37:20 -0400

    Revert to using rollup, and bundle WASM code (#1110)

--- platform/podjs/rollup.config.js
+++ platform/podjs/rollup.config.js
@@ -2,9 +2,25 @@ import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import sucrase from "@rollup/plugin-sucrase";
 import json from "@rollup/plugin-json";
+import { wasm } from '@rollup/plugin-wasm';
+
+const common = {
+    plugins: [
+        json(),
+        resolve(),
+        commonjs(),
+        sucrase({
+            exclude: ["node_modules/**"],
+            transforms: ["typescript"],
+        }),
+        wasm({targetEnv: "auto-inline"}),
+    ],
+    context: "window",
+};
 
 export default [
     {
+        ...common,
         input: "src/index.ts",
         output: [
             {
@@ -15,35 +31,16 @@ export default [
                 },
             },
         ],
-        plugins: [
-            json(),
-            resolve(),
-            commonjs(),
-            sucrase({
-                exclude: ["node_modules/**"],
-                transforms: ["typescript"],
-            }),
-        ],
-        context: "window",
         external: ["chai"],
     },
     {
+        ...common,
         input: "src/pod.ts",
         output: [
             {
                 file: "dist/pod.js",
                 format: "iife",
             },
-        ],
-        plugins: [
-            json(),
-            resolve(),
-            commonjs(),
-            sucrase({
-                exclude: ["node_modules/**"],
-                transforms: ["typescript"],
-            }),
-        ],
-        context: "window",
+        ]
     },
 ];
