a109268dffeee9f3679c470be68da360f6058906
Tim Palade <palade.timotei@gmail.com>
2022-08-17 13:52:54 +0300

    Add enum and struct tests

--- platform/core/PolyPodCoreSwift/Tests/PolyPodCoreSwiftTests/CodableTests/MessagePackEncoderTests.swift
+++ platform/core/PolyPodCoreSwift/Tests/PolyPodCoreSwiftTests/CodableTests/MessagePackEncoderTests.swift
@@ -225,4 +225,188 @@ class MessagePackEncoderTests: XCTestCase {
         let value: Dictionary<String, Int?> = ["1": 1, "2": nil, "3": nil]
         XCTAssertEqual(try MessagePackEncoder().encode(value), MessagePackValue.map(["1": .int(1), "2": .nil, "3": .nil]))
     }
+    
+    enum TestEnum: Codable {
+        case simple
+        case namedArguments(s: String, i: Int, bin: Data, date: Date)
+        case indexedArguments(String, Int, Data, Date)
+        case optionalArguments(s: String?, i: Int?, bin: Data?, date: Date?)
+    }
+    
+    func testEnumSimple() throws {
+        let value: TestEnum = TestEnum.simple
+        XCTAssertEqual(try MessagePackEncoder().encode(value), .map([.string("simple"): .map([:])]))
+    }
+    
+    func testOptionalEnumSimple() throws {
+        let value: TestEnum? = TestEnum.simple
+        XCTAssertEqual(try MessagePackEncoder().encode(value), .map([.string("simple"): .map([:])]))
+    }
+    
+    func testEnumNamedArguments() throws {
+        let date = Date()
+        let date_string = ISO8601DateFormatter().string(from: date)
+        let value: TestEnum = TestEnum.namedArguments(s: "", i: 1, bin: Data([1, 2, 3]), date: date)
+        XCTAssertEqual(try MessagePackEncoder().encode(value),
+            .map([.string("namedArguments"):
+                    .map([
+                        .string("date"): .string(date_string),
+                        .string("i"): .int(1),
+                        .string("bin"): .binary(Data([1, 2, 3])),
+                        .string("s"): .string("")
+                    ])
+            ])
+        )
+    }
+    
+    func testOptionalEnumNamedArguments() throws {
+        let date = Date()
+        let date_string = ISO8601DateFormatter().string(from: date)
+        let value: TestEnum? = TestEnum.namedArguments(s: "", i: 1, bin: Data([1, 2, 3]), date: date)
+        XCTAssertEqual(try MessagePackEncoder().encode(value),
+            .map([.string("namedArguments"):
+                    .map([
+                        .string("date"): .string(date_string),
+                        .string("i"): .int(1),
+                        .string("bin"): .binary(Data([1, 2, 3])),
+                        .string("s"): .string("")
+                    ])
+            ])
+        )
+    }
+    
+    func testEnumIndexedArguments() throws {
+        let date = Date()
+        let date_string = ISO8601DateFormatter().string(from: date)
+        let value: TestEnum = TestEnum.indexedArguments("", 1, Data([1, 2, 3]), date)
+        XCTAssertEqual(try MessagePackEncoder().encode(value),
+            .map([.string("indexedArguments"):
+                    .map([
+                        .string("_3"): .string(date_string),
+                        .string("_1"): .int(1),
+                        .string("_2"): .binary(Data([1, 2, 3])),
+                        .string("_0"): .string("")
+                    ])
+            ])
+        )
+    }
+    
+    func testOptionalEnumIndexedArguments() throws {
+        let date = Date()
+        let date_string = ISO8601DateFormatter().string(from: date)
+        let value: TestEnum? = TestEnum.indexedArguments("", 1, Data([1, 2, 3]), date)
+        XCTAssertEqual(try MessagePackEncoder().encode(value),
+            .map([.string("indexedArguments"):
+                    .map([
+                        .string("_3"): .string(date_string),
+                        .string("_1"): .int(1),
+                        .string("_2"): .binary(Data([1, 2, 3])),
+                        .string("_0"): .string("")
+                    ])
+            ])
+        )
+    }
+    
+    func testEnumOptionalArguments() throws {
+        let date = Date()
+        let date_string = ISO8601DateFormatter().string(from: date)
+        let value: TestEnum = TestEnum.optionalArguments(s: "", i: nil, bin: nil, date: date)
+        XCTAssertEqual(try MessagePackEncoder().encode(value),
+            .map([.string("optionalArguments"):
+                    .map([
+                        .string("date"): .string(date_string),
+                        .string("s"): .string("")
+                    ])
+            ])
+        )
+    }
+    
+    func testOptionalEnumOptionalArguments() throws {
+        let date = Date()
+        let date_string = ISO8601DateFormatter().string(from: date)
+        let value: TestEnum? = TestEnum.optionalArguments(s: "", i: nil, bin: nil, date: date)
+        XCTAssertEqual(try MessagePackEncoder().encode(value),
+            .map([.string("optionalArguments"):
+                    .map([
+                        .string("date"): .string(date_string),
+                        .string("s"): .string("")
+                    ])
+            ])
+        )
+    }
+    
+    struct TestStruct: Codable {
+        let date: Date
+        let data: Data
+        let string: String
+        let bool: Bool
+        let int: Int
+        let uInt: UInt
+        let double: Double
+        let float: Float
+        let array: Array<Int>
+        let map: Dictionary<String, Int>
+    }
+    
+    func testStruct() throws {
+        let date = Date()
+        let date_string = ISO8601DateFormatter().string(from: date)
+        let value: TestStruct = TestStruct(date: date,
+                                           data: Data([1, 2, 3]),
+                                           string: "",
+                                           bool: false,
+                                           int: 1,
+                                           uInt: 1,
+                                           double: 1.0,
+                                           float: 1.0,
+                                           array: [1, 2],
+                                           map: ["test": 1])
+        XCTAssertEqual(try MessagePackEncoder().encode(value),
+            .map([
+                .string("float"): .float(1.0),
+                .string("data"): .binary(Data([1, 2, 3])),
+                .string("int"): .int(1),
+                .string("bool"): .bool(false),
+                .string("uInt"): .uint(1),
+                .string("map"): .map([
+                    .string("test"): .int(1)
+                ]),
+                .string("array"): .array([.int(1), .int(2)]),
+                .string("string"): .string(""),
+                .string("date"): .string(date_string),
+                .string("double"): .double(1.0)
+            ])
+        )
+    }
+    
+    func testOptionalStruct() throws {
+        let date = Date()
+        let date_string = ISO8601DateFormatter().string(from: date)
+        let value: TestStruct? = TestStruct(date: date,
+                                           data: Data([1, 2, 3]),
+                                           string: "",
+                                           bool: false,
+                                           int: 1,
+                                           uInt: 1,
+                                           double: 1.0,
+                                           float: 1.0,
+                                           array: [1, 2],
+                                           map: ["test": 1])
+        XCTAssertEqual(try MessagePackEncoder().encode(value),
+            .map([
+                .string("float"): .float(1.0),
+                .string("data"): .binary(Data([1, 2, 3])),
+                .string("int"): .int(1),
+                .string("bool"): .bool(false),
+                .string("uInt"): .uint(1),
+                .string("map"): .map([
+                    .string("test"): .int(1)
+                ]),
+                .string("array"): .array([.int(1), .int(2)]),
+                .string("string"): .string(""),
+                .string("date"): .string(date_string),
+                .string("double"): .double(1.0)
+            ])
+        )
+    }
 }
