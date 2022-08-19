6063a90bf16743ea5c2e6c8b286c6aaaed3fc625
Richard Stoffels <richard.stoffels@polypoly.net>
2022-08-19 11:04:44 +0200

    Undo merge changes that shouldn't be undone

--- platform/android/app/src/main/kotlin/coop/polypoly/polypod/settings/MainFragment.kt
+++ platform/android/app/src/main/kotlin/coop/polypoly/polypod/settings/MainFragment.kt
@@ -3,8 +3,11 @@ package coop.polypoly.polypod.settings
 import android.os.Bundle
 import androidx.navigation.findNavController
 import androidx.preference.Preference
+import androidx.preference.DropDownPreference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.SwitchPreference
+import coop.polypoly.core.Core
+import coop.polypoly.core.UserSessionTimeoutOption
 import coop.polypoly.polypod.Authentication
 import coop.polypoly.polypod.Preferences
 import coop.polypoly.polypod.R
@@ -18,6 +21,44 @@ class MainFragment : PreferenceFragmentCompat() {
         setPreferencesFromResource(R.xml.settings, rootKey)
         findPreference<Preference>("version")?.summary = RuntimeInfo.VERSION
 
+   val timeoutOptionsConfig = Core.getUserSessionTimeoutOptionsConfig()
+        val selectedOption = Core.getUserSessionTimeoutOption()
+        val timeoutDurationsMap: LinkedHashMap<String, String> = linkedMapOf()
+        timeoutOptionsConfig.forEach {
+            val duration = if (it.duration != null) {
+                requireContext().getString(
+                    R.string.screen_lock_inactivity_timeout_duration,
+                    it.duration
+                )
+            } else {
+                requireContext().getString(
+                    R.string.screen_lock_inactivity_no_timeout
+                )
+            }
+            timeoutDurationsMap[it.option.name] = duration
+        }
+
+        val dropDownPreference =
+            findPreference<DropDownPreference>("sessionTimeout")
+        dropDownPreference?.entries = timeoutDurationsMap.values.toTypedArray()
+        dropDownPreference?.entryValues =
+            timeoutDurationsMap.keys.toTypedArray()
+        dropDownPreference?.value = selectedOption.name
+        dropDownPreference?.onPreferenceChangeListener =
+            Preference.OnPreferenceChangeListener { pref, newValue ->
+
+                (pref as DropDownPreference).summary =
+                    timeoutDurationsMap[newValue.toString()]
+                Core.setUserSessionTimeoutOption(
+                    UserSessionTimeoutOption.valueOf(newValue.toString())
+                )
+
+                true
+            }
+        dropDownPreference?.summary = timeoutDurationsMap[selectedOption.name]
+        dropDownPreference?.isVisible =
+            Preferences.isBiometricEnabled(requireContext())
+
         findPreference<Preference>("biometricEnabledKey")
             ?.onPreferenceChangeListener =
             Preference.OnPreferenceChangeListener { _, newValue ->
@@ -25,6 +66,8 @@ class MainFragment : PreferenceFragmentCompat() {
                     val pref =
                         findPreference<SwitchPreference>("biometricEnabledKey")
                     pref?.isChecked =
+                        Preferences.isBiometricEnabled(requireContext())
+                    dropDownPreference?.isVisible =
                         Preferences.isBiometricEnabled(requireContext())
                 }
                 false
