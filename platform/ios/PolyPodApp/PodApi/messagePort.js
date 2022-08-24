0f9ab3152758e149d1da5232deb3c73373aca4df
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2022-08-24 10:11:55 +0200

    Post only the payload

--- platform/ios/PolyPodApp/PodApi/messagePort.js
+++ platform/ios/PolyPodApp/PodApi/messagePort.js
@@ -2,7 +2,7 @@ window.addEventListener("message", receiveMessage, false);
 
 function receiveMessage({ data }) {
     if (data.command === "log") {
-        webkit.messageHandlers[data.command].postMessage(data);
+        webkit.messageHandlers[data.command].postMessage(data.text);
     }
 }
 
