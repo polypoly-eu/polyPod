5eede434f2b024307d4ce82cd0cd009d72f5e1ba
Tim Palade <palade.timotei@gmail.com>
2022-08-24 12:36:53 +0300

    Improve regex to match without space

--- features/google/src/model/importers/utils/lang-constants.js
+++ features/google/src/model/importers/utils/lang-constants.js
@@ -3,10 +3,10 @@ export const langConstants = {
         regex: /\/(Access Log Activity|Zugriffsprotokollaktivitäten|Aktiviteter i adgangsloggen)\/(Activities [-–] A list of Google services accessed by|Aktivitäten [-–] eine Liste der Google-Dienste, auf d|Aktiviteter [-–] en liste over Google-tjenester, som d)\.csv$/,
     },
     ActivitiesHtmlImporter: {
-        regex: /\/(My Activity|Meine Aktivitäten|Mine aktiviteter)\/.*\.html$/,
+        regex: /\/(My Activity|Meine Aktivitäten|Mine aktiviteter|MyActivity|MeineAktivitäten|MineAktiviteter)\/.*\.html$/,
     },
     ActivitiesJsonImporter: {
-        regex: /\/(My Activity|Meine Aktivitäten|Mine aktiviteter)\/.*\.json$/,
+        regex: /\/(My Activity|Meine Aktivitäten|Mine aktiviteter|MyActivity|MeineAktivitäten|MineAktiviteter)\/.*\.json$/,
     },
     SemanticLocationsImporter: {
         regex: /\/[^/]+\/(Semantic Location History)\/\d+\/[^.]+\.json$/,
