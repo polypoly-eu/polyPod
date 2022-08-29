6fcc1eb1014a75c764f6e847e262b54488025f1d
Juan Julián Merelo Guervós <jjmerelo@gmail.com>
2022-08-29 18:49:15 +0200

    Update platform/feature-api/communication/src/remote-pod/tests/node/bubblewrap.test.ts
    I don't really like the nested "describes", and am partial to "it" as opposed to "test", but I appreciate your suggestion and thank you for it :clap:
    
    Co-authored-by: irstavr <irini.stavrakantonaki@polypoly.net>

--- platform/feature-api/communication/src/remote-pod/tests/node/bubblewrap.test.ts
+++ platform/feature-api/communication/src/remote-pod/tests/node/bubblewrap.test.ts
@@ -1,19 +1,26 @@
 import { podBubblewrapClasses, podBubblewrap } from "../../remote";
 import { NamedNode } from "@polypoly-eu/api";
 
-describe("Classes are accounted for", () => {
-    it("has the right number of classes", () => {
-        expect(Object.keys(podBubblewrapClasses).length).toBe(6);
-    });
+describe("Bubblewrap", () => {
+
+	  describe("when create()", () => {
+	      test("should have the right number of classes", () => {
+	          expect(Object.keys(podBubblewrapClasses).length).toBe(6);
+	      });
+	  });
+
+	  describe("when NamedNode() is called", () => {
+	      test("should generate roundtrip classes correctly", () => {
+	          const namedNode = new NamedNode("https://example.org/n");
+	          const encoded = podBubblewrap.encode(namedNode);
+	          
+	          expect(encoded).toBeTruthy();
+	          
+	          const decoded = podBubblewrap.decode(encoded);
+	          
+	          expect(decoded).toBeInstanceOf(NamedNode);
+	          expect(decoded).toStrictEqual(namedNode);
+	      });
+	  });
 });
 
-describe("Classes roundtrip correctly", () => {
-    it("works for NamedNode", () => {
-        const namedNode = new NamedNode("https://example.org/n");
-        const encoded = podBubblewrap.encode(namedNode);
-        expect(encoded).toBeTruthy();
-        const decoded = podBubblewrap.decode(encoded);
-        expect(decoded).toBeInstanceOf(NamedNode);
-        expect(decoded).toStrictEqual(namedNode);
-    });
-});
