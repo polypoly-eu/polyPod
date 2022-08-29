a96690c9a72ca5544eefecdbfdf22848c1ae54d0
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2022-08-29 11:29:33 +0200

    Added tests for one class
    Other 5 to go...

--- platform/feature-api/communication/src/remote-pod/tests/node/bubblewrap.test.ts
+++ platform/feature-api/communication/src/remote-pod/tests/node/bubblewrap.test.ts
@@ -1,7 +1,19 @@
-import { podBubblewrapClasses } from "../../remote";
+import { podBubblewrapClasses, podBubblewrap } from "../../remote";
+import { NamedNode } from "@polypoly-eu/api";
 
 describe("Classes are accounted for", () => {
     it("has the right number of classes", () => {
         expect(Object.keys(podBubblewrapClasses).length).toBe(6);
+    });
+});
+
+describe("Classes roundtrip correctly", () => {
+    it("works for NamedNode", () => {
+        const namedNode = new NamedNode("https://example.org/n");
+        const encoded = podBubblewrap.encode(namedNode);
+        expect(encoded).toBeTruthy();
+        const decoded = podBubblewrap.decode(encoded);
+        expect(decoded).toBeInstanceOf(NamedNode);
+        expect(decoded).toStrictEqual(namedNode);
     });
 });
