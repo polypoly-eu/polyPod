ecf3298501df7b109aa625d0b73c010eb16d02ec
Richard Stoffels <richard.stoffels@polypoly.net>
2022-08-29 10:19:08 +0200

    PRefixed var and improve structure

--- feature-utils/poly-sparql/src/query-templates.js
+++ feature-utils/poly-sparql/src/query-templates.js
@@ -1,17 +1,23 @@
 import { polyNs, rdfNs, rdfsNs, xmlNs } from "./namespaces";
 
+const namespacePrefixes = `\
+    PREFIX poly: ${polyNs} \
+    PREFIX rdf: ${rdfNs} \
+    PREFIX rdfs: ${rdfsNs} \
+    PREFIX xml: ${xmlNs}`;
+
 export function sparqleInsertTemplate({ graph, triples }) {
-    return `PREFIX poly: ${polyNs} PREFIX rdf: ${rdfNs} PREFIX rdfs: ${rdfsNs} PREFIX xml: ${xmlNs} INSERT DATA { ${
-        (graph && `GRAPH ${graph} {`) || ""
-    } ${triples} } ${(graph && "}") || ""}`;
+    return `\
+    ${namespacePrefixes}\
+    INSERT DATA {\
+        ${(graph && `GRAPH ${graph} {`) || ""}\
+        ${triples}\
+    } ${(graph && "}") || ""}`;
 }
 
 export function sparqleDeleteTemplate(graph, triples) {
     return `\
-    PREFIX poly: ${polyNs} \
-    PREFIX rdf: ${rdfNs} \
-    PREFIX rdfs: ${rdfsNs} \
-    PREFIX xml: ${xmlNs} \
+    ${namespacePrefixes}\
     DELETE DATA {\
       ${(graph && `GRAPH ${graph} {`) || ""}\
       ${triples}\
