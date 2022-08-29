6507ba473427244c451beb334414b3492561cac2
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2022-08-29 19:07:58 +0200

    :rotating_light:

--- platform/feature-api/communication/src/remote-pod/tests/node/bubblewrap.test.ts
+++ platform/feature-api/communication/src/remote-pod/tests/node/bubblewrap.test.ts
@@ -2,25 +2,23 @@ import { podBubblewrapClasses, podBubblewrap } from "../../remote";
 import { NamedNode } from "@polypoly-eu/api";
 
 describe("Bubblewrap", () => {
+    describe("when create()", () => {
+        test("should have the right number of classes", () => {
+            expect(Object.keys(podBubblewrapClasses).length).toBe(6);
+        });
+    });
 
-	  describe("when create()", () => {
-	      test("should have the right number of classes", () => {
-	          expect(Object.keys(podBubblewrapClasses).length).toBe(6);
-	      });
-	  });
+    describe("when NamedNode() is called", () => {
+        test("should generate roundtrip classes correctly", () => {
+            const namedNode = new NamedNode("https://example.org/n");
+            const encoded = podBubblewrap.encode(namedNode);
 
-	  describe("when NamedNode() is called", () => {
-	      test("should generate roundtrip classes correctly", () => {
-	          const namedNode = new NamedNode("https://example.org/n");
-	          const encoded = podBubblewrap.encode(namedNode);
-	          
-	          expect(encoded).toBeTruthy();
-	          
-	          const decoded = podBubblewrap.decode(encoded);
-	          
-	          expect(decoded).toBeInstanceOf(NamedNode);
-	          expect(decoded).toStrictEqual(namedNode);
-	      });
-	  });
+            expect(encoded).toBeTruthy();
+
+            const decoded = podBubblewrap.decode(encoded);
+
+            expect(decoded).toBeInstanceOf(NamedNode);
+            expect(decoded).toStrictEqual(namedNode);
+        });
+    });
 });
-
