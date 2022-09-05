3c41cd82cc79f5c64ec3e75f06ce5a4f9a611faf
Tim Palade <palade.timotei@gmail.com>
2022-09-05 16:44:40 +0300

    Scroll to features and close them reliably

--- platform/android/app/src/androidTest/kotlin/coop/polypoly/polypod/FeatureSmokeTest.kt
+++ platform/android/app/src/androidTest/kotlin/coop/polypoly/polypod/FeatureSmokeTest.kt
@@ -1,26 +1,16 @@
 package coop.polypoly.polypod
 
-import android.content.Context
 import androidx.compose.ui.test.SemanticsNodeInteraction
+import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.hasTestTag
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onAllNodesWithTag
-import androidx.compose.ui.test.onFirst
 import androidx.compose.ui.test.performClick
-import androidx.preference.PreferenceManager
-import androidx.test.core.app.ActivityScenario
+import androidx.compose.ui.test.performScrollTo
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.matcher.RootMatchers.isDialog
-import androidx.test.espresso.matcher.ViewMatchers.withClassName
-import androidx.test.espresso.matcher.ViewMatchers.withContentDescription
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.platform.app.InstrumentationRegistry
-import org.hamcrest.Matchers.endsWith
-import org.junit.After
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -31,36 +21,36 @@ class FeatureSmokeTest {
     @get:Rule
     val composeTestRule = createAndroidComposeRule(MainActivity::class.java)
 
-//    private var activityScenario: ActivityScenario<MainActivity>? = null
-//    private var context: Context? = null
-//
-//    @Before
-//    fun preparePreferences() {
-//        context = InstrumentationRegistry.getInstrumentation().targetContext
-//        PreferenceManager.getDefaultSharedPreferences(context!!)
-//            .edit().clear().commit()
-//        Preferences.setSecurityDoNotAskAgainCheck(context!!, true)
-//        Preferences.setFirstRun(context!!, false)
-//        activityScenario = ActivityScenario.launch(MainActivity::class.java)
-//    }
-//
-//    @After
-//    fun closeActivity() {
-//        activityScenario?.close()
-//        activityScenario = null
-//    }
+    private fun nodeIsDisplayed(nodeInteraction: SemanticsNodeInteraction): Boolean { // ktlint-disable max-line-length
+        val result = nodeInteraction.runCatching {
+            nodeInteraction.assertIsDisplayed()
+        }
+
+        return result.isSuccess
+    }
+
+    private fun homeScreen(): SemanticsNodeInteraction {
+        return composeTestRule.onNode(hasTestTag("HomeScreen"))
+    }
 
     @Test
     fun onboardingShownOnFirstRun() {
         Thread.sleep(1000)
-        val nodeInteractionCollection = composeTestRule.onAllNodesWithTag("Tile") // ktlint-disable max-line-length
+        val nodeInteractionCollection = composeTestRule.onAllNodesWithTag(
+            "Tile"
+        )
         val nodeCount = nodeInteractionCollection.fetchSemanticsNodes().count()
 
         nodeInteractionCollection.run {
             for (i in 0 until nodeCount) {
-                this[i].performClick()
+                val nodeInter = this[i]
+                nodeInter.performScrollTo()
+                nodeInter.performClick()
                 Thread.sleep(1000)
-                onView(withId(R.id.close_button)).perform(click())
+                while (!nodeIsDisplayed(homeScreen())) {
+                    onView(withId(R.id.close_button)).perform(click())
+                    Thread.sleep(100)
+                }
             }
         }
         Thread.sleep(1000)
