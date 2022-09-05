77f6a7260754ce0289db857b26763da9d5cdb339
Tim Palade <palade.timotei@gmail.com>
2022-09-05 17:08:31 +0300

    Clean up

--- platform/android/app/src/androidTest/kotlin/coop/polypoly/polypod/FeatureSmokeTest.kt
+++ platform/android/app/src/androidTest/kotlin/coop/polypoly/polypod/FeatureSmokeTest.kt
@@ -10,7 +10,6 @@ import androidx.compose.ui.test.performScrollTo
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Rule
@@ -24,11 +23,9 @@ class FeatureSmokeTest {
     val composeTestRule = createAndroidComposeRule(MainActivity::class.java)
 
     private fun nodeIsDisplayed(nodeInteraction: SemanticsNodeInteraction): Boolean { // ktlint-disable max-line-length
-        val result = nodeInteraction.runCatching {
+        return nodeInteraction.runCatching {
             nodeInteraction.assertIsDisplayed()
-        }
-
-        return result.isSuccess
+        }.isSuccess
     }
 
     private fun homeScreen(): SemanticsNodeInteraction {
