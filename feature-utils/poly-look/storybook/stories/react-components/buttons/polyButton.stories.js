616bb35a5c3f25c441a2d95275f4e3812192e339
Georgiana Piscoi <georgiana.piscoi@polypoly.net>
2022-09-07 16:08:16 +0300

    updated Storybook

--- feature-utils/poly-look/storybook/stories/react-components/buttons/polyButton.stories.js
+++ feature-utils/poly-look/storybook/stories/react-components/buttons/polyButton.stories.js
@@ -13,10 +13,19 @@ export default {
     className: {
       type: "string",
     },
+    size: {
+      control: { type: "radio" },
+      options: ["small", "large"],
+    },
     centered: { type: "boolean" },
   },
 };
 
+const InlineSVG = () => (
+  <svg viewBox="0 0 100 100">
+    <path d="M 10 10 H 90 V 90 H 10 L 10 10" fill="currentColor" />
+  </svg>
+);
 const Template = (args) => <PolyButton {...args} />;
 
 export const Default = Template.bind({});
@@ -31,9 +40,18 @@ Default.parameters = {
     url: "https://www.figma.com/file/qIrr4QJrmYGqVQHQoCECux/polyPod-design-system?node-id=2891%3A2856",
   },
 };
+
 export const Disabled = Template.bind({});
 Disabled.args = {
   ...Default.args,
   disabled: true,
 };
 Disabled.parameters = { ...Default.parameters };
+
+export const WithIcon = Template.bind({});
+WithIcon.args = {
+  ...Default.args,
+  iconLeft: <InlineSVG />,
+  iconRight: <InlineSVG />,
+};
+WithIcon.parameters = { ...Default.parameters };
