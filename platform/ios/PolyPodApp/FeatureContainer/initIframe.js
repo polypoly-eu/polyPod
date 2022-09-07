b21df3d8cb1087799eef8fec839f7c421889f2c7
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2022-09-07 08:17:17 +0200

    Cryptic comment, possibly no longer relevant

--- platform/ios/PolyPodApp/FeatureContainer/initIframe.js
+++ platform/ios/PolyPodApp/FeatureContainer/initIframe.js
@@ -3,7 +3,6 @@ const { port1, port2 } = new MessageChannel();
 function initIframe(el) {
     port1.start();
     port1.onmessage = (event) => {
-        // incoming msgpack-encoded events:
         webkit.messageHandlers.event.postMessage(event.data);
     };
     // This is transferring ownership of port2 to the content window
