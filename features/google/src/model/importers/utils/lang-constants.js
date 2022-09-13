7ac1107f836d4d1929fbbc5286c7563258ec0e4e
Tim Palade <palade.timotei@gmail.com>
2022-09-13 13:11:13 +0300

    Rollback "Use unicode regexes"

--- features/google/src/model/importers/utils/lang-constants.js
+++ features/google/src/model/importers/utils/lang-constants.js
@@ -1,15 +1,15 @@
 export const langConstants = {
     AccessLogImporter: {
-        regex: /\/(Access Log Activity|Zugriffsprotokollaktivitäten|Aktiviteter i adgangsloggen)\/(Activities [-–] A list of Google services accessed by|Aktivitäten [-–] eine Liste der Google-Dienste, auf d|Aktiviteter [-–] en liste over Google-tjenester, som d)\.csv$/u,
+        regex: /\/(Access Log Activity|Zugriffsprotokollaktivitäten|Aktiviteter i adgangsloggen)\/(Activities [-–] A list of Google services accessed by|Aktivitäten [-–] eine Liste der Google-Dienste, auf d|Aktiviteter [-–] en liste over Google-tjenester, som d)\.csv$/,
     },
     ActivitiesHtmlImporter: {
-        regex: /\/(My Activity|Meine Aktivitäten|Mine aktiviteter|MyActivity|MeineAktivitäten|MineAktiviteter)\/.*\.html$/u,
+        regex: /\/(My Activity|Meine Aktivitäten|Mine aktiviteter|MyActivity|MeineAktivitäten|MineAktiviteter)\/.*\.html$/,
     },
     ActivitiesJsonImporter: {
-        regex: /\/(My Activity|Meine Aktivitäten|Mine aktiviteter|MyActivity|MeineAktivitäten|MineAktiviteter)\/.*\.json$/u,
+        regex: /\/(My Activity|Meine Aktivitäten|Mine aktiviteter|MyActivity|MeineAktivitäten|MineAktiviteter)\/.*\.json$/,
     },
     SemanticLocationsImporter: {
-        regex: /\/[^/]+\/(Semantic Location History)\/\d+\/[^.]+\.json$/u,
+        regex: /\/[^/]+\/(Semantic Location History)\/\d+\/[^.]+\.json$/,
     },
 };
 export function matchRegex(path, importer) {
@@ -19,5 +19,13 @@ export function matchRegex(path, importer) {
         " "
     );
     const importerName = importer.constructor.name;
-    return langConstants[importerName].regex.test(sanitizedPath);
+    const normalizedPath = sanitizedPath.normalize("NFC");
+    const normalizedRegex = RegExp(
+        langConstants[importerName].regex
+            .toString()
+            .normalize("NFC")
+            .slice(1, -1)
+    );
+    console.log(`matchRegex normalized path: ${normalizedPath}`);
+    return normalizedRegex.test(normalizedPath);
 }
