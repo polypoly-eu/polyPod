8f8b7ee066e8aa376b62578c61f83873509e3e9d
Tim Palade <palade.timotei@gmail.com>
2022-09-13 14:42:49 +0300

    Improve test

--- features/google/test/regex-path-matcher.test.js
+++ features/google/test/regex-path-matcher.test.js
@@ -1,7 +1,7 @@
 import { matchRegex } from "../src/model/importers/utils/lang-constants";
 
 describe("Test MatchRegex for File Paths", () => {
-    let german_paths = [
+    let german_paths_html = [
         "Takeout/Meine Aktivitäten/Maps/MeineAktivitäten.html",
         "Takeout/Meine Aktivitäten/Google Play Store/MeineAktivitäten.html",
         "Takeout/Meine Aktivitäten/YouTube/MeineAktivitäten.html",
@@ -13,8 +13,20 @@ describe("Test MatchRegex for File Paths", () => {
         "Takeout/Meine Aktivitäten/Google News/MeineAktivitäten.html",
     ];
 
+    let german_paths_json = [
+        "Takeout/Meine Aktivitäten/Maps/MeineAktivitäten.json",
+        "Takeout/Meine Aktivitäten/Google Play Store/MeineAktivitäten.json",
+        "Takeout/Meine Aktivitäten/YouTube/MeineAktivitäten.json",
+        "Takeout/Meine Aktivitäten/Shopping/MeineAktivitäten.json",
+        "Takeout/Meine Aktivitäten/Bildersuche/MeineAktivitäten.json",
+        "Takeout/Meine Aktivitäten/Datenexport/MeineAktivitäten.json",
+        "Takeout/Meine Aktivitäten/Google Suche/MeineAktivitäten.json",
+        "Takeout/Meine Aktivitäten/Anzeigen/MeineAktivitäten.json",
+        "Takeout/Meine Aktivitäten/Google News/MeineAktivitäten.json",
+    ];
+
     describe("should match all german paths with ActivitiesHtmlImporter", () => {
-        for (const path of german_paths) {
+        for (const path of german_paths_html) {
             it(`it should match for ${path}`, () => {
                 const result = matchRegex(path, "ActivitiesHtmlImporter");
                 expect(result).toBe(true);
@@ -23,7 +35,7 @@ describe("Test MatchRegex for File Paths", () => {
     });
 
     describe("should match all german paths with ActivitiesJsonImporter", () => {
-        for (const path of german_paths) {
+        for (const path of german_paths_json) {
             it(`it should match for ${path}`, () => {
                 const result = matchRegex(path, "ActivitiesJsonImporter");
                 expect(result).toBe(true);
