17f4604492fbdff135631e7ffe72d354ec71e2ee
mteresa-jimenez <mteresa.jn@gmail.com>
2022-09-13 13:30:13 +0200

    Add documentation

--- feature-utils/poly-look/src/react-components/fixedFooter.jsx
+++ feature-utils/poly-look/src/react-components/fixedFooter.jsx
@@ -1,6 +1,11 @@
 import React from "react";
-
 import "./fixedFooter.css";
+
+/**
+ * Fixed container at the bottom at the screen.
+ * If the Screen content is too long and is covered by this footer, you might need to increese the padding-bottom of the screen.
+ * @param {jsx} children JSX elements displayed inside the container.
+ */
 
 const FixedFooter = ({ children }) => {
   return <div className="poly-fixed-footer">{children}</div>;
