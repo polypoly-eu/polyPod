7687e2163b7f1d2616d242b781d08bea2989d92d
Tim Palade <palade.timotei@gmail.com>
2022-09-13 13:04:45 +0300

    Add tests

---
+++ features/google/test/regex-path-matcher.test.js
@@ -0,0 +1,39 @@
+import matchRegex from "../src/model/importers/utils/lang-constants";
+
+describe("Test MatchRegex for File Paths", () => {
+    let german_paths = [
+        "Takeout/Meine Aktivitäten/Maps/MeineAktivitäten.html",
+        "Takeout/Meine Aktivitäten/Google Play Store/MeineAktivitäten.html",
+        "Takeout/Meine Aktivitäten/YouTube/MeineAktivitäten.html",
+        "Takeout/Meine Aktivitäten/Shopping/MeineAktivitäten.html",
+        "Takeout/Meine Aktivitäten/Bildersuche/MeineAktivitäten.html",
+        "Takeout/Meine Aktivitäten/Datenexport/MeineAktivitäten.html",
+        "Takeout/Meine Aktivitäten/Google Suche/MeineAktivitäten.html",
+        "Takeout/Meine Aktivitäten/Anzeigen/MeineAktivitäten.html",
+        "Takeout/Meine Aktivitäten/Google News/MeineAktivitäten.html",
+    ];
+
+    describe("should match all german paths with ActivitiesHtmlImporter", () => {
+        for (const path of german_paths) {
+            it(`it should match for ${path}`, () => {
+                const result = matchRegex(path, "ActivitiesHtmlImporter");
+                if (!result) {
+                    console.log(path);
+                }
+                expect(result).toBe(true);
+            });
+        }
+    });
+
+    describe("should match all german paths with ActivitiesJsonImporter", () => {
+        for (const path of german_paths) {
+            it(`it should match for ${path}`, () => {
+                const result = matchRegex(path, "ActivitiesJsonImporter");
+                if (!result) {
+                    console.log(path);
+                }
+                expect(result).toBe(true);
+            });
+        }
+    });
+});
