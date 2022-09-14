6cf74f0412ad18ce4328e064c6f1f3c15909afad
mteresa-jimenez <mteresa.jn@gmail.com>
2022-09-14 16:28:53 +0200

    Fix documentation

--- feature-utils/poly-look/src/react-components/fixedFooter.jsx
+++ feature-utils/poly-look/src/react-components/fixedFooter.jsx
@@ -3,7 +3,7 @@ import "./fixedFooter.css";
 
 /**
  * Fixed container at the bottom at the screen.
- * If the Screen content is too long and is covered by this footer, you might need to increese the padding-bottom of the screen.
+ * If the Screen content is too long and is covered by this footer, you might need to increese the padding-bottom of the screen to make the content visible.
  * @param {jsx} children JSX elements displayed inside the container.
  * @param {boolean} gradient Fade out band on the top of the container.
  */
