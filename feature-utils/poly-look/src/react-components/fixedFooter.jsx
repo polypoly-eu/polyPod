829ac182694e60756c331f96bd3f3ed1339899e5
mteresa-jimenez <mteresa.jn@gmail.com>
2022-09-14 16:27:59 +0200

    Add gradient option to component in poly-look

--- feature-utils/poly-look/src/react-components/fixedFooter.jsx
+++ feature-utils/poly-look/src/react-components/fixedFooter.jsx
@@ -5,10 +5,21 @@ import "./fixedFooter.css";
  * Fixed container at the bottom at the screen.
  * If the Screen content is too long and is covered by this footer, you might need to increese the padding-bottom of the screen.
  * @param {jsx} children JSX elements displayed inside the container.
+ * @param {boolean} gradient Fade out band on the top of the container.
  */
 
-const FixedFooter = ({ children }) => {
-  return <div className="poly-fixed-footer">{children}</div>;
+const FixedFooter = ({ children, gradient = true }) => {
+  return (
+    <div
+      className={
+        gradient
+          ? "poly-fixed-footer poly-fixed-footer-gradient"
+          : "poly-fixed-footer"
+      }
+    >
+      {children}
+    </div>
+  );
 };
 
 export default FixedFooter;
