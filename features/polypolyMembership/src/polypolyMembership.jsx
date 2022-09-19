125fc5366854cfd3bf4347141adb67623004906c
mteresa-jimenez <mteresa.jn@gmail.com>
2022-09-19 13:41:29 +0200

    Add Router

--- features/polypolyMembership/src/polypolyMembership.jsx
+++ features/polypolyMembership/src/polypolyMembership.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Onboarding from "./views/onboarding.jsx";
+import Overview from "./views/overview.jsx";
 
 import "./styles.css";
 
 const PolypolyMembership = () => {
     return (
-        <>
-            <div className="poly-nav-bar-separator-overlay" />
-            <div className="feature-container membership">
-                <Onboarding />
-            </div>
-        </>
+        <Router>
+            <Routes>
+                <div className="poly-nav-bar-separator-overlay" />
+                <div className="feature-container membership">
+                    <Onboarding />
+                    <Route path="/" exact element={<Onboarding />} />
+                    <Route path="/onboarding" element={<Onboarding />} />
+                    <Route path="/overview" element={<Overview />} />
+                </div>
+            </Routes>
+        </Router>
     );
 };
 
