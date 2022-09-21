f70d8e2d01c398a62a00b2f692db66784d22df1c
mteresa-jimenez <mteresa.jn@gmail.com>
2022-09-21 16:06:18 +0200

    Add navigation to Overview screen

--- features/polypolyMembership/src/views/onboarding.jsx
+++ features/polypolyMembership/src/views/onboarding.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { Screen, PolyButton, FixedFooter } from "@polypoly-eu/poly-look";
 import i18n from "!silly-i18n";
 
 import "./onboarding.css";
 
 const Onboarding = () => {
+    const navigate = useNavigate();
+
+    const handleNavigation = () => {
+        navigate("/overview");
+    };
+
     const handleOpenUrl = () => {
         console.log("Link to https://polypoly-citizens.eu/en/becomepart");
         // This is "undefined" until we implement the navigation in the feature
@@ -15,7 +22,10 @@ const Onboarding = () => {
             <h1>{i18n.t("onboarding:title")}</h1>
             <p>{i18n.t("onboarding:text")}</p>
             <FixedFooter>
-                <PolyButton label={i18n.t("onboarding:primary.button")} />
+                <PolyButton
+                    onClick={handleNavigation}
+                    label={i18n.t("onboarding:primary.button")}
+                />
                 <div className="secondary-link" onClick={handleOpenUrl()}>
                     <u>{i18n.t("onboarding:secondary.button")}</u>
                 </div>
