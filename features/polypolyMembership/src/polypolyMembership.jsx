e1a4899e52ef6dd18a2948f82aaeec53d1f2e60c
Teresa Jiménez Novillo <70586647+mteresa-jimenez@users.noreply.github.com>
2022-09-22 12:02:21 +0200

    PROD4POD-1886 Add Router to the Membership/GDPR Helper feature (#1192)
    # ✍️ Description
    Add React Router to the feature to be able to navigate in it.
    
    You will need to run `npm ci`
    
    ### 🏗️ Fixes
    [PROD4POD-1886](https://jira.polypoly.eu/browse/PROD4POD-1886)
    
    ## ℹ️ Other information
    There have been some recent changes in React Router, for example, the
    hook `useNavigate`, the substitute for `useHistory` As far as I have
    found, they don't work _exactly_ the same, but similar.
    
    `navigate("/overview")` would be like `history.push("/overview")`
    
    Co-authored-by: Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>

--- features/polypolyMembership/src/polypolyMembership.jsx
+++ features/polypolyMembership/src/polypolyMembership.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { MemoryRouter as Router, Routes, Route } from "react-router-dom";
+
 import Onboarding from "./views/onboarding.jsx";
+import Overview from "./views/overview.jsx";
 
 import "./styles.css";
 
 const PolypolyMembership = () => {
     return (
-        <>
+        <Router>
             <div className="poly-nav-bar-separator-overlay" />
             <div className="feature-container membership">
-                <Onboarding />
+                <Routes>
+                    <Route path="/" exact element={<Onboarding />} />
+                    <Route path="/onboarding" element={<Onboarding />} />
+                    <Route path="/overview" element={<Overview />} />
+                </Routes>
             </div>
-        </>
+        </Router>
     );
 };
 
