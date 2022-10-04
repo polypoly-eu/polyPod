31e6e5b839dd20b6d9542e561f73fccb8c31ced0
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2022-10-04 10:22:27 +0200

    Test anonymized message thread id

--- features/facebookImport/test/ministory/json-file-names-analysis.test.js
+++ features/facebookImport/test/ministory/json-file-names-analysis.test.js
@@ -4,7 +4,7 @@ import { ZipFileMock } from "@polypoly-eu/poly-import";
 import { runAnalysisForExport } from "../utils/analyses-execution";
 import { expectAnalysisSuccessStatus } from "../utils/analysis-assertions";
 
-describe("Missing common JSON files analysis for empty zip", () => {
+describe("JSON files analysis for empty zip", () => {
     let status = null;
 
     beforeAll(async () => {
@@ -21,7 +21,7 @@ describe("Missing common JSON files analysis for empty zip", () => {
     });
 });
 
-describe("Missing common JSON files analysis for non-empty zip", () => {
+describe("JSON files analysis for non-empty zip", () => {
     let status = null;
     let result = null;
     const commonJsonFiles = commonStructure
@@ -52,5 +52,35 @@ describe("Missing common JSON files analysis for non-empty zip", () => {
 
     it("has the right title", () => {
         expect(result.title).toBe("JSON file names");
+    });
+});
+
+describe("JSON files analysis with anonymized path", () => {
+    let result = null;
+    let commonJsonFiles = commonStructure
+        .filter((path) => path.match(/\.json$/))
+        .map((jsonPath) => {
+            return jsonPath.substring(1);
+        });
+    commonJsonFiles.push(
+        "messages/archived_threads/facebookuser_gktxomrifg/message_1.json"
+    );
+
+    beforeAll(async () => {
+        let zipFile = new ZipFileMock();
+        commonJsonFiles.forEach((jsonPath) => {
+            zipFile.addJsonEntry(jsonPath, { foo: "bar" });
+        });
+        const { analysisResult } = await runAnalysisForExport(
+            JSONFileNamesAnalysis,
+            zipFile
+        );
+        result = analysisResult.analysis;
+    });
+
+    it("has anonymized file", () => {
+        expect(result.reportData).toContain(
+            "messages/archived_threads/uniqueid_hash/message_1.json"
+        );
     });
 });
