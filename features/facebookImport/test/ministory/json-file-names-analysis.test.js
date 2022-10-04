d807ba4c3b5e4f9280c45d5b85f1a7ca1e94d547
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2022-10-04 09:54:02 +0200

    :white_check_mark: except for rendering

--- features/facebookImport/test/ministory/json-file-names-analysis.test.js
+++ features/facebookImport/test/ministory/json-file-names-analysis.test.js
@@ -23,22 +23,34 @@ describe("Missing common JSON files analysis for empty zip", () => {
 
 describe("Missing common JSON files analysis for non-empty zip", () => {
     let status = null;
-
+    let result = null;
+    const commonJsonFiles = commonStructure
+        .filter((path) => path.match(/\.json$/))
+        .map((jsonPath) => {
+            return jsonPath.substring(1);
+        });
     beforeAll(async () => {
         let zipFile = new ZipFileMock();
-        commonStructure
-            .filter((path) => path.match(/\.json$/))
-            .forEach((jsonPath) => {
-                zipFile.addJsonEntry(jsonPath.substring(1), { foo: "bar" });
-            });
+        commonJsonFiles.forEach((jsonPath) => {
+            zipFile.addJsonEntry(jsonPath, { foo: "bar" });
+        });
         const { _, analysisResult } = await runAnalysisForExport(
             JSONFileNamesAnalysis,
             zipFile
         );
+        result = analysisResult.analysis;
         ({ status } = analysisResult);
     });
 
     it("has success status", async () => {
         expectAnalysisSuccessStatus(status);
+    });
+
+    it("has the right name", () => {
+        expect(result.reportData).toStrictEqual(commonJsonFiles);
+    });
+
+    it("has the right title", () => {
+        expect(result.title).toBe("JSON file names");
     });
 });
