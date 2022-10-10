fb307cdd7b2267c99afefb2c761f8fa90f6fa42e
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2022-10-10 07:46:10 +0200

    DRY

--- feature-utils/poly-import/test/polyIn-errors.test.js
+++ feature-utils/poly-import/test/polyIn-errors.test.js
@@ -9,13 +9,14 @@ const classData = [
 describe("Errors have the right API ", () => {
     it("throws correctly", () => {
         classData.forEach((err) => {
+            const testCause = "test";
             try {
-                throw new err[0]("test");
+                throw new err[0](testCause);
             } catch (error) {
                 expect(error).toBeInstanceOf(err[0]);
                 expect(error.message).toMatch(new RegExp(err[1]));
                 expect(error.name).toBe(new err[0]().constructor.name);
-                expect(error.cause).toBe("test");
+                expect(error.cause).toBe(testCause);
             }
         });
     });
