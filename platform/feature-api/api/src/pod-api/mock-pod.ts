4f18f18c8793ded66e351d2002c11b77b67e1f07
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2022-10-10 10:42:47 +0200

    :arrow_up: add zip reader to mock

--- platform/feature-api/api/src/pod-api/mock-pod.ts
+++ platform/feature-api/api/src/pod-api/mock-pod.ts
@@ -2,10 +2,14 @@ import { DefaultPod, DefaultPolyOut } from "./default";
 import { dataset } from "@rdfjs/dataset";
 import { Volume } from "memfs";
 import { isPolypodUri, PolyUri, PolyPodUriError } from "./uri";
+import * as zip from "@zip.js/zip.js";
 
 export class MockPolyOut extends DefaultPolyOut {
+    entries: { [uri: string]: zip.Entry[] };
+
     constructor(fs = new Volume().promises) {
         super(fs);
+        this.entries = { "": [] };
     }
 
     async importArchive(path: string, destUri?: string): Promise<string> {
@@ -16,6 +20,9 @@ export class MockPolyOut extends DefaultPolyOut {
                 throw new PolyPodUriError(`${destUri} is not a polyPod URI`);
             }
         }
+        const blob = this.fs.readFile(path);
+        const zipReader = new zip.ZipReader(new zip.BlobReader(blob));
+        this.entries[destUri] = await zipReader.getEntries();
         return destUri;
     }
 }
