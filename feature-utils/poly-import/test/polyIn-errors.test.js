054389347224a48db1650e5781ce03c98aa06e59
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2022-10-10 07:48:03 +0200

    :white_check_mark: use destructuring

--- feature-utils/poly-import/test/polyIn-errors.test.js
+++ feature-utils/poly-import/test/polyIn-errors.test.js
@@ -8,14 +8,14 @@ const classData = [
 
 describe("Errors have the right API ", () => {
     it("throws correctly", () => {
-        classData.forEach((err) => {
+        classData.forEach(([testClass, testMsg]) => {
             const testCause = "test";
             try {
-                throw new err[0](testCause);
+                throw new testClass(testCause);
             } catch (error) {
-                expect(error).toBeInstanceOf(err[0]);
-                expect(error.message).toMatch(new RegExp(err[1]));
-                expect(error.name).toBe(new err[0]().constructor.name);
+                expect(error).toBeInstanceOf(testClass);
+                expect(error.message).toMatch(new RegExp(testMsg));
+                expect(error.name).toBe(new testClass().constructor.name);
                 expect(error.cause).toBe(testCause);
             }
         });
