ec523e4aaf92e338042d229e84c32223711b2ee7
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2022-10-10 09:41:21 +0200

    Starting to test with archive
    But this is going to need some more mock code, so will leave it like this

--- feature-utils/poly-import/test/storage.test.js
+++ feature-utils/poly-import/test/storage.test.js
@@ -11,3 +11,16 @@ describe("File storage ", () => {
         expect(fileStorage.files).toBeNull;
     });
 });
+
+describe("Can remove archives ", () => {
+    let fileStorage;
+    let fileUri;
+    beforeAll(async () => {
+        const pod = new MockPod();
+        fileUri = await pod.polyOut.importArchive("noRealFile.zip");
+        fileStorage = new FeatureFileStorage(pod);
+    });
+    it("was instantiated", () => {
+        expect(fileStorage.files).toBeNull;
+    });
+});
