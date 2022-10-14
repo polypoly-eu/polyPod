818f5e223a2d55d3bbf40cab9ad57a17295f4e85
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2022-10-14 13:16:02 +0200

    Change :memo: in place of former one

--- platform/ios/PolyPodApp/FeatureContainer/initIframe.js
+++ platform/ios/PolyPodApp/FeatureContainer/initIframe.js
@@ -3,6 +3,7 @@ const { port1, port2 } = new MessageChannel();
 function initIframe(el) {
     port1.start();
     port1.onmessage = (event) => {
+        // Data coming from the feature to the pod
         webkit.messageHandlers.event.postMessage(event.data);
     };
     // This is transferring ownership of port2 to the feature's content window
