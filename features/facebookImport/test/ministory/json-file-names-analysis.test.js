ddd17cc7d312e5aad477b04abc6a537d956d335d
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2022-10-14 12:24:49 +0200

    Rather heavy refactoring requested in comments :recycle: DRY

--- features/facebookImport/test/ministory/json-file-names-analysis.test.js
+++ features/facebookImport/test/ministory/json-file-names-analysis.test.js
@@ -4,48 +4,36 @@ import { ZipFileMock } from "@polypoly-eu/poly-import";
 import { runAnalysisForExport } from "../utils/analyses-execution";
 import { expectAnalysisSuccessStatus } from "../utils/analysis-assertions";
 
-describe("JSON files analysis for empty zip", () => {
-    let status = null;
-
-    beforeAll(async () => {
-        const zipFile = new ZipFileMock();
-        const { analysisResult } = await runAnalysisForExport(
-            JSONFileNamesAnalysis,
-            zipFile
-        );
-        ({ status } = analysisResult);
+const commonJsonFiles = commonStructure
+    .filter((path) => path.match(/\.json$/))
+    .map((jsonPath) => {
+        return jsonPath.substring(1);
     });
 
+const zipFile = new ZipFileMock();
+commonJsonFiles.forEach((jsonPath) => {
+    zipFile.addJsonEntry(jsonPath, { foo: "bar" });
+});
+let status = null;
+let analysisResult;
+let result = null;
+
+beforeAll(async () => {
+    ({ analysisResult } = await runAnalysisForExport(
+        JSONFileNamesAnalysis,
+        zipFile
+    ));
+    result = analysisResult.analysis;
+    status = analysisResult.status;
+});
+
+describe("JSON files analysis for empty zip", () => {
     it("has success status", async () => {
         expectAnalysisSuccessStatus(status);
     });
 });
 
 describe("JSON files analysis for non-empty zip", () => {
-    let status = null;
-    let result = null;
-    const commonJsonFiles = commonStructure
-        .filter((path) => path.match(/\.json$/))
-        .map((jsonPath) => {
-            return jsonPath.substring(1);
-        });
-    beforeAll(async () => {
-        let zipFile = new ZipFileMock();
-        commonJsonFiles.forEach((jsonPath) => {
-            zipFile.addJsonEntry(jsonPath, { foo: "bar" });
-        });
-        const { analysisResult } = await runAnalysisForExport(
-            JSONFileNamesAnalysis,
-            zipFile
-        );
-        result = analysisResult.analysis;
-        ({ status } = analysisResult);
-    });
-
-    it("has success status", async () => {
-        expectAnalysisSuccessStatus(status);
-    });
-
     it("has the right name", () => {
         expect(result.reportData).toStrictEqual(commonJsonFiles);
     });
@@ -56,25 +44,18 @@ describe("JSON files analysis for non-empty zip", () => {
 });
 
 describe("JSON files analysis with anonymized path", () => {
-    let result = null;
-    let commonJsonFiles = commonStructure
-        .filter((path) => path.match(/\.json$/))
-        .map((jsonPath) => {
-            return jsonPath.substring(1);
-        });
-    commonJsonFiles.push(
-        "messages/archived_threads/facebookuser_gktxomrifg/message_1.json"
-    );
-
     beforeAll(async () => {
-        let zipFile = new ZipFileMock();
+        commonJsonFiles.push(
+            "messages/archived_threads/facebookuser_gktxomrifg/message_1.json"
+        );
+        const zipFile = new ZipFileMock();
         commonJsonFiles.forEach((jsonPath) => {
             zipFile.addJsonEntry(jsonPath, { foo: "bar" });
         });
-        const { analysisResult } = await runAnalysisForExport(
+        ({ analysisResult } = await runAnalysisForExport(
             JSONFileNamesAnalysis,
             zipFile
-        );
+        ));
         result = analysisResult.analysis;
     });
 
