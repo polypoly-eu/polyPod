05007dc9279504078ef69cd4170b0c36df5087c4
Juan Julián (JJ) Merelo <juan.guervos@polypoly.net>
2022-10-17 12:00:47 +0200

    Add failing test

---
+++ features/facebookImport/test/ministory/json-files-bubbles-analysis.test.js
@@ -0,0 +1,42 @@
+import JSONFilesBubblesAnalysis from "../../src/model/analyses/ministories/json-files-bubbles";
+import commonStructure from "../../src/static/commonStructure";
+import { ZipFileMock } from "@polypoly-eu/poly-import";
+import { runAnalysisForExport } from "../utils/analyses-execution";
+
+const commonJsonFiles = commonStructure
+    .filter((path) => path.match(/\.json$/))
+    .map((jsonPath) => {
+        return jsonPath.substring(1);
+    });
+
+async function analyzeZipWithFiles(files) {
+    const zipFile = new ZipFileMock();
+    if (files.length > 0) {
+        files.forEach((jsonPath) => {
+            zipFile.addJsonEntry(jsonPath, { foo: "bar" });
+        });
+    }
+    const { analysisResult } = await runAnalysisForExport(
+        JSONFilesBubblesAnalysis,
+        zipFile
+    );
+    console.log(analysisResult);
+    return analysisResult;
+}
+
+describe("JSON files analysis for non-empty zip", () => {
+    let status;
+    let analysis;
+
+    beforeAll(async () => {
+        ({ status, analysis } = await analyzeZipWithFiles(commonJsonFiles));
+    });
+
+    it("reports successful status", () => {
+        expect(status.isSuccess).toBe(true);
+    });
+
+    it("has the right title", () => {
+        expect(analysis.title).toBe("Files Bubbles");
+    });
+});
