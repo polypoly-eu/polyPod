28e4066f90740f3d512f30b70b8460965101c155
irstavr <irini.stavrakantonaki@polypoly.net>
2022-10-25 14:20:25 +0200

    fix jsdoc comments in facebookImport

--- features/facebookImport/src/model/analyses/utils/performance-telemetry.js
+++ features/facebookImport/src/model/analyses/utils/performance-telemetry.js
@@ -1,13 +1,22 @@
 /**
  * An utility class for measuring time duration.
- *
- * @class
+ * @class Telemetry
  */
 export class Telemetry {
+    /**
+     * Creates a new object with a creation time property set to the current time.
+     * @constructor
+     * @methodof Telemetry
+     */
     constructor() {
         this._creationTime = performance.now();
     }
 
+    /**
+     * It returns the time elapsed since the object was created.
+     * @returns The difference between the current time and the time the object was created.
+     * @methodof Telemetry
+     */
     elapsedTime() {
         return performance.now() - this._creationTime;
     }
