0cbcd4c10b92536874713f548abcb9f7b3fe85b2
Felix Dahlke <felix.dahlke@polypoly.net>
2022-11-15 16:00:03 +0100

    PROD4POD-1930 - Enable skipped tests

--- features/test/src/test.ts
+++ features/test/src/test.ts
@@ -27,303 +27,237 @@ describe("API object", function () {
 });
 
 describe("polyIn", function () {
-    /*
-      TODO: These tests were migrated from some early experiments, but most of
-      them never passed, presumably due to a lack of correct test data. The ones
-      that still need fixing are disabled for now.
-    */
-
-    class QuadBuilder {
-        private subject: RDF.Quad_Subject;
-        private readonly predicate: RDF.Quad_Predicate;
-        private object: RDF.Quad_Object;
-        private graph: RDF.Quad_Graph;
-
-        constructor(
-            subject: RDF.Quad_Subject,
-            predicate: RDF.Quad_Predicate,
-            object: RDF.Quad_Object,
-            graph: RDF.Quad_Graph
-        ) {
-            this.subject = subject;
-            this.predicate = predicate;
-            this.object = object;
-            this.graph = graph;
-        }
-
-        static fromInputs(): QuadBuilder {
-            const dataFactory = window.pod.dataFactory;
-            const subject = dataFactory.namedNode(inputs.s);
-            const predicate = dataFactory.namedNode(inputs.p);
-            const object = dataFactory.namedNode(inputs.o);
-            const graph = dataFactory.namedNode(inputs.g);
-            return new QuadBuilder(subject, predicate, object, graph);
-        }
-
-        static fromQuad(quad: RDF.Quad): QuadBuilder {
-            const subject = quad.subject;
-            const predicate = quad.predicate;
-            const object = quad.object;
-            const graph = quad.graph;
-            return new QuadBuilder(subject, predicate, object, graph);
-        }
-
-        withSubject(subject: RDF.Quad_Subject): QuadBuilder {
-            this.subject = subject;
-            return this;
-        }
-
-        withObject(object: RDF.Quad_Object): QuadBuilder {
-            this.object = object;
-            return this;
-        }
-
-        withGraph(graph: RDF.Quad_Graph): QuadBuilder {
-            this.graph = graph;
-            return this;
-        }
-
-        build(): RDF.Quad {
-            return window.pod.dataFactory.quad(
-                this.subject,
-                this.predicate,
-                this.object,
-                this.graph
-            );
+    async function assertAsyncThrows(fn, errorLike) {
+        try {
+            await fn();
+            throw "No error raised";
+        } catch (e) {
+            assert.throws(() => {
+                throw e;
+            }, errorLike);
         }
     }
 
-    // Historically, there were input fields with this data, hence the name.
-    const inputs = {
-        s: "http://example.com/subject",
-        p: "http://example.com/predicate",
-        o: "http://example.com/object",
-        g: "http://example.com/graph",
-    };
+    function findQuadIndex(quads: RDF.Quad[], quad: RDF.Quad) {
+        for (let i = 0; i < quads.length; i++)
+            if (quad.equals(quads[i])) return i;
+        return -1;
+    }
 
-    // This was probably being used to set up test data, but it's unclear how.
-    const quads: Array<RDF.Quad> = [];
+    function assertQuadsEqual(expected: RDF.Quad[], actual: RDF.Quad[]) {
+        assert.equal(expected.length, actual.length);
+        for (const expectedQuad of expected) {
+            const index = findQuadIndex(actual, expectedQuad);
+            assert.notEqual(
+                index,
+                -1,
+                `expected '${actual}' to include '${expectedQuad}'`
+            );
+            actual.splice(index, 1);
+        }
+    }
 
+    const dataFactory: RDF.DataFactory = window.pod.dataFactory;
     const polyIn: PolyIn = window.pod.polyIn;
 
+    const testQuads = {
+        allNamedNodes: dataFactory.quad(
+            dataFactory.namedNode("http://example.com/s"),
+            dataFactory.namedNode("http://example.com/p"),
+            dataFactory.namedNode("http://example.com/o")
+        ),
+        blankNodeSubject: dataFactory.quad(
+            dataFactory.blankNode("s"),
+            dataFactory.namedNode("http://example.com/p"),
+            dataFactory.namedNode("http://example.com/o")
+        ),
+        blankNodeObject: dataFactory.quad(
+            dataFactory.namedNode("http://example.com/s"),
+            dataFactory.namedNode("http://example.com/p"),
+            dataFactory.blankNode("o")
+        ),
+        literalObject: dataFactory.quad(
+            dataFactory.namedNode("http://example.com/s"),
+            dataFactory.namedNode("http://example.com/p"),
+            dataFactory.literal("o")
+        ),
+        defaultGraph: dataFactory.quad(
+            dataFactory.namedNode("http://example.com/s"),
+            dataFactory.namedNode("http://example.com/p"),
+            dataFactory.namedNode("http://example.com/o"),
+            dataFactory.defaultGraph()
+        ),
+        nonDefaultGraph: dataFactory.quad(
+            dataFactory.namedNode("http://example.com/s"),
+            dataFactory.namedNode("http://example.com/p"),
+            dataFactory.namedNode("http://example.com/o"),
+            dataFactory.namedNode("http://example.com/g")
+        ),
+    };
+
+    const quadBackupTimeout = 10000;
+    let savedQuads: RDF.Quad[];
+
+    // @ts-ignore
+    before(async function () {
+        this.timeout(quadBackupTimeout);
+        savedQuads = await polyIn.match({});
+        if (savedQuads.length)
+            console.warn(`Backing up ${savedQuads.length} quads`);
+        for (const quad of savedQuads) await polyIn.delete(quad);
+    });
+
+    // @ts-ignore
+    after(async function () {
+        this.timeout(quadBackupTimeout);
+        if (savedQuads.length)
+            console.warn(`Restoring ${savedQuads.length} quads`);
+        for (const quad of savedQuads) await polyIn.add(quad);
+    });
+
     describe("add", function () {
-        it.skip("can be called with single quad", async function () {
-            const quad = QuadBuilder.fromInputs().build();
-            await polyIn.add(quad);
+        it("can be called with single quad", async function () {
+            await polyIn.add(testQuads.allNamedNodes);
         });
 
-        it.skip("can be called with multiple quads", async function () {
-            await polyIn.add(...quads);
+        it("supports quads with blank node subject", async function () {
+            await polyIn.add(testQuads.blankNodeSubject);
         });
 
-        it.skip("supports quads with named node subject", async function () {
-            const quad = QuadBuilder.fromInputs().build();
-            await polyIn.add(quad);
+        it("supports quads with blank node object", async function () {
+            await polyIn.add(testQuads.blankNodeObject);
         });
 
-        it.skip("supports quads with blank node subject", async function () {
-            const subject = inputs.s;
-            const quad = QuadBuilder.fromInputs()
-                .withSubject(window.pod.dataFactory.blankNode(subject))
-                .build();
-            await polyIn.add(quad);
-        });
-
-        it.skip("supports quads with named node object", async function () {
-            const object = inputs.o;
-            const quad = QuadBuilder.fromInputs()
-                .withObject(window.pod.dataFactory.namedNode(object))
-                .build();
-            await polyIn.add(quad);
-        });
-
-        it.skip("supports quads with blank node object", async function () {
-            const object = input[3];
-            const quad = QuadBuilder.fromInputs()
-                .withObject(window.pod.dataFactory.blankNode(object))
-                .build();
-            await polyIn.add(quad);
-        });
-
-        it.skip("supports quads with literal object", async function () {
-            const object = input[3];
-            const quad = QuadBuilder.fromInputs()
-                .withObject(window.pod.dataFactory.literal(object))
-                .build();
-            await polyIn.add(quad);
-        });
-
-        it.skip("supports quads with named node graph", async function () {
-            const graph = input[4];
-            const quad = QuadBuilder.fromInputs()
-                .withGraph(window.pod.dataFactory.namedNode(graph))
-                .build();
-            await polyIn.add(quad);
-        });
-
-        it.skip("supports quads with blank node graph", async function () {
-            const graph = input[4];
-            const quad = QuadBuilder.fromInputs()
-                .withGraph(window.pod.dataFactory.blankNode(graph))
-                .build();
-            await polyIn.add(quad);
+        it("supports quads with literal object", async function () {
+            await polyIn.add(testQuads.literalObject);
         });
 
         it("supports quads with default graph", async function () {
-            const quad = QuadBuilder.fromInputs()
-                .withGraph(window.pod.dataFactory.defaultGraph())
-                .build();
-            await polyIn.add(quad);
+            await polyIn.add(testQuads.defaultGraph);
+        });
+
+        it("does not support quads with non-default graph", async function () {
+            await assertAsyncThrows(
+                () => polyIn.add(testQuads.nonDefaultGraph),
+                /^Only default graph allowed/
+            );
+        });
+
+        // Currently not supported
+        it.skip("can be called with multiple quads", async function () {
+            // @ts-ignore
+            await polyIn.add([
+                testQuads.allNamedNodes,
+                testQuads.blankNodeSubject,
+            ]);
         });
     });
 
     describe("match", function () {
+        afterEach(async function () {
+            for (const quad of await polyIn.match({}))
+                await polyIn.delete(quad);
+        });
+
         it("accepts empty matcher", async function () {
             await polyIn.match({});
         });
 
         it("accepts matcher with subject", async function () {
-            const subject = inputs.s;
+            const subject = testQuads.allNamedNodes.subject.value;
             const matcher = {
-                subject: window.pod.dataFactory.namedNode(subject),
+                subject: dataFactory.namedNode(subject),
             };
             await polyIn.match(matcher);
         });
 
         it("accepts matcher with predicate", async function () {
-            const predicate = inputs.s;
+            const predicate = testQuads.allNamedNodes.predicate.value;
             const matcher = {
-                predicate: window.pod.dataFactory.namedNode(predicate),
+                predicate: dataFactory.namedNode(predicate),
             };
             await polyIn.match(matcher);
         });
 
         it("accepts matcher with object", async function () {
-            const object = inputs.s;
+            const object = testQuads.allNamedNodes.object.value;
             const matcher = {
-                object: window.pod.dataFactory.namedNode(object),
-            };
-            await polyIn.match(matcher);
-        });
-
-        it("accepts matcher with all three fields", async function () {
-            const subject = inputs.s;
-            const predicate = inputs.p;
-            const object = inputs.o;
-            const dataFactory = window.pod.dataFactory;
-            const matcher = {
-                subject: dataFactory.namedNode(subject),
-                predicate: dataFactory.namedNode(predicate),
                 object: dataFactory.namedNode(object),
             };
             await polyIn.match(matcher);
         });
 
-        it.skip("can return empty array", async function () {
+        it("accepts matcher with all three fields", async function () {
+            const quad = testQuads.allNamedNodes;
+            const matcher = {
+                subject: dataFactory.namedNode(quad.subject.value),
+                predicate: dataFactory.namedNode(quad.predicate.value),
+                object: dataFactory.namedNode(quad.object.value),
+            };
+            await polyIn.match(matcher);
+        });
+
+        it("returns empty array", async function () {
             const result = await polyIn.match({});
             assert.isArray(result);
             assert.lengthOf(result, 0);
         });
 
-        it.skip("can return array with single squad", async function () {
-            const expectedResult = [QuadBuilder.fromQuad(quads[0]).build()];
+        it("returns single quad", async function () {
+            const quad = testQuads.allNamedNodes;
+            await polyIn.add(quad);
             const result = await polyIn.match({});
-            assert.deepEqual(result, expectedResult);
+            assertQuadsEqual(result, [quad]);
         });
 
-        it.skip("can return array with single quad with named node subject", async function () {
-            const expectedResult = [
-                QuadBuilder.fromQuad(quads[0])
-                    .withSubject(
-                        pod.dataFactory.namedNode(quads[0].subject.value)
-                    )
-                    .build(),
-            ];
+        it("returns single quad with blank node subject", async function () {
+            const quad = testQuads.blankNodeSubject;
+            await polyIn.add(quad);
             const result = await polyIn.match({});
-            assert.deepEqual(result, expectedResult);
+            assertQuadsEqual(result, [quad]);
         });
 
-        it.skip("canGetArrayWithSingleQuadWithBlankNodeSubjectFromPolyInMatch", async function () {
-            const expectedResult = [
-                QuadBuilder.fromQuad(quads[0])
-                    .withSubject(
-                        pod.dataFactory.blankNode(quads[0].subject.value)
-                    )
-                    .build(),
-            ];
+        it("returns single quad with blank node object", async function () {
+            const quad = testQuads.blankNodeObject;
+            await polyIn.add(quad);
             const result = await polyIn.match({});
-            assert.deepEqual(result, expectedResult);
+            assertQuadsEqual(result, [quad]);
         });
 
-        it.skip("canGetArrayWithSingleQuadWithNamedNodeObjectFromPolyInMatch", async function () {
-            const expectedResult = [
-                QuadBuilder.fromQuad(quads[0])
-                    .withObject(
-                        pod.dataFactory.namedNode(quads[0].object.value)
-                    )
-                    .build(),
-            ];
+        it("returns single quad with literal object", async function () {
+            const quad = testQuads.literalObject;
+            await polyIn.add(quad);
             const result = await polyIn.match({});
-            assert.deepEqual(result, expectedResult);
+            assertQuadsEqual(result, [quad]);
         });
 
-        it.skip("canGetArrayWithSingleQuadWithBlankNodeObjectFromPolyInMatch", async function () {
-            const expectedResult = [
-                QuadBuilder.fromQuad(quads[0])
-                    .withObject(
-                        pod.dataFactory.blankNode(quads[0].object.value)
-                    )
-                    .build(),
-            ];
-            const result = await polyIn.match({});
-            assert.deepEqual(result, expectedResult);
+        it("supports matcher with default graph", async function () {
+            const quad = testQuads.defaultGraph;
+            const matcher = {
+                subject: dataFactory.namedNode(quad.subject.value),
+                predicate: dataFactory.namedNode(quad.predicate.value),
+                object: dataFactory.namedNode(quad.object.value),
+                graph: dataFactory.defaultGraph(),
+            };
+            await polyIn.match(matcher);
         });
 
-        it.skip("canGetArrayWithSingleQuadWithLiteralObjectFromPolyInMatch", async function () {
-            const expectedResult = [
-                QuadBuilder.fromQuad(quads[0])
-                    .withObject(pod.dataFactory.literal(quads[0].object.value))
-                    .build(),
-            ];
-            const result = await polyIn.match({});
-            assert.deepEqual(result, expectedResult);
+        it("supports matcher with non-default graph", async function () {
+            const quad = testQuads.nonDefaultGraph;
+            const matcher = {
+                subject: dataFactory.namedNode(quad.subject.value),
+                predicate: dataFactory.namedNode(quad.predicate.value),
+                object: dataFactory.namedNode(quad.object.value),
+                graph: dataFactory.namedNode(quad.graph.value),
+            };
+            await polyIn.match(matcher);
         });
 
-        it.skip("canGetArrayWithSingleQuadWithNamedNodeGraphFromPolyInMatch", async function () {
-            const expectedResult = [
-                QuadBuilder.fromQuad(quads[0])
-                    .withGraph(pod.dataFactory.namedNode(quads[0].graph.value))
-                    .build(),
-            ];
+        it("returns multiple quads", async function () {
+            const quads = [testQuads.allNamedNodes, testQuads.blankNodeSubject];
+            for (const quad of quads) await polyIn.add(quad);
             const result = await polyIn.match({});
-            assert.deepEqual(result, expectedResult);
-        });
-
-        it.skip("canGetArrayWithSingleQuadWithBlankNodeGraphFromPolyInMatch", async function () {
-            const expectedResult = [
-                QuadBuilder.fromQuad(quads[0])
-                    .withGraph(pod.dataFactory.blankNode(quads[0].graph.value))
-                    .build(),
-            ];
-            const result = await polyIn.match({});
-            assert.deepEqual(result, expectedResult);
-        });
-
-        it.skip("canGetArrayWithSingleQuadWithDefaultGraphFromPolyInMatch", async function () {
-            const expectedResult = [
-                QuadBuilder.fromQuad(quads[0])
-                    .withGraph(pod.dataFactory.defaultGraph())
-                    .build(),
-            ];
-            const result = await polyIn.match({});
-            assert.deepEqual(result, expectedResult);
-        });
-
-        it.skip("canGetArrayWithMultipleQuadsFromPolyInMatch", async function () {
-            const result = await polyIn.match({});
-            assert.lengthOf(result, 2);
-            assert.include(result, quads[0]);
-            assert.include(result, quads[1]);
+            assertQuadsEqual(result, quads);
         });
     });
 });
