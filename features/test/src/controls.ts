e10442a23120befb71857f1985f17c50a4a4e142
Felix Dahlke <felix.dahlke@polypoly.net>
2022-11-16 14:04:40 +0100

    PROD4POD-1930 - Add permanent reset button

--- features/test/src/controls.ts
+++ features/test/src/controls.ts
@@ -1,4 +1,9 @@
 export function initControls(container: HTMLElement): void {
+    const resetButton = document.getElementById("reset") as HTMLButtonElement;
+    resetButton.addEventListener("click", function () {
+        location.href = location.href.replace(/\?.*/, "");
+    });
+
     const runAllButton = document.getElementById("runAll") as HTMLButtonElement;
     runAllButton.addEventListener("click", function () {
         const output = container.querySelector("span");
@@ -7,11 +12,6 @@ export function initControls(container: HTMLElement): void {
         // @ts-ignore - seems we're missing some types for Mocha
         mocha.run((failures: number) => {
             output.textContent = failures > 0 ? "Failed" : "All OK";
-            runAllButton.textContent = "Reset";
-            runAllButton.addEventListener("click", function () {
-                location.href = location.href.replace(/\?.*/, "");
-            });
-            runAllButton.disabled = false;
         });
     });
 }
